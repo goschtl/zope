9073eb33913231a73e850513e84e6b6ede178f4d
klm <klm@62d5b8a3-27da-0310-9561-8e5933582275>
1999-03-01 22:43:28 +0000

    Using a general parameter for the repository mirror setting.
    git-svn-id: svn://svn.zope.org/repos/main@31170 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/jens/CMF/trunk/CVSROOT/traffic_table.py
+++ Sandbox/jens/CMF/trunk/CVSROOT/traffic_table.py
@@ -4,32 +4,36 @@ A list of entries for each project that has anything going anywhere.
 
 Each entry is a tuple containing:
 
- - a string name for the entry
+ - a string name for the entry - often the repository dir
 
  - a regular expression qualifying checkins for membership
    - matched against the repository path of the checkin, sans CVSROOT
 
  - a list of addresses for delivery of checkin message
 
- - a dictionary specifying the remote host to which repository checkins
-   should be synced.  The fields are:
+ - an optional dictionary specifying the remote host to which repository
+   checkins should be synced.  The fields are:
 
     'host'
     'acct' - the pserver acct that will get access
     'repodir' - the repository directory to which syncs should be done
 
+   It's generally convenient to have a module variable, eg "MIRROR",
+   for the default host used in most entries.
+
 E.g.: 
 
- ("Test", "^test", ["klm@digicool.com"], {'host': "www.zope.org",
-                                          'acct': "anonymous",
-                                          'repodir': "/cvs-repository/test"])
+MIRROR = {'host': "www.zope.org",
+          'acct': "anonymous",
+          'repodir': "/cvs-repository"}
+("test", "^test", ["klm@digicool.com"], MIRROR)
 """
 
+MIRROR = {'host': "www.zope.org",
+          'acct': "anonymous",
+          'repodir': "/cvs-repository"}
+
 table = [
-##  ("Test", "^test", ["klm@digicool.com"], {'host': "www.zope.org",
-##                                           'acct': "anonymous",
-##                                           'repodir': "/cvs-repository/test"])
- ("Test", "^test", [], {'host': "www.zope.org",
-                                          'acct': "anonymous",
-                                          'repodir': "/cvs-repository/test"})
+    ("test", "^test", ["klm@digicool.com"], MIRROR),
+    ("CVSROOT", "^CVSROOT", ["klm@digicool.com"], MIRROR)
 ]
