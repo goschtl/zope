a64292ea30708825b7326154553c0b2ef75e5863
tseaver <tseaver@62d5b8a3-27da-0310-9561-8e5933582275>
2001-04-11 22:03:58 +0000

    - Extend TypesTool to deal appropriately with DTML factory methods    (e.g., for ZClasses).
    git-svn-id: svn://svn.zope.org/repos/main@33508 62d5b8a3-27da-0310-9561-8e5933582275

--- branches/CMF-1_0-branch/CMFCore/TypesTool.py
+++ branches/CMF-1_0-branch/CMFCore/TypesTool.py
@@ -470,7 +470,11 @@ class FactoryTypeInformation (TypeInformation):
         if m is None:
             raise 'Unauthorized', ('Cannot create %s' % self.id)
         id = str(id)
-        apply(m, (id,) + args, kw)
+        if getattr( m, 'isDocTemp', 0 ):
+            kw[ 'id' ] = id
+            apply( m, ( m.aq_parent, self.REQUEST ) + args, kw )
+        else:
+            apply(m, (id,) + args, kw)
         ob = container._getOb(id)
         if hasattr(ob, '_setPortalTypeName'):
             ob._setPortalTypeName(self.id)
