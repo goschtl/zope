833e56f7d50886449e89f5b96931f2781439c406
(no author) <(no author)@62d5b8a3-27da-0310-9561-8e5933582275>
2001-10-15 23:00:18 +0000

    This commit was manufactured by cvs2svn to create branch 'seb-defaultActionRefactor-branch'.
    git-svn-id: svn://svn.zope.org/repos/main@31628 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Sandbox/jens/CMF/branches/seb-defaultActionRefactor-branch/CMF/CMFCollector/skins/collector/collector_add_issue.py
@@ -0,0 +1,22 @@
+## Script (Python) "collector_add_issue.py"
+##parameters=submitter, title, security_related, topic, importance, classification, severity, description, reported_version, other_version_info
+##title=Submit a Request
+
+REQUEST = context.REQUEST
+
+id = context.add_issue(submitter=submitter,
+                       title=title,
+                       description=description,
+                       security_related=security_related,
+                       topic=topic,
+                       importance=importance,
+                       classification=classification,
+                       severity=severity,
+                       reported_version=reported_version,
+                       other_version_info=other_version_info,
+                       file=REQUEST.get('file'),
+                       fileid=REQUEST.get('fileid', ''),
+                       filetype=REQUEST.get('filetype', 'file'))
+
+context.REQUEST.RESPONSE.redirect("%s/%s" % (context.absolute_url(), id))
+
