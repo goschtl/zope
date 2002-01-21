d469d9ea8435bca2bdc9b5f4a5ee709d11e68c85
andrew <andrew@62d5b8a3-27da-0310-9561-8e5933582275>
2002-01-21 20:23:19 +0000

    *Fixed regular expression bug in _ALLOWED_MEMBER_ID_PATTERN *Tracker #456
    git-svn-id: svn://svn.zope.org/repos/main@34677 62d5b8a3-27da-0310-9561-8e5933582275

--- branches/CMF-1_2-branch/CMFCore/RegistrationTool.py
+++ branches/CMF-1_2-branch/CMFCore/RegistrationTool.py
@@ -124,7 +124,7 @@ class RegistrationTool (UniqueObject, SimpleItem):
         return member
 
     import re
-    __ALLOWED_MEMBER_ID_PATTERN = re.compile( "[A-Za-z][A-Za-z0-9_]*" )
+    __ALLOWED_MEMBER_ID_PATTERN = re.compile( "^[A-Za-z][A-Za-z0-9_]*$" )
     security.declareProtected(AddPortalMember, 'isMemberIdAllowed')
     def isMemberIdAllowed(self, id):
         '''Returns 1 if the ID is not in use and is not reserved.
