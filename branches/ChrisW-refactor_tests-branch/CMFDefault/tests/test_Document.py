0ada0031a81bb210f58f2f8b989cc7f3c8a775bf
chrisw <chrisw@62d5b8a3-27da-0310-9561-8e5933582275>
2002-02-15 19:33:28 +0000

    Done Document,NewsItem
    git-svn-id: svn://svn.zope.org/repos/main@34764 62d5b8a3-27da-0310-9561-8e5933582275

--- branches/ChrisW-refactor_tests-branch/CMFDefault/tests/test_Document.py
+++ branches/ChrisW-refactor_tests-branch/CMFDefault/tests/test_Document.py
@@ -5,124 +5,12 @@ from re import compile
 from Products.CMFCore.tests.base.testcase import \
      RequestTest
 
+from Products.CMFCore.tests.base.content import \
+     DOCTYPE, HTML_TEMPLATE, BASIC_HTML, ENTITY_IN_TITLE, BASIC_STRUCTUREDTEXT, \
+     STX_WITH_HTML, STX_NO_HEADERS, STX_NO_HEADERS_BUT_COLON, \
+     SIMPLE_STRUCTUREDTEXT, SIMPLE_HTML
+
 from Products.CMFDefault.Document import Document
-
-DOCTYPE = '''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">'''
-
-HTML_TEMPLATE = '''\
-<html><head>
- <title>%(title)s</title>
-</head>
-<body bgcolor="#efe843">%(body)s</body>
-</html>
-'''
-
-BASIC_HTML = '''\
-<html>
- <head>
-  <title>Title in tag</title>
-  <meta name="description" content="Describe me">
-  <meta name="contributors" content="foo@bar.com; baz@bam.net;
-    Benotz, Larry J (larry@benotz.stuff)">
-  <meta name="title" content="Title in meta">
-  <meta name="subject" content="content management">
-  <meta name="keywords" content="unit tests, framework; ,zope ">
- </head>
- <body bgcolor="#ffffff">
-  <h1>Not a lot here</h1>
- </body>
-</html>
-'''
-
-SIMPLE_HTML = '''\
-<html>
- <head>
-  <title>Title in tag</title>
-  <meta name="description" content="Describe me">
-  <meta name="contributors" content="foo@bar.com; baz@bam.net;
-    Benotz, Larry J (larry@benotz.stuff)">
-  <meta name="title" content="Title in meta">
-  <meta name="subject" content="content management">
- </head>
- <body bgcolor="#ffffff">
-  <h1>Not a lot here</h1>
- </body>
-</html>
-'''
-
-ENTITY_IN_TITLE = '''\
-<html>
- <head>
-  <title>&Auuml;rger</title>
- </head>
- <bOdY>
-  <h2>Not a lot here either</h2>
- </bodY>
-</html>
-'''
-
-BASIC_STRUCTUREDTEXT = '''\
-Title: My Document
-Description: A document by me
-Contributors: foo@bar.com; baz@bam.net; no@yes.maybe
-Subject: content management, zope
-Keywords: unit tests; , framework
-
-This is the header
-
-  Body body body body body
-  body body body.
-
-   o A list item
-   
-   o And another thing...
-'''
-
-SIMPLE_STRUCTUREDTEXT = '''\
-Title: My Document
-Description: A document by me
-Contributors: foo@bar.com; baz@bam.net; no@yes.maybe
-Subject: content management, zope
-
-This is the header
-
-  Body body body body body
-  body body body.
-
-   o A list item
-   
-   o And another thing...
-'''
-
-STX_WITH_HTML = """\
-Sometimes people do interesting things
-
-  Sometimes people do interesting things like have examples
-  of HTML inside their structured text document.  We should
-  be detecting that this is indeed a structured text document
-  and **NOT** an HTML document::
-
-    <html>
-    <head><title>Hello World</title></head>
-    <body><p>Hello world, I am Bruce.</p></body>
-    </html>
-
-  All in favor say pi!
-"""
-
-STX_NO_HEADERS = """\
-Title Phrase
-
-    This is a "plain" STX file, with no headers.  Saving with
-    it shouldn't overwrite any metadata.
-"""
-
-STX_NO_HEADERS_BUT_COLON = """\
-Plain STX:  No magic!
-
-    This is a "plain" STX file, with no headers.  Saving with
-    it shouldn't overwrite any metadata.
-"""
 
 class DocumentTests(RequestTest):
 
@@ -349,12 +237,12 @@ class DocumentTests(RequestTest):
 class TestFTPGet( RequestTest ):
 
     def testHTML( self ):
-        self.REQUEST['BODY']=SIMPLE_HTML
+        self.REQUEST['BODY']=BASIC_HTML
         d = Document( 'foo' )
         d.PUT(self.REQUEST, self.RESPONSE)
 
         rnlinesplit = compile( r'\r?\n?' )
-        simple_lines = rnlinesplit.split( SIMPLE_HTML )
+        simple_lines = rnlinesplit.split( BASIC_HTML )
         get_lines = rnlinesplit.split( d.manage_FTPget() )
 
         # strip off headers
