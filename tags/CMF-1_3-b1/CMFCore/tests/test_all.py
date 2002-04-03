fee491a277dbcb47999d6a0b968888521e97a5fa
(no author) <(no author)@62d5b8a3-27da-0310-9561-8e5933582275>
2002-04-03 17:05:16 +0000

    This commit was manufactured by cvs2svn to create tag 'CMF-1_3-b1'.
    git-svn-id: svn://svn.zope.org/repos/main@34862 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ tags/CMF-1_3-b1/CMFCore/tests/test_all.py
@@ -0,0 +1,24 @@
+import Zope
+from unittest import main
+from Products.CMFCore.tests.base.utils import build_test_suite
+
+def test_suite():
+
+    return build_test_suite('Products.CMFCore.tests',[
+        'test_ContentTypeRegistry',
+        'test_PortalFolder',
+        'test_TypesTool',
+        'test_WorkflowTool',
+        'test_ActionsTool',
+        'test_ActionInformation',
+        'test_ActionProviderBase',
+        'test_Expression',
+        'test_CatalogTool',
+        'test_DirectoryView',
+        'test_FSPythonScript',
+        'test_FSPageTemplate',
+        'test_CachingPolicyManager',
+        ])
+
+if __name__ == '__main__':
+    main(defaultTest='test_suite')
