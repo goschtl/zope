6d80fce91df0be6c3420debef0f7e6de53b8aea3
efge <efge@62d5b8a3-27da-0310-9561-8e5933582275>
2002-06-24 13:34:54 +0000

    Whitespace cleanup.
    git-svn-id: svn://svn.zope.org/repos/main@6038 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/lib/python/Zope/App/Security/Settings.py
+++ Zope3/trunk/lib/python/Zope/App/Security/Settings.py
@@ -2,14 +2,14 @@
 #
 # Copyright (c) 2001, 2002 Zope Corporation and Contributors.
 # All Rights Reserved.
-# 
+#
 # This software is subject to the provisions of the Zope Public License,
 # Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
-# 
+#
 ##############################################################################
 """ Security setting constants """
 
@@ -33,21 +33,21 @@ class PermissionSetting(object):
             instances[name] = it = object.__new__(cls)
             it._init(name, description)
         return it
-                
+
     def _init(self, name, description):
         self.__name = name
-        self.__description = description    
-    
+        self.__description = description
+
     def getDescription(self):
         return self.__description
-        
+
     def getName(self):
         return self.__name
-        
+
     def __str__(self):
         return "PermissionSetting: %s" % self.__name
 
-# register PermissionSettings to be symbolic constants by identity, 
+# register PermissionSettings to be symbolic constants by identity,
 # even when pickled and unpickled.
 import copy_reg
 copy_reg.constructor(PermissionSetting)
@@ -56,7 +56,6 @@ copy_reg.pickle(PermissionSetting,
                 PermissionSetting)
 
 
-        
 Allow = PermissionSetting('Allow',
     'Explicit allow setting for permissions')
 
