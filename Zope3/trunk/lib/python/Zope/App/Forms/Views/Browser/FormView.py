5fc555355d9d5293008bf95d54e9910ee7354300
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2002-06-25 15:30:32 +0000

    Add configure.zcml to Forms (real work done in Browser as this is all about Views). Adds a few test widgets as views, which can hook up to Schema fields (see lib/python/Schema). Also added a test view for Forms that displays a basic form made from the widgets.
    git-svn-id: svn://svn.zope.org/repos/main@6073 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/lib/python/Zope/App/Forms/Views/Browser/FormView.py
+++ Zope3/trunk/lib/python/Zope/App/Forms/Views/Browser/FormView.py
@@ -2,7 +2,13 @@ from Zope.Publisher.Browser.BrowserView import BrowserView
 from Interface import Interface
 from Schema.IField import IField
 from Zope.ComponentArchitecture import getView
+import Schema
+from Schema import _Schema # XXX wire up, should really fix this :)
 
+class ITestSchema(Interface):
+    alpha = Schema.Str(title="Alpha")
+    beta = Schema.Bool(title="Beta")
+    
 class FormView(BrowserView):
     def getWidgetsForSchema(self, schema, view_name):
         """Given a schema and a desired field name, get a list of
@@ -15,5 +21,11 @@ class FormView(BrowserView):
                 widget = getView(attr, view_name, self.request)
                 result.append(widget)
         return result
-    
+
+    def getFields(self):
+        """XXX just a test method.
+        """
+        result = self.getWidgetsForSchema(ITestSchema, 'normal')
+        print result
+        return result
     
