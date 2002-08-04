c7ee1ed1f693e2ce4ef9b5cc2e0ce073939ca2d9
efge <efge@62d5b8a3-27da-0310-9561-8e5933582275>
2002-08-04 04:04:44 +0000

    Merged change from HEAD:
    Code cleanup:
    Use declarative security instead of __ac_permissions__.
    Use InitializeClass instead of default__class_init__.
    Use AddPortalContent instead of ADD_CONTENT_PERMISSION.
    Use AddPortalFolders instead of ADD_FOLDERS_PERMISSION.
    
    
    git-svn-id: svn://svn.zope.org/repos/main@35249 62d5b8a3-27da-0310-9561-8e5933582275

--- branches/CMF-1_3-branch/CMFDefault/Link.py
+++ branches/CMF-1_3-branch/CMFDefault/Link.py
@@ -147,7 +147,6 @@ class Link( PortalContent
         self.remote_url = url
 
     security.declareProtected( CMFCorePermissions.ModifyPortalContent, 'edit' )
-
     def edit(self, remote_url ):
         """ Update and reindex. """
         self._edit( remote_url )
