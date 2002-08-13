97ab4239a951da01853d1005da0c10888e56570b
gvanrossum <gvanrossum@62d5b8a3-27da-0310-9561-8e5933582275>
2002-08-13 20:28:35 +0000

    Fix syntax error.  Python's comment character is #, not *.
    git-svn-id: svn://svn.zope.org/repos/main@6259 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/ut.py
+++ Zope3/trunk/ut.py
@@ -15,7 +15,7 @@
 
 XXX longer description goes here.
 
-$Id: ut.py,v 1.3 2002/08/13 20:08:59 rdmurray Exp $
+$Id: ut.py,v 1.4 2002/08/13 20:28:35 gvanrossum Exp $
 """
 
 from unittest import TestCase, TestSuite, main, makeSuite
@@ -25,9 +25,9 @@ from unittest import TestCase, TestSuite, main, makeSuite
 # following import and include CleanUp as a base class of your
 # test. It provides a setUp and tearDown that clear global data that
 # has registered with the test cleanup framework.  If your class has
-* its own setUp or tearDown, make sure you call the CleanUp setUp and
-* tearDown from them, or the benefits of using CleanUp will be lost.
-* Don't use CleanUp based tests outside the Zope package.
+# its own setUp or tearDown, make sure you call the CleanUp setUp and
+# tearDown from them, or the benefits of using CleanUp will be lost.
+# Don't use CleanUp based tests outside the Zope package.
 
 # from Zope.Testing.CleanUp import CleanUp # Base class w registry cleanup
 
