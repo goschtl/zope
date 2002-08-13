ba2cc35c716336c87e06e45e8feb9b39fb7eef96
rdmurray <rdmurray@62d5b8a3-27da-0310-9561-8e5933582275>
2002-08-13 20:08:59 +0000

    I started to just edit this file to fix a grammer error, but if I understand the comments on PR 7 correctly, then there is also some advice missing from the comments (ie: the necessity of calling CleanUp.tearDown). So I added it.  Someone correct me if I'm wrong.
    git-svn-id: svn://svn.zope.org/repos/main@6258 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/ut.py
+++ Zope3/trunk/ut.py
@@ -15,7 +15,7 @@
 
 XXX longer description goes here.
 
-$Id: ut.py,v 1.2 2002/06/10 23:27:34 jim Exp $
+$Id: ut.py,v 1.3 2002/08/13 20:08:59 rdmurray Exp $
 """
 
 from unittest import TestCase, TestSuite, main, makeSuite
@@ -24,8 +24,10 @@ from unittest import TestCase, TestSuite, main, makeSuite
 # If your tests change any global registries, then uncomment the
 # following import and include CleanUp as a base class of your
 # test. It provides a setUp and tearDown that clear global data that
-# has registered with the test cleanup framework.  Don't use this
-# tests outside the Zope package.
+# has registered with the test cleanup framework.  If your class has
+* its own setUp or tearDown, make sure you call the CleanUp setUp and
+* tearDown from them, or the benefits of using CleanUp will be lost.
+* Don't use CleanUp based tests outside the Zope package.
 
 # from Zope.Testing.CleanUp import CleanUp # Base class w registry cleanup
 
