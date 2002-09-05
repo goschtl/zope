15de6cc74934318fbfd2a9599d183c999d879d16
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2002-09-05 20:29:03 +0000

    Removed some old files.
    git-svn-id: svn://svn.zope.org/repos/main@6292 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/lib/python/Zope/App/Forms/Views/Browser/CheckboxWidget.py
+++
@@ -1,8 +0,0 @@
-
-from Zope.Publisher.Browser.BrowserView import BrowserView
-
-class CheckboxWidget(BrowserView):
-    def render(self):
-        return '<input type="checkbox" name="%s" value="%s" />' %\
-               (self.context.title, self.context.default)
-    
