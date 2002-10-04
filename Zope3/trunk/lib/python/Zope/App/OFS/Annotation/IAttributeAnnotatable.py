468fec0cc68f9d46b7849ec4b23822ef660ec3db
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2002-10-04 17:46:05 +0000

    Got rid of attribute declarations. This was messing up verification, as there isn't a way, currently, to say that an attribute name is reserved, but not necessarily implemented.
    git-svn-id: svn://svn.zope.org/repos/main@6384 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/lib/python/Zope/App/OFS/Annotation/IAttributeAnnotatable.py
+++ Zope3/trunk/lib/python/Zope/App/OFS/Annotation/IAttributeAnnotatable.py
@@ -13,21 +13,12 @@
 ##############################################################################
 """
 
-$Id: IAttributeAnnotatable.py,v 1.3 2002/07/20 23:31:57 sidnei Exp $
+$Id: IAttributeAnnotatable.py,v 1.4 2002/10/04 17:46:05 jim Exp $
 """
 from IAnnotatable import IAnnotatable
-from Interface.Attribute import Attribute
 
 class IAttributeAnnotatable(IAnnotatable):
     """
     Marker interface giving permission for an IAnnotations adapter to store
     data in an attribute named __annotations__.
     """
-
-    __annotations__ = Attribute(
-        """
-        This attribute may be used by an IAnnotations adapter to
-        store pickleable data in.  The object implementing this
-        interface promises not to touch the attribute other than
-        to persist it.
-        """)
