ede564e4625cb59cff4c56330a99f245d96f5164
mgedmin <mgedmin@62d5b8a3-27da-0310-9561-8e5933582275>
2002-10-09 13:53:54 +0000

    Make WidgetInputError printable
    git-svn-id: svn://svn.zope.org/repos/main@6419 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/lib/python/Zope/App/Forms/Exceptions.py
+++ Zope3/trunk/lib/python/Zope/App/Forms/Exceptions.py
@@ -2,37 +2,38 @@
 #
 # Copyright (c) 2002 Zope Corporation and Contributors.
 # All Rights Reserved.
-# 
+#
 # This software is subject to the provisions of the Zope Public License,
 # Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
-# 
+#
 ##############################################################################
 """Validation Exceptions
 
-$Id: Exceptions.py,v 1.1 2002/09/07 16:18:48 jim Exp $
+$Id: Exceptions.py,v 1.2 2002/10/09 13:53:54 mgedmin Exp $
 """
 
 
 class WidgetInputError(Exception):
-    """There were one or more user input errors 
+    """There were one or more user input errors
     """
 
     def __init__(self, widget_name, widget_title, errors):
+        Exception.__init__(self, widget_name, widget_title, errors)
         self.widget_name = widget_name
         self.widget_title = widget_title
         self.errors = errors
-    
-    
+
+
 class ConversionError(WidgetInputError):
     """If some conversion fails, this exception is raised.
     """
 
     def __init__(self, error_name, original_exception=None):
-        Exception.__init__(self)
+        Exception.__init__(self, error_name, original_exception)
         self.error_name = error_name
         self.original_exception = original_exception
 
