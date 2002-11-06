bdf518dbfaf266863f0b5d1bdd5b0ad2a3087fb7
rdmurray <rdmurray@62d5b8a3-27da-0310-9561-8e5933582275>
2002-11-06 22:35:42 +0000

    Add missing copyright.
    git-svn-id: svn://svn.zope.org/repos/main@6678 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/lib/python/Zope/Configuration/tests/BaseTestDirectivesXML.py
+++ Zope3/trunk/lib/python/Zope/Configuration/tests/BaseTestDirectivesXML.py
@@ -1,3 +1,16 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 """Tests of the XML parsing machinery
 
 This mixin passes a series of sets of configuration directives
@@ -9,6 +22,8 @@ were taken, or the expected errors raised.
 
 These tests test only the capabilities of the meta-directives
 implemented by the bootstrap versions.
+
+$Id: BaseTestDirectivesXML.py,v 1.3 2002/11/06 22:35:42 rdmurray Exp $
 """
 
 from cStringIO import StringIO
