fc5d2f346ae77e586de53f892bdfdb75a9273e76
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2002-11-11 20:17:51 +0000

    Added a service manager implementation suitable for testing placeful services. This has the advantage that it is much easier to set up services in it than in full-blown service managers. In particular, service directives aren't needed.
    git-svn-id: svn://svn.zope.org/repos/main@6723 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/trunk/lib/python/Zope/App/OFS/Services/ServiceManager/tests/TestServiceManager.py
@@ -0,0 +1,52 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+# 
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+# 
+##############################################################################
+"""
+$Id: TestServiceManager.py,v 1.1 2002/11/11 20:17:51 jim Exp $
+"""
+
+__metaclass__ = type
+
+from Zope.ComponentArchitecture.IServiceService import IServiceService
+from Zope.App.ComponentArchitecture.NextService \
+     import getNextService, getNextServiceManager
+from Zope.Proxy.ContextWrapper import ContextWrapper
+
+class TestServiceManager:
+    """Simple placeful service manager used for writing tests
+    """
+    __implements__ =  IServiceService
+
+
+    def getServiceDefinitions(self):
+        "See Zope.ComponentArchitecture.IServiceService.IServiceService"
+        return getNextServiceManager.getServiceDefinitions()
+
+    def getInterfaceFor(self, name):
+        "See Zope.ComponentArchitecture.IServiceService.IServiceService"
+        return getNextServiceManager.getServiceDefinitions()
+
+    def getService(self, name):
+        "See Zope.ComponentArchitecture.IServiceService.IServiceService"
+        if hasattr(self, name):
+            return ContextWrapper(getattr(self, name), self, name=name)
+        return getNextServiceManager.getService(name)
+
+    def queryService(self, name, default=None):
+        "See Zope.ComponentArchitecture.IServiceService.IServiceService"
+        if hasattr(self, name):
+            return ContextWrapper(getattr(self, name), self, name=name)
+        return getNextServiceManager.queryService(name, default)
+
+__doc__ = TestServiceManager.__doc__ + __doc__
+
