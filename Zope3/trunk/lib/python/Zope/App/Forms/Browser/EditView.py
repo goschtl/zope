1beaa0351974b8da6ebc50b67d861df6b0185f79
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2002-11-11 20:29:58 +0000

    Added a mechanism for customizable automatic editing views. Views are generated from a schema and can be customized with a class or an alternate template.
    git-svn-id: svn://svn.zope.org/repos/main@6727 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/trunk/lib/python/Zope/App/Forms/Browser/EditView.py
@@ -0,0 +1,180 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+# 
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+# 
+##############################################################################
+"""
+$Id: EditView.py,v 1.1 2002/11/11 20:29:58 jim Exp $
+"""
+
+from datetime import datetime
+from Zope.Publisher.Browser.BrowserView import BrowserView
+from Zope.App.Forms.Views.Browser import Widget
+from Zope.App.Forms.Exceptions import WidgetsError
+from Zope.App.Forms.Utility import setUpEditWidgets, getWidgetsData
+from Zope.App.Forms.Utility import haveWidgetsData, fieldNames
+from Zope.Configuration.Action import Action
+from Zope.App.PageTemplate.ViewPageTemplateFile import ViewPageTemplateFile
+from Zope.Security.Checker import defineChecker, NamesChecker
+from Zope.ComponentArchitecture.GlobalViewService import provideView
+from Zope.Publisher.Browser.IBrowserPresentation import IBrowserPresentation
+from Zope.App.PageTemplate.SimpleViewClass import SimpleViewClass
+
+
+class EditView(BrowserView):
+    """Simple edit-view base class
+
+    Subclasses should provide a schema attribute defining the schema
+    to be edited.
+    """
+
+    errors = ()
+    label = ''
+
+    generated_form = index = ViewPageTemplateFile('edit.pt')
+
+    def __init__(self, context, request):
+        super(EditView, self).__init__(context, request)
+        setUpEditWidgets(self, self.schema)
+
+    def setPrefix(self, prefix):
+        for widget in self.widgets():
+            widget.setPrefix(prefix)
+
+    def __call__(self, *args, **kw):
+        return self.index(*args, **kw)
+
+    def widgets(self):
+        return [getattr(self, name)
+                for name in fieldNames(self.schema)
+                ]
+
+    def apply_update(self, data):
+        """Apply data updates
+
+        Return true if data were unchanged and false otherwise.
+        This sounds backwards, but it allows lazy implementations to
+        avoid tracking changes.
+        """
+            
+        content = self.context
+
+        errors = []
+        unchanged = True
+
+        for name in data:
+            # OK, we really got a field
+            try:
+                newvalue = data[name]
+
+                # We want to see if the data changes. Unfortunately,
+                # we don't know enough to know that we won't get some
+                # strange error, so we'll carefully ignore errors and
+                # assume we should update the data if we can't be sure
+                # it's the same.
+
+                change = True
+                try:
+                    # Use self as a marker
+                    change = getattr(content, name, self) != newvalue
+                except:
+                    pass
+
+                if change:
+                    setattr(content, name, data[name])
+                    unchanged = False
+
+            except Exception, v:
+                errors.append(v)
+
+        if errors:
+            raise WidgetsError(*errors)
+
+        return unchanged
+
+    def update(self):
+        if "save_submit" in self.request:
+            unchanged = True
+            try:
+                data = getWidgetsData(self, self.schema, required=0)
+                unchanged = self.apply_update(data)
+            except WidgetsError, errors:
+                self.errors = errors
+                return u"An error occured."
+            except Exception, v:
+                self.errors = (v, )
+                return u"An error occured."
+            else:
+                setUpEditWidgets(self, self.schema, force=1)
+                if not unchanged:
+                    return "Updated %s" % datetime.utcnow()
+
+        return ''
+
+    
+
+def EditViewFactory(name, schema, label, permission, layer,
+                    template, class_, for_):
+
+    if class_ is None:
+        bases = EditView,
+    else:
+        bases = class_, EditView
+    
+    class_  = SimpleViewClass(
+        template,
+        used_for = schema, bases = bases
+        )
+
+    class_.schema = schema
+    class_.label = label
+
+    defineChecker(class_,
+                  NamesChecker(
+                    ("__call__", "__getitem__", "browserDefault"),
+                    permission,
+                    )
+                  )
+
+    provideView(for_, name, IBrowserPresentation, class_, layer)
+                  
+        
+
+def directive(_context, name, schema, label,
+              permission = 'Zope.Public', layer = "default",
+              class_ = None, for_ = None,
+              template = None):
+
+    schema = _context.resolve(schema)
+    if for_ is None:
+        for_ = schema
+    else:
+        for_ = _context.resolve(for_)
+    if class_ is not None:
+        class_ = _context.resolve(class_)
+    if template is not None:
+        template = _context.path(template)
+    else:
+        template = 'edit.pt'
+
+    template = str(template)
+
+    return [
+        Action(
+        discriminator = ('http://namespaces.zope.org/form/edit',
+                         name, for_, layer),
+        callable = EditViewFactory,
+        args = (name, schema, label, permission, layer, template, class_,
+                for_),
+        )
+        ]
+
+
---
+++ Zope3/trunk/lib/python/Zope/App/Forms/Browser/tests/testEditView.py
@@ -0,0 +1,134 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""$Id: testEditView.py,v 1.1 2002/11/11 20:29:58 jim Exp $
+"""
+from unittest import TestCase, TestSuite, main, makeSuite
+from Zope.App.tests.PlacelessSetup import PlacelessSetup
+from Interface import Interface
+from Zope.Schema import TextLine
+from Zope.Schema.IField import ITextLine
+from Zope.Publisher.Browser.BrowserRequest import TestRequest
+from Zope.Publisher.Browser.IBrowserPresentation import IBrowserPresentation
+from Zope.App.Forms.Browser.EditView import EditView
+from Zope.ComponentArchitecture.GlobalViewService \
+     import provideView, setDefaultViewName
+from Zope.App.Forms.Views.Browser.Widget import TextWidget
+
+class I(Interface):
+    foo = TextLine(title=u"Foo")
+    bar = TextLine(title=u"Bar")
+    a   = TextLine(title=u"A")
+    b   = TextLine(title=u"B")
+    baz = TextLine(title=u"Baz")
+
+class EV(EditView):
+    schema = I
+
+class C:
+    foo = u"c foo"
+    bar = u"c bar"
+    a   = u"c a"
+    b   = u"c b"
+    baz = u"c baz"
+
+class Test(PlacelessSetup, TestCase):
+
+    def setUp(self):
+        PlacelessSetup.setUp(self)
+        provideView(ITextLine, 'widget', IBrowserPresentation, TextWidget)
+        setDefaultViewName(ITextLine, IBrowserPresentation, "widget")
+
+    def test_setPrefix_and_widgets(self):
+        v = EV(C(), TestRequest())
+        v.setPrefix("test")
+        self.assertEqual(
+            [w.name for w in v.widgets()],
+            ['test.foo', 'test.bar', 'test.a', 'test.b', 'test.baz']
+            )
+
+    def test_apply_update_no_data(self):
+        c = C()
+        request = TestRequest()
+        v = EV(c, request)
+        d = {}
+        d['foo'] = u'c foo'
+        d['bar'] = u'c bar'
+        d['baz'] = u'c baz'
+        self.failUnless(v.apply_update(d))
+        self.assertEqual(c.foo, u'c foo')
+        self.assertEqual(c.bar, u'c bar')
+        self.assertEqual(c.a  , u'c a')
+        self.assertEqual(c.b  , u'c b')
+        self.assertEqual(c.baz, u'c baz')
+
+    def test_apply_update(self):
+        c = C()
+        request = TestRequest()
+        v = EV(c, request)
+        d = {}
+        d['foo'] = u'd foo'
+        d['bar'] = u'd bar'
+        d['baz'] = u'd baz'
+        self.failIf(v.apply_update(d))
+        self.assertEqual(c.foo, u'd foo')
+        self.assertEqual(c.bar, u'd bar')
+        self.assertEqual(c.a  , u'c a')
+        self.assertEqual(c.b  , u'c b')
+        self.assertEqual(c.baz, u'd baz')        
+
+    def test_update_no_update(self):
+        c = C()
+        request = TestRequest()
+        v = EV(c, request)
+        self.assertEqual(v.update(), '')
+        self.assertEqual(c.foo, u'c foo')
+        self.assertEqual(c.bar, u'c bar')
+        self.assertEqual(c.a  , u'c a')
+        self.assertEqual(c.b  , u'c b')
+        self.assertEqual(c.baz, u'c baz')
+        request.form['field.foo'] = u'r foo'
+        request.form['field.bar'] = u'r bar'
+        request.form['field.a']   = u'r a'
+        request.form['field.b']   = u'r b'
+        request.form['field.baz'] = u'r baz'
+        self.assertEqual(v.update(), '')
+        self.assertEqual(c.foo, u'c foo')
+        self.assertEqual(c.bar, u'c bar')
+        self.assertEqual(c.a  , u'c a')
+        self.assertEqual(c.b  , u'c b')
+        self.assertEqual(c.baz, u'c baz')
+
+    def test_update(self):
+        c = C()
+        request = TestRequest()
+        v = EV(c, request)
+        request.form['save_submit'] = u'Submit'
+        request.form['field.foo'] = u'r foo'
+        request.form['field.bar'] = u'r bar'
+        request.form['field.baz'] = u'r baz'
+        message = v.update()
+        self.failUnless(message.startswith('Updated '), message)
+        self.assertEqual(c.foo, u'r foo')
+        self.assertEqual(c.bar, u'r bar')
+        self.assertEqual(c.a  , u'c a')
+        self.assertEqual(c.b  , u'c b')
+        self.assertEqual(c.baz, u'r baz')
+
+def test_suite():
+    return TestSuite((
+        makeSuite(Test),
+        ))
+
+if __name__=='__main__':
+    main(defaultTest='test_suite')
