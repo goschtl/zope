273c6d12cc56d72ee439b09d81038c04e524164f
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2002-11-18 23:47:58 +0000

    Provide a object removal subscriber that checks that we can't delete objects with dependents.
    git-svn-id: svn://svn.zope.org/repos/main@6769 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/trunk/lib/python/Zope/App/OFS/Container/DependencyChecker.py
@@ -0,0 +1,43 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+# 
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+# 
+##############################################################################
+"""Objects that take care of annotating dublin core meta data times
+
+$Id: DependencyChecker.py,v 1.2 2002/11/18 23:47:58 jim Exp $
+"""
+from Zope.ComponentArchitecture import queryAdapter
+from Zope.App.DependencyFramework.IDependable import IDependable
+from Zope.App.DependencyFramework.Exception import DependencyError
+from Zope.Event.ISubscriber import ISubscriber
+from Zope.Proxy.ProxyIntrospection import removeAllProxies
+class DependencyChecker:
+    """Checking dependency  while deleting object
+    """
+    __implements__ = ISubscriber
+    
+    def __init__(self):
+        pass
+
+    def notify(self, event):
+        object = removeAllProxies(event.object)
+        dependency = queryAdapter(object, IDependable)
+        if dependency is not None:
+            if dependency.dependents():
+                raise DependencyError(" Removal of object dependable")
+            
+
+    
+CheckDependency = DependencyChecker() 
+    
+        
+        
