98f0a2adee5d65a4d07137db98a68b78312f936b
gvanrossum <gvanrossum@62d5b8a3-27da-0310-9561-8e5933582275>
2002-12-03 16:25:54 +0000

    Interfaces for TextIndex.
    git-svn-id: svn://svn.zope.org/repos/main@6943 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/trunk/lib/python/Zope/TextIndex/TextIndexInterfaces.py
@@ -0,0 +1,60 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Interfaces for a text index.
+
+$Id: TextIndexInterfaces.py,v 1.1 2002/12/03 16:25:54 gvanrossum Exp $
+"""
+
+from Interface import Interface
+
+
+class IInjection(Interface):
+    """Interface for injecting documents into an index."""
+
+    def index_doc(docid, texts):
+        """Add a document to the index.
+
+        docid: int, identifying the document
+        texts: list of unicode, the text to be indexed in a list
+        return: None
+
+        This can also be used to reindex documents.
+        """
+
+    def unindex_doc(docid):
+        """Remove a document from the index.
+
+        docid: int, identifying the document
+        return: None
+
+        If docid does not exist, KeyError is raised.
+        """
+
+class IQuerying(Interface):
+
+    def query(querytext, start, count):
+        """Execute a query.
+
+        querytext: unicode, the query expression
+        start: the first result to return (0-based)
+        count: the maximum number of results to return
+        return: ([(docid, rank), ...], total)
+
+        The return value is a triple:
+            matches: list of (int, float) tuples, docid and rank
+            total: int, the total number of matches
+
+        The matches list represents the requested batch.  The ranks
+        are floats between 0 and 1 (inclusive).
+        """
