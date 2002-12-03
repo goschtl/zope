e590f987adefc5c6297c24ceec8e8a199899632c
gvanrossum <gvanrossum@62d5b8a3-27da-0310-9561-8e5933582275>
2002-12-03 22:01:20 +0000

    Whitespace fix.
    git-svn-id: svn://svn.zope.org/repos/main@6954 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/zpl.py
+++ Zope3/trunk/zpl.py
@@ -2,18 +2,18 @@
 #
 # Copyright (c) 2002 Zope Corporation and Contributors.
 # All Rights Reserved.
-# 
+#
 # This software is subject to the provisions of the Zope Public License,
 # Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
-# 
+#
 ##############################################################################
 """XXX short summary goes here.
 
 XXX longer description goes here.
 
-$Id: zpl.py,v 1.3 2002/06/19 16:56:04 jim Exp $
+$Id: zpl.py,v 1.4 2002/12/03 22:01:20 gvanrossum Exp $
 """
