ec8a0fa1ec380a038bb6089fdd785a345acb48c9
stevea <stevea@62d5b8a3-27da-0310-9561-8e5933582275>
2002-12-05 17:15:40 +0000

    code formatting improvements.
    git-svn-id: svn://svn.zope.org/repos/main@7091 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/lib/python/Zope/App/ComponentArchitecture/tests/PlacelessSetup.py
+++ Zope3/trunk/lib/python/Zope/App/ComponentArchitecture/tests/PlacelessSetup.py
@@ -13,14 +13,12 @@
 ##############################################################################
 """Unit test logic for setting up and tearing down basic infrastructure
 
-
-$Id: PlacelessSetup.py,v 1.2 2002/11/19 23:15:14 jim Exp $
+$Id: PlacelessSetup.py,v 1.3 2002/12/05 17:15:40 stevea Exp $
 """
 
 from Zope.ComponentArchitecture import getServiceManager
 from Zope.App.ComponentArchitecture.IInterfaceService import IInterfaceService
 from Zope.App.ComponentArchitecture.InterfaceService import interfaceService
-
 
 from Zope.Event.IEventService import IEventService
 from Zope.Event.GlobalEventService import eventService
@@ -30,9 +28,9 @@ class PlacelessSetup:
 
     def setUp(self):
 
-        sm=getServiceManager(None)
-        defineService=sm.defineService
-        provideService=sm.provideService
+        sm = getServiceManager(None)
+        defineService = sm.defineService
+        provideService = sm.provideService
 
         defineService("Interfaces", IInterfaceService)
         provideService("Interfaces", interfaceService)
