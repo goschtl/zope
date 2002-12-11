288cafa47ab3297513df73cd30094f0902084a2f
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2002-12-11 16:11:14 +0000

    Added code to generate schema python code from schema. Not done yet, but this basic framework passes the tests.
    git-svn-id: svn://svn.zope.org/repos/main@7241 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/trunk/lib/python/Zope/App/schemagen/tests/test_typereg.py
@@ -0,0 +1,156 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""XXX short summary goes here.
+
+XXX longer description goes here.
+
+$Id: test_typereg.py,v 1.1 2002/12/11 16:11:14 faassen Exp $
+"""
+
+from unittest import TestCase, makeSuite, TestSuite
+import datetime
+
+from Zope.App.schemagen.typereg import TypeRepresentationRegistry,\
+     DefaultTypeRepresentation, DatetimeRepresentation,\
+     DefaultFieldRepresentation
+
+class DefaultTypeRepresentationTests(TestCase):
+    def test_getTypes(self):
+        c = DefaultTypeRepresentation
+        self.assertEquals((), tuple(c.getTypes()))
+
+    def h(self, obj):
+        r = DefaultTypeRepresentation(obj)
+        self.assertEquals(eval(r.text), obj)
+    
+    def h_builtin(self, obj):
+        r = DefaultTypeRepresentation(obj)
+        self.assertEquals(eval(r.text), obj)
+        self.assertEquals([], r.importList)
+
+    builtin_instances = [
+        'foo',
+        1,
+        1.1,
+        (),
+        (1, "foo"),
+        [],
+        [5, 2],
+        {},
+        {'foo':'bar', 'baz':'hoi'},
+        {'foo': (1, "foo")}
+        ]
+    
+    def test_builtins(self):
+        for builtin_instance in self.builtin_instances:
+            self.h_builtin(builtin_instance)
+
+    def test_recursive(self):
+        # we cannot cope with recursive structures
+        recursive = [1, 2]
+        recursive.append(recursive)
+        r = DefaultTypeRepresentation(recursive)
+        self.assertRaises(SyntaxError, eval, r.text)
+
+class DatetimeRepresentationTests(TestCase):
+
+    datetime_instances = [
+        (datetime.date(2002, 10, 30),
+         [('datetime', 'date')]),
+        (datetime.datetime(2002, 10, 30, 11, 44, 10),
+         [('datetime', 'datetime')]),
+        (datetime.time(10, 0, 1),
+         [('datetime', 'time')]),
+        ]
+    
+    def test_date(self):
+        for datetime_instance, import_list in self.datetime_instances:
+            r = DatetimeRepresentation(datetime_instance)
+            self.assertEquals(datetime_instance, evalRepresentation(r))
+            r_imports = r.importList
+            r_imports.sort()
+            import_list.sort()
+            self.assertEquals(r_imports, import_list)
+
+def evalRepresentation(r):
+    import_dict = {}
+    for import_name, import_from in r.importList:
+        module = __import__(import_name, {}, {}, [import_from])
+        import_dict[import_from] = getattr(module, import_from)
+    return eval(r.text, import_dict)
+
+class TypeRepresentationRegistryTests(TestCase):
+    def setUp(self):
+        self.tr = TypeRepresentationRegistry(DefaultTypeRepresentation)
+
+    def test_default(self):
+        self.assert_(isinstance(
+            self.tr.represent(1), DefaultTypeRepresentation))
+        
+    def test_register(self):
+        from Zope.App.schemagen.interfaces import ITypeRepresentation
+        class IntRepresentation:
+            __implements__ = ITypeRepresentation
+            def __init__(self, obj):
+                pass
+            def getTypes():
+                return (int,)
+
+            getTypes = staticmethod(getTypes)
+
+            importList = []
+            text = ''
+            
+        self.tr.register(IntRepresentation)
+        self.assert_(isinstance(self.tr.represent(1), IntRepresentation))
+
+        self.assert_(isinstance(self.tr.represent('foo'),
+                                DefaultTypeRepresentation))
+
+
+from Zope import Schema
+
+
+class DefaultFieldRepresentationTests(TestCase):
+    # XXX there is an issue with field classes which have the same name
+    # multiple 'from x import y' statements will cause one name to be
+    # shadowed by another. We can't test for this yet.
+
+    schema_fields = [
+        (Schema.Text(title=u"text"),
+         [('Zope.Schema', 'Text')]),
+        (Schema.Int(title=u"int"),
+         [('Zope.Schema', 'Int')]),
+        (Schema.TextLine(title=u"text"),
+         [('Zope.Schema', 'TextLine')]),
+        (Schema.Float(title=u"float"),
+         [('Zope.Schema', 'Float')])
+        ]
+    
+    def test_field(self):
+        for field, import_list in self.schema_fields:
+            r = DefaultFieldRepresentation(field)
+            self.assertEquals(field, evalRepresentation(r))
+            r_imports = r.importList
+            r_imports.sort()
+            import_list.sort()
+            self.assertEquals(r_imports, import_list)
+
+    
+def test_suite():
+    return TestSuite(
+        (makeSuite(DefaultTypeRepresentationTests),
+         makeSuite(DatetimeRepresentationTests),
+         makeSuite(TypeRepresentationRegistryTests),
+         makeSuite(DefaultFieldRepresentationTests)))
---
+++ Zope3/trunk/lib/python/Zope/App/schemagen/typereg.py
@@ -0,0 +1,136 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""XXX short summary goes here.
+
+XXX longer description goes here.
+
+$Id: typereg.py,v 1.1 2002/12/11 16:11:14 faassen Exp $
+"""
+
+from Interface.Implements import visitImplements
+from Zope.Schema import getFields
+
+from interfaces import ITypeRepresentation
+
+class TypeRepresentationRegistry:
+    def __init__(self, default):
+        self._registry = {}
+        self._default = default
+        
+    #def registerType(self, type, factory):
+    #    self._registry[type] = factory
+
+    def represent(self, object):
+        # returns an ITypeRepresentation object
+        factory = self._registry.get(type(object))
+        if factory is not None:
+            return factory(object)
+        return self._default(object)
+
+    def register(self, representation):
+        for type in representation.getTypes():
+            self._registry[type] = representation
+        
+class DefaultTypeRepresentation:
+    __implements__ = ITypeRepresentation
+
+    def __init__(self, object):
+        self.text = repr(object)
+        import_name = type(object).__name__
+        import_module = type(object).__module__
+        if import_module != '__builtin__':
+            self.importList = [(import_module, import_name)]
+        else:
+            self.importList = []
+            
+    def getTypes():
+        return ()
+    getTypes = staticmethod(getTypes)
+
+class DatetimeRepresentation:
+    __implements__ = ITypeRepresentation
+
+    def __init__(self, dt):
+        self.text = repr(dt)
+        self.importList = [('datetime', type(dt).__name__)]
+
+    def getTypes():
+        import datetime
+        # XXX not supporting tz or timedelta yet
+        return [
+            datetime.date,
+            datetime.datetime,
+          # datetime.datetimetz,
+            datetime.time,
+          #  datetime.timedelta,
+          # datetime.timetz
+        ]
+    getTypes = staticmethod(getTypes)
+
+class DefaultFieldRepresentation:
+    __implements__ = ITypeRepresentation
+
+    def __init__(self, field):
+        # This field is described by a schema, or schemas, as found in its
+        # __implements__ attribute. The fields of this field's schema are
+        # its properties -- that is, the things we give as arguments when
+        # constructing this field.
+        # We're going to get these properties, get source-code
+        # representations of them, and sort out appropriate imports.
+        names = {} # used as set of property names, ignoring values
+        visitImplements(field.__implements__, field,
+                        lambda interface: names.update(getFields(interface)))
+        # getFields only returns data for Fields in the interface.
+        # Otherwise, it returns an empty dict.
+    
+        # At this point, name_property is a dict with keys of the
+        # property names, and values of property objects.
+        # XXX I don't know if we're following proper MRO though.
+
+        global typeRegistry
+        self.importList =  self._getImportList(field)
+        arguments = []
+        # don't represent order, as that will be implicit
+        if 'order' in names:
+            del names['order']
+        for name in names:
+            representation = typeRegistry.represent(getattr(field, name))
+            arguments.append((name, representation.text))
+            for import_spec in representation.importList:
+                self.importList.append(import_spec) 
+
+        arguments_text = ', '.join(["%s=%s" % item for item in arguments])
+
+        self.text = "%s(%s)" % (type(field).__name__, arguments_text)
+        
+    def getTypes():
+        return ()
+    getTypes = staticmethod(getTypes)
+
+    def _getImportList(field):
+        import Zope.Schema
+
+        field_class = type(field)
+        if getattr(Zope.Schema, field_class.__name__, None) is field_class:
+            module_name = 'Zope.Schema'
+        else:
+            module_name = field_class.__module__
+        return [(module_name, field_class.__name__)]
+    
+    _getImportList = staticmethod(_getImportList)
+    
+typeRegistry = TypeRepresentationRegistry(DefaultTypeRepresentation)
+typeRegistry.register(DatetimeRepresentation)
+
+fieldRegistry = TypeRepresentationRegistry(DefaultFieldRepresentation)
