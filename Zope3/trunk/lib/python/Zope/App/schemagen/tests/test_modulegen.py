360122d7df856ba5eaf064c3b2db5e2c98589602
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2002-12-12 09:17:46 +0000

    Added some tests of the generated class.
    git-svn-id: svn://svn.zope.org/repos/main@7249 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/lib/python/Zope/App/schemagen/tests/test_modulegen.py
+++ Zope3/trunk/lib/python/Zope/App/schemagen/tests/test_modulegen.py
@@ -15,7 +15,7 @@
 
 XXX longer description goes here.
 
-$Id: test_modulegen.py,v 1.1 2002/12/11 19:07:23 faassen Exp $
+$Id: test_modulegen.py,v 1.2 2002/12/12 09:17:46 faassen Exp $
 """
 
 from unittest import TestCase, makeSuite, TestSuite
@@ -28,13 +28,15 @@ class GenerateModuleSourceTestsBase(TestCase):
 
     fields = []
 
-    def test_generateModuleSource(self):
-        
+    def setUp(self):
         source = generateModuleSource('IFoo', self.fields, "Foo")
         g = {}
         exec source in g
         del g['__builtins__'] # to ease inspection during debugging
-        IFoo = g['IFoo']
+        self.g = g
+        
+    def test_schema(self):
+        IFoo = self.g['IFoo']
 
         fieldsorter = lambda x, y: cmp(x[1].order, y[1].order)
         new_fields = getFields(IFoo).items()
@@ -45,15 +47,34 @@ class GenerateModuleSourceTestsBase(TestCase):
     # by execing generated module source again and then generating
     # module source for the schema in that. This requires the arguments
     # to fields (their properties) to be in their own schema order.
-    
+
+    def test_class(self):
+        from Zope.Schema.FieldProperty import FieldProperty
+        IFoo = self.g['IFoo']
+        Foo = self.g['Foo']
+        self.assertEquals(Foo.__schema_version__, 0)
+        self.assertEquals(Foo.__implements__, IFoo)
+        for field_name, field in self.fields:
+            prop = getattr(Foo, field_name, None)
+            self.assert_(prop is not None)
+            self.assert_(type(prop) is FieldProperty)
+
+    def test_instance(self):
+        Foo = self.g['Foo']
+        foo = Foo()
+        for field_name, field in self.fields:
+            self.assertEquals(field.default, getattr(foo, field_name))
+        
 class GenerateModuleSourceTestsEmpty(GenerateModuleSourceTestsBase):
     fields = []
 
 class GenerateModuleSourceTests1(GenerateModuleSourceTestsBase):
     fields = [('foo', Text(title=u"Foo")),
               ('bar', Int(title=u"Bar")),
-              ('hoi', Float(title=u"Float"))]
-    
+              ('hoi', Float(title=u"Float")),
+              ('dag', Int(title=u"Dag", default=42)),]
+
+ 
 def test_suite():
     return TestSuite(
         (makeSuite(GenerateModuleSourceTestsEmpty),
