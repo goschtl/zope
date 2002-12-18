5a4ac393222664cf5082568e6b8a42dff1e29002
poster <poster@62d5b8a3-27da-0310-9561-8e5933582275>
2002-12-18 19:27:02 +0000

    First half of moving event and i18n metaConfigure files to App
    git-svn-id: svn://svn.zope.org/repos/main@7315 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/trunk/lib/python/Zope/App/i18n-metaConfigure.py
@@ -0,0 +1,53 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+# 
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+# 
+##############################################################################
+"""
+This module handles the :startup directives. 
+
+$Id: i18n-metaConfigure.py,v 1.1 2002/12/18 19:27:02 poster Exp $
+"""
+
+import os
+from Zope.Configuration.Action import Action
+from Zope.I18n.GettextMessageCatalog import GettextMessageCatalog
+from Zope.I18n.GlobalTranslationService import translationService
+
+def registerTranslations(_context, directory):
+    """ """
+    actions = []
+
+    path = _context.path(directory)
+    path = os.path.normpath(path)
+
+    for language in os.listdir(path):
+        lc_messages_path = os.path.join(path, language, 'LC_MESSAGES')
+        if os.path.isdir(lc_messages_path):
+            for domain_file in os.listdir(lc_messages_path):
+                if domain_file.endswith('.mo'):
+                    domain_path = os.path.join(lc_messages_path, domain_file)
+                    domain = domain_file[:-3]
+                    catalog = GettextMessageCatalog(language, domain,
+                                                    domain_path)
+
+                    actions.append(Action(
+                        discriminator = catalog.getIdentifier(),
+                        callable = translationService.addCatalog,
+                        args = (catalog,) ))
+    return actions
+
+
+def defaultLanguages(_context, languages):
+    langs = [L.strip() for L in languages.split()]
+    return [Action(discriminator = ('gts', languages),
+                   callable = translationService.setLanguageFallbacks,
+                   args = (langs,))]
