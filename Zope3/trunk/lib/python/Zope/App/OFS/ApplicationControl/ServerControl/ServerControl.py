ed6f75a603732a4a5048086503d016dfebb2a79f
tim_one <tim_one@62d5b8a3-27da-0310-9561-8e5933582275>
2002-12-19 21:51:52 +0000

    More logging.
    git-svn-id: svn://svn.zope.org/repos/main@7355 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/lib/python/Zope/App/OFS/ApplicationControl/ServerControl/ServerControl.py
+++ Zope3/trunk/lib/python/Zope/App/OFS/ApplicationControl/ServerControl/ServerControl.py
@@ -2,22 +2,22 @@
 #
 # Copyright (c) 2001, 2002 Zope Corporation and Contributors.
 # All Rights Reserved.
-# 
+#
 # This software is subject to the provisions of the Zope Public License,
 # Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
-# 
+#
 ##############################################################################
 __doc__ = """ Server Control Implementation
 
-$Id: ServerControl.py,v 1.2 2002/06/10 23:27:51 jim Exp $"""
+$Id: ServerControl.py,v 1.3 2002/12/19 21:51:52 tim_one Exp $"""
 
 from Zope.App.OFS.ApplicationControl.ServerControl.IServerControl import \
   IServerControl, ServerControlError, DoublePriorityError, NotCallableError
-import zLOG
+import logging
 
 import sys
 
@@ -71,4 +71,4 @@ class ServerControl:
 ## simple log notification for shutdown
 def shutdownLogger():
     """simple shutdown logger"""
-    zLOG.LOG("ServerControl", zLOG.INFO, "Server is going to be shut down.")
+    logging.warn("ServerControl: Server is going to be shut down.")
