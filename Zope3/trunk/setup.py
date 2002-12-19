5a33f03af0ec204f1d18c17e153a60f17a38a267
jeremy <jeremy@62d5b8a3-27da-0310-9561-8e5933582275>
2002-12-19 19:46:00 +0000

    Provisional Extension definitions.
    git-svn-id: svn://svn.zope.org/repos/main@7331 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/setup.py
+++ Zope3/trunk/setup.py
@@ -39,3 +39,71 @@ if not "depends" in Extension.__init__.func_code.co_varnames:
                 del kwargs["depends"]
             _Extension.__init__(self, name, sources, **kwargs)
 
+base_btrees_depends = [
+    "lib/python/Persistence/cPersistence.h",
+    "lib/python/Persistence/cPersistenceAPI.h",
+    "lib/python/Persistence/BTrees/BTreeItemsTemplate.c",
+    "lib/python/Persistence/BTrees/BTreeModuleTemplate.c",
+    "lib/python/Persistence/BTrees/BTreeTemplate.c",
+    "lib/python/Persistence/BTrees/BucketTemplate.c",
+    "lib/python/Persistence/BTrees/MergeTemplate.c",
+    "lib/python/Persistence/BTrees/SetOpTemplate.c",
+    "lib/python/Persistence/BTrees/SetTemplate.c",
+    "lib/python/Persistence/BTrees/TreeSetTemplate.c",
+    "lib/python/Persistence/BTrees/sorters.c",
+    ]
+
+_flavors = {"O": "object", "I": "int"}
+
+KEY_H = "lib/python/Persistence/BTrees/%skeymacros.h"
+VALUE_H = "lib/python/Persistence/BTrees/%svaluemacros.h"
+
+def BTreeExtension(flavor):
+    key = flavor[0]
+    value = flavor[1]
+    name = "Persistence.BTrees._%sBTree" % flavor
+    sources = ["lib/python/Persistence/BTrees/_%sBTree.c" % flavor]
+    kwargs = {"include_dirs": ["lib/python/Persistence"]}
+    if flavor != "fs":
+        kwargs["depends"] = (base_btrees_depends + [KEY_H % _flavors[key],
+                                                    VALUE_H % _flavors[value]])
+    if key != "O":
+        kwargs["define_macros"] = [('EXCLUDE_INTSET_SUPPORT', None)]
+    return Extension(name, sources, **kwargs)
+
+ext_modules = [
+    BTreeExtension("OO"), BTreeExtension("IO"), BTreeExtension("OI"),
+    BTreeExtension("II"), BTreeExtension("fs"),
+    Extension("Persistence.cPersistence",
+              ["lib/python/Persistence/cPersistence.c"],
+              depends = ["lib/python/Persistence/cPersistence.h",
+                         "lib/python/Persistence/cPersistenceAPI.h",]),
+    Extension("ZODB._TimeStamp", ["lib/python/ZODB/TimeStamp.c"]),
+    Extension("ZODB.winlock", ["lib/python/ZODB/winlock.c"]),
+    Extension("BDBStorage._helper", ["lib/python/BDBStorage/_helper.c"]),
+    Extension("Zope.ContextWrapper.wrapper",
+              ["lib/python/Zope/ContextWrapper/wrapper.c"],
+              include_dirs = ["include"],
+              depends = ["include/Zope/ContextWrapper/wrapper.h",
+                         "include/Zope/Proxy/proxy.h"]),
+    Extension("Zope.Proxy.proxy", ["lib/python/Zope/Proxy/proxy.c"],
+              include_dirs = ["include"],
+              depends = ["include/Zope/Proxy/proxy.h"]),
+    Extension("Zope.Security._Proxy", ["lib/python/Zope/Security/_Proxy.c"],
+              include_dirs = ["include"],
+              depends = ["include/Zope/Proxy/proxy.h"]),
+    ]
+
+doclines = __doc__.split("\n")
+
+setup(name="Zope3",
+      version="3.0a1",
+      maintainer="Zope Corporation",
+      maintainer_email="zope3-dev@zope.org",
+      ext_modules = ext_modules,
+      headers = ["ZODB/cPersistence.h", "ZODB/cPersistenceAPI.h"],
+      license = "http://www.zope.org/Resources/ZPL",
+      platforms = ["any"],
+      description = doclines[0],
+      long_description = "\n".join(doclines[2:]),
+      )
