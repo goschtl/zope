5105b2b2f58c8a96ccf42b184a8ae8d435fac852
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2002-12-20 23:12:37 +0000

    Got rid of the no-longer needed FolderContents, since navigation to anfd creation of service managers is now accomplished via the actions menu.
    git-svn-id: svn://svn.zope.org/repos/main@7426 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/lib/python/Zope/App/OFS/Services/RoleService/Views/Browser/Contents.py
+++ Zope3/trunk/lib/python/Zope/App/OFS/Services/RoleService/Views/Browser/Contents.py
@@ -13,13 +13,10 @@
 ##############################################################################
 """ Define view component for service manager contents.
 
-$Id: Contents.py,v 1.3 2002/06/23 17:03:43 jim Exp $
+$Id: Contents.py,v 1.4 2002/12/20 23:12:37 jim Exp $
 """
 
 from Zope.App.OFS.Container.Views.Browser.Contents import Contents
-from Zope.App.OFS.Content.Folder.Views.Browser.FolderContents \
-     import FolderContents
-from Interface.Implements import flattenInterfaces, objectImplements
 
 class Contents(Contents):
     pass
