84155b64d3f70a8419a2674e32b25339cbfed354
tim_one <tim_one@62d5b8a3-27da-0310-9561-8e5933582275>
2002-12-20 17:47:04 +0000

    Expose datetime.MINYEAR and datetime.MAXYEAR.
    git-svn-id: svn://svn.zope.org/repos/main@7398 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/lib/python/datetime/__init__.py
+++ Zope3/trunk/lib/python/datetime/__init__.py
@@ -1,8 +1,11 @@
-# Python datetime `prototype
+# Python datetime prototype,
 
 # This package contains the prototype datetime module that will be included
 # in Python 2.3.  We've turned it into a package to make it easier to
 # deal with in CVS for now.  This __init__ file makes the package look
 # like the eventual module.
 
-from _datetime import timedelta, date, time, timetz, datetime, datetimetz
+from _datetime import MINYEAR, MAXYEAR
+from _datetime import timedelta
+from _datetime import time, timetz
+from _datetime import date, datetime, datetimetz
