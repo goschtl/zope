8d00cb21acbbbc50237298c7aee87b264bf3d037
stevea <stevea@62d5b8a3-27da-0310-9561-8e5933582275>
2002-12-21 20:05:46 +0000

    refactored NameComponentConfigurableView to derive from a new NameConfigurableView.
    git-svn-id: svn://svn.zope.org/repos/main@7453 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/lib/python/Zope/App/OFS/Services/Browser/NameComponentConfigurableView.py
+++ Zope3/trunk/lib/python/Zope/App/OFS/Services/Browser/NameComponentConfigurableView.py
@@ -13,44 +13,26 @@
 ##############################################################################
 """Generic INameComponentConfigurable view mixin
 
-$Id: NameComponentConfigurableView.py,v 1.1 2002/12/18 20:23:03 stevea Exp $
+$Id: NameComponentConfigurableView.py,v 1.2 2002/12/21 20:05:46 stevea Exp $
 """
 
-from Zope.Publisher.Browser.BrowserView import BrowserView
-from Zope.ComponentArchitecture import getView
 from Zope.App.Traversing import traverse
 from Zope.App.PageTemplate.ViewPageTemplateFile import ViewPageTemplateFile
+from Zope.App.OFS.Services.Browser.NameConfigurableView \
+        import NameConfigurableView
+from Zope.ComponentArchitecture import getView
 
-
-class NameComponentConfigurableView(BrowserView):
+class NameComponentConfigurableView(NameConfigurableView):
 
     indexMacros = index = ViewPageTemplateFile('NameComponentConfigurable.pt')
 
-    def update(self):
-
-        names = list(self.context.listConfigurationNames())
-        names.sort()
-
-        items = []
-        for name in names:
-            registry = self.context.queryConfigurations(name)
-            view = getView(registry, "ChangeConfigurations", self.request)
-            view.setPrefix(name)
-            view.update()
-            cfg = registry.active()
-            active = cfg is not None
-            if active:
-                ob = traverse(cfg, cfg.componentPath)
-                url = str(getView(ob, 'absolute_url', self.request))
-            else:
-                url = None
-            items.append(
-                {"name": name,
-                 "active": active,
-                 "inactive": not active,
-                 "view": view,
-                 "url": url
-                 })
-
-        return items
+    def _getItem(self, name, view, cfg):
+        item_dict = NameConfigurableView._getItem(self, name, view, cfg)
+        if cfg is not None:
+            ob = traverse(cfg, cfg.componentPath)
+            url = str(getView(ob, 'absolute_url', self.request))
+        else:
+            url = None
+        item_dict['url'] = url
+        return item_dict
 
