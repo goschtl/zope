368b1ba114cb71382fbd28c2ca661a5c55d9041c
jack-e <jack-e@62d5b8a3-27da-0310-9561-8e5933582275>
2003-02-01 19:17:39 +0000

    Rotterdam Workflow-Code Migration:
    First Draft of new Interface Structure:
    
    workflow/__init__.py: Generic Interfaces for all Workflow-Implementations
                          and WorkflowService
    workflow/stateful.py: Interfaces for Stateful Workflow (Draft)
    workflow/wfmc.py    : Interfaces for Activity-Based (WfMC) Workflow (Draft)
    
    the implementation will be developed outside of Zope3 CVS-HEAD till we
    reach a first usable state. the workflow-package will temporarily live
    in Zope-CVS Packages3/workflow.
    
    
    git-svn-id: svn://svn.zope.org/repos/main@8993 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/trunk/src/zope/app/interfaces/workflow/__init__.py
@@ -0,0 +1,156 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+    Interfaces for workflow service, definition and instance.
+"""
+
+from zope.interface import Interface
+from zope.interface import Attribute
+from zope.app.interfaces.services.configuration \
+     import INamedComponentConfiguration
+
+
+
+
+class IWorkflowService(Interface):
+    """Workflow service.
+
+    A workflow service manages the process definitions.
+    """
+
+    def getProcessDefinitionNames():
+        """Return the definition names.
+
+        Returns a sequence of names.
+        """
+
+    def getProcessDefinition(name):
+        """Return the IProcessDefinition for the name.
+        """
+
+    def createProcessInstance(definition_name):
+        """Create a process instance from a process definition.
+        """
+
+
+
+
+
+
+class IProcessDefinition(Interface):
+    """Interface for workflow process definition.
+    """
+
+    name = Attribute("""The name of the ProcessDefinition""")
+
+
+    def createProcessInstance():
+        """Create a new process instance for this process definition.
+
+        Returns an IProcessInstance.
+        """
+
+
+
+class IProcessDefinitionConfiguration(INamedComponentConfiguration):
+    """Configuration for a workflow process definition.
+    """
+
+
+
+
+
+class IProcessInstance(Interface):
+    """Workflow process instance.
+
+    Represents the instance of a process defined by a ProcessDefinition.
+    """
+
+    status = Attribute("The state in which the workitem is.")
+
+    processDefinition = Attribute("The process definition.")
+
+
+
+class IProcessInstanceContainer(Interface):
+    """Workflow process instance container.
+    """
+
+    def addProcessInstance(name, pi):
+        """Add the process instance, associated to name.
+        """
+
+    def getProcessInstance(name):
+        """Get the process instance associated to the given name.
+        """
+
+    def getProcessInstanceNames():
+        """Get the names associated to all process instances.
+        """
+
+    def delProcessInstance(name):
+        """Remove the process instance associated to name.
+        """
+
+
+class IProcessInstanceContainerAdaptable(Interface):
+    """Marker interface for components that can be
+       adapted to a process instance container.
+    """
+
+
+
+
+
+class IProcessInstanceControl(Interface):
+    """Interface to interact with a process instance.
+    """
+
+    def start():
+        """Start a process instance.
+        """
+
+    def finish():
+        """Finish a process instance.
+        """
+
+
+
+
+class IWorklistHandler(Interface):
+    """Base interface for Workflow Worklist Handler.
+    """
+
+    def getWorkitems():
+        """Return a sequence of workitem.
+        """
+
+
+
+
+
+class IProcessDefinitionImportExport(Interface):
+    """ProcessDefinition Import/Export.
+    """
+
+    def importProcessDefinition(process_definition):
+        """Import a Process Definition
+        """
+
+    def exportProcessDefinition(pd_id):
+        """Export a Process Definition
+        """
+
+
+
---
+++ Zope3/trunk/src/zope/app/interfaces/workflow/stateful.py
@@ -0,0 +1,133 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+"""Interfaces for stateful workflow process definition.
+
+$Id: stateful.py,v 1.1 2003/02/01 19:17:39 jack-e Exp $
+"""
+from zope.interface import Interface, Attribute
+from zope.app.interfaces.workflow import IProcessDefinition
+from zope.app.interfaces.workflow import IProcessInstance
+
+class IStatefulProcessDefinition(IProcessDefinition):
+    """Interface for stateful workflow process definition.
+    """
+
+    def addState(name, state):
+        """Add a IState to the process definition.
+        """
+
+    def getState(name):
+        """Get the named state.
+        """
+
+    def removeState(name):
+        """Remove a state from the process definition
+
+        Raises ValueError exception if trying to delete the initial state.
+        """
+
+    def getStateNames():
+        """Get the state names.
+        """
+
+    def getInitialStateName():
+        """Get the name of the initial state.
+        """
+
+    def addTransition(name, transition):
+        """Add a ITransition to the process definition.
+        """
+
+    def getTransition(name):
+        """Get the named transition.
+        """
+
+    def removeTransition(name):
+        """Remove a transition from the process definition.
+        """
+
+    def getTransitionNames():
+        """Get the transition names.
+        """
+
+
+
+
+class IState(Interface):
+    """Interface for state of a stateful workflow process definition.
+    """
+
+
+class ITransition(Interface):
+    """Stateful workflow transition.
+    """
+
+    destinationState = Attribute("Name of the destination state.")
+
+    condition = Attribute("""The condition that is evaluated to decide if \
+                             the condition is fired or not.""")
+
+
+class IBasicTransition(ITransition):
+    """Basic stateful workflow transition.
+    """
+
+    sourceState = Attribute("Name of the source state.")
+
+
+
+
+class IStatefulProcessInstance(IProcessInstance):
+    """Workflow process instance.
+
+    Represents the instance of a process defined by a
+    StatefulProcessDefinition.
+    """
+
+    def getOutgoingTransitions():
+        """Get the outgoing transitions.
+        """
+
+    def fireTransition(id):
+        """Fire a outgoing transitions.
+        """
+
+
+
+class IContentWorkflowsUtility(Interface):
+    """A Content Workflows Utility.
+
+    it associates content objects with some workflow process definitions.
+    """
+
+    def subscribe():
+        """Subscribe to the prevailing object hub service.
+        """
+
+    def unsubscribe():
+        """Unsubscribe from the object hub service.
+        """
+
+    def isSubscribed():
+        """Return whether we are currently subscribed.
+        """
+
+    def getProcessDefinitionNames():
+        """Get the process definition names.
+        """
+
+    def setProcessDefinitionNames(names):
+        """Set the process definition names.
+        """
