37ddbc4c80bf7a567665645fe97de21ff9b37e28
stevea <stevea@62d5b8a3-27da-0310-9561-8e5933582275>
2003-02-03 15:54:25 +0000

    fixed case where there could be two Nones in the returned list
    git-svn-id: svn://svn.zope.org/repos/main@9007 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/interface/_flatten.py
+++ Zope3/trunk/src/zope/interface/_flatten.py
@@ -15,7 +15,7 @@
 
 See Adapter class.
 
-$Id: _flatten.py,v 1.2 2002/12/25 14:13:42 jim Exp $
+$Id: _flatten.py,v 1.3 2003/02/03 15:54:25 stevea Exp $
 """
 __metaclass__ = type # All classes are new style when run with Python 2.2+
 
@@ -40,7 +40,7 @@ def _flatten(implements, include_None=0):
             flattened.append(interface)
     flattened.reverse()
 
-    if include_None:
+    if include_None and None not in flattened:
         flattened.append(None)
 
     return flattened
