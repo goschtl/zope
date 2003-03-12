659a571e7b234575f7f8a12a46765eab5d16b0cc
stevea <stevea@62d5b8a3-27da-0310-9561-8e5933582275>
2003-03-12 10:06:19 +0000

    Added ZPL header. Changed into a new-style class.
    git-svn-id: svn://svn.zope.org/repos/main@9243 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/browser/skins/debug/exceptions.py
+++ Zope3/trunk/src/zope/app/browser/skins/debug/exceptions.py
@@ -1,3 +1,22 @@
+##############################################################################
+#
+# Copyright (c) 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""View that renders a traceback for exceptions.
+
+$Id: exceptions.py,v 1.2 2003/03/12 10:06:19 stevea Exp $
+"""
+__metaclass__ = type
+
 import sys
 import traceback
 
