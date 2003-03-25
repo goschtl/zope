66be03dd5b6e9bd2e80a791bb4d7dbb43da64a63
nathan <nathan@62d5b8a3-27da-0310-9561-8e5933582275>
2003-03-25 20:50:16 +0000

    timedelta, date, time, datetime objects made rocks after verifying with Tim that they are immutable.
    git-svn-id: svn://svn.zope.org/repos/main@9399 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/security/checker.py
+++ Zope3/trunk/src/zope/security/checker.py
@@ -368,6 +368,10 @@ BasicTypes = {
     str: NoProxy,
     unicode: NoProxy,
     type(True): NoProxy, # Boolean, if available :)
+    datetime.timedelta: NoProxy,
+    datetime.datetime: NoProxy,
+    datetime.date: NoProxy,
+    datetime.time: NoProxy,
 }
 
 class _Sequence(object):
@@ -399,31 +403,6 @@ _default_checkers = {
     type(iter(())): _iteratorChecker, # different in Python 2.3.
     type(iter(_Sequence())): NamesChecker(['next']),
     type(Interface): _interfaceChecker,
-    datetime.timedelta: NamesChecker(['__str__', '__add__', '__radd__',
-                                      '__sub__', '__rsub__', '__neg__',
-                                      '__pos__', '__abs__', '__mul__',
-                                      '__rmul__', '__div__', '__floordiv__',
-                                      '__cmp__', 'days', 'seconds',
-                                      'microseconds']),
-    datetime.date: NamesChecker(['__str__', 'year', 'month', 'day',
-                                 'timetuple', 'toordinal', '__cmp__',
-                                 '__hash__', 'ctime', 'strftime', '__add__',
-                                 '__radd__', '__sub__', '__rsub__', 'weekday',
-                                 'isoweekday', 'isocalendar', 'isoformat',
-                                 'min', 'max', 'resolution']),
-    datetime.datetime: NamesChecker([
-      '__str__', 'year', 'month', 'day', 'hour', 'minute',
-      'second', 'microsecond', 'tzinfo', 'timetuple', 'utctimetuple',
-      'toordinal', '__cmp__', '__hash__', 'ctime', 'strftime',
-      '__add__', '__radd__', '__sub__', '__rsub__', 'weekday',
-      'isoweekday', 'isocalendar', 'isoformat', 'min', 'max',
-      'resolution', 'utcoffset', 'tzname', 'dst', 'date', 'time',
-      'timetz'
-      ]),
-    datetime.time: NamesChecker(['hour', 'minute', 'second', 'microsecond',
-                                 '__cmp__', '__hash__', '__str__', 'isoformat',
-                                 'strftime', 'min', 'max', 'resolution',
-                                 'tzinfo', 'utcoffset', 'tzname', 'dst'])
 }
 
 
