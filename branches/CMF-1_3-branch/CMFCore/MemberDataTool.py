4541477002c85daaa3b46241df4f0512b187b301
efge <efge@62d5b8a3-27da-0310-9561-8e5933582275>
2003-04-03 20:47:26 +0000

    Merge from HEAD: It's really KeyError that's raised (ExtensionClass peculiarity).
    git-svn-id: svn://svn.zope.org/repos/main@35623 62d5b8a3-27da-0310-9561-8e5933582275

--- branches/CMF-1_3-branch/CMFCore/MemberDataTool.py
+++ branches/CMF-1_3-branch/CMFCore/MemberDataTool.py
@@ -195,7 +195,7 @@ class CleanupTemp:
     def __del__(self):
         try:
             del self._tool._v_temps
-        except AttributeError:
+        except (AttributeError, KeyError):
             # The object has already been deactivated.
             pass
 
