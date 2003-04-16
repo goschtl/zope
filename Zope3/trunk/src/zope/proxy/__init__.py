b5c2124f7ae1f32d605d6a169bbf5b3de0e5956b
bwarsaw <bwarsaw@62d5b8a3-27da-0310-9561-8e5933582275>
2003-04-16 19:32:43 +0000

    proxy_compatible_isinstance(): An isinstance() implementation that mimics Python 2.3's version.  This compares the object's __class__ if it differs from its type, so this works with proxies (the proxied object is typechecked instead of the proxy).
    git-svn-id: svn://svn.zope.org/repos/main@9683 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/proxy/__init__.py
+++ Zope3/trunk/src/zope/proxy/__init__.py
@@ -1,2 +1,46 @@
+##############################################################################
 #
-# This file is necessary to make this directory a package.
+# Copyright (c) 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""More convenience functions for dealing with proxies.
+
+$Id: __init__.py,v 1.3 2003/04/16 19:32:43 bwarsaw Exp $
+"""
+
+def proxy_compatible_isinstance(obj, cls):
+    """Like built-in isinstance() in Python 2.3.
+
+    This honors __class__ if the standard isinstance() fails.  This is how it
+    works in Python 2.3 so that even proxied objects will succeed the test.
+    """
+    if isinstance(obj, cls):
+        return True
+    oclass = obj.__class__
+    if type(obj) == oclass:
+        # Nothing more we can do
+        return False
+    # Note that cls may be a tuple, but issubclass can't deal with that so we
+    # need to expand recursively.
+    classes = {}
+    flatten = [cls]
+    while flatten:
+        thisclass = flatten.pop(0)
+        if thisclass in classes:
+            continue
+        if isinstance(thisclass, tuple):
+            flatten.extend(list(thisclass))
+        else:
+            classes[thisclass] = True
+    for bclass in classes.keys():
+        if issubclass(oclass, bclass):
+            return True
+    return False
