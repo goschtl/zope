eecc3f59d6cfa55167ceb0ee5fdfcdaeae23510b
gvanrossum <gvanrossum@62d5b8a3-27da-0310-9561-8e5933582275>
2003-04-24 14:36:54 +0000

    Make it so the component path can't be changed in the edit form. Add XXX comments pointing out unresolved issues.
    git-svn-id: svn://svn.zope.org/repos/main@9751 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/interfaces/services/connection.py
+++ Zope3/trunk/src/zope/app/interfaces/services/connection.py
@@ -13,27 +13,42 @@
 ##############################################################################
 """A configuration for a database adapter.
 
-$Id: connection.py,v 1.4 2003/04/22 18:02:55 gvanrossum Exp $
+$Id: connection.py,v 1.5 2003/04/24 14:36:54 gvanrossum Exp $
 """
-from zope.app.services.field import ComponentPath
-from zope.app.interfaces.services import configuration 
+
+from zope.schema import TextLine
+from zope.app.interfaces.services.configuration import IComponentConfiguration
+from zope.app.interfaces.services.configuration import ComponentPath
 from zope.app.interfaces.rdb import IZopeDatabaseAdapter
 from zope.app.interfaces.rdb import IConnectionService
 from zope.app.interfaces.services.configuration \
         import INameComponentConfigurable
 
-class IConnectionConfiguration(configuration.INamedComponentConfiguration):
+class IConnectionConfiguration(IComponentConfiguration):
     """Database Connection Configuration
 
     Connection configurations are dependent on the database adapters that they
     configure. They register themselves as component dependents.
     """
+
+    name = TextLine(title=u"Name",
+                    description=u"The name that is registered",
+                    readonly=True,
+                    required=True,
+                    # XXX And yet, an empty name is accepted???
+                    )
     
     componentPath = ComponentPath(
-        type=IZopeDatabaseAdapter,
         title=u"Component path",
         description=u"The physical path to the component",
+        readonly=True,
         required=True)
+
+    # XXX There's still a problem with the inherited 'permission'.
+    #     Even though it is not optional, when not specified
+    #     upon creation, the creation fails with a
+    #       KeyError: 'permission'
+    #     in zope/app/browser/form/add.py on line 85.
 
 
 class ILocalConnectionService(IConnectionService, INameComponentConfigurable):
