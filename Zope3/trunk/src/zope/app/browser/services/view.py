4dfed649baa9f3f27b22f0c9b17ee1310db81f0d
gvanrossum <gvanrossum@62d5b8a3-27da-0310-9561-8e5933582275>
2003-04-30 22:06:31 +0000

    Rough and tumble version of "new UI paradigm" view page configuration.
    XXX This is still missing some features, e.g. the "(change
    registration)" link isn't implemented yet, and after adding a view, you
    are left in the RegistrationManager rather than at a more useful place
    (like maybe the view's edit page or the views service).  Also, it
    appears that you can get some weird effects when deleting a view that
    has some overlap in specification with another view; I need to look
    into that more.
    
    
    git-svn-id: svn://svn.zope.org/repos/main@9836 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/browser/services/view.py
+++ Zope3/trunk/src/zope/app/browser/services/view.py
@@ -21,7 +21,7 @@
 
   PageConfigurationView -- calls validation on PageConfiguration.
 
-$Id: view.py,v 1.9 2003/04/30 17:23:39 gvanrossum Exp $
+$Id: view.py,v 1.10 2003/04/30 22:06:31 gvanrossum Exp $
 """
 __metaclass__ = type
 
@@ -30,12 +30,18 @@ import md5
 from zope.interface import Interface
 from zope.schema import TextLine, BytesLine
 from zope.component.interfaces import IPresentation
-from zope.component import getView
+from zope.component import getAdapter, getServiceManager, getView
 from zope.proxy.context import ContextWrapper
 from zope.publisher.browser import BrowserView
 
-from zope.app.form.utility import setUpWidgets
+from zope.app.browser.services.configuration import AddComponentConfiguration
 from zope.app.component.interfacefield import InterfaceField
+from zope.app.form.utility import setUpWidgets
+from zope.app.interfaces.container import IZopeContainer
+from zope.app.interfaces.services.configuration import IUseConfiguration
+from zope.app.interfaces.services.configuration import Unregistered, Registered
+from zope.app.traversing import traverse, getPath, getParent, objectName
+
 # XXX These are not used in this module, but are referenced in configure.zcml.
 #  either configure.zcml should be fixed, or a comment should replace
 #  this one to explain why configure.zcml is importing these two names
@@ -47,7 +53,7 @@ class IViewSearch(Interface):
     forInterface = InterfaceField(title=u"For interface",
                                   required=False,
                                   )
-    presentationType = InterfaceField(title=u"Provided interface",
+    presentationType = InterfaceField(title=u"Presentation interface",
                                       required=False,
                                       basetype=IPresentation
                                       )
@@ -63,11 +69,98 @@ class IViewSearch(Interface):
 
 class ViewServiceView(BrowserView):
 
+    """Helper class for the default view on the Views service."""
+
     def __init__(self, *args):
         super(ViewServiceView, self).__init__(*args)
         setUpWidgets(self, IViewSearch)
 
+    def update(self):
+        """Possibly deactivate or delete one or more page configurations.
+
+        In that case, issue a message.
+        """
+        todo = self.request.get("selected")
+        doDeactivate = self.request.get("Deactivate")
+        doDelete = self.request.get("Delete")
+        if not todo:
+            if doDeactivate or doDelete:
+                return "Please select at least one checkbox"
+            return None
+        if doDeactivate:
+            return self._deactivate(todo)
+        if doDelete:
+            return self._delete(todo)
+
+    def _getInfosFromKey(self, key):
+        values = key.split(":")
+        assert len(values) == 4, `values`
+        viewName, forInterfaceName, presentationTypeName, layerName = values
+        sm = getServiceManager(self.context)
+        if forInterfaceName == "(Anything)":
+            forInterface = None
+        else:
+            forInterface = sm.resolve(forInterfaceName)
+        presentationType = sm.resolve(presentationTypeName)
+        return self.context.getRegisteredMatching(forInterface,
+                                                  presentationType,
+                                                  viewName,
+                                                  layerName)
+
+    def _deactivate(self, todo):
+        done = []
+        for key in todo:
+            infos = self._getInfosFromKey(key)
+            for info in infos:
+                (forInterface, presentationType,
+                 registry, layer, viewName) = info
+                registry = ContextWrapper(registry, self.context)
+                obj = registry.active()
+                if obj is not None:
+                    obj.status = Registered
+                    done.append(key)
+        if done:
+            return "Deactivated: " + ", ".join(done)
+        else:
+            return "None of the checked utilities were active"
+
+    def _delete(self, todo):
+        errors = []
+        registries = []
+
+        # Check that none of the registrations are active
+        for key in todo:
+            infos = self._getInfosFromKey(key)
+            for info in infos:
+                (forInterface, presentationType,
+                 registry, layer, viewName) = info
+                registry = ContextWrapper(registry, self.context)
+                assert registry
+                if registry.active() is not None:
+                    errors.append(key)
+                    continue
+                registries.append(registry)
+        if errors:
+            return ("Can't delete active page%s: %s; "
+                    "use the Deactivate button to deactivate" %
+                    (len(errors) != 1 and "s" or "", ", ".join(errors)))
+
+        # Now delete the registrations
+        for registry in registries:
+            assert registry
+            assert registry.active() is None # Phase error
+            for info in registry.info():
+                conf = info['configuration']
+                conf.status = Unregistered
+                parent = getParent(conf)
+                name = objectName(conf)
+                container = getAdapter(parent, IZopeContainer)
+                del container[name]
+
+        return "Deleted: %s" % ", ".join([key for key in todo])
+
     def configInfo(self):
+        """Do a search, or (by default) return all view pages."""
         input_for = self.forInterface.getData()
         input_type = self.presentationType.getData()
         input_name = self.viewName.getData()
@@ -79,12 +172,16 @@ class ViewServiceView(BrowserView):
 
             forInterface, presentationType, registry, layer, viewName = info
 
+            if not registry:
+                continue
+
             if forInterface is None:
                 forInterface = "(Anything)"
             else:
                 forInterface = (
                     forInterface.__module__ +"."+ forInterface.__name__)
 
+            shortType = presentationType.__name__
             presentationType = (
                 presentationType.__module__ +"."+ presentationType.__name__)
 
@@ -101,17 +198,31 @@ class ViewServiceView(BrowserView):
             if input_layer is not None:
                 layer = None
 
-            result.append(
-                {'forInterface': forInterface,
-                 'presentationType': presentationType,
-                 'view': view,
-                 'viewName': viewName,
-                 'layer': layer,
-                 })
+            key = "%s:%s:%s:%s" % (viewName, forInterface,
+                                   presentationType, layer)
+
+            rec = {'forInterface': forInterface,
+                   'presentationType': presentationType,
+                   'shortType': shortType,
+                   'view': view,
+                   'viewName': viewName,
+                   'layer': layer,
+                   'key': key,
+                   'configurl': "@@configureView.html?key=%s" % key,
+                   'url': "",
+                 }
+
+            active = registry.active()
+            if active is not None:
+                rec['url'] = getPath(active)
+
+            result.append(rec)
 
         return result
 
-class PageConfigurationView:
+class PageConfigurationView(BrowserView):
+
+    """Helper class for the page edit form."""
 
     def update(self):
         super(PageConfigurationView, self).update()
