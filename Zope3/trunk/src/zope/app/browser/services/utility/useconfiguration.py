11bc783181b9d442b7adbb6a7d80a7f9457e7cb0
gvanrossum <gvanrossum@62d5b8a3-27da-0310-9561-8e5933582275>
2003-04-30 15:15:30 +0000

    Added Deactivate and Delete buttons for utilities in the utility manager UI.
    git-svn-id: svn://svn.zope.org/repos/main@9816 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/browser/services/utility/useconfiguration.py
+++ Zope3/trunk/src/zope/app/browser/services/utility/useconfiguration.py
@@ -13,13 +13,16 @@
 ##############################################################################
 """Use-Configuration view for utilities.
 
-$Id: useconfiguration.py,v 1.5 2003/04/28 16:52:08 gvanrossum Exp $
+$Id: useconfiguration.py,v 1.6 2003/04/30 15:15:30 gvanrossum Exp $
 """
 
 from zope.app.browser.component.interfacewidget import InterfaceWidget
 from zope.app.browser.services.configuration import AddComponentConfiguration
 from zope.app.form.widget import CustomWidget
-from zope.component import getServiceManager, getView
+from zope.app.interfaces.container import IZopeContainer
+from zope.app.interfaces.services.configuration import Unregistered, Registered
+from zope.app.traversing import getPath, getParent, objectName
+from zope.component import getServiceManager, getView, getAdapter
 from zope.interface.implements import flattenInterfaces
 from zope.proxy.introspection import removeAllProxies
 from zope.publisher.browser import BrowserView
@@ -67,6 +70,81 @@ class AddConfiguration(AddComponentConfiguration):
 class Utilities(BrowserView):
 
     # self.context is the local utility service
+
+    def update(self):
+        """Possibly delete one or more utilities.
+
+        In that case, issue a message.
+        """
+        selected = self.request.get("selected")
+        doDeactivate = self.request.get("Deactivate")
+        doDelete = self.request.get("Delete")
+        if not selected:
+            if doDeactivate or doDelete:
+                return "Please select at least one checkbox"
+            return None
+        sm = getServiceManager(self.context)
+        todo = []
+        for key in selected:
+            name, ifacename = key.split(":", 1)
+            iface = sm.resolve(ifacename)
+            todo.append((key, name, iface))
+        if doDeactivate:
+            return self._deactivate(todo)
+        if doDelete:
+            return self._delete(todo)
+
+    def _deactivate(self, todo):
+        done = []
+        for key, name, iface in todo:
+            registry = self.context.queryConfigurations(name, iface)
+            obj = registry.active()
+            if obj is not None:
+                obj.status = Registered
+                done.append(key)
+        if done:
+            return "Deactivated: " + ", ".join(done)
+        else:
+            return "None of the checked utilities were active"
+
+    def _delete(self, todo):
+        errors = []
+        for key, name, iface in todo:
+            registry = self.context.queryConfigurations(name, iface)
+            assert registry
+            if registry.active() is not None:
+                errors.append(key)
+                continue
+        if errors:
+            return ("Can't delete active utilit%s: %s; "
+                    "use the Deactivate button to deactivate" %
+                    (len(errors) != 1 and "ies" or "y", ", ".join(errors)))
+
+        # 1) Delete the registrations
+        services = {}
+        for key, name, iface in todo:
+            registry = self.context.queryConfigurations(name, iface)
+            assert registry
+            assert registry.active() is None # Phase error
+            for info in registry.info():
+                conf = info['configuration']
+                obj = conf.getComponent()
+                path = getPath(obj)
+                services[path] = obj
+                conf.status = Unregistered
+                parent = getParent(conf)
+                name = objectName(conf)
+                container = getAdapter(parent, IZopeContainer)
+                del container[name]
+
+        # 2) Delete the service objects
+        for path, obj in services.items():
+            parent = getParent(obj)
+            name = objectName(obj)
+            container = getAdapter(parent, IZopeContainer)
+            del container[name]
+
+        return "Deleted: %s" % ", ".join([key for key, name, iface in todo])
 
     def getConfigs(self):
         L = []
