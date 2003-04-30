031cf63b8a1d11c1d75cf653ac05be6d01345ea6
gvanrossum <gvanrossum@62d5b8a3-27da-0310-9561-8e5933582275>
2003-04-30 15:30:41 +0000

    Added Deactivate and Delete buttons to listing of registered caches.
    git-svn-id: svn://svn.zope.org/repos/main@9817 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/browser/services/cache.py
+++ Zope3/trunk/src/zope/app/browser/services/cache.py
@@ -13,18 +13,89 @@
 ##############################################################################
 """Cache configuration support classes.
 
-$Id: cache.py,v 1.6 2003/04/28 15:04:38 gvanrossum Exp $
+$Id: cache.py,v 1.7 2003/04/30 15:30:41 gvanrossum Exp $
 """
 
 from zope.app.browser.services.configuration import AddComponentConfiguration
+from zope.app.interfaces.container import IZopeContainer
 from zope.app.interfaces.services.configuration import IUseConfiguration
 from zope.component import getAdapter, getServiceManager, getView
 from zope.publisher.browser import BrowserView
-from zope.app.traversing import traverse
+from zope.app.interfaces.services.configuration import Unregistered, Registered
+from zope.app.traversing import traverse, getPath, getParent, objectName
 
 class Caches(BrowserView):
 
     # self.context is the local caching service
+
+    def update(self):
+        """Possibly delete one or more caches.
+
+        In that case, issue a message.
+        """
+        todo = self.request.get("selected")
+        doDeactivate = self.request.get("Deactivate")
+        doDelete = self.request.get("Delete")
+        if not todo:
+            if doDeactivate or doDelete:
+                return "Please select at least one checkbox"
+            return None
+        if doDeactivate:
+            return self._deactivate(todo)
+        if doDelete:
+            return self._delete(todo)
+
+    def _deactivate(self, todo):
+        done = []
+        for name in todo:
+            registry = self.context.queryConfigurations(name)
+            obj = registry.active()
+            if obj is not None:
+                obj.status = Registered
+                done.append(name)
+        if done:
+            return "Deactivated: " + ", ".join(done)
+        else:
+            return "None of the checked caches were active"
+
+    def _delete(self, todo):
+        errors = []
+        for name in todo:
+            registry = self.context.queryConfigurations(name)
+            assert registry
+            if registry.active() is not None:
+                errors.append(name)
+                continue
+        if errors:
+            return ("Can't delete active cache%s: %s; "
+                    "use the Deactivate button to deactivate" %
+                    (len(errors) != 1 and "s" or "", ", ".join(errors)))
+
+        # 1) Delete the registrations
+        caches = {}
+        for name in todo:
+            registry = self.context.queryConfigurations(name)
+            assert registry
+            assert registry.active() is None # Phase error
+            for info in registry.info():
+                conf = info['configuration']
+                obj = conf.getComponent()
+                path = getPath(obj)
+                caches[path] = obj
+                conf.status = Unregistered
+                parent = getParent(conf)
+                name = objectName(conf)
+                container = getAdapter(parent, IZopeContainer)
+                del container[name]
+
+        # 2) Delete the cache objects
+        for path, obj in caches.items():
+            parent = getParent(obj)
+            name = objectName(obj)
+            container = getAdapter(parent, IZopeContainer)
+            del container[name]
+
+        return "Deleted: %s" % ", ".join(todo)
 
     def getConfigs(self):
         L = []
