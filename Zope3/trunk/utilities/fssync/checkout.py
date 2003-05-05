c587c4f4705e96ab7ab315f51a8531cc631fe586
gvanrossum <gvanrossum@62d5b8a3-27da-0310-9561-8e5933582275>
2003-05-05 21:24:32 +0000

    Facelift; get rid of string module.
    git-svn-id: svn://svn.zope.org/repos/main@9917 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/utilities/fssync/checkout.py
+++ Zope3/trunk/utilities/fssync/checkout.py
@@ -12,17 +12,14 @@
 #
 ##############################################################################
 
-import string, os
+import os
 
 from zope.app.fssync.syncer import toFS
 
 from common import getObject, getApplicationRoot
 from common import makeNewEntry, getObjectAdapter
 
-def checkout(fspath
-             , dbpath
-             , siteconfpath
-             , objpath):
+def checkout(fspath, dbpath, siteconfpath, objpath):
     """Checks out objects from ZODB to the file system
 
     Downloads ZODB objects in file system to the specified path
@@ -30,38 +27,32 @@ def checkout(fspath
     """
     sandbox_path = fspath
     if not os.path.isabs(objpath):
-        objpath = '/'+objpath
+        objpath = '/' + objpath
     objpath = os.path.abspath(objpath)
     root = getApplicationRoot(dbpath, siteconfpath)
     main_ob = getObject(objpath, root)
     ob_name = os.path.basename(objpath)
     if main_ob is not None:
         path = ''
-        for dir in string.split(objpath[:string.rfind(objpath,ob_name)], '/')[1:-1]:
-            path = os.path.join(path,dir)
+        for dir in objpath[:objpath.rfind(ob_name)].split('/')[1:-1]:
+            path = os.path.join(path, dir)
             sandbox_path = os.path.join(fspath, path)
             admin_dir = os.path.join(os.path.dirname(sandbox_path), '@@Zope')
             entries_path = os.path.join(os.path.dirname(sandbox_path),
-                                        '@@Zope'
-                                        , 'Entries.xml')
+                                        '@@Zope', 'Entries.xml')
             if not os.path.exists(sandbox_path):
                 os.mkdir(sandbox_path)
             if not os.path.exists(admin_dir):
                 os.mkdir(admin_dir)
             if not os.path.exists(entries_path):
                 makeNewEntry(admin_dir)
-
             o_path = '/'+path
             ob = getObject(o_path, root)
             adapter = getObjectAdapter(ob)
             type = adapter.typeIdentifier()
             factory = adapter.factory()
             name = os.path.basename(o_path)
-            makeNewEntry(admin_dir
-                         , name
-                         , type
-                         , factory
-                         , o_path)
+            makeNewEntry(admin_dir, name, type, factory, o_path)
 
         fspath = sandbox_path
         toFS(main_ob, ob_name, fspath)
