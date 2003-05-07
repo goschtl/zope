7a56dd6e5b232bba801733b668f53f9258391c29
stevea <stevea@62d5b8a3-27da-0310-9561-8e5933582275>
2003-05-07 09:32:13 +0000

    small formatting fix.
    git-svn-id: svn://svn.zope.org/repos/main@9937 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/proxy/proxy.c
+++ Zope3/trunk/src/zope/proxy/proxy.c
@@ -619,12 +619,12 @@ ProxyType = {
     &wrap_as_sequence,			/* tp_as_sequence */
     &wrap_as_mapping,			/* tp_as_mapping */
     wrap_hash,				/* tp_hash */
-    wrap_call,					/* tp_call */
+    wrap_call,				/* tp_call */
     wrap_str,				/* tp_str */
     wrap_getattro,			/* tp_getattro */
     wrap_setattro,			/* tp_setattro */
     0,					/* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC 
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC
         | Py_TPFLAGS_CHECKTYPES | Py_TPFLAGS_BASETYPE, /* tp_flags */
     0,					/* tp_doc */
     wrap_traverse,			/* tp_traverse */
