33799edda804edb92d3885f5f5b6c7406257716d
jeremy <jeremy@62d5b8a3-27da-0310-9561-8e5933582275>
2003-06-22 18:59:59 +0000

    Create placeholder for LocalInterfaceService
    The code just delegates to the global service for now.
    
    
    git-svn-id: svn://svn.zope.org/repos/main@10625 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/services/interface.py
+++ Zope3/trunk/src/zope/app/services/interface.py
@@ -11,11 +11,23 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""Interfaces for persistent modules."""
+"""Code about services and interfaces.
+
+This module contains code for interfaces in persistent modules, and
+for the local interface service.
+"""
 
 from persistence import Persistent
 from zodb.code.patch import registerWrapper, Wrapper
 from zope.interface.interface import InterfaceClass
+
+from zope.app.component.nextservice import getNextService
+from zope.app.interfaces.services.service import ISimpleService
+from zope.app.interfaces.component import IInterfaceService
+from zope.app import zapi
+from zope.app.services.servicenames import Interfaces
+from zope.component import ComponentLookupError
+from zope.interface import implements
 
 class PersistentInterfaceClass(Persistent, InterfaceClass):
     pass
@@ -30,8 +42,39 @@ class PersistentInterfaceWrapper(Wrapper):
     def unwrap(self):
         return PersistentInterfaceClass(self._obj.__name__)
 
-def register():
+def register(): # XXX has been refactored on a branch
     registerWrapper(InterfaceClass, PersistentInterfaceWrapper,
                     lambda iface: (),
                     lambda iface: iface.__dict__,
                     )
+
+class LocalInterfaceService(object):
+    """I need a doc string."""
+    
+    implements(IInterfaceService,
+               ISimpleService)
+
+    # All the methods defined here are context methods
+    zapi.ContextAwareDescriptors()
+    
+    def getInterface(self, id):
+        # Return the interface registered for the given id
+        i = self.queryInterface(id)
+        if i is None:
+            raise ComponentLookupError(id)
+        return i
+
+    def queryInterface(self, id, default=None):
+        # Return the interface registered for the given id
+        next = getNextService(self, Interfaces)
+        return next.queryInterface(id, default)
+
+    def searchInterface(self, search_string="", base=None):
+        # Return the interfaces that match the search criteria
+        next = getNextService(self, Interfaces)
+        return next.searchInterface(search_string, base)
+
+    def searchInterfaceIds(self, search_string="", base=None):
+        # Return the ids of the interfaces that match the search criteria.
+        next = getNextService(self, Interfaces)
+        return next.searchInterfaceIds(search_string, base)
