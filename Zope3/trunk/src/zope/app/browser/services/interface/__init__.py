cdcabe693998e809425d56ccb4e5d7d8ee9a87f3
jeremy <jeremy@62d5b8a3-27da-0310-9561-8e5933582275>
2003-06-23 16:14:19 +0000

    Add primitive detail view.
    git-svn-id: svn://svn.zope.org/repos/main@10647 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/browser/services/interface/__init__.py
+++ Zope3/trunk/src/zope/app/browser/services/interface/__init__.py
@@ -13,9 +13,47 @@
 ##############################################################################
 """Browser view for the LocalInterfaceService."""
 
+from zope.interface.interfaces import IMethod
+from zope.schema.interfaces import IField
+
+from zope.app import zapi
+
 class Interfaces:
 
     def getInterfaces(self):
         L = [(iface.__name__, id) for id, iface in self.context.items()]
         L.sort()
         return [{"id": id, "name": name} for name, id in L]
+
+class Detail:
+
+    def setup(self):
+        id = self.request["id"]
+        iface = self.context.getInterface(id)
+
+        from zope.proxy import getProxiedObject
+        iface = getProxiedObject(iface)
+        
+        self.name = iface.__name__
+        # XXX the doc string needs some formatting for presentation
+        # XXX self.doc = iface.__doc__
+        self.doc = iface.getDoc()
+        self.methods = []
+        self.schema = []
+
+        for name in iface:
+            defn = iface[name]
+            if IMethod.isImplementedBy(defn):
+                self.methods.append(defn)
+            elif IField.isImplementedBy(defn):
+                self.schema.append(defn)
+
+    def getServices(self):
+        sm = zapi.getServiceManager(self.context)
+        for name, iface in sm.getServiceDefinitions():
+            service = sm.queryService(name)
+            if service is None:
+                continue
+            if queryAdapter(service, IXXX):
+                pass
+
