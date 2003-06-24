c2186c0f7ec14b1a93e64d2134f1225e37935e8b
jeremy <jeremy@62d5b8a3-27da-0310-9561-8e5933582275>
2003-06-24 15:39:22 +0000

    Add preliminary ui for displaying service registrations.
    git-svn-id: svn://svn.zope.org/repos/main@10705 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/browser/services/interface/__init__.py
+++ Zope3/trunk/src/zope/app/browser/services/interface/__init__.py
@@ -15,6 +15,7 @@
 
 from zope.interface.interfaces import IMethod
 from zope.schema.interfaces import IField
+from zope.app.interfaces.services.interface import IInterfaceBasedRegistry
 
 from zope.app import zapi
 
@@ -32,28 +33,37 @@ class Detail:
         iface = self.context.getInterface(id)
 
         from zope.proxy import getProxiedObject
-        iface = getProxiedObject(iface)
+        self.iface = getProxiedObject(iface)
         
-        self.name = iface.__name__
+        self.name = self.iface.__name__
         # XXX the doc string needs some formatting for presentation
-        # XXX self.doc = iface.__doc__
-        self.doc = iface.getDoc()
+        # XXX self.doc = self.iface.__doc__
+        self.doc = self.iface.getDoc()
         self.methods = []
         self.schema = []
 
-        for name in iface:
-            defn = iface[name]
+        for name in self.iface:
+            defn = self.iface[name]
             if IMethod.isImplementedBy(defn):
                 self.methods.append(defn)
             elif IField.isImplementedBy(defn):
                 self.schema.append(defn)
 
     def getServices(self):
+        """Return an iterable of service dicts
+
+        where the service dicts contains keys "name" and "registrations."
+        registrations is a list of IRegistrations.
+        """
         sm = zapi.getServiceManager(self.context)
         for name, iface in sm.getServiceDefinitions():
             service = sm.queryService(name)
             if service is None:
                 continue
-            if queryAdapter(service, IXXX):
-                pass
-
+            registry = zapi.queryAdapter(service, IInterfaceBasedRegistry)
+            if registry is None:
+                continue
+            regs = list(registry.getRegistrationsForInterface(self.iface))
+            if regs:
+                yield {"name": name, "registrations": regs}
+            
