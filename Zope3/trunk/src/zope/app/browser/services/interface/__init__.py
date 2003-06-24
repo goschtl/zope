b9596e175584fa6263999b7a7e84a4b1e8546be8
mgedmin <mgedmin@62d5b8a3-27da-0310-9561-8e5933582275>
2003-06-24 16:42:35 +0000

    Restore Python 2.2.3 compatibility
    git-svn-id: svn://svn.zope.org/repos/main@10707 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/browser/services/interface/__init__.py
+++ Zope3/trunk/src/zope/app/browser/services/interface/__init__.py
@@ -13,6 +13,7 @@
 ##############################################################################
 """Browser view for the LocalInterfaceService."""
 
+from __future__ import generators
 from zope.interface.interfaces import IMethod
 from zope.schema.interfaces import IField
 from zope.app.interfaces.services.interface import IInterfaceBasedRegistry
