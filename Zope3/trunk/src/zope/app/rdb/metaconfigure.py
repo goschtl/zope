5e77c907b813e4385bfcd9cb0db7fa498f3dbcb9
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2003-08-03 16:07:44 +0000

    Converted 'rdb' ZCML namespace.
    git-svn-id: svn://svn.zope.org/repos/main@11164 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/rdb/metaconfigure.py
+++ Zope3/trunk/src/zope/app/rdb/metaconfigure.py
@@ -1,20 +1,30 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""'rdb' ZCML Namespace Directive Handler
+
+$Id: metaconfigure.py,v 1.2 2003/08/03 16:07:39 srichter Exp $
+"""
 from zope.component import getService
 from zope.configuration.action import Action
 from zope.app.services.servicenames import SQLDatabaseConnections
 
 def connectionhandler(_context, name, component, dsn):
-
-    component = _context.resolve(component)
-
     connection = component(dsn)
-
-    return [
-        Action(
+    _context.action(
             discriminator = ('provideConnection', name),
             callable = provideConnection,
-            args = (name, connection),
-            )
-        ]
+            args = (name, connection) )
 
 def provideConnection(name, connection):
     getService(None, SQLDatabaseConnections).provideConnection(name, connection)
---
+++ Zope3/trunk/src/zope/app/rdb/tests/test_directives.py
@@ -0,0 +1,43 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Test 'rdb' ZCML Namespace Directives
+
+$Id: test_directives.py,v 1.1 2003/08/03 16:07:44 srichter Exp $
+"""
+import unittest
+
+from zope.configuration import xmlconfig
+from zope.security.management import newSecurityManager, system_user
+from zope.app.rdb import queryConnection, getAvailableConnections
+from zope.app.rdb import ZopeConnection
+import zope.app.rdb.tests
+
+class DirectivesTest(unittest.TestCase):
+
+    def test_provideConnection(self):
+        self.assertEqual(getAvailableConnections(), [])
+        self.assertEqual(queryConnection('stub', None), None)
+        self.context = xmlconfig.file("rdb.zcml", zope.app.rdb.tests)
+        self.assertEqual(getAvailableConnections(), ["stub"])
+        self.assertEqual(queryConnection('stub').__class__, ZopeConnection)
+        self.assertEqual(queryConnection('stubbie', None), None)
+
+
+def test_suite():
+    return unittest.TestSuite((
+        unittest.makeSuite(DirectivesTest),
+        ))
+
+if __name__ == '__main__':
+    unittest.main()
