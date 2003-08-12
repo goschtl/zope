7ad2082c4ce0f1b149678d11e1911ee7c811783c
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2003-08-12 19:39:24 +0000

    - extract.py is now product aware, i.e. you can use it for your Zope   Products as well. This can be controlled via command line options.
    - extract.py makes use new talgettext domain selection.
    
    - Updated German translation based on the latest msgids.
    
    
    git-svn-id: svn://svn.zope.org/repos/main@11343 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/translation_files/extract.py
+++ Zope3/trunk/src/zope/app/translation_files/extract.py
@@ -11,20 +11,38 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""Message id extraction script
+"""Program to extract internationalization markup from Python Code,
+Page Templates and ZCML.
 
-This script extracts translatable strings and creates a single zope.pot file.
+This tool will extract all findable message strings from all
+internationalizable files in your Zope 3 product. It only extracts message ids
+of the specified domain. It defaults to the 'zope' domain and the zope.app
+package.
 
-$Id: extract.py,v 1.5 2003/08/09 11:47:14 srichter Exp $
+Note: The Python Code extraction tool does not support domain registration, so
+      that all message strings are returned for Python code.
+
+Usage: extract.py [options]
+Options:
+    -h / --help
+        Print this message and exit.
+    -d / --domain <domain>
+        Specifies the domain that is supposed to be extracted (i.e. 'zope')
+    -p / --path <path>
+        Specifies the package that is supposed to be searched
+        (i.e. 'zope/app')
 """
+__id__ = "$Id: extract.py,v 1.6 2003/08/12 19:39:21 srichter Exp $"
+
 import os, sys, fnmatch
+import getopt
 import time
 import tokenize
 import traceback
 from pygettext import safe_eval, normalize, make_escapes
 
-
 __meta_class__ = type
+
 
 pot_header = '''\
 ##############################################################################
@@ -54,6 +72,13 @@ msgstr ""
 
 '''
 
+def usage(code, msg=''):
+    # Python 2.1 required
+    print >> sys.stderr, __doc__
+    if msg:
+        print >> sys.stderr, msg
+    sys.exit(code)
+
 class POTEntry:
     """This class represents a single message entry in the POT file."""
 
@@ -80,8 +105,9 @@ class POTEntry:
 class POTMaker:
     """This class inserts sets of strings into a POT file."""
     
-    def __init__ (self, output_fn):
+    def __init__ (self, output_fn, path):
         self._output_filename = output_fn
+        self.path = path
         self.catalog = {}
 
 
@@ -98,19 +124,25 @@ class POTMaker:
                 self.catalog[msgid].addLocationComment(filename, lineno)
                 
 
-    def _getZopeVersion(self):
+    def _getProductVersion(self):
+        # First, try to get the product version
+        fn = os.path.join(self.path, 'version.txt')
+        if os.path.exists(fn):
+            return open(fn, 'r').read().strip()
+        # Second, try to find a Zope version
         import zope
         fn = os.path.join(os.path.dirname(zope.__file__), 'version.txt')
         if os.path.exists(fn):
-            return open(fn, 'r').read()
+            return open(fn, 'r').read().strip()
         else:
             return 'Zope 3 (unknown version)'
+
 
     def write(self):
         
         file = open(self._output_filename, 'w')
         file.write(pot_header % {'time': time.ctime(),
-                                 'version': self._getZopeVersion()})
+                                 'version': self._getProductVersion()})
         
         # Sort the catalog entries by filename
         catalog = self.catalog.values()
@@ -223,11 +255,10 @@ def app_dir():
         # path
 
         # Get the path of the src
-        translation_files = os.path.abspath(os.path.dirname(sys.argv[0]))
-        app = os.path.dirname(translation_files)
-        zope = os.path.dirname(app)
-        src = os.path.dirname(zope)
-        sys.path.insert(0, src)
+        path = os.path.abspath(os.path.dirname(sys.argv[0]))
+        while not path.endswith('src'):
+            path = os.path.dirname(path)
+        sys.path.insert(0, path)
 
         import zope.app
 
@@ -272,13 +303,15 @@ def py_strings(dir, domain="zope"):
 def zcml_strings(dir, domain="zope"):
     """Retrieve all ZCML messages from dir that are in the domain."""
     from zope.app._app import config
+    import zope
     dirname = os.path.dirname
-    site_zcml = os.path.join(dirname(dirname(dirname(dir))), "site.zcml")
+    site_zcml = os.path.join(dirname(dirname(dirname(zope.__file__))),
+                             "site.zcml")
     context = config(site_zcml, execute=False)
     return context.i18n_strings.get(domain, {})
 
 
-def tal_strings(dir, domain="zope"):
+def tal_strings(dir, domain="zope", include_default_domain=False):
     """Retrieve all TAL messages from dir that are in the domain."""
     # We import zope.tal.talgettext here because we can't rely on the
     # right sys path until app_dir has run
@@ -302,24 +335,59 @@ def tal_strings(dir, domain="zope"):
             print 'There was an error processing', filename
             traceback.print_exc()
 
+    # See whether anything in the domain was found
+    if not engine.catalog.has_key(domain):
+        return {}
     # We do not want column numbers.
-    catalog = engine.catalog
+    catalog = engine.catalog[domain].copy()
+    # When the Domain is 'default', then this means that none was found;
+    # Include these strings; yes or no?
+    if include_default_domain:
+        catalog.update(engine.catalog['default'])
     for msgid, locations in catalog.items():
         catalog[msgid] = map(lambda l: (l[0], l[1][0]), locations)
-    # XXX: No support for domains yet :(
     return catalog
 
 
 def main(argv=sys.argv):
-    dir = app_dir()
-    # Wehn generating the comments, we will not need the base directory info,
-    # since it is specific to everyone's installation
-    base_dir = dir.replace('src/zope/app', '')
+    try:
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            'hd:p:',
+            ['help', 'domain=', 'path='])
+    except getopt.error, msg:
+        usage(1, msg)
 
-    maker = POTMaker('zope.pot')
-    maker.add(py_strings(dir), base_dir)
-    maker.add(zcml_strings(dir), base_dir)
-    maker.add(tal_strings(dir), base_dir)
+    domain = 'zope'
+    path = app_dir()
+    include_default_domain = True
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            usage(0)
+        elif opt in ('-d', '--domain'):
+            domain = arg
+            include_default_domain = False
+        elif opt in ('-p', '--path'):
+            if not os.path.exists(arg):
+                usage(1, 'The specified path does not exist.')
+            path = arg
+            # We might not have an absolute path passed in.
+            if not path == os.path.abspath(path):
+                cwd = os.getcwd()
+                # This is for symlinks. Thanks to Fred for this trick.
+                if os.environ.has_key('PWD'):
+                    cwd = os.environ['PWD']
+                path = os.path.normpath(os.path.join(cwd, arg))
+
+    # When generating the comments, we will not need the base directory info,
+    # since it is specific to everyone's installation
+    src_start = path.find('src')
+    base_dir = path[:src_start]
+
+    maker = POTMaker(domain+'.pot', path)
+    maker.add(py_strings(path, domain), base_dir)
+    maker.add(zcml_strings(path, domain), base_dir)
+    maker.add(tal_strings(path, domain, include_default_domain), base_dir)
     maker.write()
 
 
