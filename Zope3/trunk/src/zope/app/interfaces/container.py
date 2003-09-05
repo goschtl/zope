6a79755a613fd776385eb96cfef03ea83825a62b
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2003-09-05 18:52:40 +0000

    Added zpl.
    git-svn-id: svn://svn.zope.org/repos/main@11567 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/interfaces/container.py
+++ Zope3/trunk/src/zope/app/interfaces/container.py
@@ -1,6 +1,20 @@
+##############################################################################
 #
-# This file is necessary to make this directory a package.
+# Copyright (c) 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Container-related interfaces
 
+$Id: container.py,v 1.3 2003/09/05 18:52:40 jim Exp $
+"""
 
 from zope.interface import Interface, Attribute
 from zope.component.interfaces import IView
