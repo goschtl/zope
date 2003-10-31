934d23b801d7d90369914c50f568a843c8bc6886
garrett <garrett@62d5b8a3-27da-0310-9561-8e5933582275>
2003-10-31 23:11:14 +0000

    Added the ability to view registered object (paths, hub ids, and status). Missing objects (i.e. objects that cannot be traversed to via the registered path) are highlighted in red.
    Missing objects can still be removed via a 'Remove Missing Objects'
    button.
    
    
    git-svn-id: svn://svn.zope.org/repos/main@11846 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/browser/services/hub.py
+++ Zope3/trunk/src/zope/app/browser/services/hub.py
@@ -13,12 +13,45 @@
 ##############################################################################
 """Define view component for object hub.
 
-$Id: hub.py,v 1.4 2003/08/07 17:41:03 srichter Exp $
+$Id: hub.py,v 1.5 2003/10/31 23:11:14 garrett Exp $
 """
+from zope.exceptions import NotFoundError
+
 from zope.app.interfaces.services.hub import IObjectHub
+from zope.app.i18n import ZopeMessageIDFactory as _
+
 
 class Control:
     __used_for__ = IObjectHub
 
     # XXX: Another dead chicken. Guys, this view could do soo much, like aehm,
     # display the cataloged objects with a nice filter function?
+
+
+    def objects(self):
+        """Returns a sequence of objects registered with the hub.
+
+        Each item in the sequence is a map:
+
+            path    - path to the object as stored in the hub
+            id      - object id as stored in the hub
+            status  - either 'Missing' or 'OK'
+            ok      - True if statis 'OK', False otherwise
+
+        Missing objects are those that cannot be accessed via the hub
+        with a call to getObject (NotFoundError raised).
+        """
+        result = []
+        hub = self.context
+        for path, id in hub.iterRegistrations():
+            try:
+                hub.getObject(id)
+                status = _("OK")
+                ok = True
+            except NotFoundError:
+                status = _("Missing")
+                ok = False
+            result.append({'path':path, 'id':id, 'status':status, 'ok':ok})
+        result.sort(lambda lhs, rhs: cmp(lhs['path'], rhs['path']))
+        return result
+
