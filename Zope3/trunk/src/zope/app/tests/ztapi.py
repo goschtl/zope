ac6c9e1769c5d2b66c8f81cc9df360c7c54876e3
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2003-11-21 17:09:37 +0000

    Began adding a new standard api for testing.
    git-svn-id: svn://svn.zope.org/repos/main@11898 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/trunk/src/zope/app/tests/ztapi.py
@@ -0,0 +1,46 @@
+##############################################################################
+#
+# Copyright (c) 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Testing helper functions
+
+$Id: ztapi.py,v 1.2 2003/11/21 17:09:37 jim Exp $
+"""
+from zope.app import zapi
+import zope.interface
+from zope.component.servicenames import Presentation, Adapters
+from zope.publisher.browser import IBrowserRequest
+
+def browserView(for_, name, factory, layer='default'):
+    """Define a global browser view
+    """
+    s = zapi.getService(None, Presentation)
+    return s.provideView(for_, name, IBrowserRequest, factory, layer)
+
+def browserResource(name, factory, layer='default'):
+    """Define a global browser view
+    """
+    s = zapi.getService(None, Presentation)
+    return s.provideResource(name, IBrowserRequest, factory, layer)
+    
+
+def setDefaultViewName(for_, name, layer='default'):
+    s = zapi.getService(None, Presentation)
+    s.setDefaultViewName(for_, IBrowserRequest, name, layer=layer)
+
+stypes = list, tuple
+def provideAdapter(required, provided, factory, name='', with=()):
+    s = zapi.getService(None, Adapters)
+    if not isinstance(factory, stypes):
+        factory = [factory]
+    s.provideAdapter(required, provided, factory, name, with)
+    
