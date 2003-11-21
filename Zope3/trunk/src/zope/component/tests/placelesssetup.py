ebac4a37c2cba72dde9068e39cef166502d2550f
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2003-11-21 17:09:32 +0000

    Changed to reflect merging view, resource and skin services into the presentation service.
    Also changed to create the global adapter and presentation services
    rather than using module globals.
    
    
    git-svn-id: svn://svn.zope.org/repos/main@11895 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/component/tests/placelesssetup.py
+++ Zope3/trunk/src/zope/component/tests/placelesssetup.py
@@ -14,15 +14,15 @@
 """
 Revision information:
 
-$Id: placelesssetup.py,v 1.6 2003/06/03 14:39:51 stevea Exp $
+$Id: placelesssetup.py,v 1.7 2003/11/21 17:09:32 jim Exp $
 """
 
 # A mix-in class inheriting from CleanUp that also connects the CA services
 
 from zope.testing.cleanup import CleanUp
 from zope.component import getServiceManager
-from zope.component.servicenames import Adapters, Skins, Utilities
-from zope.component.servicenames import Resources, Factories
+from zope.component.servicenames import Adapters, Utilities
+from zope.component.servicenames import Factories, Presentation
 
 class PlacelessSetup(CleanUp):
     def setUp(self):
@@ -30,35 +30,30 @@ class PlacelessSetup(CleanUp):
         sm = getServiceManager(None)
         defineService = sm.defineService
         provideService = sm.provideService
+
         # factory service
         from zope.component.interfaces import IFactoryService
         defineService(Factories, IFactoryService)
         from zope.component.factory import factoryService
         provideService(Factories, factoryService)
+
         # utility service
         from zope.component.interfaces import IUtilityService
         defineService(Utilities, IUtilityService)
         from zope.component.utility import utilityService
         provideService(Utilities, utilityService)
+
         # adapter service
         from zope.component.interfaces import IAdapterService
         defineService(Adapters, IAdapterService)
-        from zope.component.adapter import adapterService
-        provideService(Adapters, adapterService)
-        # resource service
-        from zope.component.interfaces import IResourceService
-        defineService(Resources, IResourceService)
-        from zope.component.resource import resourceService
-        provideService(Resources, resourceService)
-        # skin service
-        from zope.component.interfaces import ISkinService
-        defineService(Skins, ISkinService)
-        from zope.component.skin import skinService
-        provideService(Skins, skinService)
-        # view service
-        from zope.component.interfaces import IViewService
-        defineService('Views', IViewService)
-        from zope.component.view import viewService
-        provideService('Views', viewService)
+        from zope.component.adapter import GlobalAdapterService
+        provideService(Adapters, GlobalAdapterService())
+        
+        # presentation service
+        from zope.component.interfaces import IPresentationService
+        defineService(Presentation, IPresentationService)
+        from zope.component.presentation import GlobalPresentationService
+        provideService(Presentation, GlobalPresentationService())
+
     def tearDown(self):
         CleanUp.tearDown(self)
