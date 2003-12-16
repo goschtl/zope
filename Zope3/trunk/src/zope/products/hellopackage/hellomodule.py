a895471f1bb0a79670a28d70af5ac66a2c538a42
nmurthy <nmurthy@62d5b8a3-27da-0310-9561-8e5933582275>
2003-12-16 09:27:18 +0000

    Moved hellopackage product from zopeproducts to zope.products folder, fixed bugs and fixed the test cases.
    git-svn-id: svn://svn.zope.org/repos/main@12060 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/trunk/src/zope/products/hellopackage/hellomodule.py
@@ -0,0 +1,27 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+# 
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+# 
+##############################################################################
+"""
+$Id: hellomodule.py,v 1.1 2003/12/16 09:27:17 nmurthy Exp $
+"""
+from interfaces import IHello
+from persistence import Persistent
+from zope.interface import implements
+
+
+class HelloClass(Persistent):
+    implements(IHello)
+
+    
+    def getHello(self):
+        return "Hello world"
