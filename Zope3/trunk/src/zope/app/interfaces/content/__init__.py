26963355186fab87e58ebd48ebfd8fa4fd3e455f
eddala <eddala@62d5b8a3-27da-0310-9561-8e5933582275>
2003-12-18 16:34:17 +0000

    Interface type IContentType added
    git-svn-id: svn://svn.zope.org/repos/main@12125 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/interfaces/content/__init__.py
+++ Zope3/trunk/src/zope/app/interfaces/content/__init__.py
@@ -1,2 +1,24 @@
+##############################################################################
 #
-# This file is necessary to make this directory a package.
+# Copyright (c) 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id: __init__.py,v 1.3 2003/12/18 16:34:17 eddala Exp $
+"""
+
+from zope.interface.interfaces import IInterface
+
+class IContentType(IInterface):
+    """This interface represents a content type"""
+    
+
+
