7873fae19ee93128c5b299fcc3a7ebb77b995f68
fdrake <fdrake@62d5b8a3-27da-0310-9561-8e5933582275>
2004-01-12 15:42:34 +0000

    update the copyright year in this template as well
    git-svn-id: svn://svn.zope.org/repos/main@12170 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/ut.py
+++ Zope3/trunk/ut.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2003 Zope Corporation and Contributors.
+# Copyright (c) 2004 Zope Corporation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -15,7 +15,7 @@
 
 XXX longer description goes here.
 
-$Id: ut.py,v 1.8 2003/03/25 13:45:48 ctheune Exp $
+$Id: ut.py,v 1.9 2004/01/12 15:42:34 fdrake Exp $
 """
 
 import unittest
