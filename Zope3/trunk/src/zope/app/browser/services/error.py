1b65ad8ae8d58077d03a2091ca11e84d041bb9bf
anthony <anthony@62d5b8a3-27da-0310-9561-8e5933582275>
2004-02-09 02:09:01 +0000

    zmi_actions menu now has an 'Errors' entry in all places that takes you to the error log. Woo hoo!
    git-svn-id: svn://svn.zope.org/repos/main@12449 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/browser/services/error.py
+++ Zope3/trunk/src/zope/app/browser/services/error.py
@@ -13,9 +13,12 @@
 ##############################################################################
 """Define view component for event service control.
 
-$Id: error.py,v 1.7 2004/02/09 01:44:05 richard Exp $
+$Id: error.py,v 1.8 2004/02/09 02:09:01 anthony Exp $
 """
 from zope.app.services.error import ILocalErrorReportingService
+from zope.publisher.browser import BrowserView
+from zope.app import zapi
+from zope.app.services.servicenames import ErrorLogging
 
 class EditErrorLog:
     __used_for__ = ILocalErrorReportingService
@@ -27,3 +30,12 @@ class EditErrorLog:
             copy_to_zlog = 0
         errorLog.setProperties(keep_entries, copy_to_zlog, ignored_exceptions)
         return self.request.response.redirect('@@configure.html')
+
+
+class ErrorRedirect(BrowserView):
+
+    def action(self):
+        err = zapi.getService(self, ErrorLogging)
+        url = str(zapi.getView(err, 'absolute_url', self.request))
+        url = url + "/@@SelectedManagementView.html"
+        self.request.response.redirect(url)
