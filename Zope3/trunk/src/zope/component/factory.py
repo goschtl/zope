0820b56259c7acd50d3c1abbdb83864f66114d5e
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2004-03-09 12:39:54 +0000

    Added a simple factory implementation that can handle all sorts of objects. This way any callable can be rendered into a factory easily.
    git-svn-id: svn://svn.zope.org/repos/main@12974 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/component/factory.py
+++ Zope3/trunk/src/zope/component/factory.py
@@ -11,100 +11,31 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""Global Factory Service
+"""Factory object
 
-$Id: factory.py,v 1.8 2004/03/05 22:09:25 jim Exp $
+$Id: factory.py,v 1.9 2004/03/09 12:39:54 srichter Exp $
 """
-from zope.interface.verify import verifyObject
-from zope.interface import implements
+from zope.interface import implements, implementedBy
 from zope.component.interfaces import IFactory
-from zope.component.interfaces import IFactoryInfo
-from zope.component.interfaces import IFactoryService
-from zope.component.exceptions import ComponentLookupError
 
-class IGlobalFactoryService(IFactoryService):
+class Factory(object):
+    """Generic factory implementation.
 
-    def provideFactory(name, factory, info=None):
-        """Provide a factory for the given name.
+    The purpose of this implementation is to provide a quick way of creating
+    factories for classes, functions and other objects.
+    """
+    implements(IFactory)
 
-        If specified, info must implement IFactoryInfo.
-        """
-
-class FactoryInfo:
-
-    implements(IFactoryInfo)
-
-    def __init__(self, title, description):
+    def __init__(self, callable, title='', description=''):
+        self._callable = callable
         self.title = title
         self.description = description
 
-class GlobalFactoryService:
+    def __call__(self, *args, **kw):
+        return self._callable(*args, **kw)
 
-    implements(IGlobalFactoryService)
-
-    def __init__(self):
-        self.__factories = {}
-        self.__info = {}
-
-    def provideFactory(self, name, factory, info=None):
-        """See IGlobalFactoryService interface"""
-        # XXX At this point the verify object code does not support variable
-        # arguments well. For example, I was not able to register any factory
-        # that requires constructor arguments! (SR) 
-        # verifyObject(IFactory, factory)
-        assert IFactory.providedBy(factory)
-        self.__factories[name] = factory
-        if info is not None:
-            self.__info[name] = info
-
-    def createObject(self, name, *args, **kwargs):
-        """See IFactoryService interface"""
+    def getInterfaces(self):
         try:
-            return self.__factories[name](*args, **kwargs)
-        except KeyError:
-            raise ComponentLookupError(name)
-
-    def getFactory(self, name):
-        """See IFactoryService interface"""
-        try:
-            return self.__factories[name]
-        except KeyError:
-            raise ComponentLookupError(name)
-
-    def queryFactory(self, name, default=None):
-        """See IFactoryService interface"""
-        return self.__factories.get(name, default)
-
-    def getInterfaces(self, name):
-        """See IFactoryService interface"""
-        try: return self.__factories[name].getInterfaces()
-        except KeyError:
-            raise ComponentLookupError(name)
-
-    def getFactoriesFor(self, iface):
-        """See IFactoryService interface"""
-        factories = self.queryFactoriesFor(iface, None)
-        if factories is None:
-            raise ComponentLookupError(iface)
-        return factories
-
-    def queryFactoriesFor(self, iface, default=None):
-        """See IFactoryService interface"""
-        return [(n, f) for n, f in self.__factories.items() \
-                if iface in tuple(f.getInterfaces())] or default
-
-    def getFactoryInfo(self, name):
-        return self.__info.get(name)
-
-    _clear = __init__
-
-# the global factory service instance (see component.zcml )
-factoryService = GlobalFactoryService()
-provideFactory = factoryService.provideFactory
-
-_clear = factoryService._clear
-
-# Register our cleanup with Testing.CleanUp to make writing unit tests simpler.
-from zope.testing.cleanup import addCleanUp
-addCleanUp(_clear)
-del addCleanUp
+            return list(implementedBy(self._callable))
+        except AttributeError:
+            return []
