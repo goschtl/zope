34658808b109e28b0da6a09678bb859e2b80d466
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2004-03-13 21:02:53 +0000

    Moved talesapi interfaces to zope.app.pagetemplate.interfaces.
    git-svn-id: svn://svn.zope.org/repos/main@13060 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/pagetemplate/interfaces.py
+++ Zope3/trunk/src/zope/app/pagetemplate/interfaces.py
@@ -13,7 +13,7 @@
 ##############################################################################
 """Interfaces for apis to make available to TALES
 
-$Id: talesapi.py,v 1.4 2004/03/01 15:02:52 philikon Exp $
+$Id: interfaces.py,v 1.1 2004/03/13 21:02:53 srichter Exp $
 """
 from zope.app.dublincore.interfaces import IDCDescriptiveProperties
 from zope.app.dublincore.interfaces import IDCTimes
