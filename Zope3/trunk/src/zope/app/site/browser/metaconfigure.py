574f8549a4e403598c7cbe71ea4577bf4bae78b0
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2004-03-22 00:52:24 +0000

    Added servicetool directive; removed view factories in favor of type based classes.
    git-svn-id: svn://svn.zope.org/repos/main@13263 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/site/browser/metaconfigure.py
+++ Zope3/trunk/src/zope/app/site/browser/metaconfigure.py
@@ -13,26 +13,77 @@
 ##############################################################################
 """Configuration handlers for 'tools' directive.
 
-$Id: metaconfigure.py,v 1.1 2004/03/21 16:02:18 srichter Exp $
+$Id: metaconfigure.py,v 1.2 2004/03/22 00:52:24 srichter Exp $
 """
 from zope.publisher.interfaces.browser import IBrowserRequest, IBrowserPublisher
 from zope.app.component.metaconfigure import view, interface as ifaceDirective
+from zope.app.publisher.browser.viewmeta import view as complexView
 
 from zope.app.site.interfaces import ISiteManager
-from tools import UtilityToolsViewFactory, IUtilityToolsView, IToolType
+from tools import IToolView, IUtilityToolView, IToolType
+from tools import UtilityToolAdding, UtilityToolView
+from tools import ServiceToolView, ServiceToolAdding
+from zope.app.site.interfaces import ILocalService
 
 def tool(_context, interface, folder="tools", title=None, description=None):
-
-    factory = UtilityToolsViewFactory(interface, folder, title, description)
     name = "manage" + interface.getName() + "Tool.html"
+    addName = "Add" + interface.getName() + "Tool"
+    
     permission = 'zope.ManageContent'
-
 
     ifaceDirective(_context, interface, IToolType)
 
-    view(_context, [factory], IBrowserRequest, name, [ISiteManager],
+    class_ = type("UtilityToolView for %s" % interface.getName(),
+                  (UtilityToolView,),
+                  {'interface':interface,
+                   'folder':folder,
+                   'title':title,
+                   'description':description})
+    
+    view(_context, [class_], IBrowserRequest, name, [ISiteManager],
          permission=permission,
-         allowed_interface=[IUtilityToolsView, IBrowserPublisher],
+         allowed_interface=[IUtilityToolView, IBrowserPublisher],
          allowed_attributes=['__call__', '__getitem__'])
 
+    class_ = type("UtilityToolAdding for %s" % interface.getName(),
+                  (UtilityToolAdding,),
+                  {'_addFilterInterface': interface,
+                   'folder':folder,
+                   'title':'Add %s Tool' % interface.getName()} )
 
+    addView = complexView(_context, ISiteManager, permission, addName,
+                          class_=class_)
+    addView.page(_context, 'index.html', 'index')
+    addView.page(_context, 'action.html', 'action')
+
+    addView()
+
+def servicetool(_context, folder="tools", title=None, description=None):
+    name = "manageILocalServiceTool.html"
+    addName = "AddServiceTool"
+    
+    permission = 'zope.ManageContent'
+
+    ifaceDirective(_context, ILocalService, IToolType)
+
+    class_ = type("ServiceToolView",
+                  (ServiceToolView,),
+                  {'folder':folder,
+                   'title':title,
+                   'description':description})
+    
+    view(_context, [class_], IBrowserRequest, name, [ISiteManager],
+         permission=permission,
+         allowed_interface=[IToolView, IBrowserPublisher],
+         allowed_attributes=['__call__', '__getitem__'])
+
+    class_ = type("ServiceToolAdding",
+                  (ServiceToolAdding,),
+                  {'folder':folder} )
+
+    addView = complexView(_context, ISiteManager, permission, addName,
+                          class_=class_)
+    addView.page(_context, 'index.html', 'index')
+    addView.page(_context, 'action.html', 'action')
+
+    addView()
