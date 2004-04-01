88bc2d346d616214df23086521ff81ba4e38c4b2
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2004-04-01 17:09:25 +0000

    Started the package with an __init__.py; this just checks for XML imports.
    git-svn-id: svn://svn.zope.org/repos/main@13441 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/trunk/src/zope/app/advanced/__init__.py
@@ -0,0 +1,37 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Advanced package
+
+Perform important XML import checks.
+
+$Id: __init__.py,v 1.1 2004/04/01 17:09:25 philikon Exp $
+"""
+_MAXIMUM_XMLMINUS_VERSION = (0, 6, 1)
+
+try:
+    import _xmlminus
+except ImportError:
+    pass
+else:
+    try:
+        v = _xmlminus.version_info
+    except AttributeError:
+        # _xmlminue is too young; ignore it
+        pass
+    else:
+        if v >= _MAXIMUM_XMLMINUS_VERSION:
+            import sys
+            sys.modules[__name__] = _xmlminus
+        else:
+            del v
