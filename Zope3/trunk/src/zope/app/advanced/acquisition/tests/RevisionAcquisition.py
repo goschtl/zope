7c8b65b0b987e9bae12d60458d3761b91d5ba39e
sidnei <sidnei@62d5b8a3-27da-0310-9561-8e5933582275>
2004-04-01 18:29:00 +0000

    Ops, missed some files.
    git-svn-id: svn://svn.zope.org/repos/main@13458 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/trunk/src/zope/app/advanced/acquisition/tests/RevisionAcquisition.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Check loadSerial() on storages that support historical revisions."""
+
+from ZODB.tests.MinPO import MinPO
+from ZODB.tests.StorageTestBase import zodb_unpickle, zodb_pickle
+
+ZERO = '\0'*8
+
+class RevisionStorage:
+
+    def checkLoadSerial(self):
+        oid = self._storage.new_oid()
+        revid = ZERO
+        revisions = {}
+        for i in range(31, 38):
+            revid = self._dostore(oid, revid=revid, data=MinPO(i))
+            revisions[revid] = MinPO(i)
+        # Now make sure all the revisions have the correct value
+        for revid, value in revisions.items():
+            data = self._storage.loadSerial(oid, revid)
+            self.assertEqual(zodb_unpickle(data), value)
