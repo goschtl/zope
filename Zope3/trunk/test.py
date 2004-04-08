92fade389f18067972c77735f8fda8255d948d02
fdrake <fdrake@62d5b8a3-27da-0310-9561-8e5933582275>
2004-04-08 21:20:30 +0000

    logging configuration is evil; isolate it
    git-svn-id: svn://svn.zope.org/repos/main@13535 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/test.py
+++ Zope3/trunk/test.py
@@ -754,28 +754,13 @@ def main(module_filter, test_filter, libdir):
     if not KEEP_STALE_BYTECODE:
         os.path.walk(os.curdir, remove_stale_bytecode, None)
 
-    # Get the log.ini file from the current directory instead of possibly
-    # buried in the build directory.  XXX This isn't perfect because if
-    # log.ini specifies a log file, it'll be relative to the build directory.
-    # Hmm...
-    logini = os.path.abspath("log.ini")
-
     # Initialize the path and cwd
     global pathinit
     pathinit = PathInit(BUILD, BUILD_INPLACE, libdir)
 
     # Initialize the logging module.
 
-    import logging.config
-    logging.basicConfig()
-
-    level = os.getenv("LOGGING")
-    if os.path.exists(logini):
-        logging.config.fileConfig(logini)
-        if level:
-            logging.root.setLevel(int(level))
-    elif level:
-        logging.root.setLevel(int(level))
+    configure_logging()
 
 ##    try:
 ##        import zLOG
@@ -813,6 +798,25 @@ def main(module_filter, test_filter, libdir):
         runner(files, test_filter, DEBUG)
 
     os.chdir(pathinit.org_cwd)
+
+
+def configure_logging():
+    import logging.config
+
+    # Get the log.ini file from the current directory instead of possibly
+    # buried in the build directory.  XXX This isn't perfect because if
+    # log.ini specifies a log file, it'll be relative to the build directory.
+    # Hmm...
+    logini = os.path.abspath("log.ini")
+
+    if os.path.exists(logini):
+        logging.config.fileConfig(logini)
+    else:
+        logging.basicConfig()
+
+    if os.environ.has_key("LOGGING"):
+        level = int(os.environ["LOGGING"])
+        logging.getLogger().setLevel(level)
 
 
 def process_args(argv=None):
