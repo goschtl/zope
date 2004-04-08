8319cd22d97f9691f3c23cf45d3e488f8c474076
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2004-04-08 21:02:44 +0000

    Change to make registrations implement IPhysicallyLocatable and to use real stacks.
    git-svn-id: svn://svn.zope.org/repos/main@13533 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/registration/tests/registrationstack.py
+++ Zope3/trunk/src/zope/app/registration/tests/registrationstack.py
@@ -13,104 +13,44 @@
 ##############################################################################
 """Registration Stack
 
-$Id: registrationstack.py,v 1.1 2004/03/13 18:01:18 srichter Exp $
+$Id: registrationstack.py,v 1.2 2004/04/08 21:02:44 jim Exp $
 """
 from zope.app.registration.registration import RegistrationStatusProperty
 from zope.app.container.contained import Contained
+from zope.app.traversing.interfaces import IPhysicallyLocatable
+import zope.interface
 
 __metaclass__ = type
 
 class TestingRegistration(Contained):
+    zope.interface.implements(IPhysicallyLocatable)
+
     status = RegistrationStatusProperty()
 
     def __init__(self, id):
-        self.id = id
+        self.id = str(id)
 
     def __eq__(self, other):
         return self.id == getattr(other, 'id', 0)
 
-class TestingRegistrationStack:
+    def getPath(self):
+        return self.id
 
-    class_ = TestingRegistration
+    def __repr__(self):
+        return self.id
 
-    def __init__(self, *args):
-        self._data = args
+    def activated(self):
+        pass
 
-    def register(self, registration):
-        cid = registration.id
+    deactivated = activated
+    
+from zope.app.registration.registration import RegistrationStack
 
-        if self._data:
-            if cid in self._data:
-                return # already registered
-        else:
-            # Nothing registered. Need to stick None in front so that nothing
-            # is active.
-            self._data = (None, )
-
-        self._data += (cid, )
-
-    def unregister(self, registration):
-        cid = registration.id
-
-        data = self._data
-        if data:
-            if data[0] == cid:
-                # It's active, we need to switch in None
-                self._data = (None, ) + data[1:]
-            else:
-                self._data = tuple([item for item in data if item != cid])
-
-    def registered(self, registration):
-        cid = registration.id
-        return cid in self._data
-
-    def activate(self, registration):
-        if registration is None:
-            self._data = (None,) + filter(None, self._data)
-            if self._data[-1] is None:
-                self._data = self._data[:-1]
-            return
-        cid = registration.id
-        if self._data[0] == cid:
-            return # already active
-
-        if self._data[0] is None:
-            # Remove leading None marker
-            self._data = self._data[1:]
-
-        self._data = (cid, ) + tuple(
-            [item for item in self._data if item != cid]
-            )
-
-    def deactivate(self, registration):
-        cid = registration.id
-        if self._data[0] != cid:
-            return # already inactive
-
-        # Just stick None on the front
-        self._data = (None, ) + self._data
-
-    def active(self):
-        if self._data:
-            return self.class_(self._data[0])
-
-        return None
-
-    def __nonzero__(self):
-        return bool(self._data)
-
-    def info(self):
-        result = [{'id': path,
-                   'active': False,
-                   'registration': self.class_(path),
-                   }
-                  for path in self._data
-                  ]
-
-        if result:
-            if result[0]['registration'] is None:
-                del result[0]
-            else:
-                result[0]['active'] = True
-
-        return result
+def TestingRegistrationStack(*regs):
+    regs = list(regs)
+    regs.reverse()
+    stack = RegistrationStack(None)
+    for reg in regs:
+        stack.register(reg)
+        stack.activate(reg)
+    return stack
