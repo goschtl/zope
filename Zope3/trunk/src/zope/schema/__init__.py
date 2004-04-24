be0db005eb877565d27cf84198a82e673e5f42b4
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2004-04-24 23:20:37 +0000

    Remove outdated Enumerated fields and add Choice
    git-svn-id: svn://svn.zope.org/repos/main@13741 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/schema/__init__.py
+++ Zope3/trunk/src/zope/schema/__init__.py
@@ -13,18 +13,16 @@
 ##############################################################################
 """Schema package constructor
 
-$Id: __init__.py,v 1.17 2004/01/16 13:38:20 philikon Exp $
+$Id: __init__.py,v 1.18 2004/04/24 23:20:37 srichter Exp $
 """
-
 from zope.schema._field import Field, Container, Iterable, Orderable
-from zope.schema._field import MinMaxLen, Enumerated, Sequence
+from zope.schema._field import MinMaxLen, Sequence, Choice
 from zope.schema._field import Bytes, ASCII, BytesLine
-from zope.schema._field import Text, TextLine, Bool, Int, Float, Tuple, List
+from zope.schema._field import Text, TextLine, Bool, Int, Float
+from zope.schema._field import Tuple, List, Set
 from zope.schema._field import Password, Dict, Datetime, Date, SourceText
 from zope.schema._field import Object, URI, Id, DottedName
-from zope.schema._field import EnumeratedTextLine, EnumeratedInt
-from zope.schema._field import EnumeratedDatetime, EnumeratedFloat
-from zope.schema._field import EnumeratedDate, InterfaceField
+from zope.schema._field import InterfaceField
 from zope.schema._schema import getFields, getFieldsInOrder
 from zope.schema._schema import getFieldNames, getFieldNamesInOrder
 from zope.schema.accessors import accessors
