1c5f593197a7505f3628d89783491eea8028e802
alga <alga@62d5b8a3-27da-0310-9561-8e5933582275>
2004-05-22 01:57:18 +0000

    Removed trailing whitespace.
    git-svn-id: svn://svn.zope.org/repos/main@24873 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/workflow/stateful/browser/ftests/test_processdefinition.py
+++ Zope3/trunk/src/zope/app/workflow/stateful/browser/ftests/test_processdefinition.py
@@ -30,13 +30,13 @@ class Test(BrowserTestCase):
         response = self.publish(
             self.basepath + '/contents.html',
             basic='mgr:mgrpw')
-        
+
         self.assertEqual(response.getStatus(), 200)
-        
+
         expr = 'zope.app.browser.add.StatefulProcessDefinition.f([0-9]*)'
         m = re.search(expr, response.getBody())
         type_name = m.group(0)
-        
+
         response = self.publish(
             self.basepath + '/contents.html',
             basic='mgr:mgrpw',
@@ -47,15 +47,15 @@ class Test(BrowserTestCase):
         response = self.publish(
             self.basepath + '/pd/index.html',
             basic='mgr:mgrpw')
-        
+
         self.assertEqual(response.getStatus(), 200)
         body = response.getBody()
-        self.assert_(body.find("I'm a stateful ProcessInstance") >= 0)        
+        self.assert_(body.find("I'm a stateful ProcessInstance") >= 0)
 
         response = self.publish(
             self.basepath + '/pd/edit.html',
             basic='mgr:mgrpw')
-        
+
         self.assertEqual(response.getStatus(), 200)
         body = ' '.join(response.getBody().split())
         self.assert_(body.find('Set Workflow-Relevant Data Schema') >= 0)
@@ -63,7 +63,7 @@ class Test(BrowserTestCase):
         response = self.publish(
             self.basepath + '/pd/registration.html',
             basic='mgr:mgrpw')
-        
+
         self.assertEqual(response.getStatus(), 200)
         body = ' '.join(response.getBody().split())
         self.assert_(body.find('This object is not currently active.') >=0)
@@ -71,27 +71,27 @@ class Test(BrowserTestCase):
         response = self.publish(
             self.basepath + '/pd/importexport.html',
             basic='mgr:mgrpw')
-        
+
         self.assertEqual(response.getStatus(), 200)
         body = ' '.join(response.getBody().split())
-        self.assert_(body.find('Import / Export Process Definitions:') >= 0)       
+        self.assert_(body.find('Import / Export Process Definitions:') >= 0)
         self.assert_(body.find(
             '<a href="states/contents.html">Manage States</a>') >=0 )
         self.assert_(body.find(
-            '<a href="transitions/contents.html">Manage Transitions</a>') >=0)  
+            '<a href="transitions/contents.html">Manage Transitions</a>') >=0)
 
     def test_transitions(self):
         response = self.publish(
             self.basepath + '/pd/transitions/contents.html',
             basic='mgr:mgrpw')
-        
+
         self.assertEqual(response.getStatus(), 200)
 
     def test_states(self):
         response = self.publish(
             self.basepath + '/pd/states/contents.html',
             basic='mgr:mgrpw')
-        
+
         self.assertEqual(response.getStatus(), 200)
         body = ' '.join(response.getBody().split())
         self.assert_(body.find('INITIAL') >= 0)
