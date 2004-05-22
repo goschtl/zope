9e6fabab085c33c757028478a4f38dc31d864b5b
gintautasm <gintautasm@62d5b8a3-27da-0310-9561-8e5933582275>
2004-05-22 10:35:24 +0000

    updated a deprecated type of call to getUtility().
    git-svn-id: svn://svn.zope.org/repos/main@24880 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/z3checkins/message.py
+++ Zope3/trunk/src/z3checkins/message.py
@@ -338,7 +338,7 @@ class MessageUpload:
     def createAndAdd(self, data):
         if data.has_key('data'): # XXX should we bark if no data is given?
             msg_raw = data['data']
-            parser = getUtility(self.context, IMessageParser)
+            parser = getUtility(IMessageParser, context=self.context)
             if msg_raw.startswith("From "):
                 # detected an mbox file
                 mbox = StringIO(msg_raw)
