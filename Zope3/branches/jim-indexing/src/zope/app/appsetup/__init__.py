1443536de2357a10b00479870f21c76bd8eba9fc
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2004-05-26 10:31:47 +0000

    Abandoning this branch.  Trying to disentagle indecing from the hub is too hard and not worthwhile. It will be better to just remove the indexing and hub code until we have time to redo the hub ad an index-id utility.
    git-svn-id: svn://svn.zope.org/repos/main@24987 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/branches/jim-indexing/src/zope/app/appsetup/__init__.py
+++
@@ -1,3 +0,0 @@
-from zope.app.appsetup.appsetup import IDatabaseOpenedEvent, DatabaseOpened
-from zope.app.appsetup.appsetup import IProcessStartingEvent, ProcessStarting
-from zope.app.appsetup.appsetup import config, database
