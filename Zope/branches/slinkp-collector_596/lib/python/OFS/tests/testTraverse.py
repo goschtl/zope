c3d83f67a2e069eac758bf5064d88159ad287f3f
slinkp <slinkp@62d5b8a3-27da-0310-9561-8e5933582275>
2004-05-28 19:08:49 +0000

    Making slinkp-collector_596 branch
    git-svn-id: svn://svn.zope.org/repos/main@25096 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope/branches/slinkp-collector_596/lib/python/OFS/tests/testTraverse.py
@@ -0,0 +1,204 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import os, sys, unittest
+import string, cStringIO, re
+
+import ZODB, Acquisition
+from Acquisition import aq_base
+from OFS.Application import Application
+from OFS.Folder import manage_addFolder
+from OFS.Image import manage_addFile
+from OFS.SimpleItem import SimpleItem
+from Testing.makerequest import makerequest
+from AccessControl import SecurityManager, Unauthorized
+from AccessControl.SecurityManagement import newSecurityManager
+from AccessControl.SecurityManagement import noSecurityManager
+
+from mimetools import Message
+from multifile import MultiFile
+
+
+class UnitTestSecurityPolicy:
+    """
+        Stub out the existing security policy for unit testing purposes.
+    """
+    #
+    #   Standard SecurityPolicy interface
+    #
+    def validate( self
+                , accessed=None
+                , container=None
+                , name=None
+                , value=None
+                , context=None
+                , roles=None
+                , *args
+                , **kw):
+        return 1
+
+    def checkPermission( self, permission, object, context) :
+        return 1
+
+
+class CruelSecurityPolicy:
+    """Denies everything
+    """
+    #
+    #   Standard SecurityPolicy interface
+    #
+    def validate(self, accessed, container, name, value, *args):
+        raise Unauthorized, name
+
+    def checkPermission( self, permission, object, context) :
+        return 0
+
+
+class UnitTestUser( Acquisition.Implicit ):
+    """
+        Stubbed out manager for unit testing purposes.
+    """
+    def getId( self ):
+        return 'unit_tester'
+
+    getUserName = getId
+
+    def allowed( self, object, object_roles=None ):
+        return 1
+
+
+class BoboTraversable(SimpleItem):
+    __allow_access_to_unprotected_subobjects__ = 1
+
+    def __bobo_traverse__(self, request, name):
+        if name == 'bb_subitem':
+            return BoboTraversable().__of__(self)
+        else:
+            raise KeyError
+
+
+def makeConnection():
+    import ZODB
+    from ZODB.DemoStorage import DemoStorage
+
+    s = DemoStorage(quota=(1<<20))
+    return ZODB.DB( s ).open()
+
+
+class TestTraverse( unittest.TestCase ):
+
+    def setUp( self ):
+
+        self.connection = makeConnection()
+        try:
+            r = self.connection.root()
+            a = Application()
+            r['Application'] = a
+            self.root = a
+            responseOut = self.responseOut = cStringIO.StringIO()
+            self.app = makerequest( self.root, stdout=responseOut )
+            manage_addFolder( self.app, 'folder1' )
+            folder1 = getattr( self.app, 'folder1' )
+
+            folder1.all_meta_types = \
+                                    ( { 'name'        : 'File'
+                                      , 'action'      : 'manage_addFile'
+                                      , 'permission'  : 'Add images and files'
+                                      }
+                                    ,
+                                    )
+
+            manage_addFile( folder1, 'file'
+                          , file='', content_type='text/plain')
+
+            # Hack, we need a _p_mtime for the file, so we make sure that it
+            # has one. We use a subtransaction, which means we can rollback
+            # later and pretend we didn't touch the ZODB.
+            get_transaction().commit()
+        except:
+            self.connection.close()
+            raise
+        get_transaction().begin()
+        self.folder1 = getattr( self.app, 'folder1' )
+
+        self.policy = UnitTestSecurityPolicy()
+        self.oldPolicy = SecurityManager.setSecurityPolicy( self.policy )
+        newSecurityManager( None, UnitTestUser().__of__( self.root ) )
+
+    def tearDown( self ):
+        noSecurityManager()
+        SecurityManager.setSecurityPolicy( self.oldPolicy )
+        del self.oldPolicy
+        del self.policy
+        del self.folder1
+        get_transaction().abort()
+        self.app._p_jar.sync()
+        self.connection.close()
+        del self.app
+        del self.responseOut
+        del self.root
+        del self.connection
+
+    def testTraversePath( self ):
+        self.failUnless( 'file' in self.folder1.objectIds() )
+        self.failUnless( self.folder1.unrestrictedTraverse( ('', 'folder1', 'file' ) ))
+        self.failUnless( self.folder1.unrestrictedTraverse( ('', 'folder1' ) ))
+
+    def testTraverseURLNoSlash( self ):
+        self.failUnless( 'file' in self.folder1.objectIds() )
+        self.failUnless( self.folder1.unrestrictedTraverse( '/folder1/file' ))
+        self.failUnless( self.folder1.unrestrictedTraverse( '/folder1' ))
+
+    def testTraverseURLSlash( self ):
+        self.failUnless( 'file' in self.folder1.objectIds() )
+        self.failUnless( self.folder1.unrestrictedTraverse( '/folder1/file/' ))
+        self.failUnless( self.folder1.unrestrictedTraverse( '/folder1/' ))
+
+    def testTraverseToNone( self ):
+        self.failUnlessRaises( KeyError, self.folder1.unrestrictedTraverse, ('', 'folder1', 'file2' ) )
+        self.failUnlessRaises( KeyError, self.folder1.unrestrictedTraverse,  '/folder1/file2' )
+        self.failUnlessRaises( KeyError, self.folder1.unrestrictedTraverse,  '/folder1/file2/' )
+
+    def testTraverseThroughBoboTraverse(self):
+        # Verify it's possible to use __bobo_traverse__ with the
+        # Zope security policy.
+        noSecurityManager()
+        SecurityManager.setSecurityPolicy( self.oldPolicy )
+        bb = BoboTraversable()
+        self.failUnlessRaises(KeyError, bb.restrictedTraverse, 'notfound')
+        bb.restrictedTraverse('bb_subitem')
+
+    def testAcquiredAttributeDenial(self):
+        # Verify that restrictedTraverse raises the right kind of exception
+        # on denial of access to an acquired attribute.  If it raises
+        # AttributeError instead of Unauthorized, the user may never
+        # be prompted for HTTP credentials.
+        noSecurityManager()
+        SecurityManager.setSecurityPolicy(CruelSecurityPolicy())
+        newSecurityManager( None, UnitTestUser().__of__( self.root ) )
+        self.root.stuff = 'stuff here'
+        self.failUnlessRaises(Unauthorized,
+                              self.root.folder1.restrictedTraverse, 'stuff')
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest( unittest.makeSuite( TestTraverse ) )
+    return suite
+
+def main():
+    unittest.main(defaultTest='test_suite')
+
+if __name__ == '__main__':
+    main()
---
+++ Zope/branches/slinkp-collector_596/lib/python/Products/ZODBMountPoint/Mount.py
@@ -0,0 +1,208 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+"""ZODB Mounted database support, simplified for DBTab.
+
+$Id$"""
+
+import sys
+from logging import getLogger
+
+try:
+    from cStringIO import StringIO
+except:
+    from StringIO import StringIO
+import traceback
+
+import Persistence, Acquisition
+from Acquisition import aq_base
+from ZODB.POSException import MountedStorageError
+
+from ZODB.DB import DB
+from ZODB.Connection import Connection
+
+LOG = getLogger('Zope.ZODBMountPoint')
+
+class MountPoint(Persistence.Persistent, Acquisition.Implicit):
+    """An object that accesses a different database when traversed.
+
+    This class is intended to be used as a base class.
+    """
+
+    # Default values for non-persistent variables.
+    _v_data = None   # An object in an open connection
+    _v_connect_error = None
+
+    def __init__(self, id):
+        self.id = id
+
+    def _getDB(self):
+        """Hook for getting the DB object for this mount point."""
+        raise NotImplementedError
+
+    def _getDBName(self):
+        """Hook for getting the name of the database for this mount point."""
+        raise NotImplementedError
+
+    def _getRootDBName(self):
+        """Hook for getting the name of the root database."""
+        raise NotImplementedError
+
+    def _traverseToMountedRoot(self, root, mount_parent):
+        """Hook for getting the object to be mounted."""
+        raise NotImplementedError
+
+    def __repr__(self):
+        return "%s(id=%s)" % (self.__class__.__name__, repr(self.id))
+
+
+    def _getMountedConnection(self, anyjar):
+        db_name = self._getDBName()
+        conn = anyjar._getMountedConnection(db_name)
+        if conn is None:
+            root_conn = anyjar._getRootConnection()
+            if db_name == self._getRootDBName():
+                conn = root_conn
+            else:
+                conn = self._getDB().open(version=root_conn.getVersion())
+                root_conn._addMountedConnection(db_name, conn)
+        return conn
+
+
+    def _getOrOpenObject(self, parent):
+        t = self._v_data
+        if t is not None:
+            data = t[0]
+        else:
+            self._v_connect_error = None
+            conn = None
+            try:
+                anyjar = self._p_jar
+                if anyjar is None:
+                    anyjar = parent._p_jar
+                conn = self._getMountedConnection(anyjar)
+                root = conn.root()
+                obj = self._traverseToMountedRoot(root, parent)
+                data = aq_base(obj)
+                # Store the data object in a tuple to hide from acquisition.
+                self._v_data = (data,)
+            except:
+                # Possibly broken database.
+                self._logConnectException()
+                raise
+
+            try:
+                # XXX This method of finding the mount point is deprecated.
+                # Do not use the _v_mount_point_ attribute.
+                data._v_mount_point_ = (aq_base(self),)
+            except:
+                # Might be a read-only object.
+                pass
+
+        return data.__of__(parent)
+
+
+    def __of__(self, parent):
+        # Accesses the database, returning an acquisition
+        # wrapper around the connected object rather than around self.
+        try:
+            return self._getOrOpenObject(parent)
+        except:
+            return Acquisition.ImplicitAcquisitionWrapper(self, parent)
+
+
+    def _test(self, parent):
+        """Tests the database connection."""
+        self._getOrOpenObject(parent)
+        return 1
+
+
+    def _logConnectException(self):
+        """Records info about the exception that just occurred."""
+        exc = sys.exc_info()
+        LOG.error('Failed to mount database. %s (%s)' % exc[:2], exc_info=exc)
+        f=StringIO()
+        traceback.print_tb(exc[2], 100, f)
+        self._v_connect_error = (exc[0], exc[1], f.getvalue())
+        exc = None
+
+class MountConnection(Connection):
+    """Subclass of Connection that supports mounts."""
+
+    # XXX perhaps the code in this subclass should be folded into
+    # ZODB proper.
+
+    def __init__(self, **kwargs):
+        Connection.__init__(self, **kwargs)
+        # The _root_connection of the actual root points to self.  All
+        # other connections will have _root_connection over-ridden in
+        # _addMountedConnection().
+        self._root_connection = self
+        self._mounted_connections = {}
+        
+    def _getRootConnection(self):
+        return self._root_connection
+
+    def _getMountedConnection(self, name):
+        return self._root_connection._mounted_connections.get(name)
+
+    def _addMountedConnection(self, name, conn):
+        if conn._root_connection is not conn:
+            raise ValueError("Connection %s is already mounted" % repr(conn))
+        conns = self._root_connection._mounted_connections
+        if name in conns:
+            raise KeyError("A connection named %s already exists" % repr(name))
+        conn._root_connection = self._root_connection
+        conns[name] = conn
+
+    def _setDB(self, odb, **kwargs):
+        Connection._setDB(self, odb, **kwargs)
+        for conn in self._mounted_connections.values():
+            conn._setDB(odb, **kwargs)
+
+    def close(self):
+        if self._root_connection is not self:
+            raise RuntimeError("Should not close mounted connections directly")
+
+        # The code here duplicates much of the code in
+        # DB._closeConnection and Connection.close.  A mounted
+        # connection can't operate independently, so we don't call
+        # DB._closeConnection(), which would return it to the
+        # connection pool; only the root connection should be
+        # returned.
+        
+        for conn in self._mounted_connections.values():
+            # DB._closeConnection calls the activity monitor
+            am = conn._db.getActivityMonitor()
+            am.closedConnection(conn)
+            # Connection.close does GC
+            conn._cache.incrgc()
+            conn._storage = conn._tmp = conn.new_oid = conn._opened = None
+            conn._debug_info = ()
+            # The mounted connection keeps a reference to
+            # its database, but nothing else.
+            
+        # Close this connection only after the mounted connections
+        # have been closed.  Otherwise, this connection gets returned
+        # to the pool too early and another thread might use this
+        # connection before the mounted connections have all been
+        # closed.
+        Connection.close(self)
+
+# Replace the default database Connection
+def install():
+    DB.klass = MountConnection
+    
+# XXX This shouldn't be done as a side-effect of import, but it's not
+# clear where in the Zope initialization code it should be done.
+install()
