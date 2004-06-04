b258c982f6fca8c1ac983ec296912bab990ca610
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2004-06-04 21:25:45 +0000

    Removed weakref Python monkey patch
    So Python 2.3.4 is now required.
    
    
    git-svn-id: svn://svn.zope.org/repos/main@25266 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/__init__.py
+++ Zope3/trunk/src/zope/__init__.py
@@ -13,23 +13,3 @@
 ##############################################################################
 #
 # This file is necessary to make this directory a package.
-
-# XXX Evil monkey patch of weakref to avoid a Python 2.3.3 weakref bug that
-# causes sporadic segfaults
-
-def monkey_patch():
-    import weakref
-    import gc
-    ref = weakref.ref
-    disable = gc.disable
-    enable = gc.enable
-    def monkey_ref(*args, **kw):
-        disable()
-        r = ref(*args, **kw)
-        enable()
-        return r
-
-    weakref.ref = monkey_ref
-
-monkey_patch()
-del monkey_patch
