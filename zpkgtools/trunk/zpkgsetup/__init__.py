b9207f83b8373b7e5d52b3496832eb9d36ff3f41
fdrake <fdrake@62d5b8a3-27da-0310-9561-8e5933582275>
2004-06-21 15:03:37 +0000

    Switch zpkgtools to the ZPL 2.1 license.
    git-svn-id: svn://svn.zope.org/repos/main@25925 62d5b8a3-27da-0310-9561-8e5933582275

--- zpkgtools/trunk/zpkgsetup/__init__.py
+++ zpkgtools/trunk/zpkgsetup/__init__.py
@@ -4,7 +4,7 @@
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
-# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
@@ -14,13 +14,13 @@
 """Zope packaging utilities runtime support.
 
 :organization: Zope Corporation
-:license: `Zope Public License, Version 2.0 (ZPL)`__
+:license: `Zope Public License, Version 2.1 (ZPL)`__
 :status: Prototype implementation
 
 This package contains the build/install support for distributions
 built using the **zpkg** tool.
 
-.. __: http://www.zope.org/Resources/ZPL/ZPL-2.0
+.. __: http://www.zope.org/Resources/ZPL/ZPL-2.1
 
 """
 
--- zpkgtools/trunk/zpkgtools/__init__.py
+++ zpkgtools/trunk/zpkgtools/__init__.py
@@ -4,7 +4,7 @@
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
-# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
@@ -14,13 +14,13 @@
 """Zope packaging utilities.
 
 :organization: Zope Corporation
-:license: `Zope Public License, Version 2.0 (ZPL)`__
+:license: `Zope Public License, Version 2.1 (ZPL)`__
 :status: Prototype implementation
 
 The Zope packaging utilities are the actual implementation of the
 **zpkg** command line utility.
 
-.. __: http://www.zope.org/Resources/ZPL
+.. __: http://www.zope.org/Resources/ZPL/ZPL-2.1
 
 """
 import zpkgsetup
