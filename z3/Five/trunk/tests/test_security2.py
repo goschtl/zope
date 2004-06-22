18f3bb623a41435e5e70d6eab825a6a111a27860
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2004-06-22 14:47:56 +0000

    Improve the way we do security checks using a testing strategy from Plone.
    git-svn-id: svn://svn.zope.org/repos/main@47028 62d5b8a3-27da-0310-9561-8e5933582275

--- z3/Five/trunk/tests/test_security2.py
+++ z3/Five/trunk/tests/test_security2.py
@@ -6,13 +6,47 @@ if __name__ == '__main__':
 import unittest
 from AccessControl import getSecurityManager
 from Testing import ZopeTestCase
+from AccessControl import Unauthorized
 
 ZopeTestCase.installProduct('Five')
 ZopeTestCase.installProduct('FiveTest')
+ZopeTestCase.installProduct('PythonScripts')
 
 ViewManagementScreens = 'View management screens'
 
-class SecurityTestCase(ZopeTestCase.ZopeTestCase):
+class RestrictedPythonTest(ZopeTestCase.ZopeTestCase):
+    """
+    Test whether code is really restricted
+    
+    Kind permission from Plone to use this.
+    """
+
+    def addPS(self, id, params='', body=''):
+        # clean up any 'ps' that's already here..
+        try:
+            self.folder._getOb('ps')
+            self.folder.manage_delObjects(['ps'])
+        except AttributeError:
+            pass # it's okay, no 'ps' exists yet
+        factory = self.folder.manage_addProduct['PythonScripts']
+        factory.manage_addPythonScript(id)
+        self.folder[id].ZPythonScript_edit(params, body)
+
+    def check(self, psbody):
+        self.addPS('ps', body=psbody)
+        try: 
+            self.folder.ps()
+        except (ImportError, Unauthorized), e:
+            self.fail(e)
+
+    def checkUnauthorized(self, psbody):
+        self.addPS('ps', body=psbody)
+        try:
+            self.folder.ps()
+        except (AttributeError, Unauthorized):
+            pass
+        
+class SecurityTestCase(RestrictedPythonTest):
     
     def afterSetUp(self):
         self.folder.manage_addProduct['FiveTest'].manage_addSimpleContent(
@@ -21,32 +55,25 @@ class SecurityTestCase(ZopeTestCase.ZopeTestCase):
         uf._doAddUser('viewer', 'secret', [], [])
         uf._doAddUser('manager', 'r00t', ['Manager'], [])
 
-    def assertPermission(self, permission, object):
-         user = getSecurityManager().getUser()
-         self.assert_(user.has_permission(permission, object))
-
-    def assertNoPermission(self, permission, object):
-         user = getSecurityManager().getUser()
-         self.assert_(not user.has_permission(permission, object))
-
-    paths = [
-        'testoid/eagle.txt',
-        'testoid/falcon.html',
-        'testoid/owl.html',
-        'testoid/flamingo.html',
-        'testoid/flamingo2.html',
-        'testoid/condor.html']
+    view_names = [
+        'eagle.txt',
+        'falcon.html',
+        'owl.html',
+        'flamingo.html',
+        'flamingo2.html',
+        'condor.html']
     
     def test_no_permission(self):
-        for path in self.paths:
-            view = self.folder.unrestrictedTraverse(path)
-            self.assertNoPermission(ViewManagementScreens, view)
+        self.login('viewer')
+        for view_name in self.view_names:
+            self.checkUnauthorized(
+                'context.restrictedTraverse("testoid/%s")()' % view_name)
 
     def test_permission(self):
         self.login('manager')
-        for path in self.paths:
-            view = self.folder.unrestrictedTraverse(path)
-            self.assertPermission(ViewManagementScreens, view)
+        for view_name in self.view_names:
+            self.check(
+                'context.restrictedTraverse("testoid/%s")()' % view_name)
         
 def test_suite():
     suite = unittest.TestSuite()
