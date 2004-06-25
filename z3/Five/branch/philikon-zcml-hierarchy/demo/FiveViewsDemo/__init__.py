aa6c0a464e835f6c24fdb148b257f3a3deb70d0c
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2004-06-25 16:12:41 +0000

    The demo's configure.zcml is now picked up by <five:loadProducts />
    git-svn-id: svn://svn.zope.org/repos/main@47095 62d5b8a3-27da-0310-9561-8e5933582275

--- z3/Five/branch/philikon-zcml-hierarchy/demo/FiveViewsDemo/__init__.py
+++ z3/Five/branch/philikon-zcml-hierarchy/demo/FiveViewsDemo/__init__.py
@@ -1,11 +1,8 @@
 # this a demo of Five views
-from Products.Five import zcml
 import Products
 import simplecontent
 
 def initialize(context):
-    zcml.process('configure.zcml', package=Products.FiveViewsDemo)
-
     context.registerClass(
         simplecontent.SimpleContent,
         constructors = (simplecontent.manage_addSimpleContentForm,
