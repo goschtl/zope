b8f0dfbb83f08aeea8f6e09dafb73a75cc9c085b
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2004-06-25 11:16:45 +0000

    Moved directives for the <browser:*> namespace to browserconfigure.py Handlers are now spread over the three modules as follows:
    * metaconfigure.py: handlers for <zope:*> directives
    * browserconfigure.py: handlers for <browser:*> directives
    * fiveconfigure.py: handlers for Five-specific directives (<five:*>)
    
    
    git-svn-id: svn://svn.zope.org/repos/main@47071 62d5b8a3-27da-0310-9561-8e5933582275

--- z3/Five/trunk/fiveconfigure.py
+++ z3/Five/trunk/fiveconfigure.py
@@ -6,142 +6,18 @@
 # License (ZPL) v2.1. See COPYING.txt for more information.
 #
 ##############################################################################
-"""Five specific directives.
+"""Five-specific directive handlers
+
+These directives are specific to Five and have no equivalents in Zope 3.
 
 $Id$
 """
-import os
-from zope.interface import classImplements, Interface
-from zope.component import getService, getGlobalService,\
-     ComponentLookupError
-from zope.configuration.exceptions import ConfigurationError
-from zope.component.servicenames import Adapters, Presentation
-from zope.publisher.interfaces.browser import IBrowserRequest
-from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
-
-from AccessControl import getSecurityManager
-from zExceptions import Unauthorized
+from zope.interface import classImplements
+from zope.component.servicenames import Presentation
+from zope.app.component.metaconfigure import handler
 
 from provideinterface import provideInterface
 from viewable import Viewable
-from api import BrowserView
-from metaclass import makeClass
-from security import getSecurityInfo, protectClass, initializeClass
-
-def handler(serviceName, methodName, *args, **kwargs):
-    # specifically ask for a global service
-    method = getattr(getGlobalService(serviceName), methodName)
-    method(*args, **kwargs)
-
-def page(_context, name, permission, for_,
-         layer='default', template=None, class_=None,
-         attribute='__call__', menu=None, title=None,
-         allowed_interface=None, allowed_attributes=None,
-         ):
-
-    try:
-        s = getGlobalService(Presentation)
-    except ComponentLookupError, err:
-        pass
-
-    if not (class_ or template):
-        raise ConfigurationError("Must specify a class or template")
-
-    if attribute != '__call__':
-        if template:
-            raise ConfigurationError(
-                "Attribute and template cannot be used together.")
-
-        if not class_:
-            raise ConfigurationError(
-                "A class must be provided if attribute is used")
-
-    if template:
-        template = os.path.abspath(str(_context.path(template)))
-        if not os.path.isfile(template):
-            raise ConfigurationError("No such file", template)
-
-    if class_:
-        if attribute != '__call__':
-            if not hasattr(class_, attribute):
-                raise ConfigurationError(
-                    "The provided class doesn't have the specified attribute "
-                    )
-        cdict = getSecurityInfo(class_)
-        if template:
-            new_class = makeClassForTemplate(template, bases=(class_, ),
-                                             cdict=cdict)
-        elif attribute != "__call__":
-            # we're supposed to make a page for an attribute (read:
-            # method) and it's not __call__.  We thus need to create a
-            # new class using our mixin for attributes.
-            cdict.update({'__page_attribute__': attribute})
-            new_class = makeClass(class_.__name__,
-                                  (class_, ViewMixinForAttributes),
-                                  cdict)
-        else:
-            # we could use the class verbatim here, but we'll execute
-            # some security declarations on it so we really shouldn't
-            # modify the original.  So, instead we make a new class
-            # with just one base class -- the original
-            new_class = makeClass(class_.__name__, (class_), cdict)
-
-    else:
-        # template
-        new_class = makeClassForTemplate(template)
-
-    _handle_for(_context, for_)
-
-    _context.action(
-        discriminator = ('view', for_, name, IBrowserRequest, layer),
-        callable = handler,
-        args = (Presentation, 'provideAdapter',
-                IBrowserRequest, new_class, name, [for_], Interface, layer,
-                _context.info),
-        )
-    _context.action(
-        discriminator = ('five:protectClass', new_class),
-        callable = protectClass,
-        args = (new_class, permission)
-        )
-    _context.action(
-        discriminator = ('five:initialize:class', new_class),
-        callable = initializeClass,
-        args = (new_class,)
-        )
-
-# verbatim from Zope3/src/zope/app/publisher/browser/viewmeta.py
-class pages:
-
-    def __init__(self, _context, for_, permission,
-                 layer='default', class_=None,
-                 allowed_interface=None, allowed_attributes=None,
-                 ):
-        self.opts = dict(for_=for_, permission=permission,
-                         layer=layer, class_=class_,
-                         allowed_interface=allowed_interface,
-                         allowed_attributes=allowed_attributes,
-                         )
-
-    def page(self, _context, name, attribute='__call__', template=None,
-             menu=None, title=None):
-        return page(_context,
-                    name=name,
-                    attribute=attribute,
-                    template=template,
-                    menu=menu, title=title,
-                    **(self.opts))
-
-    def __call__(self):
-        return ()
-
-def _handle_for(_context, for_):
-    if for_ is not None:
-        _context.action(
-            discriminator = None,
-            callable = provideInterface,
-            args = ('', for_)
-            )
 
 def implements(_context, class_, interface):
     for interface in interface:
@@ -176,61 +52,3 @@ def layer(_context, name):
         callable = handler,
         args = (Presentation, 'defineLayer', name, _context.info)
         )
-
-def skin(_context, name, layers):
-    if ',' in layers:
-        raise TypeError("Commas are not allowed in layer names.")
-
-    _context.action(
-        discriminator = ('skin', name),
-        callable = handler,
-        args = (Presentation, 'defineSkin', name, layers, _context.info)
-        )
-
-def defaultSkin(_context, name):
-    _context.action(
-        discriminator = 'defaultSkin',
-        callable = handler,
-        args = (Presentation, 'setDefaultSkin', name, _context.info)
-        )
-
-#
-# mixin classes / class factories
-#
-
-class ViewMixinForAttributes(BrowserView):
-
-    # we have an attribute that we can simply tell ZPublisher to go to
-    def __browser_default__(self, request):
-        return self, (self.__page_attribute__,)
-
-    # this is technically not needed because ZPublisher finds our
-    # attribute through __browser_default__; but we also want to be
-    # able to call pages from python modules, PythonScripts or ZPT
-    def __call__(self, *args, **kw):
-        attr = self.__page_attribute__
-        meth = getattr(self, attr)
-        return meth(*args, **kw)
-
-class ViewMixinForTemplates(BrowserView):
-
-    # short cut to get to macros more easily
-    def __getitem__(self, name):
-        return self.index.macros[name]
-
-    # make the template publishable
-    def __call__(self, *args, **kw):
-        return self.index(self, *args, **kw)
-
-def makeClassForTemplate(src, template=None, used_for=None, bases=(), cdict=None):
-    # XXX needs to deal with security from the bases?
-    if cdict is None:
-        cdict = {}
-    cdict.update({'index': ViewPageTemplateFile(src, template)})
-    bases += (ViewMixinForTemplates,)
-    class_ = makeClass("SimpleViewClass from %s" % src, bases, cdict)
-
-    if used_for is not None:
-        class_.__used_for__ = used_for
-
-    return class_
