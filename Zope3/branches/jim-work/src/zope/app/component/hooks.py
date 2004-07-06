0bee58a3778cc9fd994a4b1b5d5e3634e329f9dd
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2004-07-06 17:46:22 +0000

    Created a temporary branch for working purposes
    git-svn-id: svn://svn.zope.org/repos/main@26111 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/branches/jim-work/src/zope/app/component/hooks.py
@@ -0,0 +1,87 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+
+$Id$
+"""
+
+from zope.component import getService
+from zope.component.interfaces import IServiceService
+from zope.app.site.interfaces import ISite
+from zope.component.service import serviceManager
+from zope.component.exceptions import ComponentLookupError
+from zope.proxy import removeAllProxies
+from zope.security.proxy import trustedRemoveSecurityProxy
+from zope.app.traversing.interfaces import IContainmentRoot
+from zope.app.location.interfaces import ILocation
+from zope.app.location import locate
+from zope.component.servicenames import Presentation
+from zope.interface import Interface
+import warnings
+import zope.thread
+
+siteinfo = zope.thread.local()
+
+def setSite(site=None):
+    if site is None:
+        siteinfo.services = None
+    else:
+        siteinfo.services = trustedRemoveSecurityProxy(site.getSiteManager())
+
+def getSite():
+    try:
+        services = siteinfo.services
+    except AttributeError:
+        services = siteinfo.services = None
+        
+    if services is None:
+        return None
+
+    return services.__parent__
+    
+
+def getServices_hook(context=None):
+
+    if context is None:
+        try:
+            services = siteinfo.services
+        except AttributeError:
+            services = siteinfo.services = None
+
+        if services is None:
+            return serviceManager
+
+        return services
+
+    # Deprecated support for a context that isn't adaptable to
+    # IServiceService.  Return the default service manager.
+    try:
+        return trustedRemoveSecurityProxy(IServiceService(context,
+                                                          serviceManager))
+    except ComponentLookupError:
+        return serviceManager
+    
+
+def queryView(object, name, request, default=None,
+              providing=Interface, context=None):
+    # XXX test
+    #if context is None:
+    #    context = object
+    views = getService(Presentation, context)
+    view = views.queryView(object, name, request, default=default,
+                           providing=providing)
+    if ILocation.providedBy(view):
+        locate(view, object, name)
+
+    return view
---
+++ Zope3/branches/jim-work/src/zope/app/publisher/browser/resource.py
@@ -0,0 +1,55 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+
+$Id$
+"""
+__metaclass__ = type # All classes are new style when run with Python 2.2+
+
+from zope.app import zapi
+from zope.app.location import Location
+from zope.app.site.interfaces import ISite
+from zope.app.traversing.interfaces import IContainmentRoot
+from zope.app.traversing.browser.interfaces import IAbsoluteURL
+from zope.component.interfaces import IResource
+from zope.interface import implements
+
+class Resource(Location):
+    implements(IResource)
+
+    def __init__(self, request):
+        self.request = request
+
+    def __call__(self):
+        names = []
+        name = self.__name__
+        if name.startswith('++resource++'):
+            name = name[12:]
+        names.append(name)
+
+        site = self.__parent__
+        while True:
+            if ISite.providedBy(site):
+                break
+            if IContainmentRoot.providedBy(site):
+                site = None
+                break
+            if IResource.providedBy(site) and site.__name__:
+                names.append(site.__name__)
+            site = site.__parent__
+
+        names.reverse()
+        name = '/'.join(filter(None, names))
+        url = str(zapi.getViewProviding(site, IAbsoluteURL, self.request))
+        return "%s/@@/%s" % (url, name)
