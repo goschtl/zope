1c1b4db673381141ceaafd2df674e10863c9ec71
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2004-07-08 22:52:26 +0000

    Rearranged imports to match style guide.
    git-svn-id: svn://svn.zope.org/repos/main@26266 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/dublincore/zopedublincore.py
+++ Zope3/trunk/src/zope/app/dublincore/zopedublincore.py
@@ -17,10 +17,12 @@ $Id$
 
 __metaclass__ = type
 
+from datetime import datetime
+
+from zope.interface import implements
+
 from zope.app.dublincore.interfaces import IZopeDublinCore
 from zope.app.datetimeutils import parseDatetimetz
-from datetime import datetime
-from zope.interface import implements
 
 class SimpleProperty:
 
