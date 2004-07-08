f78fcc62cf5d7801f6650a23ba852d3e1215acc5
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2004-07-08 10:42:46 +0000

    Removed a bunch of easy XXX comments.
    git-svn-id: svn://svn.zope.org/repos/main@26201 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/schemagen/interfaces.py
+++ Zope3/trunk/src/zope/app/schemagen/interfaces.py
@@ -11,9 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""XXX short summary goes here.
-
-XXX longer description goes here.
+"""Interfaces for the schema-based code generator.
 
 $Id$
 """
