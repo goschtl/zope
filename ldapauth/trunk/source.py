748353c3106b0e09e3a5311ef9abd2e327820056
rogerineichen <rogerineichen@62d5b8a3-27da-0310-9561-8e5933582275>
2004-07-11 21:37:51 +0000

    Changes in source.py Implement chaching results in a recursion error. It's just a branch. The test will fail in ldapauth till we fix this.
    git-svn-id: svn://svn.zope.org/repos/main@26416 62d5b8a3-27da-0310-9561-8e5933582275

--- ldapauth/trunk/source.py
+++ ldapauth/trunk/source.py
@@ -18,13 +18,17 @@ $Id:
 
 import ldap
 from persistent import Persistent
-from zope.app.container.contained import Contained
+from zope.app.container.contained import Contained, setitem
 from zope.app.pluggableauth.interfaces import \
         ILoginPasswordPrincipalSource, IContainerPrincipalSource
 from zope.app.location import locate
 from zope.app.pluggableauth import SimplePrincipal
 from zope.exceptions import NotFoundError
 from zope.interface import implements
+
+from zope.app.cache.ram import RAMCache
+from zope.app.cache.caching import getCacheForObject, getLocationForCache
+from zope.app.cache.annotationcacheable import AnnotationCacheable
 
 from interfaces import ILDAPBasedPrincipalSource
 
@@ -46,27 +50,77 @@ class LDAPPrincipalSource(Contained, Persistent):
     ### IContainer-related methods
 
     def __delitem__(self, login):
-        pass
+        cache = getCacheForObject(self)
+        location = getLocationForCache(self)
+        if cache and location:
+            cache.invalidate(location, login)
 
-    # XXX We should use setitem from zope.app.container.contained
-    # Would allow events and so on. This is just a test.
-    # This way of registering the principal is somehow stupid since we must
-    # use it each time a new principal is created. This is UGLY.
+    ### This is the stuff needed to add the Principal to the cache and to
+    ### make it containment friendly.
+    ### TODO: add the principal to the ldap server if it is a new one.
     def __setitem__(self, login, obj):
         obj.id = login
-        obj.__parent__ = self
+        setitem(self, self._setitem, login, obj)
+
+    def _setitem(self, key, obj):
+        cache = getCacheForObject(self)
+        location = getLocationForCache(self)
+        if cache and location:
+            principal = cache.query(location, key)
+            if principal is None:
+                cache.set(obj, location, key)
 
     def keys(self):
-        pass
+        logins = []
+        l = self.__connect()
+        l.simple_bind_s(self.manager_dn, self.manager_passwd)
+        lsearch = l.search_s(self.basedn, ldap.SCOPE_ONELEVEL, '(%s=*)' %
+                self.login_attribute)
+        for node in lsearch:
+            node_dn, node_dict = node
+            logins.append(node_dict[self.login_attribute][0])
+        return logins
 
     def __iter__(self):
-        pass
+        return self.keys()
 
-    def __getitem__(self):
-        pass
+    def __getitem__(self, key):
+        cache = getCacheForObject(self)
+        location = getLocationForCache(self)
+        if cache and location:
+            principal = cache.query(location, key)
+            if principal is None:
+                principal = self.__findInLDAP(key)
+            
+                #XXX 
+                # RuntimeError: maximum recursion depth exceeded
+                # this is calling __setitem__ if we call authenticate --> __setitem__ --> __getitem__ --> ...
+
+                #self[principal.login] = principal
+        else:
+            principal = self.__findInLDAP(key)
+            
+            #XXX 
+            # RuntimeError: maximum recursion depth exceeded
+            # this is calling __setitem__ if we call authenticate --> __setitem__ --> __getitem__ --> ...
+            
+            #self[principal.login] = principal
+        return principal
+
+    def __findInLDAP(self, login):
+        l = self.__connect()
+        l.simple_bind_s(self.manager_dn, self.manager_passwd)
+        lsearch = l.search_s(self.basedn, ldap.SCOPE_ONELEVEL,
+                '(%s=%s)' % (self.login_attribute, login))
+        if lsearch:
+            uid_dn, uid_dict = lsearch[0]
+            principal = SimplePrincipal(
+                    login = uid_dict[self.login_attribute][0],
+                    password = uid_dict['userPassword'][0])
+            return principal
 
     def get(self, key, default=None):
-        pass
+        return self[key]
 
     def values(self):
         pass
@@ -77,23 +131,15 @@ class LDAPPrincipalSource(Contained, Persistent):
     def items(self):
         pass
 
-    def __contains__(self):
+    def __contains__(self, key):
         pass
     
     ### IPrincipalSource methods
     
     def getPrincipal(self, id):
         uid = id.split('\t')[2]
-        l = self.__connect()
-        l.simple_bind_s(self.manager_dn, self.manager_passwd)
-        lsearch = l.search_s(self.basedn, ldap.SCOPE_ONELEVEL,
-                '(%s=%s)' % (self.login_attribute, uid))
-        if lsearch:
-            uid_dn, uid_dict = lsearch[0]
-            principal = SimplePrincipal(
-                    login = uid_dict[self.login_attribute][0],
-                    password = uid_dict['userPassword'][0])
-            self.__setitem__(principal.login, principal)
+        principal = self[uid]
+        if principal:
             return principal
         else:
             raise NotFoundError, id
@@ -108,12 +154,12 @@ class LDAPPrincipalSource(Contained, Persistent):
         lsearch = l.search_s(self.basedn, ldap.SCOPE_ONELEVEL, search)
         
         principals = []
-        for userinfo in lsearch:
-            uid_dn, uid_dict = userinfo
+        for node in lsearch:
+            node_dn, node_dict = node
             principal = SimplePrincipal(
-                    login = uid_dict[self.login_attribute][0],
-                    password = uid_dict['userPassword'][0])
-            self.__setitem__(principal.login, principal)
+                    login = node_dict[self.login_attribute][0],
+                    password = node_dict['userPassword'][0])
+            self[principal.login] = principal
             principals.append(principal)
 
         return principals
