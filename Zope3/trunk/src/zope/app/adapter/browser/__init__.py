bcb61b962572d3652bfc362e80756a8462e59b56
pruggera <pruggera@62d5b8a3-27da-0310-9561-8e5933582275>
2004-07-14 04:41:02 +0000

    Update doc strings to ReST
    git-svn-id: svn://svn.zope.org/repos/main@26519 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/adapter/browser/__init__.py
+++ Zope3/trunk/src/zope/app/adapter/browser/__init__.py
@@ -19,11 +19,12 @@
     search page
     browsing page
 
-  AdapterRegistrationAdd
+  `AdapterRegistrationAdd`
 
 $Id$
 """
 __metaclass__ = type
+__docformat__ = 'restructuredtext'
 
 from zope.app.form.utility import setUpWidgets, getWidgetsData
 
