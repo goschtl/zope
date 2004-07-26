ada5662fd9e33cd1c547c8898a0fde7cc726d660
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2004-07-26 14:04:12 +0000

    Create a branch for various documentation tweaks that need to be on the trunk and the 0.1 release.
    git-svn-id: svn://svn.zope.org/repos/main@47478 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3/Five/branch/faassen-docfixes/__init__.py
@@ -0,0 +1,23 @@
+##############################################################################
+#
+# Copyright (c) 2004 Five Contributors. All rights reserved.
+#
+# This software is distributed under the terms of the Zope Public
+# License (ZPL) v2.1. See COPYING.txt for more information.
+#
+##############################################################################
+"""Initialize the Five product
+
+$Id$
+"""
+import Acquisition
+from Globals import INSTANCE_HOME
+
+import monkey
+import zcml
+
+# trigger monkey patches
+monkey.monkeyPatch()
+
+def initialize(context):
+    zcml.load_site()
