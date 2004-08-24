f947f4a34bbc16f09eaa5315b3fa58c3429a72f3
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2004-08-24 17:56:37 +0000

    Made sure this will not conflict with the zope.app.demo version.
    git-svn-id: svn://svn.zope.org/repos/main@27249 62d5b8a3-27da-0310-9561-8e5933582275

--- book/trunk/insensitivefolder/__init__.py
+++ book/trunk/insensitivefolder/__init__.py
@@ -27,29 +27,6 @@ from zope.app.folder.interfaces import IFolder
 from zope.app.container.traversal import ContainerTraverser
 
 
-class CaseInsensitiveFolderTraverser(ContainerTraverser):
-
-    __used_for__ = ICaseInsensitiveFolder
-
-    def publishTraverse(self, request, name):
-        """See zope.publisher.interfaces.browser.IBrowserPublisher"""
-        subob = self._guessTraverse(name) 
-        if subob is None:
-            view = zapi.queryView(self.context, name, request)
-            if view is not None:
-                return view
-
-            raise NotFound(self.context, name, request)
-         
-        return subob
-
-    def _guessTraverse(self, name):
-        for key in self.context.keys():
-            if key.lower() == name.lower():
-                return self.context[key]
-        return None
-
-
 class ICaseInsensitiveFolder(IFolder):
     """Marker for folders whose contained items keys are case insensitive.
 
@@ -80,3 +57,26 @@ class CaseInsensitiveFolderFactory(object):
         return implementedBy(Folder) + ICaseInsensitiveFolder
 
 caseInsensitiveFolderFactory = CaseInsensitiveFolderFactory()
+
+
+class CaseInsensitiveFolderTraverser(ContainerTraverser):
+
+    __used_for__ = ICaseInsensitiveFolder
+
+    def publishTraverse(self, request, name):
+        """See zope.publisher.interfaces.browser.IBrowserPublisher"""
+        subob = self._guessTraverse(name) 
+        if subob is None:
+            view = zapi.queryView(self.context, name, request)
+            if view is not None:
+                return view
+
+            raise NotFound(self.context, name, request)
+         
+        return subob
+
+    def _guessTraverse(self, name):
+        for key in self.context.keys():
+            if key.lower() == name.lower():
+                return self.context[key]
+        return None
