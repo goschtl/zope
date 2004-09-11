7e9443b470b27864eca655e9112ed8137bed5a91
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2004-09-11 17:08:11 +0000

    Ported Zope Vesion Control to Zope 3
    This is work done by Fred Drake and myself, but mostly by Fred.
    
    To do:
    
      - Provide a basic UI and functional tests
    
      - Complete the configuration (the current configuration, 
        which just captures a permission declaration) isn't even tied
        in with the application server.
    
      - Provide packaging files.
    
      - We need to review the issues in isues.txt, removing the ones that
        have been resolved.
    
      - Probably change the strategy for copying into the repo so that the
        objects in the repo are whole.
    
    
    
    git-svn-id: svn://svn.zope.org/repos/main@27498 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/trunk/src/zope/app/versioncontrol/utility.py
@@ -0,0 +1,104 @@
+##############################################################################
+#
+# Copyright (c) 2001 Zope Corporation and Contributors. All Rights Reserved.
+# 
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+# 
+##############################################################################
+
+import time
+
+import persistent
+
+from ZODB.serialize import referencesf
+from ZODB.TimeStamp import TimeStamp
+
+import zope.security.management
+
+import zope.app.versioncontrol.interfaces
+
+
+def isAVersionableResource(obj):
+    """ True if an object is versionable.
+
+    To qualify, the object must be persistent (have its own db record), and
+    must not have an true attribute named '__non_versionable__'.
+    """
+    return zope.app.versioncontrol.interfaces.IVersionable.providedBy(obj)
+
+
+class VersionInfo(persistent.Persistent):
+    """Container for bookkeeping information.
+
+    The bookkeeping information can be read (but not changed) by
+    restricted code.
+    """
+
+    def __init__(self, history_id, version_id, status):
+        self.history_id = history_id
+        self.version_id = version_id
+        self.status = status
+        self.touch()
+
+    sticky = None
+
+    def branchName(self):
+        if self.sticky is not None and self.sticky[0] == 'B':
+            return self.sticky[1]
+        return 'mainline'
+
+    def touch(self):
+        self.user_id = _findUserId()
+        self.timestamp = time.time()
+
+
+def _findUserId():
+    interaction = zope.security.management.getInteraction()
+    return interaction.participations[0].principal.id
+
+def _findModificationTime(object):
+    """Find the last modification time for a version-controlled object.
+       The modification time reflects the latest modification time of
+       the object or any of its persistent subobjects that are not
+       themselves version-controlled objects. Note that this will
+       return None if the object has no modification time."""
+
+    mtime = getattr(object, '_p_mtime', None)
+    if mtime is None:
+        return None
+
+    latest = mtime
+    conn = object._p_jar
+    load = conn._storage.load
+    version = conn._version
+    refs = referencesf
+
+    oids=[object._p_oid]
+    done_oids={}
+    done=done_oids.has_key
+    first = 1
+
+    while oids:
+        oid=oids[0]
+        del oids[0]
+        if done(oid):
+            continue
+        done_oids[oid]=1
+        try: p, serial = load(oid, version)
+        except: pass # invalid reference!
+        else:
+            if first is not None:
+                first = None
+            else:
+                if p.find('U\x0b__vc_info__') == -1:
+                    mtime = TimeStamp(serial).timeTime()
+                    if mtime > latest:
+                        latest = mtime
+            refs(p, oids)
+
+    return latest
