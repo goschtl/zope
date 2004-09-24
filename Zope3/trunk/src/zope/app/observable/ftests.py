a86d2379eb92a94701879dd010bfa0a69c375238
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2004-09-24 18:32:02 +0000

    Fix pickling issue and thus complete the test
    git-svn-id: svn://svn.zope.org/repos/main@27681 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/observable/ftests.py
+++ Zope3/trunk/src/zope/app/observable/ftests.py
@@ -4,11 +4,15 @@ from zope.interface import implements
 from zope.app.annotation.interfaces import IAttributeAnnotatable
 from zope.app.tests.functional import FunctionalDocFileSuite
 
+def chickenModified(event):
+     print "buh-PAH! I was modified."
+
 class Chicken(Persistent):
      implements(IAttributeAnnotatable)
 
 def test_suite():
-    globs = {'Chicken': Chicken}
+    globs = {'Chicken': Chicken,
+             'chickenModified': chickenModified}
     return unittest.TestSuite((
 	FunctionalDocFileSuite('observable.txt', globs=globs),
         ))
