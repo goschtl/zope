14a01ab2894ce7c39b786e33293a62b1dfc74500
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2004-10-06 22:18:09 +0000

    Added file headers.
    git-svn-id: svn://svn.zope.org/repos/main@27775 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/pas/tests.py
+++ Zope3/trunk/src/zope/app/pas/tests.py
@@ -1,5 +1,21 @@
-"""$Id$
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Pluggable Authentication Service Tests
+
+$Id$
 """
+__docformat__ = "reStructuredText"
 import unittest
 from zope.testing import doctest
 from zope.app.tests import placelesssetup, ztapi
