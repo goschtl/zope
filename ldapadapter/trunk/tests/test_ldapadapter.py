3eae371ff78315b36931ad2334b33dea274c02c2
efge <efge@62d5b8a3-27da-0310-9561-8e5933582275>
2004-10-09 12:45:34 +0000

    Test framework to use fake _ldap module.
    git-svn-id: svn://svn.zope.org/repos/main@27820 62d5b8a3-27da-0310-9561-8e5933582275

--- ldapadapter/trunk/tests/test_ldapadapter.py
+++ ldapadapter/trunk/tests/test_ldapadapter.py
@@ -16,15 +16,31 @@
 $Id$
 """
 __docformat__ = "reStructuredText"
+import sys
 import unittest
 from zope.testing import doctest
 from zope.app.tests import placelesssetup, ztapi
 from zope.app.event.tests.placelesssetup import getEvents
 
 
+def setUp(test):
+    import fakeldap
+    if sys.modules.has_key('_ldap'):
+        test.old_ldap = sys.modules['_ldap']
+        del sys.modules['_ldap']
+    else:
+        test.old_ldap = None
+    sys.modules['_ldap'] = fakeldap
+
+def tearDown(test):
+    del sys.modules['_ldap']
+    if test.old_ldap is not None:
+        sys.modules['_ldap'] = test.old_ldap
+
 def test_suite():
     return unittest.TestSuite((
-        doctest.DocFileSuite('../README.txt'),
+        doctest.DocFileSuite('../README.txt',
+                             setUp=setUp, tearDown=tearDown),
         ))
 
 if __name__ == '__main__':
