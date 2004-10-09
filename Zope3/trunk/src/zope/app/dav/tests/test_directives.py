3f2c6533f8e290c72c31661220bdd398a7e36f7e
mj <mj@62d5b8a3-27da-0310-9561-8e5933582275>
2004-10-09 15:37:29 +0000

    Small doc fix; probably a c&p error. This test is for dav ZCML directives, not wiki.
    git-svn-id: svn://svn.zope.org/repos/main@27844 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/dav/tests/test_directives.py
+++ Zope3/trunk/src/zope/app/dav/tests/test_directives.py
@@ -11,7 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""Test the wiki ZCML namespace directives.
+"""Test the dav ZCML namespace directives.
 
 $Id$
 """
