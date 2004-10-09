0eb3f20daab47f23925db800bb32d7ef6fd1fbc6
oestermeier <oestermeier@62d5b8a3-27da-0310-9561-8e5933582275>
2004-10-09 16:06:02 +0000

    set up minimal test infrastructure
    git-svn-id: svn://svn.zope.org/repos/main@27851 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zversioning/trunk/src/versioning/tests/test_versioncontrol.py
@@ -0,0 +1,47 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+This file contains some tests of zope.app.versioncontrol to figure
+out whether the existing implementation fits our needs.
+
+
+"""
+import unittest
+import doctest
+
+from zope.interface import implements
+from zope.app.container.sample import SampleContainer
+from zope.app.tests.placelesssetup import setUp, tearDown
+from zope.app.tests import ztapi
+
+from zope.app.tests.setup import buildSampleFolderTree
+
+
+def buildSite(items=None) :
+	""" Returns s small test site of original content objects:
+		
+		>>> folders = buildSampleFolderTree()
+		>>> folders is not None
+		True
+	    
+	"""
+  
+    
+
+def test_suite():
+    return unittest.TestSuite((
+        DocTestSuite(),
+        ))
+if __name__=='__main__':
+    unittest.main(defaultTest='test_suite')
