17cb409b15c4abdbb9aa674fca711d08979a5657
rogerineichen <rogerineichen@62d5b8a3-27da-0310-9561-8e5933582275>
2004-10-09 09:54:51 +0000

    Added README.txt file for documentation and testing
    git-svn-id: svn://svn.zope.org/repos/main@27807 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ ldapadapter/trunk/README.txt
@@ -0,0 +1,15 @@
+===========
+LDAPAdapter 
+===========
+
+The LDAPAdapter provides a mechanism for to connect to a LDAP server.
+
+LDAPAdapter
+===========
+
+The primary job of an ldap adapter is to store the settings of
+a LDAP server connection.
+
+Let's look at an example:
+
+  >>> from ldapadapter.interfaces import LDAPAdapter
---
+++ ldapadapter/trunk/tests/test_ldapadapter.py
@@ -0,0 +1,32 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""LDAPAdapter tests
+
+$Id:$
+"""
+__docformat__ = "reStructuredText"
+import unittest
+from zope.testing import doctest
+from zope.app.tests import placelesssetup, ztapi
+from zope.app.event.tests.placelesssetup import getEvents
+
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite('../README.txt'),
+        ))
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
+
