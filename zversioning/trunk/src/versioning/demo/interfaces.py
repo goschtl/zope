cddebac63f51963dface11da3dc7b361aeb0e7ec
vbachs <vbachs@62d5b8a3-27da-0310-9561-8e5933582275>
2004-10-10 15:17:42 +0000

    beautified form
    git-svn-id: svn://svn.zope.org/repos/main@27915 62d5b8a3-27da-0310-9561-8e5933582275

--- zversioning/trunk/src/versioning/demo/interfaces.py
+++ zversioning/trunk/src/versioning/demo/interfaces.py
@@ -9,7 +9,7 @@ class IVProposal(zope.interface.Interface):
     
     title = TextLine(title=_("Title"))
     usecase = Text(title=_("Use Case"))
-    concept = Text(title=_("Concept"))
-    todo = Text(title=_("Todo"))
-    issues = Text(title=_("Open Issues"))
+    concept = Text(title=_("Concept"),required=False)
+    todo = Text(title=_("Todo"),required=False)
+    issues = Text(title=_("Open Issues"),required=False)
     
