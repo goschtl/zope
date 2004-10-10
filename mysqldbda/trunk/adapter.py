53368e7a1a18c5726a389745d3fd559778bee154
mriya3 <mriya3@62d5b8a3-27da-0310-9561-8e5933582275>
2004-10-10 16:19:23 +0000

    Sprinting with Claudia Albisetti
    Finished updating MySQLdb DA.
    
    The Python DBI returns unicode in an encoding. This adapter now correctly converts all strings (varchar, char, text) to unicode using 
    the correct encoding.
    
    
    git-svn-id: svn://svn.zope.org/repos/main@27920 62d5b8a3-27da-0310-9561-8e5933582275

--- mysqldbda/trunk/adapter.py
+++ mysqldbda/trunk/adapter.py
@@ -13,32 +13,74 @@
 ##############################################################################
 """MySQL database adapter.
 
-$Id: Adapter.py,v 1.3 2003/06/05 16:42:31 philikon dead $
+$Id: Adapter.py,v 1.1 2004/10/10 mriya3
 """
-
-import MySQLdb
 
 from zope.app.rdb import ZopeDatabaseAdapter, parseDSN
 
+import MySQLdb
+
 dsn2option_mapping = {'dbname':'dbname',
-                      'port':'port',
-                      'host':'host',
-                      'username':'user',
-                      'password':'passwd'}
+                    'port':'port',
+                    'host':'host',
+                    'username':'user',
+                    'password':'passwd'}
+
+class MySQLStringConverter:
+    def __init__(self, encoding):
+        self.encoding = encoding
+
+    def __call__(self, string):
+        if isinstance(string, str):
+            return string.decode(self.encoding)
+        elif isinstance(string, unicode):
+            return string
+        else:
+            return string
+            
+            
 
 class MySQLdbAdapter(ZopeDatabaseAdapter):
     """A MySQLdb adapter for Zope3"""
+    
+    
+    """ MySQLdb types codes"""
+    __STRINGtypes = (1, 247, 254, 253)
+    __BINARYtypes = (252, 251, 250, 249)
+    __DATEtypes = (10, 14)
+    __DATETIMEtypes = (7, 12)
+    __NUMBERtypes = (0, 5, 4, 9, 3, 8, 1, 13)
+    __TIMEtypes = (11)
+    
+    """ Default string converter """
+    __stringConverter =  MySQLStringConverter('UTF-8')
+    
 
     def _connection_factory(self):
         """Create a MySQLdb DBI connection based on the DSN"""
 
         conn_info = parseDSN(self.dsn)
+
         print '*'*78
         print conn_info
         print '*'*78
-        return MySQLdb.Connect(db=conn_info['dbname'],
-                               host=conn_info['host'],
-                               user=conn_info['username'],
-                               passwd=conn_info['password'],
-                               port=int(conn_info['port'] or '3306'))
-                      
+        connection = MySQLdb.Connect(db=conn_info['dbname'],
+                            host=conn_info['host'],
+                            user=conn_info['username'],
+                            passwd=conn_info['password'],
+                            port=int(conn_info['port'] or '3306'))
+ 
+        self.__stringConverter = MySQLStringConverter(str(connection.character_set_name()))
+        return connection
+                    
+    def getConverter(self, type):
+        'See IDBITypeInfo'
+        if type in self.__STRINGtypes:
+            return self.__stringConverter
+        return self.identity
+   
+
+    def identity(self, x):
+        return x
+    
+    
