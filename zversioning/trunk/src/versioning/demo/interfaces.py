7ac7cc30a405de617e41b1733b5a9693a91f66c0
vbachs <vbachs@62d5b8a3-27da-0310-9561-8e5933582275>
2004-10-10 08:39:30 +0000

    Basic Proposal-Demo added
    git-svn-id: svn://svn.zope.org/repos/main@27878 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zversioning/trunk/src/versioning/demo/interfaces.py
@@ -0,0 +1,17 @@
+import zope.interface
+from zope.schema import Text, TextLine
+from zope.i18nmessageid import MessageIDFactory
+_ = MessageIDFactory("simplebug")
+ 
+ 
+class IVProposal(zope.interface.Interface):
+    """Provides access to a versionable Proposal""" 
+    
+    title = TextLine(title=_("Title"))
+    usecase = Text(title=_("Use Case"))
+    concept = Text(title=_("Concept"))
+    todo = Text(title=_("Todo"))
+    issues = Text(title=_("Open Issues"))
+    
+    def proposal():
+        """return the Proposal"""                
---
+++ zversioning/trunk/src/versioning/demo/vproposal.py
@@ -0,0 +1,20 @@
+import persistent
+import zope.interface
+from zope.app.dublincore.interfaces import IZopeDublinCore
+from versioning.demo.interfaces import IVProposal
+
+class VProposal(persistent.Persistent):
+    """A versionable Proposal"""
+    
+    zope.interface.implements(IVProposal)
+    
+    def __init__(self,title='',usecase='',concept='',todo='',issues=''):
+        self.title = title
+        self.usecase = usecase
+        self.concept = concept
+        self.todo = todo
+        self.issues = issues
+        
+    def proposal(self):
+        return "%s\n%s" % (self.title,self.usecase,self.concept,self.todo, \
+                           self.issues)        
