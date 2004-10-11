6d281da369039d89d4bbad0f8cb09c7a2f837d8a
vbachs <vbachs@62d5b8a3-27da-0310-9561-8e5933582275>
2004-10-11 12:08:10 +0000

    some typos
    git-svn-id: svn://svn.zope.org/repos/main@27969 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zversioning/trunk/src/versioning/demo/browser/view.py
@@ -0,0 +1,20 @@
+from zope.app.renderer.rest import ReStructuredTextToHTMLRenderer
+
+class RenderReST:
+    """Provide an interface for viewing a Proposal as ReST"""
+    
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.title = context.title
+        self.usecase = self.render(context.usecase)
+        self.concept = self.render(context.concept)
+        self.todo = self.render(context.todo)
+        self.issues = self.render(context.issues)   
+
+
+    def render(self,content):    
+        """render contextattributes to ReST"""
+        html = ReStructuredTextToHTMLRenderer(content.encode('utf-8'),
+                                              self.request)
+        return unicode(html.render(),'utf-8')
