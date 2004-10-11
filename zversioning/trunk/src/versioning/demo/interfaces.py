de40afdab6e00b103568395f7a3b46520d6df4d7
vbachs <vbachs@62d5b8a3-27da-0310-9561-8e5933582275>
2004-10-11 12:01:59 +0000

    Added View-Class for ReST
    git-svn-id: svn://svn.zope.org/repos/main@27968 62d5b8a3-27da-0310-9561-8e5933582275

--- zversioning/trunk/src/versioning/demo/interfaces.py
+++ zversioning/trunk/src/versioning/demo/interfaces.py
@@ -12,15 +12,3 @@ class IVProposal(zope.interface.Interface):
     concept = Text(title=_("Concept"),required=False)
     todo = Text(title=_("Todo"),required=False)
     issues = Text(title=_("Open Issues"),required=False)
-        
-    def rusecase():
-        """Render usecase to HTML"""    
-        
-    def rconcept():
-        """Render usecase to HTML"""    
-
-    def rtodo():
-        """Render usecase to HTML"""
-        
-    def rissues():
-        """Render usecase to HTML"""        
