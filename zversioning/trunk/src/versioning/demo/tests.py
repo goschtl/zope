28863396d5889305122aa711e5c64db0e54b6d34
vbachs <vbachs@62d5b8a3-27da-0310-9561-8e5933582275>
2004-10-12 16:02:55 +0000

    some tests added
    git-svn-id: svn://svn.zope.org/repos/main@28012 62d5b8a3-27da-0310-9561-8e5933582275

--- zversioning/trunk/src/versioning/demo/tests.py
+++ zversioning/trunk/src/versioning/demo/tests.py
@@ -1,5 +1,25 @@
 import unittest
-from zope.testing.doctest import DocFileSuite
+from zope.testing.doctest import DocFileSuite, DocTestSuite
+from zope.app.tests import placelesssetup
+from zope.app.tests import ztapi
+
+from vproposal import VProposal
+from zope.app.tests.setup import setUpAnnotations
+from zope.app.annotation.interfaces import IAnnotatable, IAttributeAnnotatable
+from zope.app.dublincore.interfaces import IZopeDublinCore
+from zope.app.dublincore.annotatableadapter import ZDCAnnotatableAdapter
+from zope.interface.declarations import classImplements
+
+def setUp(test):
+    placelesssetup.setUp(test)
+    setUpAnnotations()
+    ztapi.provideAdapter(IAnnotatable, IZopeDublinCore,
+                         ZDCAnnotatableAdapter)
+    classImplements(VProposal,IAttributeAnnotatable)    
  
 def test_suite():
-    return DocFileSuite('vproposal.txt')
+    suite = unittest.TestSuite()
+    suite.addTest(DocFileSuite('vproposal.txt',setUp=setUp,
+                                tearDown=placelesssetup.tearDown))
+    return suite
+
