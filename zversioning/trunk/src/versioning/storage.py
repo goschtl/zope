d8f67959c3761d4175fde73fc7d35f678460e91e
gregweb <gregweb@62d5b8a3-27da-0310-9561-8e5933582275>
2004-10-13 16:52:38 +0000

    VersionHistory now implements 'INameHoose' directly
    git-svn-id: svn://svn.zope.org/repos/main@28107 62d5b8a3-27da-0310-9561-8e5933582275

--- zversioning/trunk/src/versioning/storage.py
+++ zversioning/trunk/src/versioning/storage.py
@@ -16,6 +16,7 @@ import unittest, doctest
 
 from zope.interface import implements
 from zope.app.copypastemove.interfaces import IObjectCopier 
+from zope.app.container.interfaces import INameChooser
 from zope.app.folder import Folder
 from zope.app.exception.interfaces import UserError
 
@@ -29,7 +30,7 @@ class VersionHistory(Folder) :
         is labeled '001', '002' etc.
     """
     
-    implements(IVersionHistory)
+    implements(IVersionHistory, INameChooser)
     
     def checkName(self, name, object):
         """Check whether an object name is valid.
