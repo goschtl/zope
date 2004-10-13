0a5c22ac12ce00945810e3f7eeaa4e4d790a6108
efge <efge@62d5b8a3-27da-0310-9561-8e5933582275>
2004-10-13 15:08:50 +0000

    Use a vocabulary choice for the LDAP adapter.
    git-svn-id: svn://svn.zope.org/repos/main@28097 62d5b8a3-27da-0310-9561-8e5933582275

--- ldappas/trunk/interfaces.py
+++ ldappas/trunk/interfaces.py
@@ -19,37 +19,44 @@ $Id$
 import re
 from zope.app.i18n import ZopeMessageIDFactory as _
 from zope.interface import Interface
+from zope.schema import Choice
 from zope.schema import TextLine
 
 class ILDAPAuthentication(Interface):
-    adapterName = TextLine(
-        title=_("LDAP Adapter"),
-        default=u'ldapadapter',
+    adapterName = Choice(
+        title=_(u"LDAP Adapter Name"),
+        description=_(u"The LDAP adapter name for the connection to be used."),
+        vocabulary="LDAP Adapter Names",
         required=True,
         )
     searchBase = TextLine(
         title=_("Search base"),
+        description=_(u"The LDAP search base where principals are found."),
         default=u'dc=example,dc=org',
         required=True,
         )
     searchScope = TextLine(
         title=_("Search scope"),
+        description=_(u"The LDAP search scope used to find principals."),
         default=u'sub',
         required=True,
         )
     loginAttribute = TextLine(
         title=_("Login attribute"),
+        description=_(u"The LDAP attribute used to find principals."),
         constraint=re.compile("[a-zA-Z][-a-zA-Z0-9]*$").match,
         default=u'uid',
         required=True,
         )
     principalIdPrefix = TextLine(
         title=_("Principal id prefix"),
+        description=_(u"The prefix to add to all principal ids."),
         default=u'ldap.',
         required=False,
         )
     idAttribute = TextLine(
         title=_("Id attribute"),
+        description=_(u"The LDAP attribute used to determine principal ids."),
         constraint=re.compile("[a-zA-Z][-a-zA-Z0-9]*$").match,
         default=u'uid',
         required=True,
