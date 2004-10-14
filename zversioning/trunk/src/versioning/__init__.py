b9e52f1cb0b3fdbd42dd28a365eac5b1da6d5e04
tob_ahle06 <tob_ahle06@62d5b8a3-27da-0310-9561-8e5933582275>
2004-10-14 13:46:44 +0000

    subsribe handler
    git-svn-id: svn://svn.zope.org/repos/main@28181 62d5b8a3-27da-0310-9561-8e5933582275

--- zversioning/trunk/src/versioning/__init__.py
+++ zversioning/trunk/src/versioning/__init__.py
@@ -1,1 +1,29 @@
-#make it a module
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""versioning package for zope3"""
+
+from versioning.interfaces import IVersionable
+from versioning.interfaces import IVersioned
+from versioning.interfaces import IHistoryStorage
+from versioning.interfaces import ICopyModifyMergeRepository
+from zope.app import zapi
+
+
+def registerVersionControl(event):
+    if IVersionable.providedBy(event.object):
+        history = zapi.getUtility(IHistoryStorage)
+        if history is not None and not IVersioned.providedBy(event.object):
+            rep = ICopyModifyMergeRepository(history)
+            get_transaction().commit()
+            rep.applyVersionControl(event.object)
