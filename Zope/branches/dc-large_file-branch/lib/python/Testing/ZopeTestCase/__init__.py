d4da5361909a91a863d51fa79b25f38af883e9b6
sidnei <sidnei@62d5b8a3-27da-0310-9561-8e5933582275>
2004-11-03 20:17:35 +0000

    Creating a branch for working on large file support for WebDAV
    git-svn-id: svn://svn.zope.org/repos/main@28334 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope/branches/dc-large_file-branch/lib/python/Testing/ZopeTestCase/__init__.py
@@ -0,0 +1,45 @@
+#
+# Names exported by the ZopeTestCase module
+#
+
+# $Id: __init__.py,v 1.13 2004/08/19 15:52:55 shh42 Exp $
+
+import ZopeLite as Zope
+import utils
+
+from ZopeLite import installProduct
+from ZopeLite import hasProduct
+from ZopeLite import _print
+
+from base import TestCase
+from base import app
+from base import close
+
+from ZopeTestCase import folder_name
+from ZopeTestCase import user_name
+from ZopeTestCase import user_password
+from ZopeTestCase import user_role
+from ZopeTestCase import standard_permissions
+from ZopeTestCase import ZopeTestCase
+
+from PortalTestCase import portal_name
+from PortalTestCase import PortalTestCase
+
+from profiler import Profiled
+from sandbox import Sandboxed
+from functional import Functional
+
+from unittest import main
+
+# Convenience class for functional unit testing
+class FunctionalTestCase(Functional, ZopeTestCase):
+    pass
+
+# b/w compatibility names
+_folder_name = folder_name
+_user_name = user_name
+_user_role = user_role
+_standard_permissions = standard_permissions
+_portal_name = portal_name
+from base import closeConnections
+
