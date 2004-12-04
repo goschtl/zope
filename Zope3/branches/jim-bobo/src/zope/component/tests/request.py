e752165b74dbdc135337156598bfd3bc16e846b4
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2004-12-04 18:41:32 +0000

    Branch to work out ideas for a new Bobo
    git-svn-id: svn://svn.zope.org/repos/main@28562 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/branches/jim-bobo/src/zope/component/tests/request.py
@@ -0,0 +1,35 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Request for tests
+
+$Id$
+"""
+import zope.interface
+
+
+class Request(object):
+
+    def __init__(self, type, skin=None):
+        zope.interface.directlyProvides(self, type)
+        # BBB goes away in 3.3
+        if skin is not None:
+            import warnings
+            warnings.warn(
+                "The skin argument is deprecated for "
+                "zope.component.tests.request.Request and will go away in "
+                "ZopeX3 3.3. Use zope.publisher.browser.TestRequest if "
+                "you need to test skins.",
+                DeprecationWarning)
+            zope.interface.directlyProvides(self, skin)
+        self._skin = skin
