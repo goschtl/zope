9366d751455c0b6c4efe0841a0cf051fbdaf9244
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2005-01-14 21:55:23 +0000

    Remove bbb package, since I am taking a different approach to backward-compatibility now.
    git-svn-id: svn://svn.zope.org/repos/main@28837 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/branches/srichter-blow-services/src/zope/app/tests/__init__.py
+++ Zope3/branches/srichter-blow-services/src/zope/app/tests/__init__.py
@@ -3,17 +3,27 @@
 
 ##############################################################################
 # BBB: backward-comptibility; 12/18/2004
+
 import sys
-#from zope.app.testing import placelesssetup
-#sys.modules['zope.app.tests.placelesssetup'] = placelesssetup
-#from zope.app.testing import setup
-#sys.modules['zope.app.tests.setup'] = setup
-#from zope.app.testing import dochttp
-#sys.modules['zope.app.tests.dochttp'] = dochttp
-#from zope.app.testing import functional
-#sys.modules['zope.app.tests.functional'] = functional
-#from zope.app.testing import test
-#sys.modules['zope.app.tests.test'] = test
-#from zope.app.testing import ztapi
-#sys.modules['zope.app.tests.ztapi'] = ztapi
+from zope.deprecation.deprecation import DeprecatedModule 
+import zope.app
+
+def deprecate(module):
+    return DeprecatedModule(module,
+                            'Test setup code moved from zope.app.tests to '
+                            'zope.app.testing. This will go away in Zope 3.3.')
+
+
+from zope.app.testing import placelesssetup
+sys.modules['zope.app.tests.placelesssetup'] = deprecate(placelesssetup)
+from zope.app.testing import setup
+sys.modules['zope.app.tests.setup'] = deprecate(setup)
+from zope.app.testing import dochttp
+sys.modules['zope.app.tests.dochttp'] = deprecate(dochttp)
+from zope.app.testing import functional
+sys.modules['zope.app.tests.functional'] = deprecate(functional)
+from zope.app.testing import test
+sys.modules['zope.app.tests.test'] = deprecate(test)
+from zope.app.testing import ztapi
+sys.modules['zope.app.tests.ztapi'] = deprecate(ztapi)
 #############################################################################
