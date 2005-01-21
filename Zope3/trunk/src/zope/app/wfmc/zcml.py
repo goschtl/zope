60c030c52320f2b9a6105ced152dce2ce88f8279
benji_york <benji_york@62d5b8a3-27da-0310-9561-8e5933582275>
2005-01-21 20:08:52 +0000

    Remove wronge license header.
    git-svn-id: svn://svn.zope.org/repos/main@28907 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/wfmc/zcml.py
+++ Zope3/trunk/src/zope/app/wfmc/zcml.py
@@ -1,17 +1,3 @@
-##############################################################################
-#
-# Copyright (c) 2003 Zope Corporation. All Rights Reserved.
-#
-# This software is subject to the provisions of the Zope Visible Source
-# License, Version 1.0 (ZVSL).  A copy of the ZVSL should accompany this
-# distribution.
-#
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE
-#
-##############################################################################
 """ZCML directives for defining privileges.
 
 $Id: $
