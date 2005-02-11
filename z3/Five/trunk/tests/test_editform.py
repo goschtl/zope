3a687f42dfee76b296fb5c78942427648693822e
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2005-02-11 18:17:33 +0000

    Basic test of edit form functionality.
    git-svn-id: svn://svn.zope.org/repos/main@50932 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3/Five/trunk/tests/test_editform.py
@@ -0,0 +1,44 @@
+import os, sys
+import glob
+
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+import unittest
+from Testing import ZopeTestCase
+from Testing.ZopeTestCase.functional import Functional
+from AccessControl import Unauthorized
+
+# we need to install FiveTest *before* Five as Five processes zcml
+# in all the products it can find.
+ZopeTestCase.installProduct('FiveTest')
+ZopeTestCase.installProduct('Five')
+
+from zope.app.form.browser.submit import Update
+
+class EditFormTestCase(Functional, ZopeTestCase.ZopeTestCase):
+    def afterSetUp(self):
+        self.folder.manage_addProduct['FiveTest'].manage_addFieldSimpleContent(
+            'edittest', 'Test')
+        uf = self.folder.acl_users
+        uf._doAddUser('viewer', 'secret', [], [])
+        uf._doAddUser('manager', 'r00t', ['Manager'], [])
+        
+    def test_editform(self):
+        response = self.publish('/test_folder_1_/edittest/edit.html',
+                                basic='manager:r00t')
+        # we're using a GET request to post variables, but seems to be
+        # the easiest..
+        response = self.publish(
+            '/test_folder_1_/edittest/edit.html?%s=1&field.title=FooTitle&field.description=FooDescription' % Update,
+            basic='manager:r00t')
+        self.assertEquals('FooTitle', self.folder.edittest.title)
+        self.assertEquals('FooDescription', self.folder.edittest.description)
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(EditFormTestCase))
+    return suite
+
+if __name__ == '__main__':
+    framework()
