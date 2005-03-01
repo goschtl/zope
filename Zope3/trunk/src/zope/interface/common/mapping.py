c4325355ba68c42cdac4190aa8f274e27d3d6caf
poster <poster@62d5b8a3-27da-0310-9561-8e5933582275>
2005-03-01 15:45:04 +0000

    Specify more (all?) of the standard mapping interface, taking care not to change the contracts of the already-defined interfaces.
    git-svn-id: svn://svn.zope.org/repos/main@29359 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/interface/common/mapping.py
+++ Zope3/trunk/src/zope/interface/common/mapping.py
@@ -76,6 +76,52 @@ class IEnumerableMapping(IReadMapping):
         """Return the number of items.
         """
 
+class IMapping(IWriteMapping, IEnumerableMapping):
+    ''' Simple mapping interface '''
 
-class IMapping(IReadMapping, IWriteMapping, IEnumerableMapping):
-    ''' Full mapping interface '''
+class IIterableMapping(IEnumerableMapping):
+
+    def iterkeys():
+        "iterate over keys; equivalent to __iter__"
+
+    def itervalues():
+        "iterate over values"
+
+    def iteritems():
+        "iterate over items"
+
+class IClonableMapping(Interface):
+    
+    def copy():
+        "return copy of dict"
+
+class IExtendedReadMapping(IIterableMapping):
+    
+    def has_key(key):
+        """Tell if a key exists in the mapping; equivalent to __contains__"""
+
+class IExtendedWriteMapping(IWriteMapping):
+    
+    def clear():
+        "delete all items"
+    
+    def update(d):
+        " Update D from E: for k in E.keys(): D[k] = E[k]"
+    
+    def setdefault(key, default=None):
+        "D.setdefault(k[,d]) -> D.get(k,d), also set D[k]=d if k not in D"
+    
+    def pop(k, *args):
+        """remove specified key and return the corresponding value
+        *args may contain a single default value, or may not be supplied.
+        If key is not found, default is returned if given, otherwise 
+        KeyError is raised"""
+    
+    def popitem():
+        """remove and return some (key, value) pair as a
+        2-tuple; but raise KeyError if mapping is empty"""
+
+class IFullMapping(
+    IExtendedReadMapping, IExtendedWriteMapping, IClonableMapping, IMapping):
+    ''' Full mapping interface ''' # IMapping included so tests for IMapping
+    # succeed with IFullMapping
