13164263b6b62e21d3a8c1cb896ef2216d80dd26
poster <poster@62d5b8a3-27da-0310-9561-8e5933582275>
2005-03-07 18:17:24 +0000

    Add some useful sequence sub-interfaces.
    git-svn-id: svn://svn.zope.org/repos/main@29404 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/interface/common/sequence.py
+++ Zope3/trunk/src/zope/interface/common/sequence.py
@@ -17,14 +17,24 @@ $Id$
 """
 from zope import interface
 
-class IReadSequence(interface.Interface):
-    "read interface shared by tuple and list"
+class IMinimalSequence(interface.Interface):
     
     def __getitem__(index):
-        "x.__getitem__(index) <==> x[index]"
+        """x.__getitem__(index) <==> x[index]
+        
+        Declaring this interface does not specify whether __getitem__
+        supports slice objects."""
 
     def __iter__():
         "x.__iter__() <==> iter(x)"
+
+class IFiniteSequence(IMinimalSequence):
+
+    def __len__():
+        "x.__len__() <==> len(x)"
+
+class IReadSequence(IFiniteSequence):
+    "read interface shared by tuple and list"
 
     def __contains__(item):
         "x.__contains__(item) <==> item in x"
@@ -47,9 +57,6 @@ class IReadSequence(interface.Interface):
     def __ge__(other):
         "x.__ge__(other) <==> x>=other"
 
-    def __len__():
-        "x.__len__() <==> len(x)"
-
     def __add__(other):
         "x.__add__(other) <==> x+other"
 
@@ -63,6 +70,8 @@ class IReadSequence(interface.Interface):
         """x.__getslice__(i, j) <==> x[i:j]
 
         Use of negative indices is not supported.
+        
+        Deprecated since Python 2.0 but still a part of UserList.
         """
 
 class IExtendedReadSequence(IReadSequence):
@@ -80,20 +89,31 @@ class IUniqueMemberWriteSequence(interface.Interface):
     "The write contract for a sequence that may enforce unique members"
 
     def __setitem__(index, item):
-        "x.__setitem__(index, item) <==> x[index]=item"
+        """x.__setitem__(index, item) <==> x[index]=item
+        
+        Declaring this interface does not specify whether __setitem__
+        supports slice objects."""
 
     def __delitem__(index):
-        "x.__delitem__(index) <==> del x[index]"
+        """x.__delitem__(index) <==> del x[index]
+        
+        Declaring this interface does not specify whether __delitem__
+        supports slice objects."""
 
     def __setslice__(i, j, other):
         """x.__setslice__(i, j, other) <==> x[i:j]=other
 
-        Use  of negative indices is not supported."""
+        Use  of negative indices is not supported.
+        
+        Deprecated since Python 2.0 but still a part of UserList.
+        """
 
     def __delslice__(i, j):
         """x.__delslice__(i, j) <==> del x[i:j]
 
         Use of negative indices is not supported.
+        
+        Deprecated since Python 2.0 but still a part of UserList.
         """
     def __iadd__(y):
         "x.__iadd__(y) <==> x+=y"
