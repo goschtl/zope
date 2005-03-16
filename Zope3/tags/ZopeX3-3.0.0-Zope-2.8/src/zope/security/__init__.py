45e74bf3b2d7f7c4e84c119f5a7f20030be0c511
tseaver <tseaver@62d5b8a3-27da-0310-9561-8e5933582275>
2005-03-16 14:45:06 +0000

    Create tag for cut-down ZopeX3 3.0.0 as used in Zope 2.8.
    git-svn-id: svn://svn.zope.org/repos/main@29499 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/tags/ZopeX3-3.0.0-Zope-2.8/src/zope/security/__init__.py
@@ -0,0 +1,22 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Security Package
+
+$Id$
+"""
+# TODO: There's a circular import problem with the proxy package.
+# The proxy framework needs some refactoring, but not today.
+import zope.proxy
+
+from zope.security.management import checkPermission
