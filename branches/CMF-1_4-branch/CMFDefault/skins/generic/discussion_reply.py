e3de3782fba9b355cbd0ced2da5f95b0c3fc7a36
sidnei <sidnei@62d5b8a3-27da-0310-9561-8e5933582275>
2005-03-17 16:35:49 +0000

    Use getToolByName
    git-svn-id: svn://svn.zope.org/repos/main@36820 62d5b8a3-27da-0310-9561-8e5933582275

--- branches/CMF-1_4-branch/CMFDefault/skins/generic/discussion_reply.py
+++ branches/CMF-1_4-branch/CMFDefault/skins/generic/discussion_reply.py
@@ -1,8 +1,9 @@
 ## Script (Python) "discussion_reply"
 ##parameters=title,text
 ##title=Reply to content
-
-Creator = context.portal_membership.getAuthenticatedMember().getId()
+from Products.CMFCore.utils import getToolByName
+pm = getToolByName(context, 'portal_membership')
+Creator = pm.getAuthenticatedMember().getId()
 replyID = context.createReply( title = title
                              , text = text
                              , Creator = Creator
