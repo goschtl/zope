b37333bb1276490a0e296b86c2768598ea5cff5e
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2005-04-21 20:29:02 +0000

    move the last remainders of test_security2 to Five.browser.tests.test_security. I still have a security issue to tackle, not all tests pass (but at least 24 out of 40 total browser tests)
    git-svn-id: svn://svn.zope.org/repos/main@52789 62d5b8a3-27da-0310-9561-8e5933582275

--- z3/Five/branch/philikon-restructuring/browser/tests/test_security.py
+++ z3/Five/branch/philikon-restructuring/browser/tests/test_security.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
     execfile(os.path.join(sys.path[0], 'framework.py'))
 
 import unittest
-from Testing.ZopeTestCase import ZopeTestCase, installProduct
+from Testing.ZopeTestCase import ZopeTestCase, FunctionalTestCase
+from Testing.ZopeTestCase import installProduct
 installProduct('Five')
 installProduct('PythonScripts')  # for RestrictedPythonTestCase
 
@@ -41,6 +42,8 @@ public_view_names = [
     'public_attribute_page',
     'public_template_page',
     'public_template_class_page']
+
+ViewManagementScreens = 'View management screens'
 
 class SecurityTest(RestrictedPythonTestCase):
 
@@ -111,10 +114,64 @@ class PageSecurityTest(ZopeTestCase):
         self.failIf(view_roles == ())
         self.assertEquals(view_roles, ('Manager',))
 
+class PublishSecurityTest(FunctionalTestCase):
+    """A functional test for security actually involving the publisher.
+    """
+    def afterSetUp(self):
+	zcml.load_config('pages.zcml', package=Products.Five.browser.tests)
+	manage_addSimpleContent(self.folder, 'testoid', 'Testoid')
+        uf = self.folder.acl_users
+        uf._doAddUser('viewer', 'secret', [], [])
+        uf._doAddUser('manager', 'r00t', ['Manager'], [])
+
+    def test_no_permission(self):
+        for view_name in view_names:
+            response = self.publish('/test_folder_1_/testoid/%s' % view_name,
+                                    basic='viewer:secret')
+            # we expect that we get a 401 Unauthorized
+            self.assertEqual(response.getStatus(), 401)
+
+    def test_all_permissions(self):
+        permissions = self.folder.possible_permissions()
+        self.folder._addRole('Viewer')
+        self.folder.manage_role('Viewer', permissions)
+        self.folder.manage_addLocalRoles('viewer', ['Viewer'])
+        
+        for view_name in view_names:
+            response = self.publish('/test_folder_1_/testoid/%s' % view_name,
+                                    basic='viewer:secret')
+            self.assertEqual(response.getStatus(), 200)
+
+    def test_almost_all_permissions(self):
+        permissions = self.folder.possible_permissions()
+        permissions.remove(ViewManagementScreens)
+        self.folder._addRole('Viewer')
+        self.folder.manage_role('Viewer', permissions)
+        self.folder.manage_addLocalRoles('viewer', ['Viewer'])
+        
+        for view_name in view_names:
+            response = self.publish('/test_folder_1_/testoid/%s' % view_name,
+                                    basic='viewer:secret')
+            # we expect that we get a 401 Unauthorized
+            self.assertEqual(response.getStatus(), 401)
+
+    def test_manager_permission(self):
+        for view_name in view_names:
+            response = self.publish('/test_folder_1_/testoid/%s' % view_name,
+                                    basic='manager:r00t')
+            # we expect that we get a 200 Ok
+            self.assertEqual(response.getStatus(), 200)
+
+    def test_public_permission(self):
+        for view_name in public_view_names:
+            response = self.publish('/test_folder_1_/testoid/%s' % view_name)
+            self.assertEqual(response.getStatus(), 200)
+
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(SecurityTest))
     suite.addTest(unittest.makeSuite(PageSecurityTest))
+    suite.addTest(unittest.makeSuite(PublishSecurityTest))
     return suite
 
 if __name__ == '__main__':
