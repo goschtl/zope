33f04f5cbd5697cf1771c22c6e2aedf26372ce83
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2005-04-28 14:26:46 +0000

    get rid of terribly old, now unnecessary branch
    git-svn-id: svn://svn.zope.org/repos/main@53378 62d5b8a3-27da-0310-9561-8e5933582275

--- z3/Five/branch/philikon-functionaldoctests/tests/zopeconf.py
+++
@@ -1,7 +0,0 @@
-import os
-from os.path import join, abspath, dirname
-
-def products():
-    prefix = abspath(dirname(__file__))
-    import Products
-    Products.__path__.append(join(prefix, 'products'))
