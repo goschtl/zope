312fa2754593cf7501b36a7d3563c753f93503f1
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2005-05-09 17:07:09 +0000

    Added pluggable workflow-integration component. Now, when specifying a workflow definition, you must also specify an object that the definition calls to get application-defined participant and work-item components.
    git-svn-id: svn://svn.zope.org/repos/main@30314 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/wfmc/metaconfigure.py
+++ Zope3/trunk/src/zope/app/wfmc/metaconfigure.py
@@ -11,18 +11,53 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""WFMC metaconfigure
+"""ZCML directives for loading XPDL workflow definitions.
 
 $Id: $
 """
 __docformat__ = "reStructuredText"
+
+import zope.interface
+import zope.schema
+import zope.configuration.fields
 from zope import wfmc
 from zope.wfmc import xpdl
 from zope.app.component.metaconfigure import utility
+import zope.wfmc.interfaces
 
-def defineXpdl(_context, file, process, id):
+class IdefineXpdl(zope.interface.Interface):
+
+    file = zope.configuration.fields.Path(
+        title=u"File Name",
+        description=u"The name of the xpdl file to read.",
+        )
+
+    process = zope.schema.TextLine(
+        title=u"Process Name",
+        description=u"The name of the process to read.",
+        )
+
+    id = zope.schema.Id(
+        title=u"ID",
+        description=(u"The identifier to use for the process.  "
+                     u"Defaults to the process name."),
+        )
+
+    integration = zope.configuration.fields.GlobalObject(
+        title=u"Integration component",
+        description=(u"Python name of the integration object.  This"
+                      " must identify an object in a module using the"
+                      " full dotted name."),
+        )
+
+def defineXpdl(_context, file, process, id, integration):
     package = xpdl.read(open(file))
     definition = package[process]
     definition.id = id
+
+    if not zope.wfmc.interfaces.IIntegration.providedBy(integration):
+        raise TypeError("Not an IIntegration", integration)
+    
+    definition.integration = integration
 
     utility(_context, wfmc.interfaces.IProcessDefinition, definition, name=id)
