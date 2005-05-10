ca4d5ff29d87c4e75067e5ce65115af734e69989
fdrake <fdrake@62d5b8a3-27da-0310-9561-8e5933582275>
2005-05-10 07:29:43 +0000

    add a way to store some Dublin Core fields on content objects
    git-svn-id: svn://svn.zope.org/repos/main@30318 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/dublincore/annotatableadapter.py
+++ Zope3/trunk/src/zope/app/dublincore/annotatableadapter.py
@@ -21,6 +21,7 @@ from persistent.dict import PersistentDict
 
 from zope.app.annotation.interfaces import IAnnotations, IAnnotatable
 from zope.app.dublincore.zopedublincore import ZopeDublinCore
+from zope.app.dublincore.zopedublincore import DateProperty, ScalarProperty
 from zope.app.location import Location
 
 
@@ -48,8 +49,6 @@ class ZDCAnnotatableAdapter(ZopeDublinCore, Location):
             self.annotations[DCkey] = self._mapping
             self.annotations = None
 
-__doc__ = ZDCAnnotatableAdapter.__doc__ + __doc__
-
 
 class ZDCAnnotationData(PersistentDict):
     """Data for a Dublin Core annotation.
@@ -58,3 +57,64 @@ class ZDCAnnotationData(PersistentDict):
     serialization to be registered.  See the
     zope.app.dublincore.fssync package.
     """
+
+
+# Hybrid adapters.
+#
+# Adapter factories created using this support the Dublin Core using a
+# mixture of annotations and data on the context object.
+
+def partialAnnotatableAdapterFactory(direct_fields):
+    if not direct_fields:
+        raise ValueError("only use partialAnnotatableAdapterFactory()"
+                         " if at least one DC field is implemented directly")
+    fieldmap = {}
+    try:
+        # is direct_fields a sequence or a mapping?
+        direct_fields[0]
+    except KeyError:
+        # direct_fields: { dc_name: attribute_name }
+        fieldmap.update(direct_fields)
+    else:
+        for attrname in direct_fields:
+            fieldmap[attrname] = attrname
+
+    class ZDCPartialAnnotatableAdapter(ZDCAnnotatableAdapter):
+
+        def __init__(self, context):
+            self.__context = context
+            # can't use super() since this isn't a globally available class
+            ZDCAnnotatableAdapter.__init__(self, context)
+
+    for dcname, attrname in fieldmap.iteritems():
+        oldprop = ZopeDublinCore.__dict__.get(dcname)
+        if oldprop is None:
+            raise ValueError("%r is not a valid DC field" % dcname)
+        if (isinstance(oldprop, DateProperty)
+            or not isinstance(oldprop, ScalarProperty)):
+            raise ValueError("%r is not a supported DC field" % dcname)
+        prop = DirectProperty(dcname, attrname)
+        setattr(ZDCPartialAnnotatableAdapter, dcname, prop)
+
+    return ZDCPartialAnnotatableAdapter
+
+
+class DirectProperty(object):
+
+    def __init__(self, name, attrname):
+        self.__name__ = name
+        self.__attrname = attrname
+
+    def __get__(self, inst, klass):
+        if inst is None:
+            return self
+        context = inst._ZDCPartialAnnotatableAdapter__context
+        return getattr(context, self.__attrname, u"")
+
+    def __set__(self, inst, value):
+        if not isinstance(value, unicode):
+            raise TypeError("Element must be unicode")
+        context = inst._ZDCPartialAnnotatableAdapter__context
+        oldvalue = getattr(context, self.__attrname, None)
+        if oldvalue != value:
+            setattr(context, self.__attrname, value)
