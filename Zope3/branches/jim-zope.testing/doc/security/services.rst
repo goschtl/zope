4e37ba0ec1049464d3df6c27953a171f6233962c
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2005-05-16 17:59:57 +0000

    Removing branch created in a failed attempt to convert zope.testing to an external without creating an svn revision in which the trunk was broken.
    git-svn-id: svn://svn.zope.org/repos/main@30375 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/branches/jim-zope.testing/doc/security/services.rst
+++
@@ -1,7 +0,0 @@
-This document is to describe services that are related to security.
-
-Cookie sessions service
-  HMAC, expiry time, session data objects
-
-Authentication service
-
--- Zope3/branches/jim-zope.testing/src/optionstorage/browser/widgets.py
+++
@@ -1,9 +0,0 @@
-from zope.app.form.browser import DropdownWidget
-
-class OptionStorageVocabularyWidget(DropdownWidget):
-
-    def _getDefault(self):
-        default = self.vocabulary.getDefaultKey()
-        if not default:
-            default = super(OptionStorageVocabularyWidget, self)._getDefault()
-        return default
