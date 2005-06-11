b66b7c94f104a0692da4804154b34728e4211d13
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2005-06-11 12:50:25 +0000

    merge revision 11433 through HEAD from philikon-i18n to this new branch based on the trunk.
    Lennart, please delete the philikon-i18n branch once you don't need it
    anymore
    
    
    git-svn-id: svn://svn.zope.org/repos/main@55082 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3/Five/branch/philikon-i18n-2/i18n.py
@@ -0,0 +1,105 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Mimick Zope3 i18n machinery for Zope 2
+
+$Id$
+"""
+from zope.i18n import interpolate
+from zope.i18n.interfaces import ITranslationDomain
+from zope.i18nmessageid import MessageID
+from zope.app import zapi
+from zope.publisher.browser import BrowserLanguages
+
+from Products.PageTemplates import GlobalTranslationService as GTS
+
+class FiveTranslationService:
+    """Translation service that delegates to ``zope.i18n`` machinery.
+    """
+    # this is mostly a copy of zope.i18n.translate, with modifications
+    # regarding fallback and Zope 2 compatability
+    def translate(self, domain, msgid, mapping=None,
+                  context=None, target_language=None, default=None):
+        if isinstance(msgid, MessageID):
+            domain = msgid.domain
+            default = msgid.default
+            mapping = msgid.mapping
+
+        util = zapi.queryUtility(ITranslationDomain, domain)
+
+        if util is None:
+            # fallback to translation service that was registered,
+            # DummyTranslationService the worst
+            ts = _fallback_translation_service
+            return ts.translate(domain, msgid, mapping=mapping, context=context,
+                                target_language=target_language, default=default)
+
+        # in Zope3, context is adapted to IUserPreferredLanguages,
+        # which means context should be the request in this case.
+        if context is not None:
+            context = context.REQUEST
+        return util.translate(msgid, mapping=mapping, context=context,
+                              target_language=target_language, default=default)
+
+class FiveBrowserLanguages(BrowserLanguages):
+
+    def getPreferredLanguages(self):
+        language_list = super(FiveBrowserLanguages, self).getPreferredLanguages()
+
+        # Support for getting a user selected languages
+        # 1. From Localizer
+        selected_language = self.request.cookies.get('LOCALIZER_LANGUAGE', None)
+
+        if selected_language is None:
+            # 2. From PlacelessTranslationService. 
+            # XXX This is untested, as I don't have  PTS, 
+            # and is based on reading the source. 
+            selected_language = self.request.cookies.get('pts_language', None)
+            
+        if selected_language: 
+            # Make sure the selected langauge is first in the list
+            if len(language_list) == 0:
+                return [selected_language]
+            if selected_language == language_list[0]:
+                # Already first
+                return langauge_list
+            if selected_language in language_list:
+                language_list.remove(selected_language)
+            language_list.insert(0, selected_language)
+            
+        return language_list 
+
+# these are needed for the monkey
+_fallback_translation_service = GTS.DummyTranslationService()
+fiveTranslationService = FiveTranslationService()
+
+def getGlobalTranslationService():
+    return fiveTranslationService
+
+def setGlobalTranslationService(newservice):
+    global _fallback_translation_service
+    oldservice, _fallback_translation_service = \
+        _fallback_translation_service, newservice
+    return oldservice
+
+def monkey():
+    # get the services that has been registered so far and plug in our
+    # new one
+    global _fallback_translation_service
+    _fallback_translation_service = \
+        GTS.setGlobalTranslationService(fiveTranslationService)
+
+    # now override the getter/setter so that noone else can mangle
+    # with it anymore
+    GTS.getGlobalTranslationService = getGlobalTranslationService
+    GTS.setGlobalTranslationService = setGlobalTranslationService
