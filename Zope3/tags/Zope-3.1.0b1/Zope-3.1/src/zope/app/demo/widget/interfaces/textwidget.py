a6088aa9310ff7ca76ac87aa6f71a387478f3791
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2005-08-01 21:38:41 +0000

    Tagging beta 1 for Zope 3.1.0 release.
    git-svn-id: svn://svn.zope.org/repos/main@37643 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/tags/Zope-3.1.0b1/Zope-3.1/src/zope/app/demo/widget/interfaces/textwidget.py
@@ -0,0 +1,82 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Demo widget implementation
+
+$Id$
+"""
+__docformat__ = 'restructuredtext'
+
+from zope.interface import Interface
+from zope.schema import TextLine
+
+from zope.app.i18n import ZopeMessageIDFactory as _
+from zope.app.demo.widget.interfaces import IDemoWidget
+
+
+class IReadDemoTextWidget(Interface):
+    """Widget read interface."""
+
+    readonly = TextLine(
+        title = _(u"readonly"),
+        description=_(u"zope.schema.TextLine field with readonly = True."),
+        required=False,
+        readonly=True,
+        default=u'readonly')
+
+
+class IWriteDemoTextWidget(Interface):
+    """Widget write interface."""
+
+    standard = TextLine(
+        title = _(u"standard"),
+        description=_(u"""zope.schema.TextLine field with only title and description."""),
+        )
+
+    required = TextLine(
+        title = _(u"required"),
+        description=_(u"zope.schema.TextLine field with required = True."),
+        required=True)
+
+    constraint = TextLine(
+        title = _(u"constraint"),
+        description=_(u"""zope.schema.TextLine field with """
+            """constraint = lambda x: x == u'constraint'."""),
+        constraint=lambda x: x == u'constraint')
+
+    default = TextLine(
+        title = _(u"default"),
+        description=_(u"""zope.schema.TextLine field with """
+            """default = u'default'."""),
+        default=u'default')
+
+    min_length = TextLine(
+        title = _(u"min_length"),
+        description=_(u"zope.schema.TextLine field with min_length = 5."),
+        min_length=5)
+
+    max_length = TextLine(
+        title = _(u"max_length"),
+        description=_(u"zope.schema.TextLine field with max_length = 10"),
+        max_length=10)
+
+    min_max = TextLine(
+        title = _(u"min_max"),
+        description=_(u"""zope.schema.TextLine field with min_lenght = 5 """
+            """and max_length = 10"""),
+        min_length=5,
+        max_length=10)
+
+
+class IDemoTextWidget(IDemoWidget, IReadDemoTextWidget, IWriteDemoTextWidget):
+    """Widget interface inherits read and write interfaces."""
