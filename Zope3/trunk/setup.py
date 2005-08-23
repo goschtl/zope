9cb1493c57ce403d76a286420c9dcf59fb66983c
fdrake <fdrake@62d5b8a3-27da-0310-9561-8e5933582275>
2005-08-23 22:14:15 +0000

    move to a more scalable way of using the zpkg support code to provide build support for Zope 3
    ZCML "slug" files now need to be included as part of the package and
    identified in the SETUP.cfg file; these will be installed into the
    instance skeleton as part of an installation and copied into the
    skeleton as part of a check during the build process
    
    distributions should be constructed from clean checkouts or exports
    
    
    git-svn-id: svn://svn.zope.org/repos/main@38056 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/setup.py
+++ Zope3/trunk/setup.py
@@ -13,19 +13,11 @@
 ##############################################################################
 
 import os
-import posixpath
-import sys
 
 import zpkgsetup.setup
 
 
 here = os.path.dirname(os.path.abspath(__file__))
-
-def join(*parts):
-    local_full_path = os.path.join(here, *parts)
-    relative_path = posixpath.join(*parts)
-    return local_full_path, relative_path
-
 
 context = zpkgsetup.setup.SetupContext(
     "Zope", "3.1.0a42", __file__)
@@ -33,22 +25,17 @@ context = zpkgsetup.setup.SetupContext(
 context.load_metadata(
     os.path.join(here, "releases", "Zope", "PUBLICATION.cfg"))
 
-#context.scan("Zope",               *join("releases", "Zope"))
-context.scan("BTrees",             *join("src", "BTrees"))
-context.scan("persistent",         *join("src", "persistent"))
-context.scan("ThreadedAsync",      *join("src", "ThreadedAsync"))
-context.scan("transaction",        *join("src", "transaction"))
-context.scan("ZConfig",            *join("src", "ZConfig"))
-context.scan("zdaemon",            *join("src", "zdaemon"))
-context.scan("ZEO",                *join("src", "ZEO"))
-context.scan("ZODB",               *join("src", "ZODB"))
-context.scan("zope",               *join("src", "zope"))
-context.scan("zope.hookable",      *join("src", "zope", "hookable"))
-context.scan("zope.i18nmessageid", *join("src", "zope", "i18nmessageid"))
-context.scan("zope.interface",     *join("src", "zope", "interface"))
-context.scan("zope.proxy",         *join("src", "zope", "proxy"))
-context.scan("zope.security",      *join("src", "zope", "security"))
-context.scan("zope.testing",       *join("src", "zope", "testing"))
-context.scan("zope.thread",        *join("src", "zope", "thread"))
-context.scan("zope.app.container", *join("src", "zope", "app", "container"))
+for root, dirs, files in os.walk("src"):
+    for d in dirs[:]:
+        # drop sub-directories that are not Python packages:
+        initfn = os.path.join(root, d, "__init__.py")
+        if not os.path.isfile(initfn):
+            dirs.remove(d)
+    if "SETUP.cfg" in files:
+        # scan this directory as a package:
+        pkgname = root[4:].replace(os.path.sep, ".")
+        local_full_path = os.path.join(here, root)
+        relative_path = root.replace(os.path.sep, "/")
+        context.scan(pkgname, local_full_path, relative_path)
+
 context.setup()
