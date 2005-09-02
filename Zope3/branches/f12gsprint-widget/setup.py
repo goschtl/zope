e32bc4874c0fbb60115d942923335c4f81e48dce
poster <poster@62d5b8a3-27da-0310-9561-8e5933582275>
2005-09-02 13:40:29 +0000

    Add branch for widget sprint work work
    git-svn-id: svn://svn.zope.org/repos/main@38243 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/branches/f12gsprint-widget/setup.py
@@ -0,0 +1,44 @@
+#############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import os
+import site
+import sys
+
+here = os.path.dirname(os.path.abspath(__file__))
+buildsupport = os.path.join(here, "buildsupport")
+
+# Process *.pth files from buildsupport/:
+last = len(sys.path)
+site.addsitedir(buildsupport)
+if len(sys.path) > last:
+    # Move all appended directories to the start
+    new = sys.path[last:]
+    del sys.path[last:]
+    sys.path[:0] = new
+
+import zpkgsetup.package
+import zpkgsetup.publication
+import zpkgsetup.setup
+
+
+context = zpkgsetup.setup.SetupContext(
+    "Zope", "3.1.0a42", __file__)
+
+context.load_metadata(
+    os.path.join(here, "releases", "Zope",
+                 zpkgsetup.publication.PUBLICATION_CONF))
+
+context.walk_packages("src")
+context.setup()
