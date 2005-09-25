82057212a2a409c53827267e3bf98c2de5da3e5f
yuppie <yuppie@62d5b8a3-27da-0310-9561-8e5933582275>
2005-09-25 21:05:15 +0000

    this branch is superseded by the yuppie-catalog_setup-redo branch
    git-svn-id: svn://svn.zope.org/repos/main@38630 62d5b8a3-27da-0310-9561-8e5933582275

--- CMF/branches/yuppie-catalog_setup/CMFDefault/skins/zpt_content/folder_edit_control.py
+++
@@ -1,9 +0,0 @@
-##parameters=title, description, **kw
-##
-from Products.CMFDefault.utils import MessageID as _
-
-if title!=context.title or description != context.description:
-    context.edit(title=title, description=description)
-    return context.setStatus(True, _('Folder changed.'))
-else:
-    return context.setStatus(False, _('Nothing to change.'))
