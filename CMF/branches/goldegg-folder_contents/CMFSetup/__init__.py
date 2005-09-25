32cff5fcbd3cf5b1bebde9e1d59523760037e44c
yuppie <yuppie@62d5b8a3-27da-0310-9561-8e5933582275>
2005-09-25 13:35:59 +0000

    branch for view experiments
    git-svn-id: svn://svn.zope.org/repos/main@38616 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ CMF/branches/goldegg-folder_contents/CMFSetup/__init__.py
@@ -0,0 +1,36 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" CMFSetup product initialization.
+
+$Id$
+"""
+
+from AccessControl import ModuleSecurityInfo
+
+from interfaces import BASE, EXTENSION
+from permissions import ManagePortal
+from registry import _profile_registry as profile_registry
+
+
+security = ModuleSecurityInfo( 'Products.CMFSetup' )
+security.declareProtected( ManagePortal, 'profile_registry' )
+
+def initialize( context ):
+
+    from Products.CMFCore.utils import ToolInit
+    from tool import SetupTool
+
+    ToolInit( 'CMF Setup Tool'
+            , tools=[ SetupTool ]
+            , icon='www/tool.png'
+            ).initialize( context )
