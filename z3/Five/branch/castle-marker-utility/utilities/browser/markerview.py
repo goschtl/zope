898cb264ccaa34022862539e369ac54f13723786
gotcha <gotcha@62d5b8a3-27da-0310-9561-8e5933582275>
2005-09-25 14:40:21 +0000

    calculate links to details view
    git-svn-id: svn://svn.zope.org/repos/main@59646 62d5b8a3-27da-0310-9561-8e5933582275

--- z3/Five/branch/castle-marker-utility/utilities/browser/markerview.py
+++ z3/Five/branch/castle-marker-utility/utilities/browser/markerview.py
@@ -9,12 +9,27 @@ class MarkerView(BrowserView):
     def __init__(self, context, view):
         BrowserView.__init__(self, context, view)
         self.utility = zapi.getUtility(IMarkerUtility)
+        self.context_url = self.context.absolute_url()
+
+    
+    def _getLinkToInterfaceDetailsView(interfaceName):
+        return self.context_url + 
+            '/view_details?iface=%s&type=zope.publisher.interfaces.browser.IBrowserRequest' % interfaceName
+
+    def _getNameLinkTuples(interfaceNames):
+        return [{'name':name,
+                 'link':self._getLinkToInterfaceDetailsView(name)}
+                for name in interfaceNames]
 
     def getAvailableInterfaceNames(self):
-        return self.utility.getAvailableInterfaceNames(self.context) 
+        return self._getNameLinkTuples(
+            self.utility.getAvailableInterfaceNames(self.context))
 
     def getDirectlyProvidedNames(self):
-        return self.utility.getDirectlyProvidedNames(self.context) 
+        return self._getNameLinkTuples(
+            self.utility.getDirectlyProvidedNames(self.context))
 
     def getProvidedNames(self):
-        return self.utility.getProvidedNames(self.context) 
+        return self._getNameLinkTuples(
+            self.utility.getProvidedNames(self.context))
+
