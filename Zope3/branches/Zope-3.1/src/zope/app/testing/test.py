dc01c1cf18a2b51ec236f4cf870c8d3c6fc89d7c
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2005-10-08 15:14:46 +0000

    --skip-functional did not work
    git-svn-id: svn://svn.zope.org/repos/main@38959 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/branches/Zope-3.1/src/zope/app/testing/test.py
+++ Zope3/branches/Zope-3.1/src/zope/app/testing/test.py
@@ -952,6 +952,7 @@ def process_args(argv=None):
                                     "pychecker", "debug", "pdebug",
                                     "gc-threshold=", "gc-option=",
                                     "loop",
+                                    "skip-functional",
                                     "test=", "module=",
                                     "profile", "progress", "refcount", "trace",
                                     "top-fifty", "verbose", "repeat=",
