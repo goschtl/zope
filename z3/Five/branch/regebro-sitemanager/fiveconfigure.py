434ec04bb7c56305d602aed2069b05b6d4868fa6
regebro <regebro@62d5b8a3-27da-0310-9561-8e5933582275>
2005-10-10 14:25:00 +0000

    Merging from the castle branch.
    git-svn-id: svn://svn.zope.org/repos/main@60129 62d5b8a3-27da-0310-9561-8e5933582275

--- z3/Five/branch/regebro-sitemanager/fiveconfigure.py
+++ z3/Five/branch/regebro-sitemanager/fiveconfigure.py
@@ -27,6 +27,7 @@ from zLOG import LOG, ERROR
 
 from zope.interface import classImplements
 from zope.configuration import xmlconfig
+from zope.configuration.exceptions import ConfigurationError
 from zope.app.component.interface import provideInterface
 
 from viewable import Viewable
@@ -55,7 +56,7 @@ def handleBrokenProduct(product):
     # in the control panel. However, all attempts to do so has failed from my 
     # side. //regebro
     exc = sys.exc_info()
-    LOG('Five', ERROR, 'Could not import Product %s' % product, error=exc)
+    LOG('Five', ERROR, 'Could not import Product %s' % product.__name__, error=exc)
 
 def loadProducts(_context):
     products = findProducts()
@@ -274,3 +275,62 @@ def cleanUp():
 from zope.testing.cleanup import addCleanUp
 addCleanUp(cleanUp)
 del addCleanUp
+
+from zope.interface.interface import InterfaceClass
+from zope.app.site.interfaces import IPossibleSite, ISite
+from zope.app.component.metaconfigure import adapter
+from zope.app.utility.interfaces import ILocalUtilityService
+from localsite import FiveSite, SimpleLocalUtilityService
+
+def classSiteHook(class_, site_class):
+    setattr(class_, 'getSiteManager',
+            site_class.getSiteManager.im_func)
+    setattr(class_, 'setSiteManager',
+            site_class.setSiteManager.im_func)
+ 
+count = 0
+def next():
+    global count
+    count += 1
+    return count
+
+def installSiteHook(_context, class_, site_class=None, utility_service=None):
+    if site_class is None:
+        if not IPossibleSite.implementedBy(class_):
+            # This is not a possible site, we need to monkey-patch it so that
+            # it is.
+            site_class = FiveSite
+    else:
+        if not IPossibleSite.implementedBy(site_class):
+            raise ConfigurationError('Site class does not implement '
+                                     'IPossibleClass: %s' % site_class)
+    if site_class is not None:
+        _context.action(
+            discriminator = (class_,),
+            callable = classSiteHook,
+            args=(class_, site_class)
+            )
+        _context.action(
+            discriminator = (class_, ISite),
+            callable = classImplements,
+            args=(class_, ISite)
+            )
+    if utility_service is None:
+        utility_service = SimpleLocalUtilityService
+    else:
+        if not ILocalUtilityService.implementedBy(utility_service):
+            raise ConfigurationError('utility_service does not implement '
+                                     'ILocalUtilityService: %s' % utility_service)
+        
+    # Generate a marker interface that should be unique, so that
+    # we can register the utility service only for this class.
+    iface = InterfaceClass('IFiveSite%s' % next())
+    adapter(_context, factory=(utility_service,),
+            provides=ILocalUtilityService,
+            for_=(iface,))
+    _context.action(
+        discriminator = (class_, 'UtilityMarker'),
+        callable = classImplements,
+        args=(class_, iface)
+        )
+
