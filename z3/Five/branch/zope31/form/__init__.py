4ccc9bb0248be570b01cda1b77d7035ffdf8ab65
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2005-10-24 12:23:35 +0000

    use new-style i18n Messages available in Zope 3.1 and required in Zope 3.2
    git-svn-id: svn://svn.zope.org/repos/main@60655 62d5b8a3-27da-0310-9561-8e5933582275

--- z3/Five/branch/zope31/form/__init__.py
+++ z3/Five/branch/zope31/form/__init__.py
@@ -34,7 +34,7 @@ from zope.app.form.interfaces import WidgetsError, MissingInputError
 from zope.app.form.utility import setUpWidgets, getWidgetsData
 from zope.app.form.interfaces import IInputWidget, WidgetsError
 from zope.app.event.objectevent import ObjectCreatedEvent, ObjectModifiedEvent
-from zope.app.i18n import ZopeMessageIDFactory as _
+from zope.app.i18n import ZopeMessageFactory as _
 
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
