08d0a32095174582455a3f108f9778adbd68158e
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2005-10-24 12:44:15 +0000

    simplify this directive handler a great deal
    git-svn-id: svn://svn.zope.org/repos/main@60661 62d5b8a3-27da-0310-9561-8e5933582275

--- z3/Five/branch/zope31/metaconfigure.py
+++ z3/Five/branch/zope31/metaconfigure.py
@@ -15,46 +15,23 @@
 
 $Id$
 """
+from Products.Five.security import CheckerPublic, protectName
+from Globals import InitializeClass as initializeClass
 
-from security import CheckerPublic
-from security import protectName, initializeClass
-
-from zope.app.component.contentdirective import ContentDirective as\
+from zope.app.component.contentdirective import ContentDirective as \
      zope_app_ContentDirective
 
 class ContentDirective(zope_app_ContentDirective):
-
-    def require(self, _context,
-                permission=None, attributes=None, interface=None,
-                like_class=None, set_attributes=None, set_schema=None):
-        super(ContentDirective, self).require(
-            _context, permission,
-            attributes, interface, like_class,
-            set_attributes, set_schema)
-        if attributes:
-            self.__protectNames(attributes, permission)
         
     def __protectName(self, name, permission_id):
-        "Set a permission on a particular name."
         self.__context.action(
             discriminator = ('five:protectName', self.__class, name),
             callable = protectName,
             args = (self.__class, name, permission_id)
             )
-        
-    def __protectNames(self, names, permission_id):
-        "Set a permission on a bunch of names."
-        for name in names:
-            self.__protectName(name, permission_id)
-            
-    def allow(self, _context, attributes=None, interface=None):
-        """Like require, but with permission_id zope.Public"""
-        # XXX this is using CheckerPublic while z3 is using
-        # PublicPermission
-        return self.require(_context, CheckerPublic, attributes, interface)
 
     def __call__(self):
-        "Handle empty/simple declaration."
+        """Handle empty/simple declaration."""
         return self.__context.action(
             discriminator = ('five:initialize:class', self.__class),
             callable = initializeClass,
