060b6ecf6f7a2950f9abb4bec19d43793215c3ef
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2005-10-26 18:26:50 +0000

    Second Zope 3.2 port branch that will tackle the port of the local sites functionality and possibly Florent's event work (hope there isn't much to port there).
    git-svn-id: svn://svn.zope.org/repos/main@60823 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3/Five/branch/philikon-zope32-port-2/site/localsite.py
@@ -0,0 +1,121 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Local sites
+
+$Id$
+"""
+from zope.event import notify
+from zope.interface import directlyProvides, directlyProvidedBy
+from zope.interface import implements
+from zope.component import getGlobalServices
+from zope.component.interfaces import IServiceService
+from zope.component.exceptions import ComponentLookupError
+from zope.component.servicenames import Utilities
+
+from zope.app.site.interfaces import ISite, IPossibleSite
+from zope.app.publication.zopepublication import BeforeTraverseEvent
+
+from ExtensionClass import Base
+from Acquisition import aq_base, aq_inner, aq_parent
+from Products.SiteAccess.AccessRule import AccessRule
+from ZPublisher.BeforeTraverse import registerBeforeTraverse
+from ZPublisher.BeforeTraverse import unregisterBeforeTraverse
+
+from Products.Five.site.interfaces import IFiveUtilityService
+
+def serviceServiceAdapter(ob):
+    """An adapter * -> IServiceService.
+
+    This is registered in place of the one in Zope 3 so that we lookup
+    using acquisition instead of ILocation.
+    """
+    current = ob
+    while True:
+        if ISite.providedBy(current):
+            return current.getSiteManager()
+        current = aq_parent(aq_inner(current))
+        if current is None:
+            raise ComponentLookupError("Could not adapt %r to"
+                                       " IServiceService" % (ob, ))
+
+HOOK_NAME = '__local_site_hook__'
+
+class LocalSiteHook(Base):
+    def __call__(self, container, request):
+        notify(BeforeTraverseEvent(container, request))
+
+
+def enableLocalSiteHook(obj):
+    """Install __before_traverse__ hook for Local Site
+    """
+    # We want the original object, not stuff in between, and no acquisition
+    obj = aq_base(obj)
+    if not IPossibleSite.providedBy(obj):
+        raise TypeError, 'Must provide IPossibleSite'
+    hook = AccessRule(HOOK_NAME)
+    registerBeforeTraverse(obj, hook, HOOK_NAME, 1)
+
+    if not hasattr(obj, HOOK_NAME):
+        setattr(obj, HOOK_NAME, LocalSiteHook())
+
+    directlyProvides(obj, ISite, directlyProvidedBy(obj))
+
+def disableLocalSiteHook(obj):
+    """Remove __before_traverse__ hook for Local Site
+    """
+    # We want the original object, not stuff in between, and no acquisition
+    obj = aq_base(obj)
+    if not ISite.providedBy(obj):
+        raise TypeError, 'Must provide ISite'
+    unregisterBeforeTraverse(obj, HOOK_NAME)
+    if hasattr(obj, HOOK_NAME):
+        delattr(obj, HOOK_NAME)
+
+    directlyProvides(obj, directlyProvidedBy(obj) - ISite)
+
+class FiveSiteManager(object):
+    implements(IServiceService)
+
+    def __init__(self, context):
+        self.context = context
+
+    def getServiceDefinitions(self):
+        """Retrieve all Service Definitions
+
+        Should return a list of tuples (name, interface)
+        """
+        return getGlobalServices().getServiceDefinitions()
+
+    def getInterfaceFor(self, name):
+        """Retrieve the service interface for the given name
+        """
+        return getGlobalServices().getInterfaceFor(name)
+
+    def getService(self, name):
+        """Retrieve a service implementation
+
+        Raises ComponentLookupError if the service can't be found.
+        """
+        if name in (Utilities,):
+            return IFiveUtilityService(self.context)
+        return getGlobalServices().getService(name)
+
+class FiveSite:
+    implements(IPossibleSite)
+
+    def getSiteManager(self):
+        return FiveSiteManager(self)
+
+    def setSiteManager(self, sm):
+        raise NotImplementedError('This class has a fixed site manager')
