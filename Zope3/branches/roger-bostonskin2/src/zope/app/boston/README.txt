1b840589e59597da2e9d8963d69b4860dcd853fe
rogerineichen <rogerineichen@62d5b8a3-27da-0310-9561-8e5933582275>
2005-10-28 01:37:28 +0000

    Boston skin --> work in progress
    TODO: reimplement menu/menu-item
    Right now the menu implementation can't be used for 
    register submenus on menu items where get registred in 
    the view or page directive. Because the view or page directive
    don't register a menu where we can use for register submenus.
    
    The submenu implementation can only be used if we register
    menu and submenu out of the box in one complex directive.
    
    git-svn-id: svn://svn.zope.org/repos/main@39683 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/branches/roger-bostonskin2/src/zope/app/boston/README.txt
@@ -0,0 +1,40 @@
+===================
+The Boston ZMI Skin
+===================
+
+The Boston skin is a new UI for the Zope Management Interface called ZMI.
+Feel free to write comments, ideas and wishes to the zope3-dev mailinglist. 
+
+
+Requirements
+-------------
+
+  - Offer a concept for registering aditional javascripts
+
+  - Offer a concept for registering additional CSS style sheets
+
+  - Offer a concept for registering own boxes under the navigation
+  
+  - Works in newer browser on each plattform, especially linux, windows, mac
+
+  - Works in newest version of the following browsers: IE, firefox (gecko),
+    Safari (KHTML/KJS)
+
+
+Dont's
+------
+
+  - Must not work on older browsers, new technologies are fine but only if 
+    they support the plattform and browsers described above.
+
+  - Must not support accessibility requirments. Please let me know, if 
+    somebody needs this. If so, we can implement a seperate ZMI skin for 
+    supporting fully accessibility support.
+
+
+Miscellaneous
+-------------
+
+  Different ideas of the Boston skin where born on the IRC with Stephan
+  Richter.  It's usual to name a skin like the town where the skin was
+  born. We decide to name it Boston because Stephan lives there.
---
+++ Zope3/branches/roger-bostonskin2/src/zope/app/boston/__init__.py
@@ -0,0 +1,69 @@
+##############################################################################
+#
+# Copyright (c) 2005 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Boston ZMI
+
+$Id$
+"""
+
+from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.viewlet import viewlet
+from zope.viewlet.interfaces import IViewletManager
+
+
+class boston(IBrowserRequest):
+    """The `boston` layer."""
+
+class Boston(boston, IDefaultBrowserLayer):
+    """The `Boston` skin.
+
+    It is available via `++skin++zope.app.boston.Boston`
+    or via `++skin++Boston`.
+    """
+
+
+class IHead(IViewletManager):
+    """Head viewlet manager."""
+
+
+class ICSS(IViewletManager):
+    """CSS viewlet manager."""
+
+
+class IJavaScript(IViewletManager):
+    """JavaScript viewlet manager."""
+
+
+class IToolBar(IViewletManager):
+    """Toolbar viewlet manager."""
+
+
+class IToolBarTopMenu(IViewletManager):
+    """Toolbar viewlet manager."""
+
+
+class ILeft(IViewletManager):
+    """Left viewlet manager."""
+
+
+BostonSkinCSSViewlet = viewlet.CSSViewlet('skin.css', 'all')
+
+BostonWidgetCSSViewlet = viewlet.CSSViewlet('widget.css', 'all')
+
+BostonXMLTreeCSSViewlet = viewlet.CSSViewlet('xmltree.css', 'all')
+
+ToolBarCSSViewlet = viewlet.CSSViewlet('toolbar.css', 'all')
+
+
+BostonJavascriptViewlet = viewlet.JavaScriptViewlet('boston.js')
