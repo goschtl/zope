2d57f9c11e9a2f7d4b6e4aec1dd5d0bdfbd37500
yuppie <yuppie@62d5b8a3-27da-0310-9561-8e5933582275>
2005-11-07 16:36:02 +0000

    - added functional test that demonstrates the bug - fixed __browser_default__
    git-svn-id: svn://svn.zope.org/repos/main@61399 62d5b8a3-27da-0310-9561-8e5933582275

--- z3/Five/branch/yuppie-browser_default-fix/tests/test_viewable.py
+++ z3/Five/branch/yuppie-browser_default-fix/tests/test_viewable.py
@@ -80,8 +80,13 @@ def test_defaultView():
     """
 
 def test_suite():
-    from Testing.ZopeTestCase import ZopeDocTestSuite
-    return ZopeDocTestSuite()
+    from Products.Five.testing.doctest import DocTestSuite
+    from Testing.ZopeTestCase import FunctionalDocFileSuite
+    return unittest.TestSuite((
+            DocTestSuite(),
+            FunctionalDocFileSuite('viewable.txt',
+                                   package="Products.Five.tests",),
+            ))
 
 if __name__ == '__main__':
     framework()
