64dfd313886de0fe698909ba1306499ccd2fe7ff
yuppie <yuppie@62d5b8a3-27da-0310-9561-8e5933582275>
2005-11-13 19:09:36 +0000

    - deleted merged branches
    git-svn-id: svn://svn.zope.org/repos/main@40086 62d5b8a3-27da-0310-9561-8e5933582275

--- CMF/branches/yuppie-collector119and201-branch/CMFDefault/skins/zpt_control/folder_copy_control.py
+++
@@ -1,7 +0,0 @@
-##parameters=ids, **kw
-##title=Copy objects from a folder to the clipboard
-##
-context.manage_copyObjects(ids, context.REQUEST)
-
-return context.setStatus( True, 'Item%s copied.' %
-                                ( len(ids) != 1 and 's' or '' ) )
