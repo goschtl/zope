c091ab79c508916a858f77ec9c2f110e2fb3f29b
chrism <chrism@62d5b8a3-27da-0310-9561-8e5933582275>
2005-11-26 21:37:49 +0000

    Add fiveproduct fixture sources.
    git-svn-id: svn://svn.zope.org/repos/main@67352 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ trunk/tests/fixtures-src/fiveproduct/Products/fiveproduct/__init__.py
@@ -0,0 +1,4 @@
+# this is a product
+def initialize(context):
+    return 'fiveproduct initialized'
+
---
+++ trunk/tests/fixtures-src/fiveproduct/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'fiveproduct',
+    version = '0.1',
+    packages = find_packages(),
+    package_data = {'':['*.zcml']},
+    namespace_packages=['Products'],
+    entry_points = {'zope2.initialize':
+                    ['initialize=Products.fiveproduct:initialize']},
+    url = 'http://www.example.com/fiveproduct',
+    author = 'Joe Bloggs',
+    author_email = 'bloggs@example.com',
+    zip_safe = False,
+    )
