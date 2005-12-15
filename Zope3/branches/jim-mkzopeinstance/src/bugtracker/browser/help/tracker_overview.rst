15104c2952442581f4e8ada2c6ffad3af541f9cd
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2005-12-15 23:11:16 +0000

    Branch to refactor mkzopeinstance to support both twisted and ZServer
    git-svn-id: svn://svn.zope.org/repos/main@40798 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope3/branches/jim-mkzopeinstance/src/bugtracker/browser/help/tracker_overview.rst
@@ -0,0 +1,6 @@
+Tracker Overview
+================
+
+This page is the main user interface for the bug tracker. It allows you search
+by various criteria. If you specify a "Filter Text", only bugs containing this
+text in their title or description will be shown. 
---
+++ Zope3/branches/jim-mkzopeinstance/src/zope/app/server/controller.py
@@ -0,0 +1,47 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Top-level controller for 'zopectl'.
+"""
+
+import os
+
+import zdaemon.zdctl
+
+
+INSTANCE_HOME = os.path.dirname(
+    os.path.dirname(os.path.dirname(zdaemon.__file__)))
+
+
+class ZopectlCmd(zdaemon.zdctl.ZDCmd):
+
+    def do_debug(self, rest):
+        cmdline = "%s/bin/debugzope" % INSTANCE_HOME
+        os.system(cmdline)
+
+    def help_debug(self):
+        print "debug -- Initialize the Zope application, providing a"
+        print "         debugger object at an interactive Python prompt."
+
+    def do_run(self, arg):
+        cmdline = "%s/bin/scriptzope %s" % (INSTANCE_HOME, arg)
+        os.system(cmdline)
+
+    def help_run(self):
+        print "run <script> [args] -- run a Python script with the Zope "
+        print "                       environment set up.  The script has "
+        print "                       'root' exposed as the root container."
+
+
+def main(args=None, options=None, cmdclass=ZopectlCmd):
+    zdaemon.zdctl.main(args, options, cmdclass)
