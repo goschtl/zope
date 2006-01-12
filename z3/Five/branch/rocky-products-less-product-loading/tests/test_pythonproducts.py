1038e4554e11d63f0e858614557428e677bde0ee
rocky <rocky@62d5b8a3-27da-0310-9561-8e5933582275>
2006-01-12 20:06:25 +0000

    Added basic doctests for testing python product registration.  At the moment it is quite difficult to do full testing without depending on an actual third-party prdoucts.
    git-svn-id: svn://svn.zope.org/repos/main@63863 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3/Five/branch/rocky-products-less-product-loading/tests/test_pythonproducts.py
@@ -0,0 +1,63 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Python packages-as-products testing
+
+$Id$
+"""
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+def test_registerProduct():
+    """
+    Testing registerProduct
+
+      >>> from zope.app.testing.placelesssetup import setUp, tearDown
+      >>> setUp()
+      >>> import Products
+      >>> import Products.Five
+      >>> import Products.Five.tests
+      >>> import Products.Five.tests.testing
+      >>> from Products.Five.tests.testing import zope2module
+      >>> from Products.Five import pythonproducts
+    
+    Make sure registerProduct only handles appropriate types::
+    
+      >>> pythonproducts.register_python_product(None)
+      Traceback (most recent call last):
+      ...
+      TypeError: The package argument must either be an instance of basestring or types.ModuleType
+
+      >>> pythonproducts.register_python_product('Products.Five.tests.adapters')
+      Traceback (most recent call last):
+      ...
+      AttributeError: The module 'Products' requires a Zope 2 style initialize function
+    
+      >>> pythonproducts.register_python_product(zope2module)
+      Traceback (most recent call last):
+      ...
+      ValueError: Registering a python package currently only supports filesystem based pure python packages
+                  
+    
+    Clean up:
+
+      >>> tearDown()
+    """
+
+def test_suite():
+    from Testing.ZopeTestCase import ZopeDocTestSuite
+    return ZopeDocTestSuite()
+
+if __name__ == '__main__':
+    framework()
