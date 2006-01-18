80f41c3479759bca65277d8fc13b7a321f754d34
oestermeier <oestermeier@62d5b8a3-27da-0310-9561-8e5933582275>
2006-01-18 15:25:44 +0000

    Added some LivePage update options
    git-svn-id: svn://svn.zope.org/repos/main@41355 62d5b8a3-27da-0310-9561-8e5933582275

--- zope3org/trunk/src/zorg/ajax/javascript/livepage.js
+++ zope3org/trunk/src/zorg/ajax/javascript/livepage.js
@@ -3,34 +3,85 @@ var livePageClientId;       /* This variable must be defined in your HTML. */
 livepageScriptFragmentMatch = /<script.*?>((?:\n|.)*?)<\/script>/img;
 livepageScriptTags = /<script.*?>|<\/script>/img;
 
+livepageImageFragmentMatch = /<img.+src[ ]*=[ ]*\"(.*?)\"/img;
+livepageImageTag = /<img.+src[ ]*=[ ]*\"|\"(?:\n|.)*?\/>/
+
 function evalResponse(request) {
     var response = request.responseText;
-    
-    var lines = response.split(/\n/);
-    var cmdline = lines.shift();
-    var parameter = cmdline.split(" ");
-    var cmd = parameter.shift();
-    
-    switch(cmd) {
-        case 'update': {
-            var body = lines.join('\n');
-            
-            id = parameter[0];
-            
-            var scripts = body.match(livepageScriptFragmentMatch);
-            var script = scripts.join('');
-            script = script.replace(livepageScriptTags, '');
+    if (response) {
+        var lines = response.split(/\n/);
+        var cmdline = lines.shift();
+        var parameter = cmdline.split(" ");
+        var cmd = parameter.shift();
         
-            $(id).innerHTML = body;
-            eval(script);
-            return;
-            }
-        case 'javascript' : {
-            var expr = parameter.join(" ");
-            eval(expr);
-            return;
-            }
+        switch(cmd) {
+            case 'update': {
+                var body = lines.join('\n');
+                var id = parameter[0];
+                
+                var scripts = body.match(livepageScriptFragmentMatch);
+               
+                if (scripts) {
+                    var script = scripts.join('');
+                    script = script.replace(livepageScriptTags, '');
+                
+                    $(id).innerHTML = body;
+                    eval(script);
+                    }
+                else {
+                    $(id).innerHTML = body;
+                    }
+               
+                return;
+                }
+                
+            case 'update': {
+                var body = lines.join('\n');
+                var id = parameter[0];
+                
+                var scripts = body.match(livepageScriptFragmentMatch);
+               
+                if (scripts) {
+                    var script = scripts.join('');
+                    script = script.replace(livepageScriptTags, '');
+                
+                    $(id).innerHTML = body;
+                    eval(script);
+                    }
+                else {
+                    $(id).innerHTML = body;
+                    }
+               
+                return;
+                }
+                
+            
+            case 'update_map': {
+                var body = lines.join('\n');
+                var id = parameter[0];
+                $(id).src = parameter[1];
+                var map_id = parameter[2];
+                var map_name = parameter[3];
+                var map = $(map_id);
+                map.innerHTML = body;
+               
+                $(id).usemap = "#" + map_name;
+                return;
+                }
+                
+            case 'update_src': {
+                var id = parameter[0];
+                $(id).src = parameter[1];
+                return;
+                }
+                
+            case 'javascript' : {
+                var expr = parameter.join(" ");
+                eval(expr);
+                return;
+                }
         }
+    }
 }
 
 function checkOutput(outputNum) {
@@ -52,4 +103,14 @@ function startClient() {
 
 function idleClient() {
     $("livepage_updates").innerHTML = "idle";
+}
+
+function switchElements(a, b) {
+    for(i=0;i<$(b).childNodes.length;i++) {
+        var child = $(b).childNodes[i];
+        $(a).appendChild(child);
+        }
+        
+        
+    return true;
 }
