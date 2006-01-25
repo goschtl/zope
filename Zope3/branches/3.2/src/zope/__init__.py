5ae6ea9f8970c32266c01c5452b7cc4a3ba85835
sidnei <sidnei@62d5b8a3-27da-0310-9561-8e5933582275>
2006-01-25 17:36:18 +0000

    - Declare the zope top-level package as a namespace package if         pkg_resources is available. This will enable distribution of         zope.* packages as individual eggs.
    git-svn-id: svn://svn.zope.org/repos/main@41437 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/branches/3.2/src/zope/__init__.py
+++ Zope3/branches/3.2/src/zope/__init__.py
@@ -13,3 +13,10 @@
 ##############################################################################
 #
 # This file is necessary to make this directory a package.
+
+try:
+    # Declare this a namespace package if pkg_resources is available.
+    import pkg_resources
+    pkg_resources.declare_namespace('zope')
+except ImportError:
+    pass
