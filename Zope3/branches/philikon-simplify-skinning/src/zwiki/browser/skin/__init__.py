b2db9f1d30146fa4e417d0023976737e60eeceee
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2006-02-19 11:27:56 +0000

    new-style skin and layer setup. I think this is the last one. By the way, when can we move out zwiki, bugtracker, etc.?
    git-svn-id: svn://svn.zope.org/repos/main@41690 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/branches/philikon-simplify-skinning/src/zwiki/browser/skin/__init__.py
+++ Zope3/branches/philikon-simplify-skinning/src/zwiki/browser/skin/__init__.py
@@ -1,1 +1,25 @@
-# make this directory a package
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Wiki skin
+
+$Id$
+"""
+from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.app.rotterdam import Rotterdam
+
+class IWikiLayer(IBrowserRequest):
+    """Layer that we register wiki-specific views with"""
+
+class IWikiSkin(IWikiLayer, Rotterdam):
+    """Wiki skin"""
