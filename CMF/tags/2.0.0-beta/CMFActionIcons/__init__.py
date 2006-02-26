15ae176d1d8d1f830c95bb06c9d4cbad885e3496
jens <jens@62d5b8a3-27da-0310-9561-8e5933582275>
2006-02-26 09:03:55 +0000

    - tagging CMF 2.0.0-beta
    git-svn-id: svn://svn.zope.org/repos/main@65492 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ CMF/tags/2.0.0-beta/CMFActionIcons/__init__.py
@@ -0,0 +1,46 @@
+##############################################################################
+#
+# Copyright (c) 2003 Zope Corporation and Contributors. All Rights Reserved.
+# 
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+# 
+##############################################################################
+""" Product: CMFActionIcons
+
+Define tool for mapping CMF actions onto icons.
+
+$Id$
+"""
+
+from Products.CMFCore.DirectoryView import registerDirectory
+from Products.CMFCore.interfaces import ISiteRoot
+from Products.CMFCore.utils import ToolInit
+from Products.GenericSetup import EXTENSION
+from Products.GenericSetup import profile_registry
+
+import ActionIconsTool
+
+actionicons_globals = globals()
+
+registerDirectory( 'skins', actionicons_globals )
+
+def initialize( context ):
+
+    ToolInit( meta_type='CMF Action Icons Tool'
+            , tools=( ActionIconsTool.ActionIconsTool, )
+            , icon="tool.gif"
+            ).initialize( context )
+
+    profile_registry.registerProfile('actionicons',
+                                     'CMFActionIcons',
+                                     'Adds action icon tool / settings.',
+                                     'profiles/actionicons',
+                                     'CMFActionIcons',
+                                     EXTENSION,
+                                     for_=ISiteRoot,
+                                     )
---
+++ CMF/tags/2.0.0-beta/CMFDefault/skins/zpt_control/setRedirect.py
@@ -0,0 +1,29 @@
+##parameters=provider, action_path, **kw
+##
+from ZTUtils import make_query
+from Products.CMFCore.utils import getToolByName
+
+utool = getToolByName(script, 'portal_url')
+ptool = getToolByName(script, 'portal_properties')
+default_charset = ptool.getProperty('default_charset', None)
+portal_url = utool()
+
+
+try:
+    target = provider.getActionInfo(action_path)['url']
+except ValueError:
+    target = portal_url
+
+message = context.REQUEST.other.get('portal_status_message', '')
+if message:
+    if isinstance(message, unicode):
+        message = message.encode(default_charset)
+    kw['portal_status_message'] = message
+for k, v in kw.items():
+    if not v:
+        del kw[k]
+
+query = kw and ( '?%s' % make_query(kw) ) or ''
+context.REQUEST.RESPONSE.redirect( '%s%s' % (target, query) )
+
+return True
