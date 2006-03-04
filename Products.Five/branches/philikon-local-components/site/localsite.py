4a217e83ad9136a950e27c42bc19b47b6a35448e
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2006-03-04 23:39:55 +0000

    Missing imoprts
    git-svn-id: svn://svn.zope.org/repos/main@65804 62d5b8a3-27da-0310-9561-8e5933582275

--- Products.Five/branches/philikon-local-components/site/localsite.py
+++ Products.Five/branches/philikon-local-components/site/localsite.py
@@ -19,6 +19,7 @@ from zope.interface import implements
 from zope.component import getGlobalSiteManager
 from zope.component.exceptions import ComponentLookupError
 from zope.app.component.interfaces import ISite, IPossibleSite
+from Acquisition import aq_parent, aq_inner
 from Products.Five.site.interfaces import IFiveSiteManager, IFiveUtilityRegistry
 
 class FiveSiteManager(object):
