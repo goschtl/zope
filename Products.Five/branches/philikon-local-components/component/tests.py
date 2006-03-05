f41c7820489b4bd03cad68339057a06c09a19f2b
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2006-03-05 00:34:09 +0000

    fux big (SyntaxError)
    git-svn-id: svn://svn.zope.org/repos/main@65815 62d5b8a3-27da-0310-9561-8e5933582275

--- Products.Five/branches/philikon-local-components/component/tests.py
+++ Products.Five/branches/philikon-local-components/component/tests.py
@@ -22,8 +22,8 @@ __docformat__ = "reStructuredText"
 
 def test_suite():
     return unittest.TestSuite([
-        DocFileSuite('component.txt', package="Products.Five.component")        
-        DocFileSuite('zpt.txt', package="Products.Five.component")        
+        DocFileSuite('component.txt', package="Products.Five.component"),
+        DocFileSuite('zpt.txt', package="Products.Five.component")
         ])
 
 if __name__ == '__main__':
