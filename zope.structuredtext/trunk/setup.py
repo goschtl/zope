00bce52ac65ea87b52c10f7db2ee8831a910b593
tseaver <tseaver@62d5b8a3-27da-0310-9561-8e5933582275>
2006-03-13 23:26:46 +0000

    Clean up copies from the template.
    git-svn-id: svn://svn.zope.org/repos/main@65990 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.structuredtext/trunk/setup.py
+++ zope.structuredtext/trunk/setup.py
@@ -23,27 +23,19 @@ try:
 except ImportError, e:
     from distutils.core import setup, Extension
 
-setup(name='zope.XXX',
+setup(name='zope.structuredtext',
       version='1.0',
-      url='http://svn.zope.org/zope.XXX',
+      url='http://svn.zope.org/zope.structuredtext',
       license='ZPL 2.1',
       description='XXX',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       long_description='',
       
-      packages=['zope', 'zope.XXX'],
+      packages=['zope', 'zope.structuredtext'],
       package_dir = {'': os.path.join(os.path.dirname(__file__), 'src')},
 
-##       ext_modules=[Extension("zope.XXX._zope_XXX",
-##                              [os.path.join('src', 'zope', 'XXX',
-##                                            "_zope_XXX.c")
-##                               ]),
-##                    ],
-
       namespace_packages=['zope',],
-      tests_require = ['zope.testing'],
-      install_requires=['zope.deprecation'],
       include_package_data = True,
 
       zip_safe = False,
