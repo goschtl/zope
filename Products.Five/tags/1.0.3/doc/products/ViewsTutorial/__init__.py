a2335bd7b9ca06aee637f1db3957600b12721114
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2006-03-31 16:37:03 +0000

    Remove prematurely created tag
    git-svn-id: svn://svn.zope.org/repos/main@66272 62d5b8a3-27da-0310-9561-8e5933582275

--- Products.Five/tags/1.0.3/doc/products/ViewsTutorial/__init__.py
+++
@@ -1,9 +0,0 @@
-import democontent
-
-def initialize(context):
-
-    context.registerClass(
-        democontent.DemoContent,
-        constructors = (democontent.manage_addDemoContentForm,
-                        democontent.manage_addDemoContent),
-        )
--- Products.Five/tags/1.0.3/tests/products/__init__.py
+++
@@ -1,1 +0,0 @@
-# Five.tests.products
