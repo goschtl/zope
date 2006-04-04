70b7c791bc77ca1a22369a90785bdefb0ffb12b1
tseaver <tseaver@62d5b8a3-27da-0310-9561-8e5933582275>
2006-04-04 16:08:46 +0000

    Prepare historical release.
    git-svn-id: svn://svn.zope.org/repos/main@66401 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.exceptions/tags/3.2.0/setup.py
@@ -0,0 +1,49 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.i18nmessageid package
+
+$Id$
+"""
+
+import os
+
+try:
+    from setuptools import setup, Extension
+except ImportError, e:
+    from distutils.core import setup, Extension
+
+setup(name='zope.exceptions',
+      version='3.0',
+      url='http://svn.zope.org/zope.exceptions',
+      license='ZPL 2.1',
+      description='exceptions',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description="This package contains exception interfaces "
+                       "and implementations which are so general purpose "
+                       "that they don't belong in a Zope application-specific "
+                       "package.",
+      
+      packages=['zope', 'zope.exceptions'],
+      package_dir = {'': os.path.join(os.path.dirname(__file__), 'src')},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      install_requires=['zope.deprecation',
+                        'zope.interface',
+                       ],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
