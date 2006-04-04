55d60e62afd414d3c11732400ef01a2a0678ddfd
tseaver <tseaver@62d5b8a3-27da-0310-9561-8e5933582275>
2006-04-04 19:56:03 +0000

    Prep for historical releases.
    git-svn-id: svn://svn.zope.org/repos/main@66439 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.interface/tags/3.2.0/setup.py
@@ -0,0 +1,51 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.interface package
+
+$Id$
+"""
+
+import os
+
+try:
+    from setuptools import setup, Extension
+except ImportError, e:
+    from distutils.core import setup, Extension
+    
+setup(name='zope.interface',
+      version='3.0',
+
+      url='http://svn.zope.org/zope.interface',
+      license='ZPL 2.1',
+      description='Zope 3 Interface Infrastructure',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      
+      packages=["zope",
+                "zope.interface",
+                "zope.interface.common",
+                "zope.interface.tests",
+               ],
+      package_dir = {'': os.path.join(os.path.dirname(__file__), 'src')},
+      ext_package='zope.interface',
+      ext_modules=[Extension("_zope_interface_coptimizations",
+                             [os.path.join('src', 'zope', 'interface',
+                                           "_zope_interface_coptimizations.c")
+                              ]),
+                   ],
+
+      tests_require = ['zope.testing'],
+      include_package_data = True,
+      zip_safe = False,
+      )
