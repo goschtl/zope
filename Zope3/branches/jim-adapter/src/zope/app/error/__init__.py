7fce26eadd42f80e40d7e828583096229ad258f6
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2006-04-04 08:10:32 +0000

    Get rid of really old BBB
    git-svn-id: svn://svn.zope.org/repos/main@66369 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/branches/jim-adapter/src/zope/app/error/__init__.py
+++ Zope3/branches/jim-adapter/src/zope/app/error/__init__.py
@@ -1,14 +1,1 @@
 # Make directory a package.
-
-###############################################################################
-# BBB: 12/14/2004
-
-from error import RootErrorReportingUtility
-from error import ErrorReportingUtility
-from error import globalErrorReportingUtility
-
-RootErrorReportingService = RootErrorReportingUtility
-ErrorReportingService = ErrorReportingUtility
-globalErrorReportingService = globalErrorReportingUtility
-
-###############################################################################
