0f46c33ec08a7046f7b10d798adc79d1dc9946b3
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2006-04-05 14:14:13 +0000

    temporarily disable BBB for zope.app.traversing; given the current state of zope.location and zope.traversing, it creates circular import problems
    git-svn-id: svn://svn.zope.org/repos/main@66537 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/branches/jim-adapter/src/zope/app/__init__.py
+++ Zope3/branches/jim-adapter/src/zope/app/__init__.py
@@ -31,15 +31,19 @@ modules_aliases_35 = {
     'zope.app.size': 'zope.size',
     'zope.app.size.interfaces': 'zope.size.interfaces',
 
-    'zope.app.traversing': 'zope.traversing',
-    'zope.app.traversing.adapters': 'zope.traversing.adapters',
-    'zope.app.traversing.api': 'zope.traversing.api',
-    'zope.app.traversing.interfaces': 'zope.traversing.interfaces',
-    'zope.app.traversing.browser': 'zope.traversing.browser',
-    'zope.app.traversing.browser.absoluteurl':
-        'zope.traversing.browser.absoluteurl',
-    'zope.traversing.browser.interfaces':
-        'zope.traversing.browser.absoluteurl',
+#XXX disabled because of circular import problems; needs to be reenabled
+#
+#    'zope.app.traversing': 'zope.traversing',
+#    'zope.app.traversing.adapters': 'zope.traversing.adapters',
+#    'zope.app.traversing.api': 'zope.traversing.api',
+#    'zope.app.traversing.interfaces': 'zope.traversing.interfaces',
+#    'zope.app.traversing.browser': 'zope.traversing.browser',
+#    'zope.app.traversing.browser.absoluteurl':
+#        'zope.traversing.browser.absoluteurl',
+#    'zope.traversing.browser.interfaces':
+#        'zope.traversing.browser.absoluteurl',
+
+#TODO provide BBB for zope.app.location
     }
 
 msg_35 = "%s has been moved to %s.  This alias will be removed in Zope 3.5."
