d2a2dea6ddbc49a8438ed64f574242d439b0ff79
amorat <amorat@62d5b8a3-27da-0310-9561-8e5933582275>
2006-04-09 10:01:31 +0000

    some more skinning
    git-svn-id: svn://svn.zope.org/repos/main@66715 62d5b8a3-27da-0310-9561-8e5933582275

--- zf.zscp/trunk/src/zf/zscp/skin/browser/__init__.py
+++ zf.zscp/trunk/src/zf/zscp/skin/browser/__init__.py
@@ -1,9 +1,1 @@
 # Make a pacakge.
-
-
-
-from zope.app.basicskin.standardmacros import StandardMacros as BaseMacros
-
-class StandardMacros(BaseMacros):
-    macro_pages = ('skin_macros', 'view_macros', 'dialog_macros'
-                  , 'navigation_macros')
