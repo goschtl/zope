e9c914aae92b4f87142b75ba43612ed3fa8a7beb
jens <jens@62d5b8a3-27da-0310-9561-8e5933582275>
2006-04-09 20:10:02 +0000

    - branch has been fully merged
    git-svn-id: svn://svn.zope.org/repos/main@66748 62d5b8a3-27da-0310-9561-8e5933582275

--- CMF/branches/tseaver-viewification/CMFDefault/skins/zpt_control/validateMemberIds.py
+++
@@ -1,9 +0,0 @@
-##parameters=ids=(), **kw
-##
-from Products.CMFDefault.utils import MessageID as _
-
-if ids:
-    return context.setStatus(True)
-else:
-    return context.setStatus(False, _('Please select one or more members '
-                                      'first.'))
