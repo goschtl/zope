577466971b38aa4e4ef415b287ea428a67c990e8
tseaver <tseaver@62d5b8a3-27da-0310-9561-8e5933582275>
2006-04-15 00:55:23 +0000

    3.2.0.1:  suppress inclusion of setup.cfg in sdist builds.
    git-svn-id: svn://svn.zope.org/repos/main@66995 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.thread/tags/3.2.0/setup.py
+++ zope.thread/tags/3.2.0/setup.py
@@ -24,7 +24,7 @@ except ImportError, e:
     from distutils.core import setup, Extension
 
 setup(name='zope.thread',
-      version='3.2.0',
+      version='3.2.0.1',
       url='http://svn.zope.org/zope.thread/tags/3.2.0',
       license='ZPL 2.1',
       description='Zope3 Thread-Local Storage',
