7033312c33085e15cfffb6a00c000acb2a5f3e84
tseaver <tseaver@62d5b8a3-27da-0310-9561-8e5933582275>
2006-04-15 17:52:17 +0000

    'package_dir' must be a relative path.
    git-svn-id: svn://svn.zope.org/repos/main@67036 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.documenttemplate/trunk/setup.py
+++ zope.documenttemplate/trunk/setup.py
@@ -24,7 +24,7 @@ except ImportError, e:
     from distutils.core import setup, Extension
 
 setup(name='zope.documenttemplate',
-      version='1.0',
+      version='3.3-dev',
       url='http://svn.zope.org/zope.documenttemplate',
       license='ZPL 2.1',
       description='Zope3 Document Template Markup Language (DTML)',
@@ -35,7 +35,7 @@ setup(name='zope.documenttemplate',
                 'zope.documenttemplate',
                 'zope.documenttemplate.untrusted',
                ],
-      package_dir = {'': os.path.join(os.path.dirname(__file__), 'src')},
+      package_dir = {'': 'src'},
 
       namespace_packages=['zope',],
       tests_require = ['zope.testing'],
