41166859eb039c877d17bc138d21bbc14dfbda24
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2006-04-19 17:43:42 +0000

    new improt paths
    git-svn-id: svn://svn.zope.org/repos/main@67158 62d5b8a3-27da-0310-9561-8e5933582275

--- Products.Five/branches/philikon-local-components/traversable.py
+++ Products.Five/branches/philikon-local-components/traversable.py
@@ -21,11 +21,10 @@ from zope.component import getMultiAdapter, ComponentLookupError
 from zope.interface import implements, Interface
 from zope.publisher.interfaces import ILayer
 from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.publisher.browser import setDefaultSkin
 from zope.traversing.interfaces import ITraverser, ITraversable
 from zope.traversing.adapters import DefaultTraversable
 from zope.traversing.adapters import traversePathElement
-
-from zope.app.publication.browser import setDefaultSkin
 from zope.app.interface import queryType
 
 from AccessControl import getSecurityManager
