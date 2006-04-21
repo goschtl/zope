494d168a92668e2707e2db31a2b1bab50f093845
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2006-04-21 19:54:38 +0000

    Added rudimentary ftests.  Probably doesn't cover all edge cases, but it definitely shows that pages created this way are publishable, with security and all that.
    git-svn-id: svn://svn.zope.org/repos/main@67255 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.browserzcml2/trunk/src/zope/browserzcml2/ftests.py
@@ -0,0 +1,46 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Tests
+
+$Id$
+"""
+import unittest
+import zope.formlib.interfaces
+import zope.interface
+from zope.app.pagetemplate import ViewPageTemplateFile
+from zope.app.testing.functional import FunctionalDocFileSuite
+
+class MacGyverPage(zope.formlib.Page):
+    def __call__(self):
+        return u"I've got a Swiss Army knife"
+
+@zope.interface.implementer(zope.formlib.interfaces.IPage)
+def makeAMacGyverPage(context, request):
+    return MacGyverPage(context, request)
+
+class MacGyverTemplatePage(zope.formlib.Page):
+    __call__ = ViewPageTemplateFile('test.pt')
+
+class PhoenixPages(object):
+    macgyver = ViewPageTemplateFile('test.pt')
+    def pete(self):
+        return u'Peter Thornton'
+
+def test_suite():
+    return unittest.TestSuite([
+        FunctionalDocFileSuite('ftest.txt')
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
