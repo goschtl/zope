1c31c22115663545d9fb2e12458be87f0a5ddf9d
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2006-04-26 18:49:33 +0000

    Added missing attributes.
    git-svn-id: svn://svn.zope.org/repos/main@67626 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/branches/jim-adapter/src/zope/app/event/interfaces.py
+++ Zope3/branches/jim-adapter/src/zope/app/event/interfaces.py
@@ -5,3 +5,9 @@ zope.deprecation.moved(
     'zope.lifecycleevent.interfaces',
     "Zope 3.5",
     )
+
+import zope.deferredimport
+zope.deferredimport.deprecated(
+    "IObjectEvent has moved to zope.component.interfaces",
+    IObjectEvent = 'zope.component.interfaces:IObjectEvent',
+    )
--- Zope3/branches/jim-adapter/src/zope/app/event/objectevent.py
+++ Zope3/branches/jim-adapter/src/zope/app/event/objectevent.py
@@ -5,3 +5,15 @@ zope.deprecation.moved(
     'zope.lifecycleevent',
     "Zope 3.5",
     )
+
+import zope.deferredimport
+zope.deferredimport.deprecated(
+    "It has moved to zope.component.interfaces.  This reference will be "
+    "gone in Zope 3.5.",
+    ObjectEvent = 'zope.component.interfaces:ObjectEvent',
+    )
+zope.deferredimport.deprecated(
+    "It has moved to zope.component.event.  This reference will be gone "
+    "in Zope 3.5",
+    objectEventNotify = 'zope.component.event:objectEventNotify',
+    )
