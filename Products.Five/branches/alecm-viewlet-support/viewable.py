65ebe5d9a156a1d0c55b28dd41b9c3834b577421
alecm <alecm@62d5b8a3-27da-0310-9561-8e5933582275>
2006-05-01 03:43:53 +0000

    Branching for viewlet support.
    git-svn-id: svn://svn.zope.org/repos/main@67779 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Products.Five/branches/alecm-viewlet-support/viewable.py
@@ -0,0 +1,51 @@
+##############################################################################
+#
+# Copyright (c) 2004, 2005 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Machinery for making things viewable
+
+$Id$
+"""
+import inspect
+from zExceptions import NotFound
+from zope.component import ComponentLookupError
+from zope.interface import implements
+from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.app.zapi import getDefaultViewName
+
+from Products.Five.traversable import FakeRequest
+from Products.Five.interfaces import IBrowserDefault
+
+_marker = object
+
+import warnings
+import zope.deprecation
+
+class BrowserDefault(object):
+    implements(IBrowserDefault)
+
+    def __init__(self, context):
+        if zope.deprecation.__show__():
+            warnings.warn("The BrowserDefault class is no longer needed and "  \
+                          "will be removed in Zope 2.12. \n If you rely on "   \
+                          "it to get the default view, replace the call with " \
+                          "zope.app.publisher.browser.queryDefaultViewName",
+                          DeprecationWarning, 2)
+        self.context = context
+
+    def defaultView(self, request):
+        context = self.context
+        try:
+            name = getDefaultViewName(context, request)
+            return context, [name,]
+        except ComponentLookupError:
+            return context, None
