65828f909296a203f938912169f2013617190bfa
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2006-05-02 20:16:21 +0000

    officially deprecate stupid FakeRequet construct
    git-svn-id: svn://svn.zope.org/repos/main@67863 62d5b8a3-27da-0310-9561-8e5933582275

--- Products.Five/trunk/traversable.py
+++ Products.Five/trunk/traversable.py
@@ -13,26 +13,19 @@
 ##############################################################################
 """Machinery for making things traversable through adaptation
 
+BBB 2006/05/01 -- to be removed after 12 months
+
 $Id$
 """
-from zope.interface import implements
-from zope.publisher.interfaces.browser import IBrowserRequest
-
-class FakeRequest(dict):
-    implements(IBrowserRequest)
-
-    def has_key(self, key):
-        return False
-
-    def getURL(self):
-        return "http://codespeak.net/z3/five"
-
-# BBB 2006/05/01 -- to be removed after 12 months
 import zope.deferredimport
 zope.deferredimport.deprecated(
     "__bobo_traverse__ and ITraverser/ITraversable for controlling "
-    "URL traversal has become obsolete. Use an IPublishTraverse "
-    "adapter instead.",
+    "URL traversal have become obsolete. Use an IPublishTraverse "
+    "adapter instead.  This reference will go away in Zope 2.12.",
     Traversable = "Products.Five.bbb.Traversable",
     FiveTraversable = "zope.traversing.adapters.DefaultTraversable",
+    )
+zope.deferredimport.deprecated(
+    "Use zope.publisher.browser.TestRequest instead.",
+    FakeRequest = "zope.publisher.browser:TestRequest",
     )
