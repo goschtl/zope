1fd43d14c0afe1030daa78cc04748a18c4afafc3
dobe <dobe@62d5b8a3-27da-0310-9561-8e5933582275>
2006-05-10 15:19:22 +0000

    added keyreference implementation
    git-svn-id: svn://svn.zope.org/repos/main@68074 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.zalchemy/branches/jukart-SA-0.2/src/z3c/zalchemy/intid/tests.py
@@ -0,0 +1,39 @@
+import unittest
+import doctest
+from zope.testing.doctestunit import DocFileSuite
+from zope.app.testing import setup
+import os, tempfile
+import shutil
+from z3c.zalchemy.datamanager import AlchemyEngineUtility
+from zope import component
+import sqlalchemy
+from threading import local
+
+def setUp(test):
+    setup.placefulSetUp()
+    test.tmpDir = tempfile.mkdtemp()
+    dbFile = os.path.join(test.tmpDir,'z3c.zalchemy.test.db')
+    
+    engineUtil = AlchemyEngineUtility(
+        'database','sqlite:///%s' % dbFile)
+    component.provideUtility(engineUtil)
+    test.globs['engineUtil'] = engineUtil
+    
+
+def tearDown(test):
+    setup.placefulTearDown()
+    shutil.rmtree(test.tmpDir)
+
+
+def test_suite():
+    return unittest.TestSuite((
+        DocFileSuite('keyreference.txt',
+                     setUp=setUp, tearDown=tearDown,
+                     optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+                     ),
+        ))
+
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
