069daadf81b0816cffc5bef4e72acc88ac34be04
rocky <rocky@62d5b8a3-27da-0310-9561-8e5933582275>
2006-05-29 20:24:13 +0000

    Five now requires site.zcml to live in /etc
    git-svn-id: svn://svn.zope.org/repos/main@68359 62d5b8a3-27da-0310-9561-8e5933582275

--- Products.Five/trunk/zcml.py
+++ Products.Five/trunk/zcml.py
@@ -32,13 +32,14 @@ def load_site():
 
     # load instance site configuration file
     site_zcml = os.path.join(INSTANCE_HOME, "etc", "site.zcml")
-    if os.path.exists(site_zcml):
-        file = site_zcml
-    else:
-        file = os.path.join(os.path.dirname(__file__), "skel", "site.zcml")
+    if not os.path.exists(site_zcml):
+        raise IOError, "site.zcml is now required to live at '%s', for " \
+                       "sites upgraded from Zope 2.9 please copy site.zcml " \
+                       "from your installed Zope's skel/etc directory" \
+                       % site_zcml
 
     global _context
-    _context = xmlconfig.file(file)
+    _context = xmlconfig.file(site_zcml)
 
 
 def load_config(file, package=None, execute=True):
