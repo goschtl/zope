6816810f1ba1fa6e1a661acdbe429e1d79f42e17
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2006-06-05 21:07:12 +0000

    Updated to use localhost and to allow excluded addresses.
    git-svn-id: svn://svn.zope.org/repos/main@68498 62d5b8a3-27da-0310-9561-8e5933582275

--- zope-foundation-admin/trunk/initial-members/invite.py
+++ zope-foundation-admin/trunk/initial-members/invite.py
@@ -4,14 +4,24 @@ from email.MIMEText import MIMEText
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
-    [data] = args
+    data = args[0]
+    exclude = args[1:]
 
     server = smtplib.SMTP()
     from_addr = 'legal@zope.org'
     invitation = open('invitation.txt').read()
 
+    xlogin = set()
+    for f in exclude:
+        for record in open(f):
+            login, first, last, email = record.strip().split('\t')
+            xlogin.add(login)
+
     for record in open(data):
         login, first, last, email = record.strip().split('\t')
+        if login in xlogin:
+            continue
+
         if first == '(unknown)':
             if last == '(unknown)':
                 name = ''
@@ -34,9 +44,10 @@ def main(args=None):
         msg['From'] = 'Zope Foundation <legal@zope.org>'
         msg['Cc'] = 'Zope Foundation <legal@zope.org>'
         msg['To'] = to
+        print login
         server.connect()
         server.sendmail(from_addr, [email, from_addr], msg.as_string())
         server.close()
-
+        
 if __name__ == '__main__':
     main()
