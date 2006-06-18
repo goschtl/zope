cdac164b2ecd5d085fb44a294ae41f526b00388c
jens <jens@62d5b8a3-27da-0310-9561-8e5933582275>
2006-06-18 11:44:00 +0000

    - get rid of a deprecation warning under Zope > 2.8
    git-svn-id: svn://svn.zope.org/repos/main@68733 62d5b8a3-27da-0310-9561-8e5933582275

--- PluginRegistry/trunk/tests/test_exportimport.py
+++ PluginRegistry/trunk/tests/test_exportimport.py
@@ -33,7 +33,11 @@ else:
     from Products.GenericSetup.utils import _getDottedName
 
     from zope.interface import Interface
-    from zope.app.tests.placelesssetup import PlacelessSetup
+    try:
+        from zope.app.testing.placelesssetup import PlacelessSetup
+    except ImportError:
+        # BBB for Zope 2.8
+        from zope.app.tests.placelesssetup import PlacelessSetup
 
     class IFoo(Interface):
         pass
