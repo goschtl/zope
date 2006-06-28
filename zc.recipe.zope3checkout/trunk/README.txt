577b4d1d477b189459c3a7654089e1b546b5bc6d
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2006-06-28 23:09:40 +0000

    initial version
    git-svn-id: svn://svn.zope.org/repos/main@68896 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zc.recipe.zope3checkout/trunk/README.txt
@@ -0,0 +1,17 @@
+Zope 3 Checkout
+===============
+
+Recipe for creating a Zope 3 checkout in a buildout.
+
+Hopefully, when Zope is packages as eggs, this won't be necessary.
+
+The recipe has a single option, which is the Sunbersion URL to use to
+checkout Zope.  For example, to get the 3.3 branch, use:
+
+   url = svn://svn.zope.org/repos/main/branches/3.3
+
+The checkout is installed into a subdirectory of the buildout parts
+directory whos name is the part named used for the recipe.
+
+This location is recorded in a 'location' option within the section
+that other recipes can query to get the location.
---
+++ zc.recipe.zope3checkout/trunk/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+name = "zc.recipe.zope3checkout"
+
+setup(
+    name = name,
+    version = "1.0",
+    author = "Jim Fulton",
+    author_email = "jim@zope.com",
+    description = "ZC Buildout recipe for installing a Zope 3 checkout",
+    long_description = open('README.txt').read(),
+    license = "ZPL 2.1",
+    keywords = "zope3 buildout",
+    url='http://svn.zope.org/zc.recipe.zope3checkout',
+    classifiers = [
+        'Framework :: Buildout',
+        'License :: OSI Approved :: Zope Public License',
+        ],
+
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['zc', 'zc.recipe'],
+    install_requires = ['zc.buildout', 'zope.testing', 'setuptools'],
+    dependency_links = ['http://download.zope.org/distribution/'],
+    entry_points = {'zc.buildout':
+                    ['default = zc.recipe.zope3checkout:Zope3Checkout']},
+    )
