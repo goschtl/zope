512c7b8108673cb8dc0f2f25f0701de4a170cc3f
dobe <dobe@62d5b8a3-27da-0310-9561-8e5933582275>
2006-07-14 08:30:44 +0000

    added license information
    git-svn-id: svn://svn.zope.org/repos/main@69125 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.multiform/Sandbox/src/z3c/multiform/__init__.py
@@ -0,0 +1,24 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Z3Z Multiform Package
+
+This Package provides an API to handle multiple forms with matching
+form fields on multiple items. The creation of multiforms is derived
+from the Form class of the formlib package.
+
+$Id$
+"""
+__docformat__ = "reStructuredText"
+
+
