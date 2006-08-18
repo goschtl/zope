33db7171351abcca648f0f07cbb14f196ba2bd4d
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2006-08-18 16:07:28 +0000

    Improved the output and error handling a bit.
    Get rid of -vv, which makes it hard to look at the output. :)
    
    
    git-svn-id: svn://svn.zope.org/repos/main@69655 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/branches/3.3/releases/Zope-test.py
+++ Zope3/branches/3.3/releases/Zope-test.py
@@ -1,19 +1,32 @@
 import os, sys
 
-def do(command):
-    print command
+def do(command, errors=None):
+    print 'DOING:', command
     if os.system(command):
-        sys.exit(1)
+        if errors is None:
+            sys.exit(1)
+        else:
+            errors.append(command)
+
+def chdir(d):
+    print 'CHDIR', d
+    os.chdir(d)
 
 
+errors = []
 do('rm -rf Zope-0.0.0*')
 do('../../zpkgtools/bin/zpkg -caCZope.cfg Zope')
 do('tar xzf Zope-0.0.0.tgz')
-os.chdir('Zope-0.0.0')
-do('./configure --prefix `pwd`/z --with-python=%s' %sys.executable)
+chdir('Zope-0.0.0')
+do('./configure --prefix `pwd`/z --with-python=%s' % sys.executable)
 do('make install')
-os.chdir('z')
-do("bin/zopetest -m. -vv")
+chdir('z')
+do("bin/zopetest -m. --all", errors)
 do("bin/mkzopeinstance -d`pwd`/../i -uadmin:123 -m SHA1")
-os.chdir('../i')
-do("bin/test --testzope -vv")
+chdir('../i')
+do("bin/test --testzope --all", errors)
+
+if errors:
+    print 'Failed:'
+    for c in errors:
+        print c
