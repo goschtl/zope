7cd0c3ced08063ee1f8df75bdd99e846f906b28c
jukart <jukart@62d5b8a3-27da-0310-9561-8e5933582275>
2006-09-21 23:34:47 +0000

    eggification
    git-svn-id: svn://svn.zope.org/repos/main@70314 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.layer/trunk/src/z3c/__init__.py
+++ z3c.layer/trunk/src/z3c/__init__.py
@@ -12,6 +12,11 @@
 #
 ##############################################################################
 """
-$Id:$
+$Id$
 """
 
+# this is a namespace package
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    pass
