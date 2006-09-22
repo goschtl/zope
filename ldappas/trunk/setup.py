560b18b2112cf54d8cde7f64a0297a5ca94a5690
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2006-09-22 09:32:27 +0000

    Add a setup.py.
    git-svn-id: svn://svn.zope.org/repos/main@70332 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ ldappas/trunk/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ldappas',
+    version='0.5',
+    author='Zope 3 developers',
+    author_email='zope3-dev@zope.org',
+    url='http://svn.zope.org/ldappas',
+    description="""\
+LDAP-based authenticator for Zope 3. It uses ldapadapter to talk to an
+LDAP server.
+""",
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    zip_safe=False,
+    license='ZPL 2.1',
+    keywords='Zope3 authentication ldap',
+    classifiers = ['Framework :: Zope 3'],
+    install_requires=['ldapadapter'],
+    )
+
+    
