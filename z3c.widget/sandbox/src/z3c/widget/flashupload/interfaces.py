d08aa956f7414ead93bb95da8a3299f438f47cac
opetznick <opetznick@62d5b8a3-27da-0310-9561-8e5933582275>
2006-09-27 13:48:47 +0000

    added  flashupload project
    git-svn-id: svn://svn.zope.org/repos/main@70398 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.widget/sandbox/src/z3c/widget/flashupload/interfaces.py
@@ -0,0 +1,10 @@
+from zope import interface
+
+class IUploadFileView(interface.Interface):
+
+    """a file upload view"""
+
+class IFlashUploadForm(interface.Interface):
+
+    """Form containing the swf for upload movie"""
+    
