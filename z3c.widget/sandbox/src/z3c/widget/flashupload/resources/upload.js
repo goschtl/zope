de142caaf557cd15a58fb7a652ebb4933e31d7c3
schwendinger <schwendinger@62d5b8a3-27da-0310-9561-8e5933582275>
2006-10-05 20:21:25 +0000

    flash player detection implemented
    git-svn-id: svn://svn.zope.org/repos/main@70549 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.widget/sandbox/src/z3c/widget/flashupload/resources/upload.js
+++ z3c.widget/sandbox/src/z3c/widget/flashupload/resources/upload.js
@@ -62,7 +62,7 @@ function createFlashUpload(){
     var success = so.write("flashuploadtarget");
     if (!success){
         // flash plugin missing or too old
-        var error_msg = "<div class=\"error\">Flash Player detection failed. ";
+        var error_msg = "<div class=\"flash-detection-error\">Flash Player detection failed. ";
         error_msg += "Please install the Flash Player Plugin. You can install the plugin ";
         error_msg += "<a href=\"http://www.adobe.com/go/getflashplayer\">here</a> for free.</div>";
         document.getElementById("flashuploadtarget").innerHTML = error_msg;
