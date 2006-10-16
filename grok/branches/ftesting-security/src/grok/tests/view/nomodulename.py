234a291c58fe7a42d1a1bdf776723ac0350f7071
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2006-10-16 16:22:38 +0000

    get rid of no longer needed brnach
    git-svn-id: svn://svn.zope.org/repos/main@70713 62d5b8a3-27da-0310-9561-8e5933582275

--- grok/branches/ftesting-security/src/grok/tests/view/nomodulename.py
+++
@@ -1,9 +0,0 @@
-"""
-You can't call grok.name on a module:
-
-  >>> import grok.tests.view.nomodulename_fixture
-  Traceback (most recent call last):
-    ...
-  GrokError: grok.name can only be used on class level.
-
-"""
