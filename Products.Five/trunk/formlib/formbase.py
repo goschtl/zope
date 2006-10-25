7899976379d81d7bc42100faf3ce8865fbd8c24b
yuppie <yuppie@62d5b8a3-27da-0310-9561-8e5933582275>
2006-10-25 12:01:35 +0000

    - made some Zope 3 formlib fixes available
    git-svn-id: svn://svn.zope.org/repos/main@70912 62d5b8a3-27da-0310-9561-8e5933582275

--- Products.Five/trunk/formlib/formbase.py
+++ Products.Five/trunk/formlib/formbase.py
@@ -15,40 +15,50 @@
 
 $Id$
 """
+import os.path
+
 from datetime import datetime
 import Acquisition
 
 import zope.event
+import zope.formlib
 import zope.lifecycleevent
 from zope import interface
 from zope.formlib import interfaces, form, namedtemplate
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory("zope")
 
-from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.Five.browser.decode import processInputs, setPageEncoding
+
+_FORMLIB_DIR = os.path.dirname(zope.formlib.__file__)
+_PAGEFORM_PATH = os.path.join(_FORMLIB_DIR, 'pageform.pt')
+_SUBPAGEFORM_PATH = os.path.join(_FORMLIB_DIR, 'subpageform.pt')
+
 
 class FiveFormlibMixin(Acquisition.Explicit):
 
     # Overrides the formlib.form.FormBase.template attributes implemented 
     # using NamedTemplates. NamedTemplates using ViewPageTemplateFile (like
     # formlib does by default) cannot work in Zope2.
-    
+
     # XXX Maybe we need to have Five-compatible NamedTemplates?
-    
-    template = ZopeTwoPageTemplateFile('pageform.pt')
-    
+
+    template = ViewPageTemplateFile(_PAGEFORM_PATH)
+
     # Overrides formlib.form.FormBase.update. Make sure user input is
     # decoded first and the page encoding is set before proceeding.
-    
+
     def update(self):
         processInputs(self.request)
         setPageEncoding(self.request)
         super(FiveFormlibMixin, self).update()
 
+
 class FormBase(FiveFormlibMixin, form.FormBase):
     pass
-    
+
+
 class EditFormBase(FiveFormlibMixin, form.EditFormBase):
 
     # Overrides formlib.form.EditFormBase.handle_edit_action, to remove
@@ -58,7 +68,7 @@ class EditFormBase(FiveFormlibMixin, form.EditFormBase):
     def handle_edit_action(self, action, data):
         if form.applyChanges(
             self.context, self.form_fields, data, self.adapters):
-            
+
             zope.event.notify(
                 zope.lifecycleevent.ObjectModifiedEvent(self.context)
                 )
@@ -69,18 +79,22 @@ class EditFormBase(FiveFormlibMixin, form.EditFormBase):
                 )
         else:
             self.status = _('No changes')
-    
+
+
 class DisplayFormBase(FiveFormlibMixin, form.DisplayFormBase):
     pass
 
+
 class AddFormBase(FiveFormlibMixin, form.AddFormBase):
     pass
-    
+
+
 class PageForm(FormBase):
 
     interface.implements(interfaces.IPageForm)
 
 Form = PageForm
+
 
 class PageEditForm(EditFormBase):
 
@@ -88,11 +102,13 @@ class PageEditForm(EditFormBase):
 
 EditForm = PageEditForm
 
+
 class PageDisplayForm(DisplayFormBase):
 
     interface.implements(interfaces.IPageForm)
 
 DisplayForm = PageDisplayForm
+
 
 class PageAddForm(AddFormBase):
 
@@ -100,20 +116,23 @@ class PageAddForm(AddFormBase):
 
 AddForm = PageAddForm
 
+
 class SubPageForm(FormBase):
 
-    template = ZopeTwoPageTemplateFile('subpageform.pt')
-    
+    template = ViewPageTemplateFile(_SUBPAGEFORM_PATH)
+
     interface.implements(interfaces.ISubPageForm)
+
 
 class SubPageEditForm(EditFormBase):
 
-    template = ZopeTwoPageTemplateFile('subpageform.pt')
-    
+    template = ViewPageTemplateFile(_SUBPAGEFORM_PATH)
+
     interface.implements(interfaces.ISubPageForm)
+
 
 class SubPageDisplayForm(DisplayFormBase):
 
-    template = ZopeTwoPageTemplateFile('subpageform.pt')
-    
+    template = ViewPageTemplateFile(_SUBPAGEFORM_PATH)
+
     interface.implements(interfaces.ISubPageForm)
