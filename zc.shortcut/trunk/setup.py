70a6dd969f484d737c7e1cba77daa1315f73010e
gary <gary@62d5b8a3-27da-0310-9561-8e5933582275>
2006-12-07 18:46:06 +0000

    version based on zc.buildout
    git-svn-id: svn://svn.zope.org/repos/main@71487 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.shortcut/trunk/setup.py
+++ zc.shortcut/trunk/setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name = "zc.shortcut",
-    description = "Symlinks for Zope 3",
     version = "1.0",
 
     packages = find_packages('src'),
@@ -15,5 +14,21 @@ setup(
     # XXX leaving out most of the zope 3 dependencies for now,
     # since Zope 3 hasn't been packages yet.
     ],
+    zip_safe=False,
+    author='Zope Project',
+    author_email='zope3-dev@zope.org',
+    description=open("README.txt").read(),
+    long_description=
+        open("src/zc/shortcut/shortcut.txt").read() +
+        '\n' +
+        open("src/zc/shortcut/proxy.txt").read() +
+        '\n' +
+        open("src/zc/shortcut/adapters.txt").read() +
+        '\n' +
+        open("src/zc/shortcut/adding.txt").read() +
+        '\n' +
+        open("src/zc/shortcut/factory.txt").read(),
+    license='ZPL 2.1',
+    keywords="zope zope3",
     dependency_links = ['http://download.zope.org/distribution/'],
     )
