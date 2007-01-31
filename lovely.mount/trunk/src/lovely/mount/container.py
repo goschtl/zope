566fa82de06071cc8c903cb31388a2c6f83ca698
dobe <dobe@62d5b8a3-27da-0310-9561-8e5933582275>
2007-01-31 15:02:20 +0000

    hm, we have to reset parent upon getting, something is wrong with the cross databse references
    git-svn-id: svn://svn.zope.org/repos/main@72277 62d5b8a3-27da-0310-9561-8e5933582275

--- lovely.mount/trunk/src/lovely/mount/container.py
+++ lovely.mount/trunk/src/lovely/mount/container.py
@@ -3,12 +3,36 @@ import root
 from zope import interface
 import interfaces
 from zope.schema.fieldproperty import FieldProperty
+from zope.app.container.interfaces import IContained
 
 class MountpointContainer(root.DBRoot, Contained):
     
     interface.implements(interfaces.IMountpointContainer)
     
     dbName = FieldProperty(interfaces.IMountpointContainer['dbName'])
+
+    def get(self, key, default=None):
+        '''See interface `IReadContainer`'''
+        try:
+            self.__getitem__(key)
+        except KeyError:
+            return default
+
+    def items(self):
+        '''See interface `IReadContainer`'''
+        return [(k,self.__getitem__(k)) for k in self.keys()]
+
+    def values(self):
+        '''See interface `IReadContainer`'''
+        return [self.__getitem__(k) for k in self.keys()]
+
+    def __getitem__(self, key):
+        #XXX why do we have to do this?
+        # cross references should work
+        item = self._data.__getitem__(key)
+        if hasattr(item, '__parent__'):
+            item.__parent__ = self
+        return item
 
     def __setitem__(self, key, object):
         '''See interface `IWriteContainer`'''
