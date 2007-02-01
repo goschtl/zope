6b72d2dfdcedf09d45134ced9f2b2861db1772e7
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2007-02-01 11:21:10 +0000

    added some basic functionality in order to experiment a bit
    git-svn-id: svn://svn.zope.org/repos/main@72289 62d5b8a3-27da-0310-9561-8e5933582275

--- Product.ZSQLAlchemy/trunk/ZSQLAlchemy.py
+++ Product.ZSQLAlchemy/trunk/ZSQLAlchemy.py
@@ -11,19 +11,82 @@ from AccessControl import ClassSecurityInfo
 from OFS.PropertyManager import PropertyManager
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 
+
+import sqlalchemy
+import psycopg2 as psycopg
+
+psycopg = sqlalchemy.pool.manage(psycopg)
+
+
+class SessionProxy(object):
+
+    security = ClassSecurityInfo()
+
+    def __init__(self, engine):
+        self._engine = engine
+
+
+    security.declarePublic('getEngine')
+    def getEngine(self):
+        """ return the engine """
+        return self._engine
+
+
+    security.declarePublic('getMetaData')
+    def getMetaData(self):      
+        """ return a MetaData instance """
+        return sqlalchemy.BoundMetaData(self._engine)
+
+
+InitializeClass(SessionProxy)
+
+
+
 class ZSQLAlchemy(SimpleItem, PropertyManager):
 
     meta_type = 'ZSQLAlchemy'
-    connection_url = ''
+    hostname = ''
+    username = ''
+    password = ''
+    database = ''
 
     manage_options = SimpleItem.manage_options + \
                      PropertyManager.manage_options    
 
-    _properties=({'id':'connection_url',
-                  'type':'string',
-                  'mode':'wrd'},)
+    _properties=(
+                 {'id':'hostname', 'type':'string', 'mode':'wrd'},
+                 {'id':'username', 'type':'string', 'mode':'wrd'},
+                 {'id':'password', 'type':'string', 'mode':'wrd'},
+                 {'id':'database', 'type':'string', 'mode':'wrd'},
+                 )
 
     security = ClassSecurityInfo()
+
+    def _getConnection(self):
+        """ connection factory """
+        db = psycopg.connect(database=self.database, 
+                             user=self.username,
+                             password=self.password,
+                             host=self.hostname)
+
+        return db
+
+
+    def _createEngine(self):
+        """ create an engine """
+        
+        p = sqlalchemy.pool.QueuePool(self._getConnection, max_overflow=10, pool_size=10, use_threadlocal=True)
+        engine = sqlalchemy.create_engine('postgres://', pool=p)
+        return engine
+
+
+    security.declarePublic('getSession')
+    def getSession(self):
+        """ return a session proxy """
+
+        engine = self._createEngine()
+        proxy = SessionProxy(engine)
+        return proxy
 
 
 InitializeClass(ZSQLAlchemy)
