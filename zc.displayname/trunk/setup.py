09df0521218cdb0070ad1ae1d9fd850498f86910
alex <alex@62d5b8a3-27da-0310-9561-8e5933582275>
2007-02-08 19:19:42 +0000

    Updated the version to reflect the publication of the egg
    git-svn-id: svn://svn.zope.org/repos/main@72470 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.displayname/trunk/setup.py
+++ zc.displayname/trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "zc.displayname",
-    version = "1.0",
+    version = "1.1dev",
 
     packages = find_packages('src'),
     include_package_data = True,
