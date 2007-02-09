60407c674e74c7384b0c02cb4b3a325dd23af2b6
alex <alex@62d5b8a3-27da-0310-9561-8e5933582275>
2007-02-09 19:32:20 +0000

    Updated the version number to reflect the publication of the egg
    git-svn-id: svn://svn.zope.org/repos/main@72490 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.security/trunk/setup.py
+++ zc.security/trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "zc.security",
-    version = "1.0",
+    version = "4.1dev",
     author = "Zope Corporation",
     author_email = "zope3-dev@zope.org",
     description = "Principal-searching UI for Zope 3 Pluggable Authentication",
