d206066f6eed343a25050c19b8499344f520e6ef
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2007-02-15 15:14:50 +0000

    put the doctest into a module-level docstring, like we typically do in grok tests.
    git-svn-id: svn://svn.zope.org/repos/main@72600 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.five/trunk/src/megrok/five/tests/test_container.py
+++ megrok.five/trunk/src/megrok/five/tests/test_container.py
@@ -1,64 +1,62 @@
+"""
+Let's make a container and add a few items.  As you can see, we can
+use the regular IContainer API (Python's mapping API):
+
+  >>> from megrok.five import Container, Model
+  >>> folder = Container()
+  >>> folder['garfield'] = Model('garfield')
+  >>> folder['john'] = Model('john')
+  >>> folder['odie'] = Model('odie')
+
+The rest of the mapping API is also supported:
+
+  >>> sorted(folder)  # test __iter__
+  ['garfield', 'john', 'odie']
+
+  >>> sorted(folder.keys())
+  ['garfield', 'john', 'odie']
+
+  >>> sorted(model.getId() for model in folder.values())
+  ['garfield', 'john', 'odie']
+
+  >>> sorted((name, model.getId()) for name, model in folder.items()) # doctest: +NORMALIZE_WHITESPACE
+  [('garfield', 'garfield'),
+   ('john', 'john'),
+   ('odie', 'odie')]
+
+  >>> 'garfield' in folder  # test __contains__
+  True
+
+  >>> len(folder)  # test __len__
+  3
+
+  >>> folder.get('garfield') == folder['garfield']
+  True
+  >>> folder.get('not-there') is None
+  True
+  >>> folder.get('not-there', 'then take me')
+  'then take me'
+
+  >>> del folder['odie']  # test __delitem__
+  >>> len(folder)
+  2
+  >>> sorted(folder)
+  ['garfield', 'john']
+
+To maintain compatibility with Zope 2, a container is also an object
+manager:
+
+  >>> folder.garfield.getId()
+  'garfield'
+  >>> sorted(folder.objectIds())
+  ['garfield', 'john']
+
+etc... (no point here to further dive into the horrors of the object
+manager API.)
+"""
+
 import unittest
 from zope.testing.doctest import DocTestSuite
-
-def test_container():
-    """
-    Let's make a container and add a few items.  As you can see, we
-    can use the regular IContainer API (Python's mapping API):
-
-      >>> from megrok.five import Container, Model
-      >>> folder = Container()
-      >>> folder['garfield'] = Model('garfield')
-      >>> folder['john'] = Model('john')
-      >>> folder['odie'] = Model('odie')
-
-    The rest of the mapping API is also supported:
-
-      >>> sorted(folder)  # test __iter__
-      ['garfield', 'john', 'odie']
-
-      >>> sorted(folder.keys())
-      ['garfield', 'john', 'odie']
-
-      >>> sorted(model.getId() for model in folder.values())
-      ['garfield', 'john', 'odie']
-
-      >>> sorted((name, model.getId()) for name, model in folder.items()) # doctest: +NORMALIZE_WHITESPACE
-      [('garfield', 'garfield'),
-       ('john', 'john'),
-       ('odie', 'odie')]
-
-      >>> 'garfield' in folder  # test __contains__
-      True
-
-      >>> len(folder)  # test __len__
-      3
-
-      >>> folder.get('garfield') == folder['garfield']
-      True
-      >>> folder.get('not-there') is None
-      True
-      >>> folder.get('not-there', 'then take me')
-      'then take me'
-
-      >>> del folder['odie']  # test __delitem__
-      >>> len(folder)
-      2
-      >>> sorted(folder)
-      ['garfield', 'john']
-
-    To maintain compatibility with Zope 2, a container is also an
-    object manager:
-
-      >>> folder.garfield.getId()
-      'garfield'
-      >>> sorted(folder.objectIds())
-      ['garfield', 'john']
-
-    etc... (no point here to further dive into the horrors of the
-    object manager API.)
-
-    """
 
 def test_suite():
     return unittest.TestSuite([
