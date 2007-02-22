ad2f9cea3a05292dd9c11cf9755c42d55aebf896
rocky <rocky@62d5b8a3-27da-0310-9561-8e5933582275>
2007-02-22 23:37:57 +0000

    Added appropriate zcml inclusion of five.localsitemanager ... this required moving the sys.path mangling out of the initialize function.
    git-svn-id: svn://svn.zope.org/repos/main@72780 62d5b8a3-27da-0310-9561-8e5933582275

--- CMF/branches/jens_tools_as_utilities/CMFCore/__init__.py
+++ CMF/branches/jens_tools_as_utilities/CMFCore/__init__.py
@@ -68,15 +68,15 @@ __module_aliases__ = ( ( 'Products.BTreeFolder2.CMFBTreeFolder'
                      ,
                      )
 
-def initialize(context):
-    try:
-        import five.localsitemanager
-    except ImportError, e:
-        import sys
-        import os
-        sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
-        import five.localsitemanager
+try:
+    import five.localsitemanager
+except ImportError, e:
+    import sys
+    import os
+    sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+    import five.localsitemanager
 
+def initialize(context):
     context.registerClass(
         DirectoryView.DirectoryView,
         constructors=(('manage_addDirectoryViewForm',
