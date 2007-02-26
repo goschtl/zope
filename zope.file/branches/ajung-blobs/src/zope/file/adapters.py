a74b267301f0da4a6df6fc1d6eeaa7873546af55
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2007-02-26 18:37:33 +0000

    adapters no longer needed
    git-svn-id: svn://svn.zope.org/repos/main@72832 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.file/branches/ajung-blobs/src/zope/file/adapters.py
+++ zope.file/branches/ajung-blobs/src/zope/file/adapters.py
@@ -38,6 +38,7 @@ class WriteFileAdapter(object):
         self.context = context
 
     def write(self, data):
+        import pdb; pdb.set_trace() 
         f = self.context.open('w')
         f.write(data)
         f.close()
