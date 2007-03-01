14aeff7b445cb8cdd38a78c74ca0fcd1800d1a08
ctheune <ctheune@62d5b8a3-27da-0310-9561-8e5933582275>
2007-03-01 01:16:12 +0000

    - merged buildout branch
    git-svn-id: svn://svn.zope.org/repos/main@72932 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.zalchemy/trunk/setup.py
+++ z3c.zalchemy/trunk/setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='z3c.zalchemy',
-    version='trunk',
-    author='ROBOTECH???',
-    url='https://svn.zope.org.repos/main',
-    description="""\
-SQLAlchemy integration into Zope 3
-""",
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},
-    include_package_data = True,
-    zip_safe=False,
-    license='ZPT 2.1',
-    install_requires=['setuptools',
-                      'SQLAlchemy'],
-)
+setup(name='z3c.zalchemy',
+      version='trunk',
+      author='Juergen Kartnaller',
+      url='https://svn.zope.org.repos/main',
+      description="""SQLAlchemy integration into Zope 3""",
+      license='ZPL 2.1',
 
-
-
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      include_package_data = True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'SQLAlchemy',
+                        'ZODB3',
+                        'zope.component',
+                        'zope.interface',
+                        'zope.schema',
+                        'zope.app',
+                       ],
+      extras_require = dict(test=['pysqlite']))
