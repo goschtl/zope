1aadfb16be33210056763ba893550b9465ec6774
fdrake <fdrake@62d5b8a3-27da-0310-9561-8e5933582275>
2007-03-02 23:13:03 +0000

    add the ability to select server types (Twisted or ZServer)
    git-svn-id: svn://svn.zope.org/repos/main@72963 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.zope3recipes/dev/setup.py
+++ zc.zope3recipes/dev/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 name = "zc.zope3recipes"
 setup(
     name = name,
-    version = "0.2",
+    version = "0.3",
     author = "Jim Fulton",
     author_email = "jim@zope.com",
     description = "ZC Buildout recipe for defining Zope 3 applications",
