83200047320dd558f1b8f6037d0ce0f2138a696e
fdrake <fdrake@62d5b8a3-27da-0310-9561-8e5933582275>
2007-03-08 04:06:43 +0000

    - convert to a buildout - rename ntests module to tests
    git-svn-id: svn://svn.zope.org/repos/main@73049 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.preview/trunk/setup.py
+++ zc.preview/trunk/setup.py
@@ -4,9 +4,14 @@ setup(
     name="zc.preview",
     version="0.1dev",
     packages=find_packages('src'),
-    package_dir={'':'src'},
+    package_dir={'': 'src'},
     namespace_packages=['zc'],
     include_package_data=True,
-    install_requirements = ['setuptools'],
-    zip_safe = False
+    install_requires=[
+        'setuptools',
+        'zope.file',
+        'zope.mimetype',
+        'zc.shortcut',  # only needed for tests?
+        ],
+    zip_safe=False,
     )
