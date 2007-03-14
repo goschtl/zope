8db33389ed772f285f96c441b546de86fe8e4b62
schwendinger <schwendinger@62d5b8a3-27da-0310-9561-8e5933582275>
2007-03-14 16:05:17 +0000

    custom widget for imagefields. widget does not delete field values when delete is not explicitly selected
    git-svn-id: svn://svn.zope.org/repos/main@73170 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.widget/trunk/src/z3c/widget/image/widget.py
@@ -0,0 +1,34 @@
+from zope.app.form.browser.widget import SimpleInputWidget
+from zope.app.file.image import Image
+
+template = u"""
+   <div class="z3cImageWidget">
+     Image Widget
+     <input type="file" name="%(id)s" id="%(id)s" />
+     <input type="checkbox" name="%(id)s.delete" value="true" /> delete image
+   </div>
+"""
+
+class ImageWidget(SimpleInputWidget):
+    pass
+
+    def __call__(self):
+        return template % {
+            'id' : self.name
+            }
+
+    def _getFormInput(self):
+        filedata = self.request.get(self.name)
+        delete = self.request.has_key(self.name+'.delete')
+        if delete:
+            return None
+        else:
+            if not filedata:
+                return self.context.get(self.context.context)
+            else:
+                fileObj = Image(filedata)
+                return fileObj
+        
+    def _toFieldValue(self, input):
+        return input
+
