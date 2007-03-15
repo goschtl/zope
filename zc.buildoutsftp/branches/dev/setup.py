60478c5a0c81920a782549b93c4eec898b4dd889
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2007-03-15 20:13:56 +0000

    Updated release info.
    git-svn-id: svn://svn.zope.org/repos/main@73208 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.buildoutsftp/branches/dev/setup.py
+++ zc.buildoutsftp/branches/dev/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 name='zc.buildoutsftp'
 setup(
     name=name,
-    version = "0.2.1",
+    version = "0.2.2",
     author = "Jim Fulton",
     author_email = "jim@zope.com",
     description =
