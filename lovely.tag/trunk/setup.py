133330a8186e24bc59945b8d60c79adff63528d2
dobe <dobe@62d5b8a3-27da-0310-9561-8e5933582275>
2007-03-22 13:19:47 +0000

    version 0.2, add cfg
    git-svn-id: svn://svn.zope.org/repos/main@73441 62d5b8a3-27da-0310-9561-8e5933582275

--- lovely.tag/trunk/setup.py
+++ lovely.tag/trunk/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='lovely.tag',
-      version='0.1',
+      version='0.2',
       author = "Stephan Richter, Jodok Batlogg",
       author_email = "srichter@cosmos.phy.tufts.edu, jodok.batlogg@lovelysystems.com",
       description = "A tagging engine for zope 3",
