ea8abc7309f07bd15c77948d77f6488d6806ddc3
batlogg <batlogg@62d5b8a3-27da-0310-9561-8e5933582275>
2007-03-22 15:17:24 +0000

    added z3c.sampledata as egg
    git-svn-id: svn://svn.zope.org/repos/main@73452 62d5b8a3-27da-0310-9561-8e5933582275

--- lovely.tag/trunk/setup.py
+++ lovely.tag/trunk/setup.py
@@ -1,28 +1,30 @@
 #!python
 from setuptools import setup, find_packages
 
-setup(name='lovely.tag',
-      version='0.2',
-      author = "Stephan Richter, Jodok Batlogg",
-      author_email = "srichter@cosmos.phy.tufts.edu, jodok.batlogg@lovelysystems.com",
-      description = "A tagging engine for zope 3",
-      license = "ZPL 2.1",
-      keywords = "zope3 web20 zope tagging",
-      url='svn://svn.zope.org/repos/main/lovely.tag',
-
-      packages=find_packages('src'),
-      include_package_data=True,
-      package_dir = {'':'src'},
-      namespace_packages=['lovely'],
-      extras_require = dict(
-            test=['z3c.sampledata']
-            ),
-      install_requires = ['setuptools',
-                          'zope.interface',
-                          'zope.component',
-                          'ZODB3',
-                          'zope.app.container',
-                          'zope.i18nmessageid'],
-      dependency_links=['http://download.zope.org/distribution']
-     )
+setup (
+    name='lovely.tag',
+    version='0.2',
+    author = "Lovely Systems",
+    author_email = "office@lovelysystems.com",
+    description = "A tagging engine for zope 3",
+    license = "ZPL 2.1",
+    keywords = "zope3 web20 zope tagging",
+    url = 'svn://svn.zope.org/repos/main/lovely.tag',
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['lovely'],
+    extras_require = dict(
+        test = ['z3c.sampledata']
+        ),
+    install_requires = [
+        'setuptools',
+        'zope.interface',
+        'zope.component',
+        'ZODB3',
+        'zope.app.container',
+        'zope.i18nmessageid',
+        ],
+    dependency_links = ['http://download.zope.org/distribution']
+    )
 
