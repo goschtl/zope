ab818beba3475e3f21604d9c01f47686c6f76883
baijum <baijum@62d5b8a3-27da-0310-9561-8e5933582275>
2007-03-23 11:57:36 +0000

    - Add buildout support  - Re-organized package structure.
    git-svn-id: svn://svn.zope.org/repos/main@73484 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zwiki/branches/baijum-experimental-zwiki/setup.py
@@ -0,0 +1,49 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zwiki package
+
+$Id: setup.py 73414 2007-03-21 09:21:03Z baijum $
+"""
+
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name = 'zwiki',
+      version = '0.3dev',
+      url = 'http://svn.zope.org/zwiki/trunk',
+      license = 'ZPL 2.1',
+      description = 'A Zope 3 wiki',
+      author = 'Zope Corporation and Contributors',
+      author_email = 'zope3-dev@zope.org',
+      long_description=(
+        read('README.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        ),
+ 
+      packages = find_packages('src'),
+      package_dir = {'': 'src'},
+
+      tests_require = ['zope.testing'],
+      install_requires = ['setuptools',
+                          'zope.sendmail',
+                          'zope.app'],
+
+      include_package_data = True,
+      zip_safe = False,
+      )
