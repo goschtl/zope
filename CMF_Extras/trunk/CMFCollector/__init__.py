c1e63e1bf409aef9620484f850c34fb28406b298
tseaver <tseaver@62d5b8a3-27da-0310-9561-8e5933582275>
2007-03-25 02:20:23 +0000

    Added a GenericSetup extension profile for installing the collector
    o Includes types, skin, and workflow.
     
    
    
    git-svn-id: svn://svn.zope.org/repos/main@73523 62d5b8a3-27da-0310-9561-8e5933582275

--- CMF_Extras/trunk/CMFCollector/__init__.py
+++ CMF_Extras/trunk/CMFCollector/__init__.py
@@ -12,79 +12,69 @@
 ##############################################################################
 import sys
 
-from Products.CMFCore import utils
-from Products.CMFCore.DirectoryView import registerDirectory
-from Products.CMFDefault import Portal
-
-import Collector
-import CollectorIssue
-import WebTextDocument
-import CollectorSubset
-from permissions import View
-from permissions import AddPortalContent
-from permissions import AddCollectorIssue
-from permissions import EditCollectorIssue
-from permissions import AddCollectorIssueFollowup
-
-# register workflow factory
-import workflow
-del workflow
-
 this_module = sys.modules[ __name__ ]
-
-factory_type_information = (
-    (Collector.factory_type_information
-     + CollectorIssue.factory_type_information
-     + Collector.catalog_factory_type_information
-     + CollectorSubset.factory_type_information
-
-     + ({'id': 'Collector Issue Transcript',
-         #     'content_icon': 'event_icon.gif',
-         'meta_type': 'WebText Document',
-         'description': ('A transcript of issue activity, including comments,'
-                         ' state changes, and so forth.'), 
-         'product': 'CMFCollector',
-         'factory': None,               # So not included in 'New' add form
-         'allowed_content_types': None,
-         'immediate_view': 'collector_transcript_view',
-         'actions': (
-               { 'id': 'view',
-                 'name': 'View',
-                 'action': 'string:${object_url}/../',
-                 'permissions': (View,) },
-               { 'id': 'addcomment',
-                 'name': 'Add Comment',
-                 'action':
-                     'string:${object_url}/collector_transcript_comment_form',
-                 'permissions':
-                          (AddCollectorIssueFollowup,) },
-               { 'id': 'edittranscript',
-                 'name': 'Edit Transcript',
-                 'action':
-                        'string:${object_url}/collector_transcript_edit_form',
-                 'permissions': (EditCollectorIssue,) },
-             ),
-         },
-        )
-     )
-    )
-
-contentClasses = (Collector.Collector, CollectorIssue.CollectorIssue,
-                  Collector.CollectorCatalog, CollectorSubset.CollectorSubset)
-contentConstructors = (Collector.addCollector,
-                       CollectorIssue.addCollectorIssue,
-                       CollectorSubset.addCollectorSubset)
-z_bases = utils.initializeBasesPhase1(contentClasses, this_module)
-# This is used by a script (external method) that can be run
-# to set up collector in an existing CMF Site instance.
 collector_globals = globals()
 
-# Make the skins available as DirectoryViews
-registerDirectory('skins', globals())
-registerDirectory('skins/collector', globals())
-
 def initialize(context):
-    utils.initializeBasesPhase2(z_bases, context)
+    from Products.CMFCore.interfaces import ISiteRoot
+    from Products.CMFCore.DirectoryView import registerDirectory
+    from Products.GenericSetup import EXTENSION
+    from Products.GenericSetup import profile_registry
+
+    import Collector
+    import CollectorIssue
+    import WebTextDocument
+    import CollectorSubset
+    from permissions import View
+    from permissions import AddPortalContent
+    from permissions import AddCollectorIssue
+    from permissions import EditCollectorIssue
+    from permissions import AddCollectorIssueFollowup
+
+    factory_type_information = (
+        (Collector.factory_type_information
+        + CollectorIssue.factory_type_information
+        + Collector.catalog_factory_type_information
+        + CollectorSubset.factory_type_information
+
+        + ({'id': 'Collector Issue Transcript',
+            #     'content_icon': 'event_icon.gif',
+            'meta_type': 'WebText Document',
+            'description': (
+                'A transcript of issue activity, including comments,'
+                ' state changes, and so forth.'), 
+            'product': 'CMFCollector',
+            'factory': None,               # So not included in 'New' add form
+            'allowed_content_types': None,
+            'immediate_view': 'collector_transcript_view',
+            'actions': (
+                { 'id': 'view',
+                    'name': 'View',
+                    'action': 'string:${object_url}/../',
+                    'permissions': (View,) },
+                { 'id': 'addcomment',
+                    'name': 'Add Comment',
+                    'action':
+                      'string:${object_url}/collector_transcript_comment_form',
+                    'permissions':
+                            (AddCollectorIssueFollowup,) },
+                { 'id': 'edittranscript',
+                    'name': 'Edit Transcript',
+                    'action':
+                         'string:${object_url}/collector_transcript_edit_form',
+                    'permissions': (EditCollectorIssue,) },
+                ),
+            },
+            )
+        )
+        )
+
+    contentClasses = (Collector.Collector, CollectorIssue.CollectorIssue,
+                    Collector.CollectorCatalog, CollectorSubset.CollectorSubset)
+    contentConstructors = (Collector.addCollector,
+                        CollectorIssue.addCollectorIssue,
+                        CollectorSubset.addCollectorSubset)
+
     context.registerHelp(directory='help')
     context.registerHelpTitle('CMF Collector Help')
 
@@ -99,3 +89,14 @@ def initialize(context):
     context.registerClass(CollectorSubset.CollectorSubset,
                           constructors = (CollectorSubset.addCollectorSubset,),
                           permission = AddPortalContent)
+
+    registerDirectory('skins', globals())
+    registerDirectory('skins/collector', globals())
+
+    profile_registry.registerProfile('CMFCollector',
+                                     'CMF Collector',
+                                     'Types, skins, workflow for collector.',
+                                     'profiles/collector',
+                                     'CMFCollector',
+                                     EXTENSION,
+                                     for_=ISiteRoot)
