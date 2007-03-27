8fad543da38d642f1050e7dcb4ec76384db520d2
baijum <baijum@62d5b8a3-27da-0310-9561-8e5933582275>
2007-03-27 13:46:06 +0000

    eggification & buildoutification
    git-svn-id: svn://svn.zope.org/repos/main@73739 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.app.intid/trunk/setup.py
@@ -0,0 +1,54 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.intid package
+
+$Id$
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+setup(name = 'zope.app.intid',
+      version = '3.4dev',
+      url = 'http://svn.zope.org/zope.app.intid',
+      license = 'ZPL 2.1',
+      description = 'Zope intid',
+      author = 'Zope Corporation and Contributors',
+      author_email = 'zope3-dev@zope.org',
+      long_description = "",
+
+      packages = find_packages('src'),
+      package_dir = {'': 'src'},
+
+      namespace_packages = ['zope', 'zope.app'],
+      tests_require = ['zope.testing'],
+      install_requires = ['setuptools',
+                          'ZODB3',
+                          'zope.app.component',
+                          'zope.app.container',
+                          'zope.app.folder',
+                          'zope.app.keyreference',
+                          'zope.app.testing',
+                          'zope.app.zapi',
+                          'zope.component',
+                          'zope.event',
+                          'zope.interface',
+                          'zope.location',
+                          'zope.security',
+                          ],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
