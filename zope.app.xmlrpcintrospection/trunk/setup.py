a491f8b40f8bbe1f2c123efe4d761d703e2f9e6b
dobe <dobe@62d5b8a3-27da-0310-9561-8e5933582275>
2007-04-10 14:27:08 +0000

    xmlrpcintrospection project for egg
    git-svn-id: svn://svn.zope.org/repos/main@74092 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.app.xmlrpcintrospection/trunk/setup.py
@@ -0,0 +1,42 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.xmlrpcintrospection package
+
+$Id$
+"""
+
+import os
+
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.app.xmlrpcintrospection',
+      version='3.4dev',
+      url='http://svn.zope.org/zope.app.xmlrpcintrospection',
+      license='ZPL 2.1',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+	  packages=find_packages('src'),
+	  package_dir = {'': 'src'},
+      extras_require=dict(test=['zope.app.testing',
+                                'zope.app.folder']),
+      namespace_packages=['zope', 'zope.app'],
+      install_requires=['setuptools',
+                        'zope.component',
+                        'zope.interface',
+                        'zope.publisher',
+                        ],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
