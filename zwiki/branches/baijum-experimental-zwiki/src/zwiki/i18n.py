d63d6e646880a17e5bca16bf1f56652b67f0eb24
baijum <baijum@62d5b8a3-27da-0310-9561-8e5933582275>
2007-04-10 03:40:14 +0000

    - zope.app.wiki is no more  - empty __init__.py
    git-svn-id: svn://svn.zope.org/repos/main@74078 62d5b8a3-27da-0310-9561-8e5933582275

--- zwiki/branches/baijum-experimental-zwiki/src/zwiki/i18n.py
+++ zwiki/branches/baijum-experimental-zwiki/src/zwiki/i18n.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2003-2007 Zope Corporation and Contributors.
+# Copyright (c) 2003 Zope Corporation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -15,3 +15,5 @@
 
 $Id$
 """
+from zope.i18nmessageid import MessageFactory
+ZWikiMessageFactory = MessageFactory("zwiki")
