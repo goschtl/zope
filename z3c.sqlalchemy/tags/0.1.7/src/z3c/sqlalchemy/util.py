7839ad33ddda829db775d81f3fdd700c35e806e6
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2007-04-16 16:46:44 +0000

    0.1.7
    git-svn-id: svn://svn.zope.org/repos/main@74188 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.sqlalchemy/tags/0.1.7/src/z3c/sqlalchemy/util.py
@@ -0,0 +1,82 @@
+##########################################################################
+# z3c.sqlalchemy - A SQLAlchemy wrapper for Python/Zope
+#
+# (C) Zope Corporation and Contributor
+# Written by Andreas Jung for Haufe Mediengruppe, Freiburg, Germany
+# and ZOPYX Ltd. & Co. KG, Tuebingen, Germany
+##########################################################################
+
+
+""" 
+Some helper methods
+"""
+
+
+from sqlalchemy.engine.url import make_url
+
+from zope.component import getService, getGlobalServices, getUtilitiesFor
+from zope.component.utility import GlobalUtilityService
+from zope.component.interfaces import IUtilityService, ComponentLookupError
+from zope.component.servicenames import Utilities 
+
+from z3c.sqlalchemy.interfaces import ISQLAlchemyWrapper
+from z3c.sqlalchemy.postgres import ZopePostgresWrapper, PythonPostgresWrapper 
+from z3c.sqlalchemy.base import BaseWrapper
+
+__all__ = ('createSQLAlchemyWrapper', 'registerSQLAlchemyWrapper', 'allRegisteredSQLAlchemyWrappers')
+
+
+def createSQLAlchemyWrapper(dsn, model=None, forZope=False, **kw):
+    """ Convenience method to generate a wrapper for a DSN and a model.
+        This method hides all database related magic from the user. 
+        Set 'forZope' to True for a Zope related wrapper.
+    """
+
+    url = make_url(dsn)
+    driver = url.drivername
+
+    klass = BaseWrapper
+
+    if driver == 'postgres':
+        klass = forZope and ZopePostgresWrapper or PythonPostgresWrapper
+
+    return klass(dsn, model, **kw)
+
+
+def registerSQLAlchemyWrapper(wrapper, name):
+    """ register a SQLAlchemyWrapper as named utility """
+
+    try:
+        # Zope 2.9
+        from zope.component import provideUtility
+        provideUtility(wrapper, name=name)           
+    except ImportError:
+        # Zope 2.8
+        from zope.component import getService, getGlobalServices, getUtilitiesFor
+        from zope.component.utility import GlobalUtilityService
+        from zope.component.interfaces import IUtilityService
+        from zope.component.servicenames import Utilities
+        sm = getGlobalServices()
+        try:
+            utilityService = getService(Utilities)
+        except ComponentLookupError:
+            sm.defineService(Utilities, IUtilityService)
+            sm.provideService(Utilities, GlobalUtilityService())
+            utilityService = getService(Utilities)
+
+        utilityService.provideUtility(ISQLAlchemyWrapper, wrapper, name)
+
+def allRegisteredSQLAlchemyWrappers():
+    """ return a dict containing information for all
+        registered wrappers.
+    """
+
+    for name, wrapper in getUtilitiesFor(ISQLAlchemyWrapper):
+        yield {'name' : name,
+               'dsn' : wrapper.dsn,
+               'echo' : wrapper.echo,
+              }
+
+
+if __name__ == '__main__':
+    print createWrapper('postgres://test:test@db.example.com/TestDB', None)
