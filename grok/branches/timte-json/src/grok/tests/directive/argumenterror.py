19e30c9e134931277634c34eb54a343fd47a183f
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2007-04-20 16:00:28 +0000

    This branch was merged into the trunk.
    git-svn-id: svn://svn.zope.org/repos/main@74269 62d5b8a3-27da-0310-9561-8e5933582275

--- grok/branches/timte-json/src/grok/tests/directive/argumenterror.py
+++
@@ -1,6 +0,0 @@
-"""
-   >>> import grok.tests.directive.argumenterror_fixture
-   Traceback (most recent call last):
-     ...
-   TypeError: grok.templatedir takes exactly 1 argument (3 given)
-"""
