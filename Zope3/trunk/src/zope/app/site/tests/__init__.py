120d0cf09195c14e7383ab1718e82d950b254fc9
baijum <baijum@62d5b8a3-27da-0310-9561-8e5933582275>
2007-04-21 10:54:28 +0000

    Fixes #98535 Use zope.deprecation to show deprecation of the module
    git-svn-id: svn://svn.zope.org/repos/main@74318 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/trunk/src/zope/app/site/tests/__init__.py
+++ Zope3/trunk/src/zope/app/site/tests/__init__.py
@@ -1,12 +1,5 @@
 import zope.deferredimport
 
-zope.deferredimport.deprecatedModule(
-    "zope.app.site.tests.placefulsetup",
-    "zope.app.component.testing",
-    "zope.app.site.tests.placefulsetup is deprecated and will go away "
-    "in Zope 3.5. Use zope.app.component.testing instead"
-    )
-
 zope.deferredimport.deprecated(
     "Import of PlacefulSetup from zope.app.site.testing is deprecated "
     "and will be disabled in Zope 3.5.  Import from "
