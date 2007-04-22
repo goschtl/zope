77177e0d296d16b8ec6c8593794cf58ee6b248f6
ctheune <ctheune@62d5b8a3-27da-0310-9561-8e5933582275>
2007-04-22 11:01:37 +0000

    Tagging 3.4.0a1
    git-svn-id: svn://svn.zope.org/repos/main@74510 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.i18n/tags/3.4.0a1/setup.py
@@ -0,0 +1,45 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.i18n package
+
+$Id$
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+setup(name='zope.i18n',
+      version='3.4dev',
+      url='http://svn.zope.org/zope.i18n',
+      license='ZPL 2.1',
+      description='Zope3 Internationalization Support',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      install_requires=['setuptools',
+                        'pytz',
+                        'zope.i18nmessageid',
+                        'zope.component',
+                        'zope.configuration',
+                        'zope.security', 
+                       ],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
