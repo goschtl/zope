fa7f9692d4f4830917e1207e38f9ac92017f0c0e
ctheune <ctheune@62d5b8a3-27da-0310-9561-8e5933582275>
2007-04-22 10:57:07 +0000

    Tagging 3.4.0a1
    git-svn-id: svn://svn.zope.org/repos/main@74492 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.contenttype/tags/3.4.0a1/setup.py
@@ -0,0 +1,41 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.contenttype package
+
+$Id$
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+setup(name='zope.contenttype',
+      version='3.4dev',
+      url='http://svn.zope.org/zope.contenttype',
+      license='ZPL 2.1',
+      description='Zope contenttype',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description="A utility module for content-type handling.",
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      install_requires=[],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
