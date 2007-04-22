395fb54bbbc60a6fa412998fffd29ec18f90b6bc
ctheune <ctheune@62d5b8a3-27da-0310-9561-8e5933582275>
2007-04-22 11:08:32 +0000

    Tagging 3.4.0a1
    git-svn-id: svn://svn.zope.org/repos/main@74534 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.sendmail/tags/3.4.0a1/setup.py
@@ -0,0 +1,50 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.sendmail package
+
+$Id$
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+setup(name='zope.sendmail',
+      version='3.4dev',
+      url='http://svn.zope.org/zope.sendmail',
+      license='ZPL 2.1',
+      description='Zope sendmail',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description="A package for email sending from Zope 3 applications.",
+
+      packages=find_packages('src'),
+	  package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      install_requires=['setuptools',
+                        'ZODB3',
+                        'zope.component',
+                        'zope.configuration',
+                        'zope.i18nmessageid',
+                        'zope.interface',
+                        'zope.schema',
+                        'zope.security',
+                        'zope.app.component',
+                       ],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
