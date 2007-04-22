d9aa73d07a30f4a292ea44f0cb89dbe39f09c0f2
ctheune <ctheune@62d5b8a3-27da-0310-9561-8e5933582275>
2007-04-22 11:00:57 +0000

    Updating version.
    git-svn-id: svn://svn.zope.org/repos/main@74507 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.fssync/tags/3.4.0a1/setup.py
+++ zope.fssync/tags/3.4.0a1/setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='zope.fssync',
-      version='3.4dev',
+      version = '3.4.0a1',
       url='http://svn.zope.org/zope.fssync',
       license='ZPL 2.1',
       description='Zope fssync',
