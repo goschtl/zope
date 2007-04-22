38a9c4af0f48e07583ff3b907e3d7876c2278c7c
ctheune <ctheune@62d5b8a3-27da-0310-9561-8e5933582275>
2007-04-22 11:10:09 +0000

    Tagging 3.4.0a1
    git-svn-id: svn://svn.zope.org/repos/main@74540 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.structuredtext/tags/3.4.0a1/setup.py
@@ -0,0 +1,39 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.i18nmessageid package
+
+$Id$
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+setup(name='zope.structuredtext',
+      version='3.4dev',
+      url='http://svn.zope.org/zope.structuredtext',
+      license='ZPL 2.1',
+      description='XXX',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description='',
+      
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
