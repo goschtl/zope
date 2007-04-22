69c339f8f681566ffff5fdc38dbeb7f88c9dc298
ctheune <ctheune@62d5b8a3-27da-0310-9561-8e5933582275>
2007-04-22 11:05:26 +0000

    Updating version.
    git-svn-id: svn://svn.zope.org/repos/main@74523 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.modulealias/tags/3.4.0a1/setup.py
+++ zope.modulealias/tags/3.4.0a1/setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zope.modulealias",
-    version="3.4dev",
+    version = '3.4.0a1',
     url='http://svn.zope.org/zope.modulealias',
     license='ZPL 2.1',
     description='Zope modulealias',
