f261d98044330e9cc0b422080f28ef5b20db2737
fdrake <fdrake@62d5b8a3-27da-0310-9561-8e5933582275>
2007-04-26 18:37:54 +0000

    new version number, newer than all existing releases
    git-svn-id: svn://svn.zope.org/repos/main@74810 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.app.intid/trunk/setup.py
+++ zope.app.intid/trunk/setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name = 'zope.app.intid',
-      version = '3.4dev',
+      version = '3.4.0a2',
       url = 'http://svn.zope.org/zope.app.intid',
       license = 'ZPL 2.1',
       description = 'Zope intid',
