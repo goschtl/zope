f404e4e7a343b12dfd4e9ee99bb07d24eee714f1
ksmith_mcweekly <ksmith_mcweekly@62d5b8a3-27da-0310-9561-8e5933582275>
2007-04-28 22:35:23 +0000

    out of sync
    git-svn-id: svn://svn.zope.org/repos/main@74883 62d5b8a3-27da-0310-9561-8e5933582275

--- grok/branches/ksmith_mcweekly-viewlet/doc/grok2pdf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-rst2latex.py --use-latex-toc --stylesheet=style.tex tutorial.txt > tutorial.tex
-pdflatex tutorial.tex
--- grok/branches/ksmith_mcweekly-viewlet/src/grok/tests/grokker/onlyonce_fixture/_meta.py
+++
@@ -1,8 +0,0 @@
-import grok
-from component import Alpha
-
-class AlphaGrokker(grok.ClassGrokker):
-    component_class = Alpha
-
-    def register(self, context, name, factory, module_info, templates):
-        print "alpha"
