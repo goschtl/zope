534cd77291c4711dccd304ecc7a6b3effe9cbfa0
ksmith_mcweekly <ksmith_mcweekly@62d5b8a3-27da-0310-9561-8e5933582275>
2007-05-02 07:15:30 +0000

    adjust svn layout
    git-svn-id: svn://svn.zope.org/repos/main@74989 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.quarry/src/megrok/quarry/__init__.py
+++
@@ -1,5 +0,0 @@
-# this directory is a package
-
-
-from directive import layer, template
-from components import Layer, Skin, View
--- megrok.quarry/src/megrok/quarry/directive.py
+++
@@ -1,7 +0,0 @@
-from grok.directive import InterfaceOrClassDirective, ClassOrModuleDirectiveContext
-from grok.directive import SingleTextDirective, ClassDirectiveContext
-
-layer = InterfaceOrClassDirective('quarry.layer',
-                           ClassOrModuleDirectiveContext())
-
-template = SingleTextDirective('quarry.template', ClassDirectiveContext())
