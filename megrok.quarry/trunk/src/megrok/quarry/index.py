8f833624b064183a01e03a50aa2d385380e39abd
ksmith_mcweekly <ksmith_mcweekly@62d5b8a3-27da-0310-9561-8e5933582275>
2007-05-04 00:29:51 +0000

    add preliminary support for zc.catalog indexes Set, Value, DatetimeSet, DatetimeValue
    git-svn-id: svn://svn.zope.org/repos/main@75246 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ megrok.quarry/trunk/src/megrok/quarry/index.py
@@ -0,0 +1,32 @@
+from grok.index import IndexDefinition
+from zc.catalog.catalogindex import ( SetIndex,
+                                      ValueIndex,
+                                      DateTimeValueIndex,
+                                      DateTimeSetIndex )
+
+
+class Set(IndexDefinition):
+    index_class = SetIndex
+
+
+class Value(IndexDefinition):
+    index_class = ValueIndex
+
+
+class DatetimeValue(IndexDefinition):
+    index_class = DateTimeValueIndex
+
+
+class DatetimeSet(IndexDefinition):
+
+    # DatetimeSet generates an error
+    # when index_class is set directly
+    # I set up this method for debugging
+    # and as soon as I did it worked :)
+    # YMMV
+    
+    @property
+    def index_class(self):
+        #import pdb; pdb.set_trace()
+        return DateTimeSetIndex
+
