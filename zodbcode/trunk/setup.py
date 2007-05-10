402e00df6b8f0d54548eb769e6615aa89fa4d5c2
baijum <baijum@62d5b8a3-27da-0310-9561-8e5933582275>
2007-05-10 13:16:02 +0000

    - version bumbed to 3.4.0b1 for dev  - set svn props
    git-svn-id: svn://svn.zope.org/repos/main@75670 62d5b8a3-27da-0310-9561-8e5933582275

--- zodbcode/trunk/setup.py
+++ zodbcode/trunk/setup.py
@@ -1,7 +1,25 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zodbcode package
+
+$Id$
+"""
+
 from setuptools import setup, find_packages, Extension
 
 setup(name='zodbcode',
-      version='0.1dev',
+      version='3.4.0b1',
       url='http://svn.zope.org/zodbcode',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
@@ -10,6 +28,8 @@ setup(name='zodbcode',
       package_dir = {'': 'src'},
       include_package_data = True,
 
+      install_requires = ['ZODB3',
+                          'zope.interface'],
       zip_safe = False,
       )
 
