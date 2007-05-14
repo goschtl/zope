46b29711a747521be46d792028dc17080affdbcd
ctheune <ctheune@62d5b8a3-27da-0310-9561-8e5933582275>
2007-05-14 15:14:40 +0000

    - completed buildout to run tests
    git-svn-id: svn://svn.zope.org/repos/main@75737 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.app.schemacontent/trunk/setup.py
+++ zope.app.schemacontent/trunk/setup.py
@@ -1,16 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = '',
+    name = 'zope.app.schemacontent',
     version = '0.1',
-    author = 'Jim Fulton',
-    author_email = 'jim@zope.com',
+    author = 'Zope Corporation and Contributors',
+    author_email = 'zope3-dev@zope.org',
     description = '',
     license = 'ZPL 2.1',
-    
+
     packages = find_packages('src'),
     namespace_packages = ['zope', 'zope.app'],
     package_dir = {'': 'src'},
-    install_requires = ['setuptools'],
+    install_requires = ['setuptools',
+                        'zope.app.i18n',
+                        'zope.app.zapi',
+                       'zope.app.form',
+                       'zope.component',
+                       'zope.interface',
+                       'zope.publisher',
+                       'zope.schema',
+                       'zope.security',
+                       'ZODB3',
+                       'zope.app.container',
+                       'zope.app.publisher'],
+    extras_require=dict(test=['zope.app.testing', 'zope.annotation']),
     zip_safe = False,
     )
