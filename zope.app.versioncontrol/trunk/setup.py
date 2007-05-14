0887b1c3ad2e3ec12b05fca7ceeb5775327ad88c
ctheune <ctheune@62d5b8a3-27da-0310-9561-8e5933582275>
2007-05-14 16:08:49 +0000

    - removed residual recorder code  - added bootstrap  - fixed dependencies and buildout configuration
    git-svn-id: svn://svn.zope.org/repos/main@75745 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.app.versioncontrol/trunk/setup.py
+++ zope.app.versioncontrol/trunk/setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = '',
+    name = 'zope.app.versioncontrol',
     version = '0.1',
     author = 'Jim Fulton',
     author_email = 'jim@zope.com',
     description = '',
     license = 'ZPL 2.1',
-    
+
     packages = find_packages('src'),
     namespace_packages = ['zope', 'zope.app'],
     package_dir = {'': 'src'},
-    install_requires = ['setuptools'],
+    extras_require=dict(test=['zope.event',
+                              'zope.traversing',
+                              'zope.annotation>=3.4.0b1.dev-r75055', 
+                              'zope.component']),
+    install_requires = ['setuptools',
+                        'ZODB3',
+                        'zope.location',
+                        'zope.security'],
     zip_safe = False,
     )
