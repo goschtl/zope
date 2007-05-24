780b2fe1d483d6c1513f9e8b2d943758c3b3b6d1
jinty <jinty@62d5b8a3-27da-0310-9561-8e5933582275>
2007-05-24 23:15:01 +0000

    Tag release.
    git-svn-id: svn://svn.zope.org/repos/main@75950 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.app.wfmc/tags/0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'zope.app.wfmc',
+    version = '0.1',
+    author = 'Zope Corporation and Contributors',
+    author_email = 'zope3-dev@zope.org',
+    description = '',
+    license = 'ZPL 2.1',
+
+    packages = find_packages('src'),
+    namespace_packages = ['zope', 'zope.app'],
+    package_dir = {'': 'src'},
+    extras_require=dict(test=['zope.app.testing']),
+    install_requires = ['setuptools',
+                       'zope.interface',
+                       'zope.schema',
+                       'zope.configuration',
+                       'zope.wfmc',
+                       'zope.component'],
+    zip_safe = False,
+    )
