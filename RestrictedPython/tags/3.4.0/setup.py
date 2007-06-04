839be0fce6ee087d103472004d08e0e3d23f8f4d
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2007-06-04 17:54:17 +0000

    Tag release
    git-svn-id: svn://svn.zope.org/repos/main@76325 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ RestrictedPython/tags/3.4.0/setup.py
@@ -0,0 +1,49 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for RestrictedPython package
+
+$Id$
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+name = 'RestrictedPython'
+setup(name=name,
+      version='3.4.0',
+      url='http://svn.zope.org/'+name,
+      license='ZPL 2.1',
+      description='Restricted Python executiion handlers',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description=(
+        read('README.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        ),
+
+      packages = find_packages('src'),
+      package_dir = {'': 'src'},
+
+      tests_require = ['zope.testing'],
+      install_requires = ['setuptools'],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
