f386687f50a086576da1da909cc0d261fe2585fc
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2007-06-10 14:06:48 +0000

    Fixed up setup. (Checking in changes made a while ago.)
    git-svn-id: svn://svn.zope.org/repos/main@76580 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.rebootstrap/trunk/setup.py
+++ zc.rebootstrap/trunk/setup.py
@@ -1,19 +1,26 @@
 kw = dict(
     name="zc.rebootstrap",
-    version="0.2",
+    version="0.2.2",
     author="Zope Corporation and Contributors",
     author_email="zope3-dev@zope.org",
     url="http://svn.zope.org/zc.rebootstrap",
     description=(
         "Helper package to re-bootstrap packages for RPM construction."),
-    packages=["zc.rebootstrap"],
+    packages=["zc", "zc.rebootstrap"],
     package_dir={"": "src"},
     )
+
+entry_points = """
+[zc.buildout]
+default = zc.rebootstrap.bootstrap:Recipe
+"""
 
 try:
     from setuptools import setup
     kw["namespace_packages"] = ["zc"]
     kw["zip_safe"] = True
+    kw['entry_points'] = entry_points
+    kw['namespace_packages'] = ['zc']
 except ImportError:
     from distutils.core import setup
     kw["packages"].insert(0, "zc")
