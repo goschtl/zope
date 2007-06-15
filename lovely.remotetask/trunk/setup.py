f001afbde4bd4bb5e86232726bd6fd67ef770b90
jukart <jukart@62d5b8a3-27da-0310-9561-8e5933582275>
2007-06-15 13:49:41 +0000

    We are not zip safe
    git-svn-id: svn://svn.zope.org/repos/main@76714 62d5b8a3-27da-0310-9561-8e5933582275

--- lovely.remotetask/trunk/setup.py
+++ lovely.remotetask/trunk/setup.py
@@ -14,6 +14,7 @@ setup (
     include_package_data = True,
     package_dir = {'':'src'},
     namespace_packages = ['lovely'],
+    zip_save = False,
     extras_require = dict(test = ['zope.app.testing',
                                   'zope.testing',
                                   'zope.app.securitypolicy',
