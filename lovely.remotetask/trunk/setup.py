532c0e7deb0e0b7b858f603239dd763f0824b0dc
jukart <jukart@62d5b8a3-27da-0310-9561-8e5933582275>
2007-06-15 13:30:30 +0000

    Version bumped to 0.2.1
    git-svn-id: svn://svn.zope.org/repos/main@76713 62d5b8a3-27da-0310-9561-8e5933582275

--- lovely.remotetask/trunk/setup.py
+++ lovely.remotetask/trunk/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup (
     name='lovely.remotetask',
-    version='0.2.0',
+    version='0.2.1',
     author = "Lovely Systems",
     author_email = "office@lovelysystems.com",
     description = "A remotetask client utiltiy for zope 3",
