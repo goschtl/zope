49c176518f511d64a9ad477a511650fe126f8e22
jinty <jinty@62d5b8a3-27da-0310-9561-8e5933582275>
2007-06-22 00:46:19 +0000

    Add the ZPL, better dependencies and a manifest to install extra files.
    git-svn-id: svn://svn.zope.org/repos/main@76928 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.optionstorage/trunk/setup.py
+++ z3c.optionstorage/trunk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'optionstorage',
+    name = 'z3c.optionstorage',
     version = '0.1',
     author = 'Zope Corporation and Contributors',
     author_email = 'zope3-dev@zope.org',
@@ -10,6 +10,13 @@ setup(
 
     packages = find_packages('src'),
     package_dir = {'': 'src'},
-    install_requires = ['setuptools'],
+    install_requires = ['zope.annotation',
+                        'setuptools',
+                        'zope.i18n',
+                        'zope.interface',
+                        'zope.proxy',
+                        'zope.schema',
+                        'zope.security',
+                        'zope.app.zapi'],
     zip_safe = False,
     )
