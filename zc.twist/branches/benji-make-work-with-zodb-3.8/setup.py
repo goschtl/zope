c7840562989c2eb5f13d4541889275089f65a7c1
benji_york <benji_york@62d5b8a3-27da-0310-9561-8e5933582275>
2007-06-28 21:05:06 +0000

    create a branch to make zc.twist work with ZODB 3.8
    git-svn-id: svn://svn.zope.org/repos/main@77193 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zc.twist/branches/benji-make-work-with-zodb-3.8/setup.py
@@ -0,0 +1,23 @@
+import os
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zc.twist',
+    version='0.1',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    url='http://svn.zope.org/zc.twist',
+    zip_safe=False,
+    author='Gary Poster',
+    description='Mixing Twisted and ZODB',
+    license='ZPL',
+    install_requires=[
+        'ZODB3 ==3.7',
+        'zope.component',
+        'setuptools',
+        'twisted ==2.1dev',
+        'zope.testing',
+        ],
+    include_package_data=True,
+    )
