36a3472f8debfedf16c8b727fc6f38eae7e0abea
pcardune <pcardune@62d5b8a3-27da-0310-9561-8e5933582275>
2007-06-29 03:48:37 +0000

    Added more enhanced code viewing.
    git-svn-id: svn://svn.zope.org/repos/main@77201 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/pcardune/z3c.formjsdemo/trunk/src/z3c/formjsdemo/button/browser.py
+++ Sandbox/pcardune/z3c.formjsdemo/trunk/src/z3c/formjsdemo/button/browser.py
@@ -1,24 +1,40 @@
 import os.path
 import zope.interface
-from z3c.form import form, button
+from z3c.form import form, button, field
 from z3c.formui import layout
 from z3c.formjs import jsbutton, jsevent
 
 class IButtons(zope.interface.Interface):
-    show = jsbutton.JSButton(title=u'Show Code')
-    hide = jsbutton.JSButton(title=u'Hide Code')
+    show = jsbutton.JSButton(title=u'Show JavaScript')
+    hide = jsbutton.JSButton(title=u'Hide JavaScript')
+
+class IFields(zope.interface.Interface):
+    file = zope.schema.Choice(
+        title=u"File",
+        description=u"The file to show.",
+        required=True,
+        default=u"None",
+        values=(u"None",u"browser.py",u"button.pt",u"configure.zcml")
+        )
 
 class ButtonForm(layout.FormLayoutSupport, form.EditForm):
 
     buttons = button.Buttons(IButtons)
+    fields = field.Fields(IFields)
 
-    @jsevent.handler(IButtons['show'])
+    @jsevent.handler(buttons['show'])
     def apply(self, id):
-        return '$("#code").slideDown()'
+        return '$("#javascript").slideDown()'
 
-    @jsevent.handler(IButtons['hide'])
+    @jsevent.handler(buttons['hide'])
     def apply(self, id):
-        return '$("#code").slideUp()'
+        return '$("#javascript").slideUp()'
+
+    @jsevent.handler(fields['file'], event=jsevent.CHANGE)
+    def handleFileChange(self, id):
+        return '''
+            $(".code").hide();
+            $("#"+$("#%s").val().replace(".","-")).show();''' % id
 
     def getFile(self, filename):
         here = os.path.dirname(os.path.abspath(__file__))
