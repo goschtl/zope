5143741c3d42fc1c9138d654aca028f81d57f06c
dobe <dobe@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-03 16:08:14 +0000

    tag release
    git-svn-id: svn://svn.zope.org/repos/main@77363 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ lovely.memcached/tags/0.1.2/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup (
+    name='lovely.memcached',
+    version='0.1.2',
+    author = "Lovely Systems",
+    author_email = "office@lovelysystems.com",
+    description = "A memcached client utiltiy for zope 3",
+    license = "ZPL 2.1",
+    keywords = "zope3 zope memcached cache ram",
+    url = 'svn://svn.zope.org/repos/main/lovely.memcached',
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['lovely'],
+    extras_require = dict(test = ['z3c.configurator',
+                                  'z3c.testing',
+                                  'zope.app.keyreference',
+                                  'zope.app.securitypolicy',
+                                  'zope.app.testing',
+                                  'zope.app.zcmlfiles',
+                                  'z3c.testing',
+                                  'zope.testbrowser',
+                                  ]),
+    install_requires = ['setuptools',
+                        'python-memcached',
+                        'zope.app.component',
+                        'zope.app.intid',
+                        'zope.event',
+                        'zope.interface',
+                        'zope.lifecycleevent',
+                        'zope.schema',
+                        'zope.security'
+                        ],
+    zip_safe = False,
+    )
