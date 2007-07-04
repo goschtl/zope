4a00b44c43224655156ab5dcbb08c499c9eccfa1
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-04 23:01:57 +0000

    Experimental code to deal with SVN. This is rather hard to test automatically...
    git-svn-id: svn://svn.zope.org/repos/main@77439 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.vcsync/trunk/src/z3c/vcsync/svn.py
+++ z3c.vcsync/trunk/src/z3c/vcsync/svn.py
@@ -13,12 +13,38 @@ class SvnCheckout(CheckoutBase):
     
     It is assumed to be initialized with py.path.svnwc
     """
-    
+
+    def __init__(self, path):
+        super(SvnCheckout, self).__init__(path)
+        self._log_info = {'D':[], 'A':[], 'M':[]}
+      
     def up(self):
+        original_rev = self.path.status().rev
+
         self.path.update()
 
+        now_rev = self.path.status().rev
+        logs = self.path.log(now_rev, original_rev + 1, verbose=True)
+        log_info = {'D': [], 'A': [], 'M':[]}
+        for log in logs:
+            entries = log_info[log.action]
+            for p in log.strpaths:
+                entries.append(py.path.local(p.strpath))
+        self._log_info = log_info
+        
     def resolve(self):
         pass
 
     def commit(self, message):
         self.path.commit(message)
+
+    def added(self):
+        return self._log_info['A']
+    
+    def deleted(self):
+        return self._log_info['D']
+
+    def modified(self):
+        return self._log_info['M']
+
+        
