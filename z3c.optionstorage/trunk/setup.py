00cdb586a8254ccbc08a537260fa4eb5cf90586d
jinty <jinty@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-04 07:27:49 +0000

    Another bugfix, argh
    git-svn-id: svn://svn.zope.org/repos/main@77388 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.optionstorage/trunk/setup.py
+++ z3c.optionstorage/trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'z3c.optionstorage',
-    version = '0.1.1',
+    version = '0.1.2',
     author = 'Zope Corporation and Contributors',
     author_email = 'zope3-dev@zope.org',
     description = '',
