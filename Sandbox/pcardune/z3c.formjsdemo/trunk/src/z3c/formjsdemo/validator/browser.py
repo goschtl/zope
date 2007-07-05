56582f14a4ada8829e0f20a770154671dd072f76
pcardune <pcardune@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-05 13:34:33 +0000

    first work on a javascript validator demo.  This won't work until the validator traverser in z3c.formjs works.
    git-svn-id: svn://svn.zope.org/repos/main@77458 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Sandbox/pcardune/z3c.formjsdemo/trunk/src/z3c/formjsdemo/validator/browser.py
@@ -0,0 +1,29 @@
+import os.path
+import zope.interface
+from z3c.form import form, button, field
+from z3c.form.interfaces import IField, IWidgets
+from z3c.formui import layout
+from z3c.formjs import jsbutton, jsevent, jsvalidator, interfaces
+
+
+class IFields(zope.interface.Interface):
+    zip = zope.schema.Int(
+        title=u"File",
+        description=u"The file to show.",
+        required=True)
+
+
+class ValidatorForm(layout.FormLayoutSupport, jsvalidator.MessageValidator, form.Form):
+    zope.interface.implements(interfaces.IAJAXValidator)
+    fields = field.Fields(IFields)
+
+    @jsevent.handler(IField, event=jsevent.CHANGE)
+    def fieldValidator(self, id):
+        return self.ValidationRenderer(self, id).render()
+
+    def updateWidgets(self):
+        '''See interfaces.IForm'''
+        self.widgets = zope.component.getMultiAdapter(
+            (self, self.request, self.getContent()), IWidgets)
+        self.widgets.ignoreContext = True
+        self.widgets.update()
