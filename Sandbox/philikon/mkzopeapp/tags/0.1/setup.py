d58c934157109f793fa39590beb70291d1deef1b
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-06 19:18:50 +0000

    Tag 0.1
    git-svn-id: svn://svn.zope.org/repos/main@77526 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Sandbox/philikon/mkzopeapp/tags/0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mkzopeapp',
+    version='0.1',
+    author='Philipp von Weitershausen',
+    author_email='philipp@weitershausen.de',
+    url='http://zope.org',
+    download_url='svn://svn.zope.org/repos/main/Sandbox/philikon/mkzopeapp/trunk#egg=mkzopeapp-dev',
+    description='Setup script for a Zope application',
+    long_description=open('README.txt').read(),
+    license='ZPL',
+
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['PasteScript>=1.3',],
+    entry_points="""
+    [console_scripts]
+    mkzopeapp = mkzopeapp:main
+    [paste.paster_create_template]
+    zope_app = mkzopeapp:ZopeApp
+    """,
+)
