cbaa332ee3a7253a73d54e3152ec425289f0da5a
darrylcousins <darrylcousins@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-06 21:08:45 +0000

    git-svn-id: svn://svn.zope.org/repos/main@77532 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/darrylcousins/mars.formdemo/mars.adapter/src/mars/adapter/interfaces.py
+++
@@ -1,8 +0,0 @@
-import zope.interface
-
-class IMarsAdapterDirectives(zope.interface.Interface):
-
-    def factory(factory):
-        """The factory to be registered as an adapter"""
-        pass
-
--- Sandbox/darrylcousins/mars.formdemo/mars.template/src/mars/template/directive.py
+++
@@ -1,8 +0,0 @@
-from martian.directive import (SingleTextDirective,
-                               ClassDirectiveContext)
-
-macro = SingleTextDirective('mars.template.macro',
-                           ClassDirectiveContext())
-content_type = SingleTextDirective('mars.template.content_type',
-                           ClassDirectiveContext())
-
