d7ed9c2df794a52bc58beb24bba69850a9686085
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-06 19:19:11 +0000

    bump version number on trunk after tagging release
    git-svn-id: svn://svn.zope.org/repos/main@77527 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/philikon/mkzopeapp/trunk/setup.py
+++ Sandbox/philikon/mkzopeapp/trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mkzopeapp',
-    version='0.1',
+    version='0.2',
     author='Philipp von Weitershausen',
     author_email='philipp@weitershausen.de',
     url='http://zope.org',
