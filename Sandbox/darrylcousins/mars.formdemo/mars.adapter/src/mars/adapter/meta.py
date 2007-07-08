c6c8d6696adddacf4fcb1d3a33ef1eaa3e644f00
darrylcousins <darrylcousins@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-08 10:37:29 +0000

    Tried to improve adaper grokker. Because I ran into problems with getAnnotation passing thrugh martian as an unbound method, and therefore failing
    git-svn-id: svn://svn.zope.org/repos/main@77609 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/darrylcousins/mars.formdemo/mars.adapter/src/mars/adapter/meta.py
+++ Sandbox/darrylcousins/mars.formdemo/mars.adapter/src/mars/adapter/meta.py
@@ -12,19 +12,18 @@ class AdapterFactoryGrokker(martian.ClassGrokker):
     component_class = mars.adapter.AdapterFactory
 
     def grok(self, name, factory, context, module_info, templates):
-        adapter_context = util.determine_class_context(factory, context)
-        provides = util.class_annotation(factory, 'grok.provides', None)
+        #adapter_context = util.determine_class_context(factory, context)
+        #provides = util.class_annotation(factory, 'grok.provides', None)
         name = util.class_annotation(factory, 'grok.name', '')
         factory = util.class_annotation(factory, 'mars.adapter.factory', None)
         #print '\nName: ', name, 'Factory:', factory, '\n'
+        provided = zope.component.registry._getAdapterProvided(factory)
+        required = zope.component.registry._getAdapterRequired(factory, None)
         if factory is None:
-            # TODO error message
+            # error message
             pass
         else:
-            #zope.component.provideAdapter(factory, adapts=(zope.interface.Interface,),
-            #                         provides=provides,
-            #                         name=name)
-            zope.component.provideAdapter(factory,
+            zope.component.provideAdapter(factory, adapts=required, provides=provided,
                                      name=name)
         return True
 
