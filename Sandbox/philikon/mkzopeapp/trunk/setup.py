83201ab75d534d27a3d257730adec651a33386af
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-11 20:34:05 +0000

    Rename egg from mkzopeapp to make-zope-app.
    git-svn-id: svn://svn.zope.org/repos/main@77698 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/philikon/mkzopeapp/trunk/setup.py
+++ Sandbox/philikon/mkzopeapp/trunk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mkzopeapp',
+    name='make-zope-app',
     version='0.2',
     author='Philipp von Weitershausen',
     author_email='philipp@weitershausen.de',
