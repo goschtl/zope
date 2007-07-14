f8bef13bea615387d3c62f08f59b70f78909ecbf
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-14 17:40:50 +0000

    Move grok template away
    git-svn-id: svn://svn.zope.org/repos/main@77966 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/philikon/zopeproject/trunk/zopeproject/templates.py
+++ Sandbox/philikon/zopeproject/trunk/zopeproject/templates.py
@@ -24,29 +24,3 @@ class ZopeApp(Template):
     _template_dir = 'zope_app'
     summary = 'Package that contains a Zope application'
     required_templates = ['zope_deploy']
-
-class GrokApp(Template):
-    _template_dir = 'template'
-    summary = 'Package that contains a Grok application'
-    required_templates = ['zope_deploy']
-
-    vars = [
-        var('module', 'Name of a demo Python module placed into the package',
-            default='app.py')
-        ]
-
-    def check_vars(self, vars, cmd):
-        vars = super(GrokApp, self).check_vars(vars, cmd)
-        module = vars['module']
-        if '.' in module:
-            if module.endswith('.py'):
-                vars['module'] = module[:-3]
-            else:
-                raise command.BadCommand('Bad module name: %s' % module)
-        if vars['package'] in ('grok', 'zope'):
-            print
-            print "Error: The chosen project name results in an invalid " \
-                  "package name: %s." % vars['package']
-            print "Please choose a different project name."
-            sys.exit(1)
-        return vars
