b8568e0bac5e599c93a872bf43387b0ecff5ddde
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-17 03:01:59 +0000

    Prepare for merge with trunk.
    git-svn-id: svn://svn.zope.org/repos/main@78049 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Sandbox/ulif/grok-adminui-mergeprepare2/src/grok/admin/view.py
@@ -0,0 +1,515 @@
+import grok
+import os
+import inspect
+from grok.admin import docgrok
+from grok.admin.docgrok import DocGrok, DocGrokPackage, DocGrokModule
+from grok.admin.docgrok import DocGrokClass, DocGrokInterface, DocGrokGrokApplication
+from grok.admin.docgrok import DocGrokTextFile
+
+import zope.component
+from zope.interface import Interface
+from zope.app.folder.interfaces import IRootFolder
+
+from zope.app import zapi
+from zope.app.applicationcontrol.interfaces import IServerControl
+from zope.app.applicationcontrol.applicationcontrol import applicationController
+from zope.app.applicationcontrol.runtimeinfo import RuntimeInfo
+from zope.app.applicationcontrol.browser.runtimeinfo import RuntimeInfoView
+
+from zope.interface.interface import InterfaceClass
+from zope.app.apidoc import utilities, codemodule
+from zope.app.apidoc.utilities import getPythonPath, renderText, columnize
+from zope.app.apidoc.codemodule.module import Module
+from zope.app.apidoc.codemodule.class_ import Class
+from zope.app.apidoc.codemodule.function import Function
+from zope.app.apidoc.codemodule.text import TextFile
+from zope.app.apidoc.codemodule.zcml import ZCMLFile
+
+
+from zope.proxy import removeAllProxies
+
+import z3c.flashmessage.interfaces
+
+
+grok.context(IRootFolder)
+grok.define_permission('grok.ManageApplications')
+
+
+class Add(grok.View):
+    """Add an application.
+    """
+
+    grok.require('grok.ManageApplications')
+
+    def update(self, inspectapp=None, application=None):
+        if inspectapp is not None:
+            self.redirect( self.url("docgrok") + "/%s/index"%(application.replace('.','/'),))
+        return 
+
+    def render(self, application, name, inspectapp=None):
+        if name is None or name == "":
+            self.redirect(self.url(self.context))
+            return
+        app = zope.component.getUtility(grok.interfaces.IApplication,
+                                        name=application)
+        self.context[name] = app()
+        self.flash(u'Added %s `%s`.' % (application, name))
+        self.redirect(self.url(self.context))
+
+
+class Delete(grok.View):
+    """Delete an application.
+    """
+
+    grok.require('grok.ManageApplications')
+
+    def render(self, items=None):
+        if items is None:
+            self.redirect(self.url(self.context))
+            return
+        if not isinstance(items, list):
+            items = [items]
+        for name in items:
+            del self.context[name]
+        self.redirect(self.url(self.context))
+
+
+class GAIAView(grok.View):
+    """A grok.View with a special application_url.
+
+    We have to compute the application_url different from common
+    grok.Views, because we have no root application object in the
+    adminUI. To avoid mismatch, we also call it 'root_url'.
+
+    """
+
+    def root_url(self, name=None):
+        obj = self.context
+        result = ""
+        while obj is not None:
+            if __grok_context__.providedBy(obj):
+                return self.url(obj, name)
+            obj = obj.__parent__
+        raise ValueError("No application nor root element found.")
+
+    def in_docgrok(self):
+        return '/docgrok/' in self.url()
+
+
+def getDottedPathDict(dotted_path):
+    """Get a dict containing parts of a dotted path as links.
+    """
+    if dotted_path is None:
+        return {}
+    
+    result = []
+    part_path = ""
+    for part in dotted_path.split( '.' ):
+        name = part
+        if part_path != "":
+            name = "." + part
+        part_path += part
+        result.append( {
+            'name':name,
+            'url':"/docgrok/%s" % (part_path,)
+            })
+        part_path += "/"
+    return result
+
+
+
+class Inspect(GAIAView):
+    """Basic object browser.
+    """
+    grok.context(Interface)
+
+    def __init__(self, context, request):
+        # Leave out the Introspector init, because it requires
+        # ++apidoc++ to be enabled and setups skin-related stuff we
+        # don't want.
+        super(GAIAView, self).__init__(context,request)
+
+ 
+    def getPathParts(self,dotted_path):
+        return getDottedPathDict(dotted_path)
+
+    def getId(self):
+        if hasattr( self.context, '__name__'):
+            return self.context.__name__
+        if hasattr( self.context, 'id' ):
+            return self.context.id
+        return
+
+    def getZODBPath(self):
+        # XXX To be implemented.
+        return
+
+    def getDottedPath(self):
+        if hasattr(self.context, '__class__'):
+            klassname = str(self.context.__class__)
+            return klassname.rsplit("'", 2)[1]
+        return
+
+
+    def getSize(self):
+        # XXX To be implemented.
+        return
+
+    def getCreationDate(self):
+        # XXX To be implemented.
+        return
+
+    def getModificationDate(self):
+        # XXX To be implemented.
+        return
+
+    def isBroken(self):
+        # XXX To be implemented.
+        return
+
+    def getSecurityInfo(self):
+        # XXX To be implemented.
+        return
+
+    def getParent(self):
+        if hasattr( self.context, '__parent__'):
+            return self.context.__parent__
+        return
+
+    def getChildren(self):
+        # XXX To be implemented.
+        return
+
+    def getType(self):
+        if hasattr(self.context, '__class__'):
+            klassname = str(self.context.__class__)
+            return klassname.rsplit("'", 2)[1]
+        return str(self.context)
+
+
+class Index(GAIAView):
+    """A redirector to the real frontpage."""
+
+    grok.name('index.html') # The root folder is not a grok.Model
+    grok.require('grok.ManageApplications')
+
+    def update(self):
+        apps = zope.component.getAllUtilitiesRegisteredFor(
+            grok.interfaces.IApplication)
+        self.applications = ("%s.%s" % (x.__module__, x.__name__)
+                             for x in apps)
+        # Go to the first page immediately.
+        self.redirect(self.url('applications'))
+
+
+class Applications(GAIAView):
+    """View for application management."""
+
+    grok.name('applications')
+    grok.require('grok.ManageApplications')
+
+    def getDocOfApp(self, apppath, headonly = True):
+        doctor = docgrok.handle(apppath)
+        result = doctor.getDoc(headonly)
+        if result is None:
+            result = ""
+        return result
+
+    def update(self):
+        apps = zope.component.getAllUtilitiesRegisteredFor(
+            grok.interfaces.IApplication)
+        inst_apps = [x for x in self.context.values()
+                     if hasattr(x, '__class__') and x.__class__ in apps]
+        self.applications = (
+          {'name': "%s.%s" % (x.__module__, x.__name__),
+           'docurl':("%s.%s" % (x.__module__, x.__name__)).replace( '.', '/')}
+          for x in apps)
+        self.installed_applications = inst_apps
+
+
+class AdminMessageSource(grok.GlobalUtility):
+
+    grok.name('admin')
+    zope.interface.implements(z3c.flashmessage.interfaces.IMessageSource)
+
+    message = None
+
+    def send(self, message, type='admin'):
+        self.message = z3c.flashmessage.message.PersistentMessage(message,
+                                                                  type)
+
+    def list(self, type=None):
+        if self.message is None:
+            return
+        if type is None or self.message.type == type:
+            yield self.message
+
+    def delete(self, message):
+        if message is self.message:
+            self.message = None
+        else:
+            raise KeyError(message)
+
+
+class Server(GAIAView):
+    """Zope3 management screen."""
+
+    grok.require('grok.ManageApplications')
+
+    @property
+    def server_control(self):
+        return zapi.getUtility(IServerControl)
+
+    @property
+    def runtime_info(self):
+        riv = RuntimeInfoView()
+        riv.context = applicationController
+        return riv.runtimeInfo()
+
+    @property
+    def current_message(self):
+        source = zope.component.getUtility(
+          z3c.flashmessage.interfaces.IMessageSource, name='admin')
+        messages = list(source.list())
+        if messages:
+            return messages[0]
+
+    def update(self, time=None, restart=None, shutdown=None,
+              admin_message=None, submitted=False):
+        if not submitted:
+            return
+        # Admin message control
+        source = zope.component.getUtility(
+          z3c.flashmessage.interfaces.IMessageSource, name='admin')
+        if admin_message is not None:
+            source.send(admin_message)
+        elif source.current_message:
+            source.delete(source.current_message)
+
+        # Restart control
+        if time is not None:
+            try:
+                time = int(time)
+            except:
+                time = 0
+        else:
+            time = 0
+
+        if restart is not None:
+            self.server_control.restart(time)
+        elif shutdown is not None:
+            self.server_control.shutdown(time)
+
+        self.redirect(self.url())
+
+
+class Macros(GAIAView):
+    """Provides the o-wrap layout."""
+
+    grok.context(Interface)
+
+
+class DocGrokView(GAIAView):
+    """A base DocGrok view.
+
+    This view is used for all things not covered by other, more
+    specialized views.
+    """
+
+    grok.context(DocGrok)
+    grok.name( 'index' )
+
+    def getDoc(self, text=None, heading_only=False):
+        """Get the doc string of the module STX formatted."""
+        if text is None:
+            return None
+            if (hasattr(self.context, "apidoc") and
+                hasattr(self.context.apidoc, "getDocString")):
+                text = self.context.apidoc.getDocString()
+            else:
+                return None
+        lines = text.strip().split('\n')
+        if len(lines) and heading_only:
+            # Find first empty line to separate heading from trailing text.
+            headlines = []
+            for line in lines:
+                if line.strip() == "":
+                    break
+                headlines.append(line)
+            lines = headlines
+        # Get rid of possible CVS id.
+        lines = [line for line in lines if not line.startswith('$Id')]
+        return renderText('\n'.join(lines), self.context.getPath())
+
+    def getDocHeading( self, text=None):
+        return self.getDoc( text, True)
+
+    def getPathParts(self, path=None):
+        """Get parts of a dotted name as url and name parts.
+        """
+        if path is None:
+            path = self.context.path
+        if path is None:
+            return None
+        return getDottedPathDict(path)
+        result = []
+        part_path = ""
+        for part in path.split( '.' ):
+            name = part
+            if part_path != "":
+                name = "." + part
+            part_path += part
+            result.append( {
+                'name':name,
+                'url':"/docgrok/%s" % (part_path,)
+                })
+            part_path += "/"
+        return result
+
+    def getEntries( self, columns=True ):
+        """Return info objects for all modules and classes in the
+        associated apidoc container.
+
+        """
+        if (not hasattr(self.context, "apidoc") or
+            not hasattr(self.context.apidoc, "items")):
+            return None
+        entries = []
+        for name, obj in self.context.apidoc.items():
+            entry = {
+                'name': name,
+                'obj' : obj,
+                'path': getPythonPath(removeAllProxies(obj)),
+                'url' : u'',
+                'doc' : None,
+                'ispackage' : False,
+                'ismodule' : False,
+                'isinterface' : False,
+                'isclass' : False,
+                'isfunction' : False,
+                'istextfile' : False,
+                'iszcmlfile' : False,
+                'signature' : None
+                }
+            entry['url'] = "%s/%s" % (self.context.path.replace('.','/'), name)
+            if hasattr(obj,"getDocString"):
+                entry['doc'] = self.getDocHeading(obj.getDocString())
+            elif hasattr(obj, "getDoc") and isinstance(
+                removeAllProxies(obj), InterfaceClass):
+                entry['doc'] = self.getDocHeading(obj.getDoc())
+            if isinstance(obj, Class):
+                entry['isclass'] = True
+            elif isinstance(obj, TextFile):
+                entry['istextfile'] = True
+            elif isinstance(obj, ZCMLFile):
+                entry['iszcmlfile'] = True
+            elif isinstance(obj,Function):
+                entry['isfunction'] = True
+                if hasattr(obj, 'getSignature'):
+                    entry['signature'] = obj.getSignature()
+            elif (isinstance(obj,Module) and
+                  os.path.basename(obj.getFileName()) in
+                    ['__init.py__', '__init__.pyc', '__init__.pyo']):
+                entry['ispackage'] = True
+            elif isinstance(obj,Module):
+                entry['ismodule'] = True
+            entries.append(entry)
+
+        entries.sort(lambda x, y: cmp(x['name'], y['name']))
+        return entries
+
+    def update(self):
+        self.docgrok_root = self.context._traversal_root
+        self.app_root = self.docgrok_root.__parent__
+        pass
+
+
+class DocGrokPackageView(DocGrokView):
+    """A view for packages handled by DocGrok."""
+
+    grok.context(DocGrokPackage)
+    grok.name( 'index' )
+
+
+class DocGrokModuleView(DocGrokView):
+    """A view for modules handled by DocGrok."""
+
+    grok.context(DocGrokModule)
+    grok.name( 'index' )
+
+
+class DocGrokClassView(DocGrokView):
+    """A view for classes handled by DocGrok."""
+
+    grok.context(DocGrokClass)
+    grok.name( 'index' )
+
+    def getBases(self):
+        return self._listClasses(self.context.apidoc.getBases())
+
+    def getInterfaces(self):
+        return self._listClasses(
+          [iface for iface in self.context.apidoc.getInterfaces()])
+
+    def getAttributes(self):
+        attrs = self.context.getAttributes()
+        for a in attrs:
+            a['interface'] = self._listClasses([a['interface']])
+        return attrs
+
+    def getMethods(self):
+        methods = self.context.getMethods()
+        for m in methods:
+            m['doc'] = renderText(m['attr'].__doc__ or '',
+                                  inspect.getmodule(m['attr']))
+            m['interface'] = self._listClasses([m['interface']])
+        return methods
+
+    def _listClasses(self, classes):
+        info = []
+        for cls in classes:
+            unwrapped_cls = removeAllProxies(cls)
+            fullpath = getPythonPath(unwrapped_cls)
+            if not fullpath:
+                continue
+            path, name = fullpath.rsplit('.', 1)
+            info.append( {
+                'path': path or None,
+                'path_parts' : self.getPathParts( path ) or None,
+                'name': name,
+                'url': fullpath and fullpath.replace('.','/') or None,
+                'doc': self.getDocHeading( cls.__doc__ ) or None
+                })
+        return info
+
+
+class DocGrokInterfaceView(DocGrokClassView):
+
+    grok.context(DocGrokInterface)
+    grok.name( 'index' )
+
+
+class DocGrokGrokApplicationView(DocGrokClassView):
+
+    grok.context(DocGrokGrokApplication)
+    grok.name( 'index' )
+
+
+class DocGrokTextFileView(DocGrokView):
+
+    grok.context(DocGrokTextFile)
+    grok.name( 'index' )
+
+    def getContent(self):
+        lines = self.context.getContent()
+        if self.context.path.endswith('.stx'):
+            format = 'zope.source.stx'
+        else:
+            format = 'zope.source.rest'
+        return renderText(lines, format=format)
+
+    def getPackagePathParts(self):
+        return self.getPathParts(
+            self.context.getPackagePath())
+
+
---
+++ Sandbox/ulif/grok-adminui-mergeprepare2/src/grok/ftests/admin/apps.py
@@ -0,0 +1,75 @@
+"""
+  
+We fetch the standard page, which should provide us a menu to get all
+installable grok applications/components.
+
+  >>> import grok
+  >>> grok.grok('grok.ftests.admin.apps')
+
+  >>> from zope.testbrowser.testing import Browser
+  >>> browser = Browser()
+  >>> browser.addHeader('Authorization', 'Basic mgr:mgrpw')
+  >>> browser.handleErrors = False
+  >>> browser.open("http://localhost/")
+  >>> print browser.contents
+  <html xmlns="http://www.w3.org/1999/xhtml">
+  ...
+  ...      <legend>Add application</legend>
+  ...
+
+We are able to add a mammoth manager...
+
+  >>> browser.getControl('Name your new app:',index=13).value = 'my-mammoth-manager'
+  >>> browser.getControl('Create',index=13).click()
+
+  >>> print browser.contents
+  <html xmlns="http://www.w3.org/1999/xhtml">
+  ...
+  ... <legend>Installed applications</legend>
+  ... <input type="checkbox" class="checkbox" name="items"
+             value="my-mammoth-manager" />
+      <a href="http://localhost/my-mammoth-manager">
+           my-mammoth-manager
+           (MammothManager)
+        </a>
+  ... <legend>Add application</legend>
+  ...
+
+Launch the added mammoth manager
+
+  >>> mylink = browser.getLink('my-mammoth-manager (MammothManager)').click()
+  >>> print browser.contents
+  Let's manage some mammoths!
+
+  >>> print browser.url
+  http://localhost/my-mammoth-manager
+
+We are able to delete installed mammoth-managers
+
+  >>> browser.open("http://localhost/")
+  >>> print browser.contents
+  <html xmlns="http://www.w3.org/1999/xhtml">
+  ...
+  ... <legend>Installed applications</legend>
+  ...
+  >>> ctrl = browser.getControl(name='items')
+  >>> ctrl.getControl(value='my-mammoth-manager').selected = True
+  >>> browser.getControl('Delete Selected').click()
+  >>> print browser.contents
+  <html xmlns="http://www.w3.org/1999/xhtml">
+  ...
+  ...<legend>Add application</legend>
+  ...
+
+"""
+
+import grok
+
+class MammothManager(grok.Application, grok.Container):
+    """A mammoth manager"""
+    pass
+
+class Index(grok.View):#
+
+    def render(self):
+        return u"Let's manage some mammoths!"
