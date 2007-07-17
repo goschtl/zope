a8ceb51870575e953fcdf39b0a60fde908d58833
darrylcousins <darrylcousins@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-17 11:56:01 +0000

    Working up tests
    git-svn-id: svn://svn.zope.org/repos/main@78067 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/darrylcousins/mars.contentprovider/src/mars/contentprovider/tests.py
+++ Sandbox/darrylcousins/mars.contentprovider/src/mars/contentprovider/tests.py
@@ -1,6 +1,20 @@
 import unittest
 from zope.testing import doctest
 
+import zope.interface
+
+from martian.interfaces import IModuleInfo
+
+class ModuleInfo(object):
+    zope.interface.implements(IModuleInfo)
+    path = ''
+    package_dotted_name = ''
+
+    def getAnnotation(self, name, default):
+        return default
+
+globs = dict(module_info=ModuleInfo())
+
 optionflags = doctest.NORMALIZE_WHITESPACE + doctest.ELLIPSIS
 
 def setUp(test):
