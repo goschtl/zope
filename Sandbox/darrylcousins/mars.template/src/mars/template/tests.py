f9090345ffdd071ecb88d61fd6b7f4fb12cd09a3
darrylcousins <darrylcousins@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-17 11:51:40 +0000

    Working up tests
    git-svn-id: svn://svn.zope.org/repos/main@78064 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/darrylcousins/mars.template/src/mars/template/tests.py
+++ Sandbox/darrylcousins/mars.template/src/mars/template/tests.py
@@ -1,6 +1,18 @@
 import unittest
 from zope.testing import doctest
 
+import zope.interface
+from martian.interfaces import IModuleInfo
+class ModuleInfo(object):
+    zope.interface.implements(IModuleInfo)
+    path = ''
+    package_dotted_name = ''
+
+    def getAnnotation(self, name, default):
+        return default
+
+globs = dict(module_info=ModuleInfo())
+
 optionflags = doctest.NORMALIZE_WHITESPACE + doctest.ELLIPSIS
 
 def setUp(test):
@@ -9,7 +21,7 @@ def setUp(test):
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([doctest.DocFileSuite('./template.txt',
-                             setUp=setUp,
+                             setUp=setUp, globs=globs,
                              optionflags=optionflags),
                    ])
 
