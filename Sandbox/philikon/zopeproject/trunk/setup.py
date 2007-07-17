319d3416c50200603f99b8d39322d23f287ac6d5
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-17 17:29:40 +0000

    bump version number on the trunk after release
    git-svn-id: svn://svn.zope.org/repos/main@78085 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/philikon/zopeproject/trunk/setup.py
+++ Sandbox/philikon/zopeproject/trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zopeproject',
-    version='0.3.2',
+    version='0.3.3',
     author='Philipp von Weitershausen',
     author_email='philipp@weitershausen.de',
     url='http://cheeseshop.python.org/pypi/zopeproject',
