3c857976d610a8b869f3f53555c0cee6e865afb4
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-19 16:41:18 +0000

    Implemented widget switch demo.
    git-svn-id: svn://svn.zope.org/repos/main@78177 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.formjsdemo/trunk/src/z3c/formjsdemo/widgetswitch/browser.py
@@ -0,0 +1,56 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Widget Switch Content
+
+$Id$
+"""
+__docformat__="restructuredtext"
+import zope.component
+import zope.interface
+from zope.app.session.interfaces import ISession
+from zope.viewlet.viewlet import JavaScriptViewlet
+
+from z3c.form import field, form
+from z3c.form.interfaces import IWidgets, DISPLAY_MODE
+from z3c.formjs import jsswitch
+from z3c.formui import layout
+from z3c.formjsdemo.widgetswitch import interfaces, content
+
+WidgetSwitchJSViewlet = JavaScriptViewlet('widgetswitch.js')
+
+class ContactForm(layout.FormLayoutSupport,
+                  jsswitch.WidgetModeSwitcher,
+                  form.EditForm):
+    fields = field.Fields(interfaces.IContact)
+    label = u'Contact Add Form'
+
+    def updateWidgets(self):
+        self.widgets = zope.component.getMultiAdapter(
+            (self, self.request, self.getContent()), IWidgets)
+        self.widgets.mode = DISPLAY_MODE
+        self.widgets.update()
+
+    def getContent(self):
+        session = ISession(self.request)['z3c.formjsdemo.widgetswitch']
+        obj = session.get('content')
+        if obj is None:
+            obj = session['content'] = content.Contact()
+            # Initialize data.
+            obj.name = u'Stephan Richter'
+            obj.street = u'110 Main Street'
+            obj.city = u'Maynard'
+            obj.zip = u'01754'
+            obj.phone = u'(555) 373-2134'
+            obj.age = 27
+        return obj
---
+++ z3c.formjsdemo/trunk/src/z3c/formjsdemo/widgetswitch/widgetswitch.js
@@ -0,0 +1,22 @@
+function switchWidget(widgetId, code) {
+    element = $('#' + widgetId + '-row div.widget');
+    element.html(code);
+}
+
+function saveWidget(widgetId, msg) {
+    element = $('#' + widgetId + '-row div.error');
+    if (element) {
+        element.remove();
+    }
+    $('#' + widgetId + '-row').append(
+        '<div class="error">' + msg+  '</div>');
+
+    if (msg != '') {
+        $('#' + widgetId).removeClass('valid');
+        $('#' + widgetId).addClass('invalid');
+    }
+    else {
+        $('#' + widgetId).removeClass('invalid');
+        $('#' + widgetId).addClass('valid');
+    }
+}
