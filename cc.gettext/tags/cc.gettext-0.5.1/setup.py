532e5289b9564e80c868573547170c007ab0978d
nathan <nathan@62d5b8a3-27da-0310-9561-8e5933582275>
2007-08-01 22:25:10 +0000

    Tagging 0.5.1 before I remove the forking silliness.
    git-svn-id: svn://svn.zope.org/repos/main@78534 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ cc.gettext/tags/cc.gettext-0.5.1/setup.py
@@ -0,0 +1,61 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Compile gettext catalogs from .po files.
+"""
+
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name = "cc.gettext",
+    version = "0.5.1",
+    packages = find_packages('.'),
+
+    # scripts and dependencies
+    install_requires = ['setuptools',
+                        'zc.buildout',
+                        ],
+
+    entry_points = {'zc.buildout':['msgfmt = cc.gettext:MsgFmt'],
+                    },
+    
+    # author metadata
+    author = 'Nathan R. Yergler',
+    author_email = 'nathan@creativecommons.org',
+    description = "Recipe for manipulating gettext message catalogs.",
+    long_description = (
+        read('README.txt')
+        + '\n' +
+        read('CHANGES.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        ),
+    license = 'ZPL 2.1',
+    keywords = 'development build gettext',
+    url = 'http://python.org/pypi/cc.gettext/',
+
+    classifiers = [
+       'Framework :: Buildout',
+       'Development Status :: 4 - Beta',
+       'Intended Audience :: Developers',
+       'License :: OSI Approved :: Zope Public License',
+       'Topic :: Software Development :: Build Tools',
+       'Topic :: Software Development :: Libraries :: Python Modules',
+       ],
+ 
+    )
