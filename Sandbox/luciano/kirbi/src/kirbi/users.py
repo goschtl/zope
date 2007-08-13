c256f0fe9a819064349fc4f40c23a8ab0b16d444
luciano <luciano@62d5b8a3-27da-0310-9561-8e5933582275>
2007-08-13 23:53:28 +0000

    implementing User class
    git-svn-id: svn://svn.zope.org/repos/main@78805 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/luciano/kirbi/src/kirbi/users.py
+++ Sandbox/luciano/kirbi/src/kirbi/users.py
@@ -1,13 +1,64 @@
 import grok
-from zope.app.authentication.principalfolder import PrincipalFolder, PrincipalInfo, InternalPrincipal
 from zope.app.authentication.interfaces import IPrincipalInfo
+from zope.interface import implements, invariant, Invalid
+import sha
 
-
-class UserFolder(grok.Container, PrincipalFolder):
+class UserFolder(grok.Container):
     pass
 
-class User(InternalPrincipal):
-    pass
+class User(object):
+    """
+    A Kirbi user. To implement IPrincipalInfo but still use more familiar
+    attribute names, we use properties to make ``id`` the same as ``login``
+    and ``title`` the same as ``name``.
+
+    >>> alice = User('alice', u'Vincent Damon Furnier', u'headless-chicken')
+    >>> alice.id, alice.title, alice.description
+    ("alice", u"Vincent Damon Furnier", u"Vincent Damon Furnier (alice)")
+    
+    >>> alice.title = u'Alice Cooper'
+    >>> alice.name
+    u"Alice Cooper"
+    
+    >>> alice.passwd_hash
+    ABC
+
+    """
+
+    implements(IPrincipalInfo)
+    
+    login = ''
+    name = ''
+    passwd_hash = ''
+        
+    def __init__(self, login, name, passwd):
+        self.login = login
+        self.name = name
+        self.passwd = sha.new(passwd).hexdigest()
+
+    def getId(self):
+        return self.login
+
+    def setId(self, id):
+        self.login = id
+
+    id = property(getId, setId)
+
+    def getTitle(self):
+        return self.name
+
+    def setTitle(self, title):
+        self.name = title
+
+    id = property(getId, setId)
+
+    @property
+    def description(self):
+        return '%s (%s)' % (self.name, self.login)
+    
+    
+        
+
 
 class Index(grok.View):
     grok.context(UserFolder)
