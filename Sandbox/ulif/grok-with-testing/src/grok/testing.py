1140d7a9dff89520221686e31d15c1124f7a5fd2
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2007-08-18 15:59:41 +0000

    Oops! Wrong license. Fixed it.
    git-svn-id: svn://svn.zope.org/repos/main@78949 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/ulif/grok-with-testing/src/grok/testing.py
+++ Sandbox/ulif/grok-with-testing/src/grok/testing.py
@@ -1,6 +1,22 @@
-import grok
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Support for registering tests with grok.
+"""
+
 import unittest
 import os.path
+import grok
 
 from martian.error import GrokImportError
 from martian.directive import (MultipleTimesDirective, BaseTextDirective,
