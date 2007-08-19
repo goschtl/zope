a9790c5e6dd6a97fec3f933d34d344f6322ffa09
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2007-08-19 15:12:48 +0000

    Branch for principal management in grok admin-UI.
    git-svn-id: svn://svn.zope.org/repos/main@78996 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Sandbox/ulif/grok-adminui-with-principals/setup.py
@@ -0,0 +1,72 @@
+from setuptools import setup, find_packages
+
+# some of the dependencies containing C code have been hardcoded to
+# make sure we only depend on versions for which there is a windows
+# binary. In some cases this means we rely on an earlier version than the
+# latest/greatest version as no Windows binary has been released for it yet.
+
+setup(
+    name='grok',
+    version='0.9',
+    author='Grok Team',
+    author_email='grok-dev@zope.org',
+    url='https://launchpad.net/grok',
+    download_url='svn://svn.zope.org/repos/main/grok/trunk#egg=grok-dev',
+    description='Grok: Now even cavemen can use Zope 3!',
+    long_description=open('README.txt').read(),
+    license='ZPL',
+
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    zip_safe=False,
+    install_requires=['setuptools',
+                      'martian',
+                      'simplejson',
+                      'pytz',
+                      'ZODB3 == 3.8.0b2',
+                      'zope.annotation',
+                      'zope.app.apidoc',
+                      'zope.app.applicationcontrol',
+                      'zope.app.appsetup',
+                      'zope.app.authentication',
+                      'zope.app.catalog',
+                      'zope.app.component',
+                      'zope.app.container == 3.5.0.a1',
+                      'zope.app.folder',
+                      'zope.app.intid',
+                      # XXX not binary, but needed for ZODB 3.8.0b22
+                      'zope.app.keyreference == 3.4.0a1',
+                      'zope.app.pagetemplate',
+                      'zope.app.publication',
+                      'zope.app.publisher',
+                      'zope.app.renderer',
+                      'zope.app.security',
+                      'zope.app.securitypolicy',
+                      'zope.app.testing',
+                      'zope.app.twisted',
+                      'zope.app.zcmlfiles',
+                      'zope.component',
+                      'zope.configuration',
+                      'zope.dottedname',
+                      'zope.deprecation',
+                      'zope.event',
+                      'zope.formlib',
+                      # XXX there is a later 3.4.0 release
+                      'zope.hookable == 3.4.0a1',
+                      # XXX there is a later 3.4.0 release
+                      'zope.i18nmessageid == 3.4.0a1',
+                      'zope.interface == 3.4.0',
+                      'zope.lifecycleevent',
+                      'zope.pagetemplate',
+                      'zope.proxy == 3.4.0',
+                      'zope.publisher',
+                      'zope.schema',
+                      'zope.security == 3.4.0b5',
+                      'zope.testing',
+                      'zope.traversing',
+                      'zope.testbrowser',
+                      'zc.catalog',
+                      'z3c.flashmessage >=1.0dev-r77761',
+                      ],
+)
