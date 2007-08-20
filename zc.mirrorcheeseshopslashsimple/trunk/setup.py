f0cde64d087650ee49fc6f50cafa3e23287ec2b6
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2007-08-20 22:29:56 +0000

    Moved some things around.
    git-svn-id: svn://svn.zope.org/repos/main@79055 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.mirrorcheeseshopslashsimple/trunk/setup.py
+++ zc.mirrorcheeseshopslashsimple/trunk/setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
+
 setup(
     name='ppix',
-    version='0.1',
+    version='0.2',
     package_dir = {'': 'src'},
     install_requires = ['zc.lockfile'],
+    entry_points = dict(console_scripts=[
+        'update-simple-mirror = zc.mirrorcheeseshopslashsimple:update',
+        ])
     )
