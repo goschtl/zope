d7b0e594a6d7fa8ba086a809ddf8628171318353
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2007-08-23 19:07:38 +0000

    Made AJAX feature thread-safe.
    git-svn-id: svn://svn.zope.org/repos/main@79168 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.formjs/trunk/src/z3c/formjs/ajax.py
+++ z3c.formjs/trunk/src/z3c/formjs/ajax.py
@@ -38,26 +38,27 @@ class AJAXHandlers(SelectionManager):
     def __repr__(self):
         return "<%s %r>" % (self.__class__.__name__, self.keys())
 
+
+class AJAXHandler(object):
+    zope.interface.implements(interfaces.IAJAXHandler)
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, view):
+        return self.func(view)
+
+    def __repr__(self):
+        return "<%s %r>" % (self.__class__.__name__, self.func.__name__)
+
+
 class AJAXRequestHandler(object):
+    """Mix-in class for forms to support AJAX calls."""
     zope.interface.implements(interfaces.IAJAXRequestHandler,
                               interfaces.IFormTraverser)
 
     ajaxRequestHandlers = AJAXHandlers()
 
-
-class AJAXHandler(BrowserPage):
-    zope.interface.implements(interfaces.IAJAXHandler)
-
-    context = None
-
-    def __init__(self, func):
-        self.func = func
-
-    def __call__(self):
-        return self.func(self.context)
-
-    def __repr__(self):
-        return "<%s %r>" % (self.__class__.__name__, self.func.__name__)
 
 def handler(func):
     """A decorator for defining an AJAX request handler."""
@@ -67,6 +68,18 @@ def handler(func):
     handlers = f_locals.setdefault('ajaxRequestHandlers', AJAXHandlers())
     handlers.addHandler(func.__name__, handler)
     return handler
+
+
+class AJAXView(BrowserPage):
+    """A wrapper class around AJAX handler to allow it to be publishable."""
+
+    def __init__(self, handler, request, view):
+        self.context = self.handler = handler
+        self.request = request
+        self.__parent__ = self.view = view
+
+    def __call__(self):
+        return self.handler(self.view)
 
 
 class AJAXRequestTraverserPlugin(object):
@@ -83,5 +96,4 @@ class AJAXRequestTraverserPlugin(object):
         if handler is None:
             raise NotFound(self.context, name, request)
 
-        handler.context = self.context
-        return handler
+        return AJAXView(handler, self.request, self.context)
