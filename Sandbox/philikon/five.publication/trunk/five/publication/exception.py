18077cfc240a46e3ec29b64c3f58b86f7c70cf32
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2007-08-26 10:44:26 +0000

    Two more exception views
    git-svn-id: svn://svn.zope.org/repos/main@79283 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/philikon/five.publication/trunk/five/publication/exception.py
+++ Sandbox/philikon/five.publication/trunk/five/publication/exception.py
@@ -11,3 +11,17 @@ class Redirect(BrowserPage):
 
     def __call__(self):
         self.request.response.redirect(self.context.args[0])
+
+class NotFound(BrowserPage):
+
+    def __call__(self):
+        self.request.response.setStatus(404)
+        self.request.response.setHeader('Content-Type', 'text/plain')
+        return 'Not found'
+
+class Unauthorized(BrowserPage):
+
+    def __call__(self):
+        self.request.response.setStatus(401)
+        self.request.response.setHeader('Content-Type', 'text/plain')
+        return 'Unauthorized'
