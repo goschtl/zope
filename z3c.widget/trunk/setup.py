a3dd5a91817c8f13d5eb89561f49c8d8d4ef29a2
berndroessl <berndroessl@62d5b8a3-27da-0310-9561-8e5933582275>
2007-09-04 23:27:34 +0000

    prepare for new release
    git-svn-id: svn://svn.zope.org/repos/main@79474 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.widget/trunk/setup.py
+++ z3c.widget/trunk/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='z3c.widget',
-      version='0.1.3',
+      version='0.1.4',
       author = "Zope Community",
       author_email = "zope3-dev@zope.org",
       description = "Additional Zope3 Widgets",
