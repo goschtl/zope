bccd310242deab72842d7e78897ba66699a4a3ad
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2007-09-21 20:26:25 +0000

    Got a classifier wrong.
    git-svn-id: svn://svn.zope.org/repos/main@79805 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.dependencytool/trunk/setup.py
+++ zope.dependencytool/trunk/setup.py
@@ -35,7 +35,7 @@ setup (
     license = "ZPL 2.1",
     keywords = "package dependency discovery",
     classifiers = [
-        'Development Status :: 5 - Stable/Production',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Zope Public License',
         'Programming Language :: Python',
