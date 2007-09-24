4e7b471b98471a2e8165ded6c1c2069aa41cac0f
chrism <chrism@62d5b8a3-27da-0310-9561-8e5933582275>
2007-09-24 23:03:57 +0000

    setup.py and ez_setup.py
    git-svn-id: svn://svn.zope.org/repos/main@79955 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Sandbox/chrism/eggcmf/2.1.0/setup.py
@@ -0,0 +1,28 @@
+# CMF packaged as an egg
+__version__ = '2.1.0.0'
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.dirname(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+
+setup(
+    name='cmflib',
+    version='2.1.0.0',
+    description='Zope CMF Product libraries',
+    long_description=README,
+    keywords='web application server cmf zope',
+    author='Zope Corporation and Contributors',
+    author_email='zope@zope.org',
+    url='http://www.zope.org',
+    license='ZPL 2.1',
+    packages=find_packages(),
+    include_package_data=True,
+    namespace_packages=['Products'],
+    zip_safe=False,
+    )
