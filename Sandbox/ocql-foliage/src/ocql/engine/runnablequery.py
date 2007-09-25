4535e549388cd8535fe2105b9d05f9470e3361b8
adamg <adamg@62d5b8a3-27da-0310-9561-8e5933582275>
2007-09-25 13:04:48 +0000

    started debuggable functions
    git-svn-id: svn://svn.zope.org/repos/main@79998 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/ocql-foliage/src/ocql/engine/runnablequery.py
+++ Sandbox/ocql-foliage/src/ocql/engine/runnablequery.py
@@ -3,6 +3,18 @@
 # This will return the resultset
 #
 
+#import __builtin__
+
+_marker = object()
+
+def d_reduce(function, sequence, initializer=_marker):
+    if initializer is _marker:
+        rv = reduce(function, sequence)
+    else:
+        rv = reduce(function, sequence, initializer)
+    return rv
+    
+
 class RunnableQuery:
     """
         metadata: ocql.metadata instance
@@ -16,15 +28,19 @@ class RunnableQuery:
     def reanalyze(self):
         self.code = self.engine.compile_algebra(self.alg)
     
-    def execute(self):
+    def execute(self, debug=False):
         metadata = self.engine.metadata
         
         #return reduce(set.union, map(lambda c: reduce(set.union, map(lambda d: ((d.name=="Computing Science") and (((d==set(filter(lambda i: i.runBy,c))) and (((c.credits<=3) and (((1<=c.credits) and (set([c])) or (set()))) or (set()))) or (set()))) or (set())),set(metadata.getAll("IDepartments"))) , set()),set(metadata.getAll("ICurses"))) , set())
         
         #TODO: why is the metadata not working in locals?
         import operator
-        return eval(self.code,
-                    {'metadata': self.engine.metadata, 'operator': operator},
-                    {'metadata': self.engine.metadata, 'operator': operator})
+        
+        mapping = {'metadata': self.engine.metadata,
+                   'operator': operator}
+        if debug:
+            mapping['reduce'] = d_reduce
+        
+        return eval(self.code, mapping, mapping)
         
         #return eval(self.code)
