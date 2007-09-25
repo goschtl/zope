f54dcf9667b716b645c3399f62ec82705900b364
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2007-09-25 23:27:16 +0000

    added a low-memory collection format
    git-svn-id: svn://svn.zope.org/repos/main@80063 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.fsutils/branches/dev/src/zc/fsutil/references.py
+++ zc.fsutils/branches/dev/src/zc/fsutil/references.py
@@ -20,6 +20,103 @@ import ZODB.FileStorage.FileStorage
 import ZODB.utils
 
 
+def collect(iterator, output):
+    """Create a database of database references.
+
+    Return a dictionary mapping oids to dictionaries with keys 'from'
+    and 'to' with values that are dictionaries mapping oids to lists
+    of references.
+    """
+    pickler = cPickle.Pickler(open(output, 'w'))
+    
+    for trans in iterator:
+        trandata = trans.tid, trans._tpos
+        for record in trans:
+            refs = []
+            u = cPickle.Unpickler(cStringIO.StringIO(record.data))
+            u.persistent_load = refs
+            u.noload()
+            u.noload()
+            pickler.dump((trandata, record.oid, record.tid, refs))
+    
+def collect_script(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    [inp, outp] = args
+
+    iterator = sys.modules['ZODB.FileStorage.FileStorage' # :(
+                           ].FileIterator(inp)
+    data = collect(iterator, outp)
+
+def load(fname):
+    unpickler = cPickle.Unpickler(open(fname))
+    result = {}
+    while 1:
+        try:
+            data = unpickler.load()
+        except EOFError:
+            return result
+        _update(result, *data)
+
+        
+def _update(result, tinfo, oid, serial, refs):
+    """Create a database of database references.
+
+    Return a dictionary mapping oids to dictionaries with keys 'from'
+    and 'to' with values that are dictionaries mapping oids to lists
+    of references.
+    """
+    from_oid = ZODB.utils.oid_repr(oid)
+    from_data = result.get(from_oid)
+    if from_data is None:
+        from_data = result[from_oid] = {
+            'from': {}, 'to': {}, 'serials': [],
+            }
+    from_data['serials'].append(serial)
+
+    for ref in refs:
+        if isinstance(ref, tuple):
+            to_oid = ZODB.utils.oid_repr(ref[0])
+        elif isinstance(ref, str):
+            to_oid = ZODB.utils.oid_repr(ref)
+        elif isinstance(ref, list):
+            if len(ref) == 1:
+                to_oid = ZODB.utils.oid_repr(ref[0])
+            else:
+                try:
+                    reference_type, args = ref
+                except ValueError:
+                    print 'wtf', ref
+                    continue
+
+                if reference_type == 'w':
+                    to_oid = ZODB.utils.oid_repr(args[0])
+                elif reference_type in 'nm':
+                    to_oid = args[0], ZODB.utils.oid_repr(args[1])
+                else:
+                    print wtf, reference_type, args
+        else:
+            print 'wtf', ref
+            continue
+
+        ref = dict(ref=ref, tinfo=tinfo)
+
+        from_to = from_data['to'].get(to_oid)
+        if from_to is None:
+            from_to = from_data['to'][to_oid] = []
+        from_to.append(ref)
+
+        to_data = result.get(to_oid)
+        if to_data is None:
+            to_data = result[to_oid] = {
+                'to': {}, 'from': {}, 'serials': [],
+                }
+        to_from = to_data['from'].get(from_oid)
+        if to_from is None:
+            to_from = to_data['from'][from_oid] = []
+        to_from.append(ref)
+
 def references(iterator):
     """Create a database of database references.
 
@@ -29,62 +126,15 @@ def references(iterator):
     """
     result = {}
     for trans in iterator:
+        trandata = trans.tid, trans._tpos
         for record in trans:
-            from_oid = ZODB.utils.oid_repr(record.oid)
-            from_data = result.get(from_oid)
-            if from_data is None:
-                from_data = result[from_oid] = {
-                    'from': {}, 'to': {}, 'serials': [],
-                    }
-            from_data['serials'].append(record.tid)
-
             refs = []
             u = cPickle.Unpickler(cStringIO.StringIO(record.data))
             u.persistent_load = refs
             u.noload()
             u.noload()
-            for ref in refs:
-                if isinstance(ref, tuple):
-                    to_oid = ZODB.utils.oid_repr(ref[0])
-                elif isinstance(ref, str):
-                    to_oid = ZODB.utils.oid_repr(ref)
-                elif isinstance(ref, list):
-                    if len(ref) == 1:
-                        to_oid = ZODB.utils.oid_repr(ref[0])
-                    else:
-                        try:
-                            reference_type, args = ref
-                        except ValueError:
-                            print 'wtf', ref
-                            continue
-
-                        if reference_type == 'w':
-                            to_oid = ZODB.utils.oid_repr(args[0])
-                        elif reference_type in 'nm':
-                            to_oid = args[0], ZODB.utils.oid_repr(args[1])
-                        else:
-                            print wtf, reference_type, args
-                else:
-                    print 'wtf', ref
-                    continue
-
-                ref = dict(ref=ref, tid=trans.tid, tpos=trans._tpos)
-                
-                from_to = from_data['to'].get(to_oid)
-                if from_to is None:
-                    from_to = from_data['to'][to_oid] = []
-                from_to.append(ref)
-
-                to_data = result.get(to_oid)
-                if to_data is None:
-                    to_data = result[to_oid] = {
-                        'to': {}, 'from': {}, 'serials': [],
-                        }
-                to_from = to_data['from'].get(from_oid)
-                if to_from is None:
-                    to_from = to_data['from'][from_oid] = []
-                to_from.append(ref)
-                
+            _update(result, trandata, record.oid, record.tid, refs)
+            
     return result
 
 def references_script(args=None):
@@ -97,7 +147,3 @@ def references_script(args=None):
                            ].FileIterator(inp)
     data = references(iterator)
     cPickle.Pickler(open(outp, 'w'), 1).dump(data)
-
-
-
-    
