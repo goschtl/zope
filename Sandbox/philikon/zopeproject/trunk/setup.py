3d076e738e26153710989c209437f6ea11cd3d56
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2007-09-27 11:05:19 +0000

    Moved changelog from ``README.txt`` into separate ``CHANGES.txt`` file.
    git-svn-id: svn://svn.zope.org/repos/main@80216 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/philikon/zopeproject/trunk/setup.py
+++ Sandbox/philikon/zopeproject/trunk/setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+long_description = (open('README.txt').read() +
+                    '\n\n' +
+                    open('CHANGES.txt').read())
+
 setup(
     name='zopeproject',
     version='0.4.1dev',
@@ -9,7 +13,7 @@ setup(
     download_url='svn://svn.zope.org/repos/main/Sandbox/philikon/zopeproject/trunk#egg=zopeproject-dev',
     description='Tools and scripts for creating development sandboxes for '
                 'web applications that primarily use Zope',
-    long_description=open('README.txt').read(),
+    long_description=long_description,
     license='ZPL',
     classifiers=['Environment :: Console',
                  'Intended Audience :: Developers',
