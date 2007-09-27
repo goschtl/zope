44e5823058e26d409bae2316cc42115ef02755b7
rogerineichen <rogerineichen@62d5b8a3-27da-0310-9561-8e5933582275>
2007-09-27 20:46:40 +0000

    Replace __init__.py
    git-svn-id: svn://svn.zope.org/repos/main@80256 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.skin.pagelet/trunk/src/z3c/__init__.py
+++ z3c.skin.pagelet/trunk/src/z3c/__init__.py
@@ -1,6 +1,6 @@
+# namespace package boilerplate
 try:
-    # Declare this a namespace package if pkg_resources is available.
-    import pkg_resources
-    pkg_resources.declare_namespace('z3c')
-except ImportError:
-    pass
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError, e:
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
