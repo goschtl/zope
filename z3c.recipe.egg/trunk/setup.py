b4fb44852fdc1a18f63d7994a2cb2f2b7973843b
rossp <rossp@62d5b8a3-27da-0310-9561-8e5933582275>
2007-09-30 23:58:00 +0000

    Recipies based on zc.recipe.egg for working with source distributions.
    git-svn-id: svn://svn.zope.org/repos/main@80431 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.recipe.egg/trunk/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='z3c.recipe.egg',
+      version=version,
+      description="Recipies based on zc.recipe.egg for working with source distributions.",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Ross Patterson',
+      author_email='me@rpatterson.net',
+      url='http://cheeseshop.python.org/pypi/z3c.recipe.egg',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['z3c.recipe'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
