9a1333d55233fe3ac7e047caa3f3014174297bc2
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2007-10-02 13:15:45 +0000

    Now that I am unblocked from the 'zope.app' dependency, I can move forward with releasing more packages.
    Getting ready for the next release.
    
    
    git-svn-id: svn://svn.zope.org/repos/main@80485 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.documenttemplate/trunk/setup.py
+++ zope.documenttemplate/trunk/setup.py
@@ -15,28 +15,48 @@
 
 $Id$
 """
-
 import os
 from setuptools import setup, find_packages
 
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.documenttemplate',
-      version = '3.4.0b1',
-      url='http://svn.zope.org/zope.documenttemplate',
-      license='ZPL 2.1',
-      description='Zope3 Document Template Markup Language (DTML)',
+      version = '3.4.0',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
-
+      description='Document Templating Markup Language (DTML)',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 template dtml",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.documenttemplate',
+      license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
-
-      namespace_packages=['zope',],
-      tests_require = ['zope.testing'],
-      install_requires = ['setuptools',
-                          'zope.structuredtext'],
-      extras_require = {'untrusted': ["zope.security[untrustedpython]"]},
+      namespace_packages=['zope'],
+      extras_require = dict(
+          test=['zope.security',
+                'zope.testing',
+                ],
+          untrusted=['zope.security[untrustedpython]'
+                ],
+          ),
+      install_requires = [
+          'setuptools',
+          'zope.structuredtext'],
       include_package_data = True,
-
       zip_safe = False,
       )
