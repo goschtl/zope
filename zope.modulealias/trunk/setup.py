559d971438d3874473fad788a36a098940b87155
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2007-10-03 02:46:31 +0000

    Update meta-data and get ready for release.
    git-svn-id: svn://svn.zope.org/repos/main@80524 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.modulealias/trunk/setup.py
+++ zope.modulealias/trunk/setup.py
@@ -15,25 +15,45 @@
 
 $Id$
 """
-
+import os
 from setuptools import setup, find_packages
 
-setup(
-    name="zope.modulealias",
-    version = '3.4.0b1',
-    url='http://svn.zope.org/zope.modulealias',
-    license='ZPL 2.1',
-    description='Zope modulealias',
-    author='Zope Corporation and Contributors',
-    author_email='zope3-dev@zope.org',
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-    packages=find_packages('src'),
-    package_dir={'':'src'},
-    namespace_packages=['zope'],
-    include_package_data=True,
+setup(name="zope.modulealias",
+      version = '3.4.0',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      description='Zope modulealias',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 module alias",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.modulealias',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      extras_require = dict(
+          test=['zope.testing',
+                ]),
     install_requires = ['setuptools',
                         'zope.interface',
                         'zope.configuration'],
+    include_package_data=True,
     zip_safe = False
     )
 
