e603cd65094cc4525b790f0d5da5b221cadcecea
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2007-10-08 10:39:45 +0000

    Setup buildout stuff.
    git-svn-id: svn://svn.zope.org/repos/main@80709 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/ulif/grokadmin/trunk/setup.py
+++ Sandbox/ulif/grokadmin/trunk/setup.py
@@ -1,90 +1,40 @@
-from setuptools import setup, find_packages
 import os
-
-# some of the dependencies containing C code have been hardcoded to
-# make sure we only depend on versions for which there is a windows
-# binary. In some cases this means we rely on an earlier version than the
-# latest/greatest version as no Windows binary has been released for it yet.
-# in some cases we also need to do this for non-binary dependencies, as
-# more recent versions rely on versions for which no binary eggs exist.
+from setuptools import setup, find_packages
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-long_description = (
-    read('README.txt')
-    + '\n' +
-    read('CHANGES.txt')
-    + '\n' +
-    'Download\n'
-    '********\n'
-    )
-
-setup(
-    name='grok',
-    version='0.11dev',
-    author='Grok Team',
-    author_email='grok-dev@zope.org',
-    url='http://grok.zope.org',
-    download_url='http://cheeseshop.python.org/pypi/grok/',
-    description='Grok: Now even cavemen can use Zope 3!',
-    long_description=long_description,
-    license='ZPL',
-    classifiers=['Environment :: Web Environment',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: Zope Public License',
-                 'Programming Language :: Python',
-                 'Framework :: Zope3',
-                 ],
-
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},
-    include_package_data = True,
-    zip_safe=False,
-    install_requires=['setuptools',
-                      'martian',
-                      'simplejson',
-                      'pytz',
-                      'ZODB3',
-                      'zope.annotation',
-                      'zope.app.apidoc',
-                      'zope.app.applicationcontrol',
-                      'zope.app.appsetup',
-                      'zope.app.authentication',
-                      'zope.app.catalog',
-                      'zope.app.component',
-                      'zope.app.container',
-                      'zope.app.folder',
-                      'zope.app.intid',
-                      'zope.app.keyreference',
-                      'zope.app.pagetemplate',
-                      'zope.app.publication',
-                      'zope.app.publisher',
-                      'zope.app.renderer',
-                      'zope.app.security',
-                      'zope.app.securitypolicy',
-                      'zope.app.testing',
-                      'zope.app.twisted',
-                      'zope.app.zcmlfiles',
-                      'zope.component',
-                      'zope.configuration',
-                      'zope.dottedname',
-                      'zope.deprecation',
-                      'zope.event',
-                      'zope.formlib',
-                      'zope.hookable',
-                      'zope.i18nmessageid',
-                      'zope.interface',
-                      'zope.lifecycleevent',
-                      'zope.pagetemplate',
-                      'zope.proxy',
-                      'zope.publisher',
-                      'zope.schema',
-                      'zope.security',
-                      'zope.testing',
-                      'zope.traversing',
-                      'zope.testbrowser',
-                      'zc.catalog',
-                      'z3c.flashmessage',
-                      ],
-)
+setup(name='grokadmin',
+      version='0.1.0',
+      description="GrokAdmin: The Grok administration and development UI",
+      long_description=(
+        read('README.txt')
+        ),
+      # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Development Status :: 1 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3 :: Grok'], 
+      keywords="zope3 grok grokadmin",
+      author="Uli Fouquet and lots of contributors from grok community",
+      author_email="grok-dev@zope.org",
+      url="http://svn.zope.org/Sandbox/ulif/grokadmin",
+      license="ZPL 2.1",
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      include_package_data=True,
+      zip_safe=False,
+      namespace_packages = ['grokadmin'],
+      install_requires=['setuptools',
+                        'grok',
+                        ],
+      entry_points="""
+      # Add entry points here
+      """,
+      )
