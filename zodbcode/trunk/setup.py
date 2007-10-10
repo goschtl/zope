40c86f1235852b16529d27ca5a32ac06e3f3abb8
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2007-10-10 16:36:59 +0000

    Bump version number, *sigh*.
    git-svn-id: svn://svn.zope.org/repos/main@80809 62d5b8a3-27da-0310-9561-8e5933582275

--- zodbcode/trunk/setup.py
+++ zodbcode/trunk/setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 
 setup (
     name='zodbcode',
-    version='3.4.0',
+    version='3.5.0dev',
     author='Zope Corporation and Contributors',
     author_email='zope3-dev@zope.org',
     description = "Allows Python code to live in the ZODB",
