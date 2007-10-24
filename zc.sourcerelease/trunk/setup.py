28da0bc53d5badd1e3a6676bcb745818499be9eb
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2007-10-24 22:42:08 +0000

    set version to next rev
    git-svn-id: svn://svn.zope.org/repos/main@81071 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.sourcerelease/trunk/setup.py
+++ zc.sourcerelease/trunk/setup.py
@@ -22,7 +22,7 @@ open('doc.txt', 'w').write(long_description)
 setup(
     name = "zc.sourcerelease",
     description = "Utility script to create source releases from buildouts",
-    version = "0.1",
+    version = "0.2",
     license = "ZPL 1.0",
     url='http://www.python.org/pypi/zc.sourcerelease',
     author='Jim Fulton', author_email='jim@zope.com',
