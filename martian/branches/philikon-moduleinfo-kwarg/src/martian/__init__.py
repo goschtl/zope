94391a9aa4ec9c1d0a536f88a57f3261facd5c57
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2007-10-30 20:36:55 +0000

    Remove branch that has been merged.
    git-svn-id: svn://svn.zope.org/repos/main@81235 62d5b8a3-27da-0310-9561-8e5933582275

--- martian/branches/philikon-moduleinfo-kwarg/src/martian/__init__.py
+++
@@ -1,4 +0,0 @@
-from core import ModuleGrokker, MultiGrokker, MetaMultiGrokker, grok_dotted_name,\
-     grok_package, grok_module
-from components import GlobalGrokker, ClassGrokker, InstanceGrokker
-from util import scan_for_classes
