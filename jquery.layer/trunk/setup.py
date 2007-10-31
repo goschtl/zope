688cf0c89352f6c6cccefe540cd23e767f331e8f
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2007-10-31 01:15:51 +0000

    Get ready for release.
    git-svn-id: svn://svn.zope.org/repos/main@81256 62d5b8a3-27da-0310-9561-8e5933582275

--- jquery.layer/trunk/setup.py
+++ jquery.layer/trunk/setup.py
@@ -21,30 +21,30 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-setup (
-    name='jquery.layer',
-    version='0.1.0c1',
-    author = "Roger Ineichen and the Zope Community",
-    author_email = "zope3-dev@zope.org",
-    description = "Base layers for JQuery functionality",
-    long_description=(
-        read('README.txt')
-        ),
-    license = "ZPL 2.1",
-    keywords = "zope3 layer jquery viewlet",
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Zope Public License',
-        'Programming Language :: Python',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP',
-        'Framework :: Zope3'],
-    url = 'http://svn.zope.org/jquery.layer',
+setup(name='jquery.layer',
+      version='1.0.0',
+      author = "Roger Ineichen and the Zope Community",
+      author_email = "zope3-dev@zope.org",
+      description = "Base layers for JQuery functionality",
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      license = "ZPL 2.1",
+      keywords = "zope3 layer jquery viewlet",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/jquery.layer',
     packages = find_packages('src'),
-    include_package_data = True,
     package_dir = {'':'src'},
     namespace_packages = ['jquery'],
     extras_require = dict(
@@ -55,6 +55,6 @@ setup (
         'zope.publisher',
         'zope.viewlet',
         ],
-    dependency_links = ['http://download.zope.org/distribution'],
+    include_package_data = True,
     zip_safe = False,
     )
