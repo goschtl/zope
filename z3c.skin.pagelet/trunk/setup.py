dce8889657840c390fc64e90227157e35bbdd858
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2007-11-01 20:55:08 +0000

    Fix deprecation warning.
    git-svn-id: svn://svn.zope.org/repos/main@81381 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.skin.pagelet/trunk/setup.py
+++ z3c.skin.pagelet/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name = 'z3c.skin.pagelet',
-    version = '1.0.0',
+    version = '1.0.1',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "A base skin for pagelet-based UIs",
