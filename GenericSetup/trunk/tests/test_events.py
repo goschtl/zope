807a36242f3c78915e51688f3129c1a91129fbe3
wichert <wichert@62d5b8a3-27da-0310-9561-8e5933582275>
2007-11-04 18:49:49 +0000

    Add missing file
    git-svn-id: svn://svn.zope.org/repos/main@81477 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ GenericSetup/trunk/tests/test_events.py
@@ -0,0 +1,41 @@
+import unittest
+from zope.interface.verify import verifyObject
+from Products.GenericSetup.events import BeforeProfileImportEvent
+from Products.GenericSetup.interfaces import IBeforeProfileImportEvent
+from Products.GenericSetup.events import ProfileImportedEvent
+from Products.GenericSetup.interfaces import IProfileImportedEvent
+
+class BaseEventTests(unittest.TestCase):
+    def testInterface(self):
+        event=self.klass("profile_id", "steps", "full_import")
+        verifyObject(self.iface, event)
+
+    def testNormalConstruction(self):
+        event=self.klass("profile_id", "steps", "full_import")
+        self.assertEqual(event.profile_id, "profile_id")
+        self.assertEqual(event.steps, "steps")
+        self.assertEqual(event.full_import, "full_import")
+
+    def testKeywordConstruction(self):
+        event=self.klass(profile_id="profile_id", steps="steps", full_import="full_import")
+        self.assertEqual(event.profile_id, "profile_id")
+        self.assertEqual(event.steps, "steps")
+        self.assertEqual(event.full_import, "full_import")
+
+
+class BeforeProfileImportEventTests(BaseEventTests):
+    klass = BeforeProfileImportEvent
+    iface = IBeforeProfileImportEvent
+
+
+class ProfileImportedEventTests(BaseEventTests):
+    klass = ProfileImportedEvent
+    iface = IProfileImportedEvent
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(BeforeProfileImportEventTests))
+    suite.addTest(unittest.makeSuite(ProfileImportedEventTests))
+    return suite
+
