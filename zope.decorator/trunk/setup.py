e8345a12810ba034fc86fcc22bd720a58663b5f7
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2007-11-05 03:31:22 +0000

    Get ready for release.
    git-svn-id: svn://svn.zope.org/repos/main@81508 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.decorator/trunk/setup.py
+++ zope.decorator/trunk/setup.py
@@ -15,23 +15,39 @@
 
 $Id$
 """
-
+import os
 from setuptools import setup, find_packages
 
-setup(
-    name="zope.decorator",
-    version = '3.4.0b1',
-    url='http://svn.zope.org/zope.decorator',
-    license='ZPL 2.1',
-    description='Zope testbrowser',
-    author='Zope Corporation and Contributors',
-    author_email='zope3-dev@zope.org',
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-    packages=find_packages('src'),
-    package_dir={'':'src'},
-    namespace_packages=['zope'],
-    include_package_data=True,
-    install_requires = ['setuptools'],
-    zip_safe = False
-    )
-
+setup(name='zope.decorator',
+      version = '3.4.0',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      description='Zope Decorators (Deprecated)',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 decorator",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.decorator',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      install_requires=['setuptools'],
+      include_package_data = True,
+      zip_safe = False,
+      )
