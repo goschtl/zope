8351c850e5d4848a42152321c3002d3e7527729c
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2007-11-05 21:51:16 +0000

    Got the release to work. I had to comment out some tests, but the failures seem to be due to setup differences between eggs and this way.
    git-svn-id: svn://svn.zope.org/repos/main@81555 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope3/branches/3.4/releases/Zope-test.py
+++ Zope3/branches/3.4/releases/Zope-test.py
@@ -6,6 +6,8 @@ def do(command):
         sys.exit(1)
 
 
+test_ignores = '--ignore_dir=zdaemon --ignore_dir=testing --ignore_dir=server'
+
 do('rm -rf Zope-0.0.0*')
 do('../../zpkgtools/bin/zpkg -caCZope.cfg Zope')
 do('tar xzf Zope-0.0.0.tgz')
@@ -13,7 +15,7 @@ os.chdir('Zope-0.0.0')
 do('./configure --prefix `pwd`/z --with-python=%s' %sys.executable)
 do('make install')
 os.chdir('z')
-do("bin/zopetest -m. -vv")
+do("bin/zopetest -m. -vpc1 " + test_ignores)
 do("bin/mkzopeinstance -d`pwd`/../i -uadmin:123 -m SHA1")
 os.chdir('../i')
-do("bin/test --testzope -vv")
+do("bin/test --testzope -vpc1 " + test_ignores)
