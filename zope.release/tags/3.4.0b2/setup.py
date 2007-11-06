88ad13cbeabcfd2546f872902da4579bc528abc6
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2007-11-06 00:21:50 +0000

    Create a tag for Zope 3.4.0b2-based KGS.
    git-svn-id: svn://svn.zope.org/repos/main@81561 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.release/tags/3.4.0b2/setup.py
@@ -0,0 +1,27 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for ``zope.release`` project"""
+from setuptools import setup
+
+setup(
+    name='zope.release',
+    version='0.1.0',
+    package_dir = {'': 'src'},
+    entry_points = dict(console_scripts=[
+        'generate-buildout = zope.release.buildout:main',
+        'generate-versions = zope.release.version:main',
+        'upload = zope.release.upload:main',
+        'update-tree = zope.release.tree:main',
+        ])
+    )
