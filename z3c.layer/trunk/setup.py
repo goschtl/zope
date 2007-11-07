b4fc6f3cb301a02a947776f3e8cdad2ae2b5a11a
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2007-11-07 05:47:13 +0000

    vb.
    git-svn-id: svn://svn.zope.org/repos/main@81574 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.layer/trunk/setup.py
+++ z3c.layer/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name = 'z3c.layer',
-      version = '0.2.3',
+      version = '0.3.0dev',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Collection of Alternative Base Layers',
