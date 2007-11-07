7804d457c028801fb1c642f6d638e902be7ac06e
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2007-11-07 15:48:51 +0000

    Tag for release.
    git-svn-id: svn://svn.zope.org/repos/main@81595 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.vcsync/tags/0.8.1/src/z3c/vcsync/interfaces.py
@@ -0,0 +1,116 @@
+from zope.interface import Interface, Attribute
+
+class IVcDump(Interface):
+    def save(checkout, path):
+        """Save context object to path in checkout.
+
+        checkout - an ICheckout object
+        path - a py.path object referring to directory to save in.
+
+        This might result in the creation of a new file or directory under
+        the path, or alternatively to the modification of an existing file
+        or directory.
+
+        Returns the path just created.
+        """
+
+class ISerializer(Interface):
+    def serialize(f):
+        """Serialize object to file object.
+        """
+
+class IParser(Interface):
+    def parse(f):
+        """Parse object and load it into new object, returning it.
+        """
+
+    def parse_into(f):
+        """Parse object and replace current object's content with it.
+        """
+
+class IVcFactory(Interface):
+    def __call__():
+        """Create new instance of object.
+        """
+
+class ISynchronizer(Interface):
+    """Synchronizer between state and version control.
+    """
+    checkout = Attribute('Version control system checkout')
+    state = Attribute('Persistent state')
+    
+    def sync(dt, message=''):
+        """Synchronize persistent Python state with version control system.
+
+        dt - date since when to look for state changes. datestamp should
+             have actual timezone identifier (non-naive).
+        message - message to commit any version control changes.
+        """
+
+    def save(dt):
+        """Save state to filesystem location of checkout.
+
+        dt - timestamp after which to look for state changes.
+        """
+
+    def load(dt):
+        """Load the filesystem information into persistent state.
+
+        dt - timestamp after which to look for filesystem changes.
+        """
+    
+class ICheckout(Interface):
+    """A version control system checkout.
+    """        
+    path = Attribute('Path to checkout root')
+
+    def up():
+        """Update the checkout with the state of the version control system.
+        """
+
+    def resolve():
+        """Resolve all conflicts that may be in the checkout.
+        """
+
+    def commit(message):
+        """Commit checkout to version control system.
+        """
+
+    def files(dt):
+        """Files added/modified in state since dt.
+
+        Returns paths to files that were added/modified since dt.
+        """
+
+    def removed(dt):
+        """Files removed in state since dt.
+
+        Returns filesystem (py) paths to files that were removed.
+        """
+
+class IState(Interface):
+    """Information about Python object state.
+    """
+    root = Attribute('The root container')
+
+    def objects(dt):
+        """Objects modified/added in state since dt.
+
+        Ideally, only those objects that have been modified or added
+        since dt should be returned. Returning more objects (as long
+        as they exist) is safe, however, though less efficient.
+        """
+
+    def removed(dt):
+        """Paths removed since dt.
+
+        The path is a path from the state root object to the actual
+        object that was removed. It is therefore not the same as the
+        physically locatable path.
+
+        Ideally, only those paths that have been removed since dt
+        should be returned. It is safe to return paths that were added
+        again later, so it is safe to return paths of objects returned
+        by the 'objects' method.
+        """
+
---
+++ z3c.vcsync/tags/0.8.1/src/z3c/vcsync/vc.py
@@ -0,0 +1,178 @@
+import os
+from datetime import datetime
+
+from zope.interface import Interface
+from zope.component import queryUtility, getUtility, queryAdapter
+from zope.app.container.interfaces import IContainer
+from zope.traversing.interfaces import IPhysicallyLocatable
+
+from z3c.vcsync.interfaces import (IVcDump, ISerializer,
+                                   IState, IVcFactory, ISynchronizer)
+
+import grok
+
+class VcDump(grok.Adapter):
+    """General VcDump for arbitrary objects.
+
+    Can be overridden for specific objects (such as containers).
+    """
+    grok.provides(IVcDump)
+    grok.context(Interface)
+
+    def save(self, path):
+        serializer = ISerializer(self.context)
+        path = path.join(serializer.name())
+        path.ensure()
+        f = path.open('w')
+        serializer.serialize(f)
+        f.close()
+        return path
+    
+class ContainerVcDump(grok.Adapter):
+    grok.provides(IVcDump)
+    grok.context(IContainer)
+        
+    def save(self, path):
+        path = path.join(self.context.__name__)
+        path.ensure(dir=True)
+
+def resolve(root, root_path, path):
+    rel_path = path.relto(root_path)
+    steps = rel_path.split('/')
+    steps = [step for step in steps if step != '']
+    steps = steps[1:]
+    obj = root
+    for step in steps:
+        name, ex = os.path.splitext(step)
+        try:
+            obj = obj[name]
+        except KeyError:
+            return None
+    return obj
+
+def resolve_container(root, root_path, path):
+    rel_path = path.relto(root_path)
+    steps = rel_path.split('/')
+    steps = [step for step in steps if step != '']
+    if not steps:
+        return None
+    steps = steps[1:-1]
+    obj = root
+    for step in steps:
+        try:
+            obj = obj[step]
+        except KeyError:
+            return None
+    return obj
+
+class Synchronizer(object):
+    grok.implements(ISynchronizer)
+
+    def __init__(self, checkout, state):
+        self.checkout = checkout
+        self.state = state
+
+    def sync(self, dt, message=''):
+        self.save(dt)
+        self.checkout.up()
+        self.checkout.resolve()
+        self.load(dt)
+        self.checkout.commit(message)
+
+    def save(self, dt):
+        # remove all files that have been removed in the database
+        path = self.checkout.path
+        for removed_path in self.state.removed(dt):
+            # construct path to directory containing file/dir to remove
+            steps = removed_path.split('/')
+            container_dir_path = path.join(*steps[:-1])
+            # construct path to potential directory to remove
+            name = steps[-1]
+            potential_dir_path = container_dir_path.join(name)
+            if potential_dir_path.check():
+                # the directory exists, so remove it
+                potential_dir_path.remove()
+            else:
+                # there is no directory, so it must be a file to remove
+                # find the file and remove it
+                file_paths = list(container_dir_path.listdir(
+                    str('%s.*' % name)))
+                # (could be 0 files in some cases)
+                for file_path in file_paths:
+                    file_path.remove()
+
+        # now save all files that have been modified/added
+        root = self.state.root
+        for obj in self.state.objects(dt):
+            IVcDump(obj).save(self._get_container_path(root, obj))
+
+    def load(self, dt):
+        # remove all objects that have been removed in the checkout
+        root = self.state.root
+        # sort to ensure that containers are deleted before items in them
+        removed_paths = self.checkout.removed(dt)
+        removed_paths.sort()
+        for removed_path in removed_paths:
+            obj = resolve(root, self.checkout.path, removed_path)
+            if obj is root:
+                continue
+            if obj is not None:
+                del obj.__parent__[obj.__name__]
+        # now modify/add all objects that have been modified/added in the
+        # checkout
+        file_paths = self.checkout.files(dt)
+        # to ensure that containers are created before items we sort them
+        file_paths.sort()
+        for file_path in file_paths:
+            # we might get something that doesn't actually exist anymore
+            # as it was since removed, so skip it
+            if not file_path.check():
+                continue
+            container = resolve_container(root, self.checkout.path, file_path)
+            if container is None:
+                continue
+            factory = getUtility(IVcFactory, name=file_path.ext)
+            name = file_path.purebasename
+            if name in container:
+                del container[name]
+            container[name] = factory(file_path)
+
+    def _get_container_path(self, root, obj):
+        steps = []
+        while obj is not root:
+            obj = obj.__parent__
+            steps.append(obj.__name__)
+        steps.reverse()
+        return self.checkout.path.join(*steps)
+
+class AllState(object):
+    """A special state object.
+
+    It reports all objects in the state as modified, and reports nothing
+    removed.
+    """
+    grok.implements(IState)
+
+    def __init__(self, root):
+        self.root = root
+
+    def objects(self, dt):
+        for container in self._containers(dt):
+            for item in container.values():
+                if not IContainer.providedBy(item):
+                    yield item
+            yield container
+
+    def removed(self, dt):
+        return []
+    
+    def _containers(self, dt):
+        return self._containers_helper(self.root)
+
+    def _containers_helper(self, container):
+        yield container
+        for obj in container.values():
+            if not IContainer.providedBy(obj):
+                continue
+            for sub_container in self._containers_helper(obj):
+                yield sub_container
