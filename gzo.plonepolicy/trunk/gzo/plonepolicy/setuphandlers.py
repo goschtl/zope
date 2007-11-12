75d6816602d17ea9d4b47d1a0d8bf407c66f2f1f
kteague <kteague@62d5b8a3-27da-0310-9561-8e5933582275>
2007-11-12 09:02:29 +0000

    adjust the default portlets
    git-svn-id: svn://svn.zope.org/repos/main@81763 62d5b8a3-27da-0310-9561-8e5933582275

--- gzo.plonepolicy/trunk/gzo/plonepolicy/setuphandlers.py
+++ gzo.plonepolicy/trunk/gzo/plonepolicy/setuphandlers.py
@@ -1,14 +1,36 @@
-def importVarious(context):
-    """Miscellanous steps import handle
-    """
-    
-    # Ordinarily, GenericSetup handlers check for the existence of XML files.
-    # Here, we are not parsing an XML file, but we use this text file as a 
-    # flag to check that we actually meant for this import step to be run.
-    # The file is found in profiles/default.
-    
-    if context.readDataFile('gzo.policy_various.txt') is None:
+from zope.component import getUtility
+from zope.component import getMultiAdapter
+
+from plone.portlets.interfaces import IPortletAssignmentMapping
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletType
+
+from plone.app.portlets import portlets
+
+def assignPortlets(context):
+    "Portlet Assignment"
+
+    if context.readDataFile('gzo.portlet_config.txt') is None:
         return
     
     portal = context.getSite()
+    
+    leftColumn = getUtility(IPortletManager, name=u'plone.leftcolumn', context=portal)
+    rightColumn = getUtility(IPortletManager, name=u'plone.rightcolumn', context=portal)
+
+    left = getMultiAdapter((portal, leftColumn,), IPortletAssignmentMapping, context=portal)
+    right = getMultiAdapter((portal, rightColumn,), IPortletAssignmentMapping, context=portal)
+    
+    # delete stock portlets
+    for key in left.keys():
+        # keep navigation
+        if key == u'navigation':
+            continue
+        del left[key]
+    for key in right.keys():
+        del right[key]
+    
+    left[u'top'] = portlets.classic.Assignment('portlet_leftcolumntop','portlet')
+    left[u'news'] = portlets.classic.Assignment('portlet_news','portlet')
+    left[u'download'] = portlets.classic.Assignment('portlet_download','portlet')
 
