4957507638ddb59256f79b527b1f3ebbb369927b
zagy <zagy@62d5b8a3-27da-0310-9561-8e5933582275>
2007-11-13 08:41:01 +0000

    marking trunk 0.2.1dev
    git-svn-id: svn://svn.zope.org/repos/main@81808 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.zalchemy/trunk/setup.py
+++ z3c.zalchemy/trunk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='z3c.zalchemy',
-      version='0.2',
+      version='0.2.1dev',
       author='Juergen Kartnaller and the Zope Community',
       author_email='zope3-dev@zope.org',
       url='http://pypi.python.org/pypi/z3c.sqlalchemy',
