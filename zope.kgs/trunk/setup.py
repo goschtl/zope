0e16c0e29180e086cbe65d8a83d5b1bdb9d3e34c
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2007-11-15 22:01:15 +0000

    Upload skeleton, so that I can svn cp the existing files into it.
    git-svn-id: svn://svn.zope.org/repos/main@81863 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.kgs/trunk/setup.py
@@ -0,0 +1,60 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for ``zope.kgs`` project"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.kgs',
+      version = '0.2.0',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      description='Known-Good-Set (KGS) Support',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('src', 'zope', 'kgs', 'README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 setuptools egg kgs",
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.kgs',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      extras_require = dict(
+          test=['zope.testing']),
+      install_requires=[
+          'setuptools',
+          'zc.buildout',
+          ],
+      entry_points = dict(console_scripts=[
+          'generate-buildout = zope.kgs.buildout:main',
+          'generate-versions = zope.kgs.version:main',
+          ]),
+      include_package_data = True,
+      zip_safe = False,
+      )
