02382e2981da03ff946ff5ef786ec36c5baf3d89
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2007-11-15 21:08:15 +0000

    tagged
    git-svn-id: svn://svn.zope.org/repos/main@81859 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zc.z3monitor/tags/0.2.0/src/zc/z3monitor/tests.py
@@ -0,0 +1,43 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+import re, unittest
+import logging, sys
+
+import ZODB.MappingStorage
+
+from zope.testing import doctest, renormalizing
+
+class FauxCache:
+
+    @property
+    def fc(self):
+        return self
+
+    def getStats(self):
+        return 42, 4200, 23, 2300, 1000
+
+ZODB.MappingStorage.MappingStorage._cache = FauxCache()
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite(
+            'README.txt',
+            checker=renormalizing.RENormalizing([
+                (re.compile("Vm(Size|RSS):\s+\d+\s+kB"), 'Vm\\1 NNN kB'),
+                (re.compile("\d\d+\s+\d\d+\s+\d+"), 'RRR WWW C'),
+                (re.compile("\d+[.]\d+ seconds"), 'N.NNNNNN seconds'),
+                ]),
+            ),
+        
+        ))
