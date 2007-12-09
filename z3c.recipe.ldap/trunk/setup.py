1bfa6b6f0f51d62ab352e456c8e08abfb4b54a79
rossp <rossp@62d5b8a3-27da-0310-9561-8e5933582275>
2007-12-09 05:20:59 +0000

    Initial project layout.
    git-svn-id: svn://svn.zope.org/repos/main@82208 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.recipe.ldap/trunk/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of z3c.recipe.ldap
+"""
+import os
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+README = os.path.join(os.path.dirname(__file__), 
+                      'z3c',
+                      'recipe',
+                      'ldap', 'docs', 'README.txt')
+
+long_description = open(README).read() + '\n\n' 
+
+entry_point = 'z3c.recipe.ldap:Recipe'
+
+entry_points = {"zc.buildout": ["default = %s" % entry_point]}
+
+setup(name='z3c.recipe.ldap',
+      version=version,
+      description="Deploy an OpenLDAP serve in a zc.buildout",
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Ross Patterson',
+      author_email='me@rpatterson.net',
+      url='http://cheeseshop.python.org/pypi/z3c.recipe.ldap',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['z3c.recipe'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=['setuptools',
+                        'zope.testing',
+                        'zc.buildout'
+                        # -*- Extra requirements: -*-
+                        ],
+      entry_points=entry_points,
+      )
---
+++ z3c.recipe.ldap/trunk/z3c/recipe/ldap/docs/README.txt
@@ -0,0 +1,19 @@
+;-*-Doctest-*-
+=======================
+z3c.recipe.ldap package
+=======================
+
+.. contents::
+
+What is z3c.recipe.ldap ?
+=========================
+
+This recipe can be used to deploy an OpenLDAP server in a
+zc.buildout.  More specifically it provides for initializing an LDAP
+database from an LDIF file and for setting up an LDAP instance in the
+buildout.  This recipe can also be used to provide an isolated LDAP
+instance as a test fixture.
+
+How to use z3c.recipe.ldap ?
+============================
+
