f46fce027bbb778a70f21a4fbd5ff57bf3f5e6af
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-01-04 12:33:17 +0000

    Added extensions to support standard python doc tags in documentation.
    git-svn-id: svn://svn.zope.org/repos/main@82665 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Sandbox/ulif/grok-reference-with-rest2/doc/grokref/extensions/addnodes.py
@@ -0,0 +1,72 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from docutils import nodes
+
+# index markup
+class index(nodes.Invisible, nodes.Inline, nodes.TextElement): pass
+
+# description units (classdesc, funcdesc etc.)
+class desc(nodes.Admonition, nodes.Element): pass
+class desc_content(nodes.General, nodes.Element): pass
+class desc_signature(nodes.Part, nodes.Inline, nodes.TextElement): pass
+class desc_classname(nodes.Part, nodes.Inline, nodes.TextElement): pass
+class desc_name(nodes.Part, nodes.Inline, nodes.TextElement): pass
+class desc_parameterlist(nodes.Part, nodes.Inline, nodes.TextElement): pass
+class desc_parameter(nodes.Part, nodes.Inline, nodes.TextElement): pass
+class desc_optional(nodes.Part, nodes.Inline, nodes.TextElement): pass
+
+# refcount annotation
+class refcount(nodes.emphasis): pass
+
+# \versionadded, \versionchanged, \deprecated
+class versionmodified(nodes.Admonition, nodes.TextElement): pass
+
+# seealso
+class seealso(nodes.Admonition, nodes.Element): pass
+
+# productionlist
+class productionlist(nodes.Admonition, nodes.Element): pass
+class production(nodes.Part, nodes.Inline, nodes.TextElement): pass
+
+# toc tree
+class toctree(nodes.General, nodes.Element): pass
+
+
+# centered
+class centered(nodes.Part, nodes.Element): pass
+
+# pending xref
+class pending_xref(nodes.Element): pass
+
+# compact paragraph -- never makes a <p>
+class compact_paragraph(nodes.paragraph): pass
+
+# sets the highlighting language for literal blocks
+class highlightlang(nodes.Element): pass
+
+# like emphasis, but doesn't apply further text processors, e.g. smartypants
+class literal_emphasis(nodes.emphasis): pass
+
+# glossary
+class glossary(nodes.Element): pass
+
+# make them known to docutils. this is needed, because the HTMl writer
+# will choke at some point if these are not added
+nodes._add_node_class_names("""index desc desc_content desc_signature
+      desc_classname desc_name desc_parameterlist desc_parameter desc_optional
+      centered versionmodified seealso productionlist production toctree
+      pending_xref compact_paragraph highlightlang literal_emphasis
+      glossary""".split())
+
---
+++ Sandbox/ulif/grok-reference-with-rest2/doc/grokref/grokref2html.py
@@ -0,0 +1,98 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Create the grok reference in various output formats."
+
+import os
+import sys
+import codecs
+
+import docutils.core
+#from docutils import nodes
+from docutils.writers.html4css1 import Writer
+
+from grokref.translators import HTMLTranslator
+from grokref.extensions import addnodes, roles, directives
+
+#from zope.app.renderer.rest import ZopeTranslator
+
+class ReStructuredTextToHTMLRenderer:
+    """Convert from Restructured Text to HTML."""
+
+    def __init__(self, content):
+        self.content = content
+
+    def render(self):
+        settings_overrides = {
+            'halt_level': 6,
+            'input_encoding': 'utf8',
+            'output_encoding': 'utf8',
+            'initial_header_level': 2,
+            # don't try to include the stylesheet (docutils gets hiccups)
+            'stylesheet_path': '',
+        }
+
+        #docutils.nodes._add_node_class_names('desc')
+        writer = Writer()
+        #print docutils.nodes
+        #writer.translator_class = ZopeTranslator
+        writer.translator_class = HTMLTranslator
+        html = docutils.core.publish_string(
+                        self.content,
+                        writer=writer,
+                        settings_overrides=settings_overrides,)
+        html = codecs.decode(html, 'utf_8')
+        return html
+
+
+class ReSTFile(object):
+    source_text = None
+    file_path = None
+
+    def __init__(self, filename=None):
+        self.source_text = codecs.open(filename, "r", 'utf-8').read()
+        self.file_path = filename
+
+    def getHTML(self):
+        return ReStructuredTextToHTMLRenderer(self.source_text).render()
+        
+
+def getRestFiles(filepath):
+    if os.path.isdir(filepath):
+        return [os.path.join(filepath, filename)
+                for filename in os.listdir(filepath)
+                if filename.endswith('.rst')]
+    return [filepath]
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    
+    if not len(argv):
+        print "Usage: %s REFDIR|FILE" % (sys.argv[0])
+        sys.exit(1)
+
+    if not os.path.isdir(argv[0]) and not os.path.isfile(argv[0]):
+        print "%s: No such file or directory" % (argv[0])
+        sys.exit(1)
+
+    rest_files = getRestFiles(argv[0])
+    print "files: ", rest_files
+
+    print "Content: "
+    for filename in rest_files:
+        print ReSTFile(filename).getHTML()
+
+    
+if __name__ == '__main__':
+    main()
