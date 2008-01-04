4642e051c530ce52ae7d7c9cc3deda450dc92348
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-01-04 12:36:07 +0000

    Let buildout generate a reference generator script.
    git-svn-id: svn://svn.zope.org/repos/main@82666 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/ulif/grok-reference-with-rest2/doc/setup.py
+++ Sandbox/ulif/grok-reference-with-rest2/doc/setup.py
@@ -7,9 +7,10 @@ setup(
                       'zope.app.renderer',
                       'Pygments'
                       ],
-    py_modules = ['grok2html'],
+    py_modules = ['grok2html', 'grokref.grokref2html'],
     entry_points="""
     [console_scripts]
     grok2html = grok2html:main
+    grokref2html = grokref.grokref2html:main
     """
     )
