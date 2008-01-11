82f68dfec4805421319ec1f43aa697ab0bf1cb46
wichert <wichert@62d5b8a3-27da-0310-9561-8e5933582275>
2008-01-11 14:27:19 +0000

    Add tests for the site installation
    git-svn-id: svn://svn.zope.org/repos/main@82808 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zopeorg.deployment/trunk/zopeorg/deployment/tests/integrationcase.py
@@ -0,0 +1,44 @@
+"""Base class for integration tests, based on ZopeTestCase and PloneTestCase.
+
+Note that importing this module has various side-effects: it registers a set of
+products with Zope, and it sets up a sandbox Plone site with the appropriate
+products installed.
+"""
+
+from Testing import ZopeTestCase
+from Products.Five import zcml
+# Import PloneTestCase - this registers more products with Zope as a side effect
+from Products.PloneTestCase.layer import PloneSite
+from Products.PloneTestCase.PloneTestCase import PloneTestCase
+from Products.PloneTestCase.PloneTestCase import setupPloneSite
+
+# Set up a Plone site, and apply our custom extension profile
+PROFILES = ('zopeorg.deployment:default',)
+setupPloneSite(extension_profiles=PROFILES)
+
+import zopeorg.deployment
+
+class ZopeOrgLayer(PloneSite):
+    @classmethod
+    def setUp(cls):
+        zcml.load_config('configure.zcml', zopeorg.deployment)
+        ZopeTestCase.installPackage("zopeorg.deployment")
+
+    @classmethod
+    def tearDown(cls):
+        pass
+
+
+class IntegrationTestCase(PloneTestCase):
+    """Base class for integration tests for the '${package}' product.
+
+    This may provide specific set-up and tear-down operations, or provide
+    convenience methods.
+    """
+
+    layer = ZopeOrgLayer
+
+    def createMemberarea(self, name):
+        # bypass PTC's creation of a home folder for the default user
+        pass
+
---
+++ zopeorg.deployment/trunk/zopeorg/deployment/tests/testSetupHandlers.py
@@ -0,0 +1,19 @@
+from zopeorg.deployment.tests.integrationcase import IntegrationTestCase
+
+class TestDefaultHandler(IntegrationTestCase):
+    """Test actions performed by the setup handler code.
+    """
+    def testMemberAreaDisabled(self):
+        mt = self.portal.portal_membership
+        self.assertEqual(mt.memberareaCreationFlag, 0)
+
+    def testThemeInstalled(self):
+        qi = self.portal.portal_quickinstaller
+        self.assertEqual(qi.isProductInstalled("zopeorg.theme"), True)
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite=TestSuite()
+    suite.addTest(makeSuite(TestDefaultHandler))
+    return suite
+
