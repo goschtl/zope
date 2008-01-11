0aadd9a7b8b2518f9f06323628a1c85e861c121b
wichert <wichert@62d5b8a3-27da-0310-9561-8e5933582275>
2008-01-11 14:30:54 +0000

    Make sure the default theme is set correctly
    git-svn-id: svn://svn.zope.org/repos/main@82809 62d5b8a3-27da-0310-9561-8e5933582275

--- zopeorg.deployment/trunk/zopeorg/deployment/tests/testSetupHandlers.py
+++ zopeorg.deployment/trunk/zopeorg/deployment/tests/testSetupHandlers.py
@@ -4,12 +4,14 @@ class TestDefaultHandler(IntegrationTestCase):
     """Test actions performed by the setup handler code.
     """
     def testMemberAreaDisabled(self):
-        mt = self.portal.portal_membership
+        mt=self.portal.portal_membership
         self.assertEqual(mt.memberareaCreationFlag, 0)
 
     def testThemeInstalled(self):
-        qi = self.portal.portal_quickinstaller
+        qi=self.portal.portal_quickinstaller
         self.assertEqual(qi.isProductInstalled("zopeorg.theme"), True)
+        st=self.portal.portal_skins
+        self.assertEqual(st.getDefaultSkin(), "Zope.org Theme")
 
 def test_suite():
     from unittest import TestSuite, makeSuite
