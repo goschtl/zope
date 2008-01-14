acdfbcb69d333da3c7890ecb5f762f851cc106c3
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-01-14 02:14:33 +0000

    Use new ReST directives.
    git-svn-id: svn://svn.zope.org/repos/main@82863 62d5b8a3-27da-0310-9561-8e5933582275

--- gzo.plonepolicy/trunk/gzo/plonepolicy/__init__.py
+++ gzo.plonepolicy/trunk/gzo/plonepolicy/__init__.py
@@ -1,7 +1,9 @@
 #
 # ReST features that are not included in Plone 3.0.x that we are using
-# 
-import rest_hacks
+#
+# See rest_extensions/README.txt for further infos.
+#
+from rest_extensions import directives_plain, roles_plain, pygments_directive
 
 def initialize(context):
     """Intializer called when used as a Zope 2 product."""
