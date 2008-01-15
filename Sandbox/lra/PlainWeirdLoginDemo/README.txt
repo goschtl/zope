abe2166544d71d0f2833ceb5aab9c3d68af86d21
lra <lra@62d5b8a3-27da-0310-9561-8e5933582275>
2008-01-15 04:14:53 +0000

    PlainLoginDemo experiments
    git-svn-id: svn://svn.zope.org/repos/main@82887 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Sandbox/lra/PlainWeirdLoginDemo/README.txt
@@ -0,0 +1,28 @@
+PlainLoginDemo is a sample application showing how to implement user account
+creation and login using Grok. 
+
+Main features
+-------------
+
+- top bar displays user data and login status
+
+- links in top bar change according to login status
+
+- user can create own account
+
+- a list of created accounts can be seen in the member listing page
+
+- member listing page is protected by permission, requiring user log in
+
+- good functional test coverage (see text files in src/plainlogindemo/ftests)
+
+Todo
+----
+
+- allow user to choose a password manager when joining
+
+- allow user to edit his data
+
+- add a password confirmation field (and validation) to join form
+
+- allow manager to delete user accounts through the member listing
---
+++ Sandbox/lra/PlainWeirdLoginDemo/src/plainlogindemo/app.py
@@ -0,0 +1,189 @@
+import grok
+
+from urllib import urlencode
+
+from zope.interface import Interface, implements
+from zope.component import getUtility, getUtilitiesFor
+from zope.app.authentication import PluggableAuthentication
+from zope.app.authentication.interfaces import IPasswordManager
+from zope.app.authentication.principalfolder import PrincipalFolder
+from zope.app.authentication.principalfolder import InternalPrincipal
+from zope.app.authentication.principalfolder import IInternalPrincipal
+from zope.app.authentication.session import SessionCredentialsPlugin
+from zope.app.security.interfaces import IAuthentication
+from zope.app.security.interfaces import IUnauthenticatedPrincipal
+from zope.security.management import checkPermission
+from zope.app.securitypolicy.interfaces import IPrincipalPermissionManager
+from zope.schema import getFieldNamesInOrder, ValidationError
+from zope.schema.interfaces import IField, IIterableSource
+from zope.i18n import MessageFactory
+
+_ = MessageFactory('plainlogindemo')
+
+def setup_pau(pau):
+    '''
+    Callback to setup the Pluggable Authentication Utility
+    
+    A reference to this function is passed as a parameter in the
+    declaration of the PAU (see PlainLoginDemo class)
+    '''
+    # the principal source is a PrincipalFolder, stored in ZODB
+    pau['principals'] = PrincipalFolder() 
+    pau.authenticatorPlugins = ('principals',)
+    # the SessionCredentialsPlugin isused for cookie-based authentication
+    pau['session'] = session = SessionCredentialsPlugin()
+    session.loginpagename = 'login' # the page to redirect for login
+    # configuration of the credentials plugin
+    pau.credentialsPlugins = ('No Challenge if Authenticated', 'session',)
+        
+class PlainLoginDemo(grok.Application, grok.Container):
+    """
+    An app that lets users create accounts, login, logout and change their
+    passwords.
+    """
+    # register the authentication utility; see setup_pau for settings
+    grok.local_utility(PluggableAuthentication, IAuthentication,
+                       setup=setup_pau)
+           
+class ViewMemberListing(grok.Permission):
+    ''' Permission to see the member listing '''
+    grok.name('plainlogindemo.ViewMemberListing')
+
+class Master(grok.View):
+    """
+    The master page template macro.
+    
+    The template master.pt is used as page macro in most views. Since this
+    template uses the logged_in method and message attributes below, it's best
+    to make all other views in this app subclasses of Master.
+    """
+    grok.context(Interface)  # register this view for all objects
+
+    message = '' # used to give feedback
+
+    def logged_in(self):
+        # this is the canonical way to tell whether the user is authenticated
+        # in Zope 3: check if the principal provides IUnauthenticatedPrincipal
+        return not IUnauthenticatedPrincipal.providedBy(self.request.principal)
+    
+class Index(Master):
+    """
+    The main page, showing user data and member count.
+    """
+
+    def member_count(self):
+        # get the authentication utility
+        pau = getUtility(IAuthentication)
+        return len(pau['principals'])
+
+    
+class Login(Master):
+    """
+    Login form and handler.
+    """
+    def update(self, login_submit=None):
+        if login_submit is not None: # we are handling the login submission
+            if self.logged_in(): # if the login was accepted then...
+                # redirect to where the user came from, or to the main page
+                dest = self.request.get('camefrom', self.application_url())
+                self.redirect(dest)
+            else: # if the user is still not logged in...
+                # then an incorrect login or password was provided
+                self.message = _(u'Invalid login name and/or password')
+
+class Logout(grok.View):
+    """
+    Logout handler.
+    """
+    grok.context(Interface)
+    def render(self):
+        # get the session plugin and tell it to logout
+        session = getUtility(IAuthentication)['session']
+        session.logout(self.request)
+        # redirect to the main page
+        self.redirect(self.application_url())
+        
+class Join(grok.AddForm, Master):
+    """
+    User registration form.
+    """
+    form_fields = grok.AutoFields(IInternalPrincipal).omit('passwordManagerName')
+    label = _(u'User registration')
+    template = grok.PageTemplateFile('form.pt')
+    
+    @grok.action('Save')
+    def save(self, **data):
+        '''
+        Create an InternalPrincipal with the user data.
+        
+        This method also grants the ViewMemberListing permission to the user.
+        '''
+        login = data['login']
+        pau = getUtility(IAuthentication)
+        principals = pau['principals']
+        # XXX: the login name must be unique; need better handling of this
+        if login in principals:
+            msg = _(u'Login name taken. Please choose a different one.') 
+            self.widgets['login']._error = ValidationError(msg)
+            self.form_reset = False
+        else:
+            # create an instance of InternalPrincipal
+            principal = InternalPrincipal(passwordManagerName='SHA1', **data)
+            principals[login] = principal
+            # grant the user permission to view the member listing
+            permission_mngr = IPrincipalPermissionManager(grok.getSite())
+            permission_mngr.grantPermissionToPrincipal(
+               'plainlogindemo.ViewMemberListing', principals.prefix + login)
+
+            self.redirect(self.url('login')+'?'+urlencode({'login':login}))
+                    
+class Account(grok.View):
+    
+    def render(self):
+        return 'Not implemented'
+    
+class Listing(Master):
+    '''
+    Member listing view.
+    
+    This demonstrates how to require a permission to view, and also how to
+    obtain a list of annotated principals.
+    '''
+
+    grok.require('plainlogindemo.ViewMemberListing')
+
+    def field_names(self):        
+        return getFieldNamesInOrder(IInternalPrincipal)
+
+    def members(self):
+        pau = getUtility(IAuthentication)
+        principals = pau['principals']
+        roster = []
+        for id in sorted(principals.keys()):
+            user = principals[id]
+            fields = {}
+            for field in self.field_names():
+                fields[field] = getattr(user, field)
+            roster.append(fields)
+        return roster
+    
+    def delete_allowed(self):
+        # XXX: this is not the right way to do it... it's just a test
+        return self.request.principal.id.endswith('.manager')
+
+class PasswordManagerChoices(object):
+    implements(IIterableSource)
+    
+    def __init__(self):
+        self.choices = [name for name, util in
+                            sorted(getUtilitiesFor(IPasswordManager))]
+        
+    def __iter__(self):
+        return iter(self.choices)
+    
+    def __len__(self):
+        return len(self.choices)
+    
+    def __contains__(self, value):
+        return value in self.choices
+    
