c8dff497f41bbcbce50eb3f68c030489ccd2f476
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-01-20 01:37:29 +0000

    Add setup.
    git-svn-id: svn://svn.zope.org/repos/main@82965 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Sandbox/ulif/fallback-auth/trunk/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('src', 'z3c', 'fallbackauth', 'README.txt')
+    + '\n\n'
+    + read('CHANGES.txt')
+    + '\n\n'
+    )
+
+setup(
+    name='z3c.fallbackauth',
+    version='0.1dev',
+    author='Uli Fouquet',
+    author_email='uli@gnufix.de',
+    url='',
+    download_url='',
+    description='A fallback authenticator plugin for Zope 3',
+    long_description=long_description,
+    license='ZPL',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Environment :: Web Environment',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: Zope Public License',
+                 'Programming Language :: Python',
+                 'Framework :: Zope3',
+                 ],
+
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages['z3c'],
+    include_package_data = True,
+    zip_safe=False,
+    install_requires=['setuptools',
+                      'zope.app.authentication',
+                      'zope.app.security',
+                      'zope.interface',
+                      'zope.testing',
+                      ],
+)
