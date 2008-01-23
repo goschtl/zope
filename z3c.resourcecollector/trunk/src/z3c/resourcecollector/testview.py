53ac0072d3188194d47e8a50c6a896ea721edc44
zotya <zotya@62d5b8a3-27da-0310-9561-8e5933582275>
2008-01-23 21:41:28 +0000

    initial import
    git-svn-id: svn://svn.zope.org/repos/main@83126 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.resourcecollector/trunk/src/z3c/resourcecollector/testview.py
@@ -0,0 +1,12 @@
+
+class res1(object):
+    def __call__(self):
+        return "test resource #1"
+
+class res2(object):
+    def __call__(self):
+        return "test resource #2"
+
+class res3(object):
+    def __call__(self):
+        return "test resource #3"
