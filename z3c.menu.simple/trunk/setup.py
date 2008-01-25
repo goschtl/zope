9fce16ebbf3114a6fd726f2ca4b9722f91e18356
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2008-01-25 03:53:58 +0000

    version bump.
    git-svn-id: svn://svn.zope.org/repos/main@83218 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.menu.simple/trunk/setup.py
+++ z3c.menu.simple/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.menu.simple',
-    version='0.5.1',
+    version='0.5.2dev',
     author = "Stephan Richter, Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "A simple menu system for Zope3",
