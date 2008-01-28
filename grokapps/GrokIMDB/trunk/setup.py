1b46bb156ef6fc75ded56aa6b322c1a55c61c571
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-01-28 00:48:09 +0000

    Initial import.
    git-svn-id: svn://svn.zope.org/repos/main@83265 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ grokapps/GrokIMDB/trunk/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+long_description = (
+    read('README.txt')
+    + '\n\n'
+    )
+
+setup(name='GrokIMDB',
+      version=version,
+      description="A Grok demo application for editing data "
+                  "retrieved from IMDb.",
+      long_description=long_description,
+      # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Zope Public License',
+                   'Programming Language :: Python',
+                   'Framework :: Zope3',
+    ], 
+      keywords="",
+      author="Uli Fouquet and Jan-Ulrich Hasecke",
+      author_email="uli@gnufix.de",
+      url="http://cheeseshop.python.org/GrokIMDB",
+      license="ZPL",
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'grok',
+                        # Add extra requirements here
+                        'IMDbPY',
+                        ],
+      entry_points="""
+      # Add entry points here
+      """,
+      )
