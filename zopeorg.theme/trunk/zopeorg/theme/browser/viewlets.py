e6867b4760f3a77a1cb33308aaf452915d735506
spliter <spliter@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-03 19:04:28 +0000

    made Feature viewlets to work with real content from the object
    git-svn-id: svn://svn.zope.org/repos/main@83460 62d5b8a3-27da-0310-9561-8e5933582275

--- zopeorg.theme/trunk/zopeorg/theme/browser/viewlets.py
+++ zopeorg.theme/trunk/zopeorg/theme/browser/viewlets.py
@@ -7,24 +7,21 @@ from plone.app.layout.viewlets.common import ViewletBase, SearchBoxViewlet, Pers
 
 class FeatureViewlet(ViewletBase):
     render = ViewPageTemplateFile('templates/feature.pt')
+        
+    def tag(self, **kwargs):
+        return self.context.getField('image').tag(self.context, **kwargs)
     
-    # def update(self):
-    #     self.portal_state = getMultiAdapter((self.context, self.request),
-    #                                         name=u'plone_portal_state')
-    #     self.portal_url = self.portal_state.portal_url()
-    #     url_tool = getToolByName(self.context, 'portal_url')
-    #     contentPath = url_tool.getRelativeContentPath(self.context)
-    #     
-    #     if contentPath:
-    #         self.section = 'default'
-    #     else:
-    #         self.section = contentPath[0]
+    def blurb(self):
+        return self.context.getBlurb()
+        
+class ZopeorgHeaderDividerViewlet(ViewletBase):
+    render = ViewPageTemplateFile('templates/divider.pt')  
+    
+    def divider(self):
+        return self.context.getDivider()      
     
 class ZopeorgSearchBoxViewlet(SearchBoxViewlet):
     render = ViewPageTemplateFile('templates/searchbox.pt')
     
 class ZopeorgPersonalBarViewlet(PersonalBarViewlet):
-    render = ViewPageTemplateFile('templates/anonymous_personal_bar.pt')    
-    
-class ZopeorgHeaderDividerViewlet(ViewletBase):
-    render = ViewPageTemplateFile('templates/divider.pt')    
+    render = ViewPageTemplateFile('templates/anonymous_personal_bar.pt')        
