80f1871fc2b6f04b50a9a5dbe03e5fcb6bfe5209
spliter <spliter@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-03 23:16:44 +0000

    made FeatureViewlet to be visible for IViewView only - we don't need it for Edit or any other screen. Used dirty way of achieving this. Simple view="plone.app.layout.globals.interfaces.IViewView" for a viewlet didn't work out for un-known (yet ;)) reason
    git-svn-id: svn://svn.zope.org/repos/main@83475 62d5b8a3-27da-0310-9561-8e5933582275

--- zopeorg.theme/trunk/zopeorg/theme/browser/viewlets.py
+++ zopeorg.theme/trunk/zopeorg/theme/browser/viewlets.py
@@ -2,12 +2,19 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from plone.app.layout.viewlets.common import ViewletBase, SearchBoxViewlet, PersonalBarViewlet
 
+from plone.app.layout.globals.interfaces import IViewView 
+
 class FeatureViewlet(ViewletBase):
     render = ViewPageTemplateFile('templates/feature.pt')
     
-    # def update(self):
-    #     if IViewView.providedBy(self.__parent__):
-    #         alsoProvides(self, IViewView)
+    def visible(self):
+        # we should *climb up* to object itself from
+        # 1. viewlet to viewletManager
+        # 2. viewletManager to object
+        if IViewView.providedBy(self.__parent__.__parent__):
+            return True
+        else:
+            return False
         
     def tag(self, **kwargs):
         return self.context.getField('image').tag(self.context, **kwargs)
