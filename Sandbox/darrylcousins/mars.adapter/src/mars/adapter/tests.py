79628483f0b4c4727a2c4c7f76bd8d7ddf26f94e
darrylcousins <darrylcousins@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-04 03:16:22 +0000

    Updating finally
    git-svn-id: svn://svn.zope.org/repos/main@83476 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/darrylcousins/mars.adapter/src/mars/adapter/tests.py
+++ Sandbox/darrylcousins/mars.adapter/src/mars/adapter/tests.py
@@ -3,21 +3,12 @@ import unittest
 from zope.testing import doctest
 from zope.configuration.config import ConfigurationMachine
 
-from martian import scan
-
 optionflags = doctest.NORMALIZE_WHITESPACE + doctest.ELLIPSIS
 
-def setUp(test):
-    test.globs['config'] = ConfigurationMachine()
+globs = dict(config=ConfigurationMachine())
 
 def test_suite():
     return doctest.DocFileSuite(
-            'adapter.txt',
-            setUp=setUp,
+            'adapter.txt', globs=globs,
             optionflags=optionflags
             )
-
-
-if __name__ == '__main__':
-    unittest.main(defaultTest='test_suite')
-
