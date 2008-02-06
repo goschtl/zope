d53086c622ae7cace252c147e4f16f9dc966f367
jackie <jackie@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-06 14:16:57 +0000

    version bump
    git-svn-id: svn://svn.zope.org/repos/main@83597 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.twist/tags/0.1.1/setup.py
+++ zc.twist/tags/0.1.1/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zc.twist',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages('src'),
     package_dir={'':'src'},
     url='http://svn.zope.org/zc.twist',
