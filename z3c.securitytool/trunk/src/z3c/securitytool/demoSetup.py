68d30b95040c5a851e1b918ef9775462c2a4936a
blackburnd <blackburnd@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-06 00:58:59 +0000

    Adding dummy security setup when database opens
    git-svn-id: svn://svn.zope.org/repos/main@83567 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.securitytool/trunk/src/z3c/securitytool/demoSetup.py
@@ -0,0 +1,31 @@
+import transaction
+
+from zope.app.folder import Folder
+
+from zope.app import zapi
+
+from zope.app.appsetup.bootstrap import getInformationFromEvent
+from zope.app.securitypolicy.interfaces import IPrincipalPermissionManager
+
+
+        
+
+class Participation:
+    interaction = None
+    
+class CreateStructure(object):
+    def __init__(self,event):
+        db, connection, root, root_folder = getInformationFromEvent(event)
+        # Lets get the root folder so we can assign some permissions to
+        # specific contexts
+        root=zapi.getRoot(root_folder)
+        rootPermManager =IPrincipalPermissionManager(root)
+
+        rootPermManager.denyPermissionToPrincipal('concord.ReadIssue','zope.daniel')
+        rootPermManager.denyPermissionToPrincipal('concord.CreateIssue','zope.daniel')
+        rootPermManager.denyPermissionToPrincipal('concord.CreateIssue','zope.stephan')
+        rootPermManager.denyPermissionToPrincipal('concord.CreateIssue','zope.markus')
+        rootPermManager.denyPermissionToPrincipal('concord.CreateIssue','zope.anybody')
+
+        transaction.commit()
+
