7972ff0de0df39bc27dc08122839fff5d6845f56
darrylcousins <darrylcousins@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-10 02:36:11 +0000

    Copying falsely deleted directory back in place.
    git-svn-id: svn://svn.zope.org/repos/main@83697 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Sandbox/darrylcousins/mars.view/mars/mars/view/components.py
@@ -0,0 +1,72 @@
+import zope.component
+import zope.interface
+from zope.component.interfaces import ComponentLookupError
+from zope.publisher.browser import BrowserPage
+from zope.publisher.publish import mapply
+from zope.pagetemplate.interfaces import IPageTemplate
+
+from z3c.template.interfaces import ILayoutTemplate
+from z3c.pagelet.interfaces import IPagelet
+
+import grok
+from grok.interfaces import IGrokView
+
+class TemplateViewBase(object):
+    """Mixin to reuse render method"""
+    template = None
+    _template_name = u'' # will be set if grok.template defined
+    _template_interface = IPageTemplate
+
+    def render(self):
+        template = getattr(self, 'template', None)
+        if template is None:
+            template = zope.component.getMultiAdapter(
+                (self, self.request), self._template_interface, 
+                name=self._template_name)
+            return template(self)
+        return template(self)
+
+
+class TemplateView(TemplateViewBase, BrowserPage):
+    zope.interface.implements(IGrokView)
+
+    def __init__(self, context, request):
+        super(TemplateView, self).__init__(context, request)
+
+
+class LayoutViewBase(object):
+    layout = None # allows developer to directly define template
+    _layout_name = u'' # will be set if mars.view.layout defined
+    _layout_interface = ILayoutTemplate
+
+    def __call__(self):
+        update = getattr(self, 'update', None)
+        if update is not None:
+            self.update()
+        if self.request.response.getStatus() in (302, 303):
+            return
+        layout = getattr(self, 'layout', None)
+        if layout is None:
+            layout = zope.component.getMultiAdapter(
+                    (self, self.request), self._layout_interface, 
+                    name=self._layout_name)
+            #print layout
+            #return u'Layout'
+            return layout(self)
+        return layout(self)
+
+class LayoutView(LayoutViewBase, BrowserPage):
+    zope.interface.implements(IGrokView)
+
+    def __init__(self, context, request):
+        BrowserPage.__init__(self, context, request)
+        self.__name__ = self.__view_name__
+
+class PageletView(TemplateViewBase, LayoutViewBase, BrowserPage):
+    zope.interface.implements(IPagelet, IGrokView)
+
+    def __init__(self, context, request):
+        BrowserPage.__init__(self, context, request)
+        self.__name__ = self.__view_name__
+
+
