4fc0fb185ee8fadddb6a528ad63935714a83cb1c
satchit <satchit@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-15 16:09:45 +0000

    making a release tag.
    git-svn-id: svn://svn.zope.org/repos/main@83868 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zc.objectlog/tags/0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+long_description = open("src/zc/objectlog/log.txt").read()
+
+setup(
+    name="zc.objectlog",
+    version="0.1dev",
+    license="ZPL 2.1",
+    author="Zope Corporation",
+    author_email="zope-dev@zope.org",
+    description=open('README.txt').read(),
+    long_description=long_description,
+    keywords="zope zope3 logging"
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    namespace_packages=['zc'],
+    include_package_data=True,
+    install_requirements = ['setuptools'],
+    zip_safe = False
+    )
