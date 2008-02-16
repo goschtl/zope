240da3f702a0915989b078f74dc42a9e1b911cf5
blackburnd <blackburnd@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-16 23:49:43 +0000

    Updated the demoSetup.py with some random values for permission settings and roles so users can see the usefulness
    git-svn-id: svn://svn.zope.org/repos/main@83959 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.securitytool/trunk/src/z3c/securitytool/demoSetup.py
+++ z3c.securitytool/trunk/src/z3c/securitytool/demoSetup.py
@@ -17,27 +17,65 @@ class CreateStructure(object):
         # Lets get the root folder so we can assign some permissions to
         # specific contexts
         root=zapi.getRoot(root_folder)
+        if 'Folder1' not in root:
+            root['Folder1'] = Folder()
+
+        if 'Folder2' not in root['Folder1']:
+            root['Folder1']['Folder2'] = Folder()
+            
+        sysPrincipals = zapi.principals()
+        principals = [x.id for x in sysPrincipals.getPrincipals('')]
+
 
         roleManager = IPrincipalRoleManager(root)
+        permManager = IPrincipalPermissionManager(root)
+        
         roleManager.assignRoleToPrincipal('zope.Editor', 'zope.daniel')
         roleManager.assignRoleToPrincipal('zope.Writer', 'zope.daniel')
+        roleManager.assignRoleToPrincipal('zope.Writer', 'zope.stephan')
 
-        permManager = IPrincipalPermissionManager(root)
+        for principal in principals:
+            permManager.grantPermissionToPrincipal('concord.ReadIssue',
+                                              principal)
+            permManager.denyPermissionToPrincipal('concord.DeleteArticle',
+                                              principal)
+            permManager.denyPermissionToPrincipal('concord.CreateArticle',
+                                              principal)
 
-        permManager.denyPermissionToPrincipal('concord.ReadIssue',
+
+        roleManager = IPrincipalRoleManager(root['Folder1'])
+        permManager = IPrincipalPermissionManager(root['Folder1'])
+
+        roleManager.assignRoleToPrincipal('zope.Janitor', 'zope.markus')
+        roleManager.assignRoleToPrincipal('zope.Writer', 'zope.daniel')
+
+        for principal in principals:
+            permManager.denyPermissionToPrincipal('concord.ReadIssue',
+                                              principal)
+            permManager.grantPermissionToPrincipal('concord.DeleteIssue',
+                                              principal)
+            permManager.grantPermissionToPrincipal('concord.CreateArticle',
+                                              principal)
+
+
+
+        roleManager = IPrincipalRoleManager(root['Folder1']['Folder2'])
+        permManager = IPrincipalPermissionManager(root['Folder1']['Folder2'])
+
+        roleManager.assignRoleToPrincipal('zope.Janitor', 'zope.markus')
+        roleManager.assignRoleToPrincipal('zope.Writer', 'zope.daniel')
+
+
+        permManager.denyPermissionToPrincipal('concord.CreateArticle',
                                               'zope.daniel')
-
         permManager.denyPermissionToPrincipal('concord.CreateIssue',
                                               'zope.daniel')
-
         permManager.denyPermissionToPrincipal('concord.CreateIssue',
                                               'zope.stephan')
-
         permManager.denyPermissionToPrincipal('concord.CreateIssue',
                                               'zope.markus')
-
         permManager.denyPermissionToPrincipal('concord.CreateIssue',
                                               'zope.anybody')
 
-        transaction.commit()
 
+        transaction.commit()
