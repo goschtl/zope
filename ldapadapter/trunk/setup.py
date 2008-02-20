fa63b5fe9101b05bbbdabdf2a63bb7d0f5b9be34
torsti <torsti@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-20 12:29:38 +0000

    Bind password is now a schema.Password field
    git-svn-id: svn://svn.zope.org/repos/main@84075 62d5b8a3-27da-0310-9561-8e5933582275

--- ldapadapter/trunk/setup.py
+++ ldapadapter/trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ldapadapter',
-    version='0.7iwm',
+    version='0.7dev',
     author='Zope 3 developers',
     author_email='zope3-dev@zope.org',
     url='http://svn.zope.org/ldapadapter',
