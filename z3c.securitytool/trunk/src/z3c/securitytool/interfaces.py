e6e0902acd6b9c71555435858918d3edd6a1bad5
blackburnd <blackburnd@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-20 17:25:50 +0000

    updated interfaces.py to be accurate
    git-svn-id: svn://svn.zope.org/repos/main@84080 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.securitytool/trunk/src/z3c/securitytool/interfaces.py
+++ z3c.securitytool/trunk/src/z3c/securitytool/interfaces.py
@@ -1,4 +1,50 @@
 from zope.interface import Interface
 
 class ISecurityChecker(Interface):
-    """TODO"""
+
+    def getPermissionSettingsForAllViews(self,interfaces,skin,selectedPermission=None):
+        """ gets the permission settings for all views"""
+    def aggregateMatrices(self):
+        """ aggregates the two matricies together """
+        
+    def getReadPerm(self,view_reg):
+        """ gets the read permission for the view """
+        
+    def populateMatrix(self,viewInstance,view_reg):
+        """ workhorse of the SecurityChecker class """
+        
+    def updateRolePermissionSetting(self,permSetting,principal,role,name):
+        """ updates the permission settings """
+        
+    def populatePermissionMatrix(self,read_perm,principalPermissions):
+        """ populates the permission matrix """
+
+
+class IPermissionDetails(Interface):
+    def permissionDetails(principal,read_perm,settings, rolePermissions):
+        """ workhorse of the PermissionDetails class """
+
+class IPrincipalDetails(Interface):
+    def principalPermissions(principal_id, skin):
+        """ main workhorse of the class """
+    def orderRoleTree(self):
+        """ This is an ordering method for the roleTree """
+        
+    def updatePrincipalMatrix( settings):
+        """ this is called to update the roles and permissions"""
+        
+    def updatePrincipalMatrixRoles( name, item):
+        """ method to up date the matrix roles """
+        
+    def updateRoleTree(item,parentList,curRole):
+        """ method to update the matrix roletree """
+
+    def updateRoles(item,role,curRole):
+        """ method to update the roles """
+        
+    def updatePrincipalMatrixPermissions( item):
+        """ method to update the permissions """
+        
+    def updatePermissionTree(item,prinPerms):
+        """ method to update the permission tree """
+
