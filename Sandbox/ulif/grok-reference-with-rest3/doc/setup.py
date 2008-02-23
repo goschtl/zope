1b9389f6b9ca127872baee55ebc7300cda25d86c
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-23 00:28:10 +0000

    Move grok2html into build/.
    git-svn-id: svn://svn.zope.org/repos/main@84157 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/ulif/grok-reference-with-rest3/doc/setup.py
+++ Sandbox/ulif/grok-reference-with-rest3/doc/setup.py
@@ -7,9 +7,12 @@ setup(
                       'zope.app.renderer',
                       'Pygments'
                       ],
-    py_modules = ['grok2html'],
+    packages=find_packages('build'),
+    package_dir = {'': 'build'},
+    py_modules = ['grok2html', 'grokdocs'],
     entry_points="""
     [console_scripts]
     grok2html = grok2html:main
+    grokdocs = grokdocs:grokdocs
     """
     )
