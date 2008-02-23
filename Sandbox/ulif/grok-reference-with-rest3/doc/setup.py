94899a6a46776b4d8a926e41fcc6c9d14b28cd9a
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-23 02:00:46 +0000

    Create grokref2html with buildout.
    git-svn-id: svn://svn.zope.org/repos/main@84169 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/ulif/grok-reference-with-rest3/doc/setup.py
+++ Sandbox/ulif/grok-reference-with-rest3/doc/setup.py
@@ -7,12 +7,12 @@ setup(
                       'zope.app.renderer',
                       'Pygments'
                       ],
-    packages=find_packages('build'),
     package_dir = {'': 'build'},
     py_modules = ['grok2html', 'grokdocs'],
     entry_points="""
     [console_scripts]
     grok2html = grok2html:main
     grokdocs = grokdocs:grokdocs
+    grokref2html = grokdocs:grokref
     """
     )
