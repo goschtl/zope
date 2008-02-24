00c2d6e32e555fff06702ad29d92bb368b41475c
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-24 15:18:28 +0000

    Adapt to new sphinx version.
    git-svn-id: svn://svn.zope.org/repos/main@84213 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/ulif/grok-reference-with-rest3/doc/conf.py
+++ Sandbox/ulif/grok-reference-with-rest3/doc/conf.py
@@ -105,6 +105,19 @@ html_index = 'build/docindex.template'
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'Grokdoc'
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# The style sheet to use for HTML and HTML Help pages. A file of that name
+# must exist either in Sphinx' static/ path, or in one of the custom paths
+# given in html_static_path.
+html_style = 'grok.css'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['.static']
+
 
 # Options for LaTeX output
 # ------------------------
