991db1cc995a936240bc14db2e0aec0d39a1b644
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-24 17:19:25 +0000

    Add new options to conf.py.
    git-svn-id: svn://svn.zope.org/repos/main@84221 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/ulif/grok-reference-with-rest3/doc/reference/conf.py
+++ Sandbox/ulif/grok-reference-with-rest3/doc/reference/conf.py
@@ -105,6 +105,16 @@ html_use_smartypants = True
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'Grok Referencedoc'
 
+# The style sheet to use for HTML and HTML Help pages. A file of that name
+# must exist either in Sphinx' static/ path, or in one of the custom paths
+# given in html_static_path.
+html_style = 'default.css'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+#html_static_path = [path.join(path.abspath(curdir), '.static')]
+
 
 # Options for LaTeX output
 # ------------------------
