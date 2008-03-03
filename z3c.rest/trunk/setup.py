a38b20aba57057653926915de1978ce403061772
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2008-03-03 21:10:11 +0000

    version bump
    git-svn-id: svn://svn.zope.org/repos/main@84449 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.rest/trunk/setup.py
+++ z3c.rest/trunk/setup.py
@@ -31,7 +31,7 @@ chapters = '\n'.join(
 
 setup (
     name='z3c.rest',
-    version='0.2.0',
+    version='0.3.0dev',
     author = "Stephan Richter and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "A REST Framework for Zope 3 Applications",
