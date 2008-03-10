996ba86fdb4c5ad6604d6b71975ad1c0b7282abd
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2008-03-10 10:41:54 +0000

    Prepare for release.
    git-svn-id: svn://svn.zope.org/repos/main@84565 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.vcsync/tags/0.11/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import sys, os
+
+setup(name='z3c.vcsync',
+      version='0.11dev',
+      description="Sync ZODB data with version control system, currently SVN",
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['z3c'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'grok',
+        'py',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
