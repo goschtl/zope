493371374451c70d5950053efe621376e60e104f
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2008-03-15 21:14:05 +0000

    refactoring datamanager
    git-svn-id: svn://svn.zope.org/repos/main@84677 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.zalchemy/branches/ajung-tm-refactor/src/z3c/zalchemy/demo/demo_1/message.py
@@ -0,0 +1,43 @@
+import sqlalchemy
+import sqlalchemy.orm
+
+from zope.component.factory import Factory
+from zope.interface import implements
+from zope.schema.fieldproperty import FieldProperty
+
+import z3c.zalchemy
+
+from interfaces import IHelloWorldMessage
+
+
+# Define and create the table object for storing messages
+HelloWorldMessageTable = sqlalchemy.Table(
+        'message',
+        z3c.zalchemy.metadata('DemoEngine-1'),
+        sqlalchemy.Column('id', sqlalchemy.Integer, primary_key = True),
+        sqlalchemy.Column('who', sqlalchemy.Unicode),
+        )
+
+z3c.zalchemy.createTable('message', 'DemoEngine-1')
+
+# Define the content class for messages
+class HelloWorldMessage(object):
+    implements(IHelloWorldMessage)
+
+    who = FieldProperty(IHelloWorldMessage['who'])
+
+    def __init__(self, who):
+        self.who = who
+
+    def __repr__(self):
+        return '<%s from %r>' %(self.__class__.__name__, self.who)
+
+
+# Map the table to the class
+sqlalchemy.orm.mapper(HelloWorldMessage, HelloWorldMessageTable)
+
+messageFactory=Factory(
+    HelloWorldMessage,
+    title=u"Create a new message",
+    description=u"This factory instantiates new messages"
+    )
