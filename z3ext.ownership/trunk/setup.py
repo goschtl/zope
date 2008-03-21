0aeb11ca67e9e3cb6387c429404a536f3ba8a696
fafhrd <fafhrd@62d5b8a3-27da-0310-9561-8e5933582275>
2008-03-21 06:29:27 +0000

    vb
    git-svn-id: svn://svn.zope.org/repos/main@84819 62d5b8a3-27da-0310-9561-8e5933582275

--- z3ext.ownership/trunk/setup.py
+++ z3ext.ownership/trunk/setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version='1.0.0'
+version='1.1.0dev'
 
 
 setup(name = 'z3ext.ownership',
