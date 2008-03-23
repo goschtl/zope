c060d6aa198ce4a017e9b312148450158a7e7719
rogerineichen <rogerineichen@62d5b8a3-27da-0310-9561-8e5933582275>
2008-03-23 11:37:28 +0000

    Removed navigation.js, this works now out of the box using the z3cJSONTree CSS class id
    git-svn-id: svn://svn.zope.org/repos/main@84872 62d5b8a3-27da-0310-9561-8e5933582275

--- zamplugin.navigation/trunk/src/zamplugin/navigation/zamplugin.navigation.js
+++
@@ -1,5 +0,0 @@
-//---[ adding menu ]-----------------------------------------------------------
-$(document).ready(function(){
-    // load zamplugin navigation
-    $("#zamPluginTree").z3cJSONTree()
-});
