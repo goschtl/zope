790887a4f6f83880336f7da9fe0e5111971d354f
darrylcousins <darrylcousins@62d5b8a3-27da-0310-9561-8e5933582275>
2008-03-26 21:57:18 +0000

    Remove defunct branch.
    git-svn-id: svn://svn.zope.org/repos/main@84956 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.zalchemy/branches/darrylcousins-sqlalchemy04-experiment/src/z3c/zalchemy/browser/views.py
+++
@@ -1,8 +0,0 @@
-
-from zope.formlib import form
-from z3c.zalchemy.interfaces import IAlchemyEngineUtility
-
-
-class EditView(form.EditForm):
-    form_fields = form.Fields(IAlchemyEngineUtility)
-
