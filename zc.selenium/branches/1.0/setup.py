9432025f0fd62222769ee587facfa675f3b30938
aaron <aaron@62d5b8a3-27da-0310-9561-8e5933582275>
2008-03-27 17:35:53 +0000

    Branch in preparation for hot tag action.
    git-svn-id: svn://svn.zope.org/repos/main@84971 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zc.selenium/branches/1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zc.selenium',
+    version='0.1dev',
+    author='Zope Corporation',
+    author_email='info@zope.com',
+    url='http://svn.zope.org/zc.seleinum',
+    description="Selenium integration for Zope 3",
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='ZPL 2.1',
+    namespace_packages = ['zc'],
+    install_requires=[
+        'setuptools'
+    ],
+    )
