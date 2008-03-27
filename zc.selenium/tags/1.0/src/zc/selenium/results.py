5636f236f5c19cd4dd9d9cba6e91a2e5c0b821b4
aaron <aaron@62d5b8a3-27da-0310-9561-8e5933582275>
2008-03-27 18:12:40 +0000

    Tag before egg release.
    git-svn-id: svn://svn.zope.org/repos/main@84977 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zc.selenium/tags/1.0/src/zc/selenium/results.py
@@ -0,0 +1,35 @@
+##############################################################################
+#
+# Copyright (c) 2005, 2006 Zope Corporation. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Visible Source
+# License, Version 1.0 (ZVSL).  A copy of the ZVSL should accompany this
+# distribution.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Result reporting resource for zc.selenium.
+
+"""
+
+import sys
+
+import zope.app.pagetemplate
+
+import zc.selenium.resource
+
+class Results(zc.selenium.resource.ResourceBase):
+
+    template = zope.app.pagetemplate.ViewPageTemplateFile('results.pt')
+
+    def POST(self):
+        print >>sys.__stderr__, "in results resource"
+        # get the queue used to communicate with the test thread, this will
+        # fail horribly if not running in "Selenium test" mode
+        messages = sys.modules['__main__'].messages
+        messages.put(self.request)
+        return self.template(self)
