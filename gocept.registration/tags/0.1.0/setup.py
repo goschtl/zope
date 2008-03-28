b391e4c5aed9e4f80a192449dc5c014ed7881c5d
pcardune <pcardune@62d5b8a3-27da-0310-9561-8e5933582275>
2008-03-28 17:10:42 +0000

    remove dev marker from setup.py
    git-svn-id: svn://svn.zope.org/repos/main@85001 62d5b8a3-27da-0310-9561-8e5933582275

--- gocept.registration/tags/0.1.0/setup.py
+++ gocept.registration/tags/0.1.0/setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup(
     name = 'gocept.registration',
-    version = "0.1.0dev",
+    version = "0.1.0",
     author = "Christian Theune, Stephan Richter and others",
     author_email = "mail@gocept.com",
     description = "User self-registration",
