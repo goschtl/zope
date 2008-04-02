773b1701ac17a989240c0c137246680e83139812
satchit <satchit@62d5b8a3-27da-0310-9561-8e5933582275>
2008-04-02 19:23:44 +0000

    upped version.
    git-svn-id: svn://svn.zope.org/repos/main@85076 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.objectlog/trunk/setup.py
+++ zc.objectlog/trunk/setup.py
@@ -7,7 +7,7 @@ long_description = (open("README.txt").read() +
 
 setup(
     name="zc.objectlog",
-    version="0.1.1",
+    version="0.1.2dev",
     license="ZPL 2.1",
     author="Zope Corporation",
     author_email="zope-dev@zope.org",
