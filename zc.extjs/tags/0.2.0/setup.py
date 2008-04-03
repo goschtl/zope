7135dc43afd704b492906a58f909e9ba1d5529f7
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2008-04-03 22:48:19 +0000

    Update rev #
    git-svn-id: svn://svn.zope.org/repos/main@85090 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.extjs/tags/0.2.0/setup.py
+++ zc.extjs/tags/0.2.0/setup.py
@@ -13,7 +13,7 @@
 ##############################################################################
 
 name = 'zc.extjs'
-version = '0.3dev'
+version = '0.2.0'
 
 import os, re
 from setuptools import setup, find_packages
