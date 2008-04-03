cbf88688f8db6a297f7d74b773aa0ae276b1852c
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2008-04-03 22:40:40 +0000

    Update rev #
    git-svn-id: svn://svn.zope.org/repos/main@85088 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.extjs/branches/dev/setup.py
+++ zc.extjs/branches/dev/setup.py
@@ -13,7 +13,7 @@
 ##############################################################################
 
 name = 'zc.extjs'
-version = '0.1.1'
+version = '0.3dev'
 
 import os, re
 from setuptools import setup, find_packages
