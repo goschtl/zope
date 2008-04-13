35e5ba00f030eccdefc636a9405a68ccd68a62bd
rogerineichen <rogerineichen@62d5b8a3-27da-0310-9561-8e5933582275>
2008-04-13 15:19:50 +0000

    Added dev marker
    git-svn-id: svn://svn.zope.org/repos/main@85332 62d5b8a3-27da-0310-9561-8e5933582275

--- zam.skin/trunk/setup.py
+++ zam.skin/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='zam.skin',
-    version='0.5.1',
+    version='0.5.2dev',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "Zope 3 Application Management skin",
