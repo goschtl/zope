fac2f1928625a8b4178c3450fde3f3214cea1754
rogerineichen <rogerineichen@62d5b8a3-27da-0310-9561-8e5933582275>
2008-04-13 15:14:37 +0000

    Added dev marker
    git-svn-id: svn://svn.zope.org/repos/main@85330 62d5b8a3-27da-0310-9561-8e5933582275

--- zam.api/trunk/setup.py
+++ zam.api/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='zam.api',
-    version='0.5.2',
+    version='0.5.3dev',
     author = "Stephan Richter, Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "API for ZAM (Zope Application Management",
