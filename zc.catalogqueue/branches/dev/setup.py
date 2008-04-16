0deb06a4552bed792c8ae1247b4cce0f6d9a8275
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2008-04-16 22:58:54 +0000

    Update to next rev
    git-svn-id: svn://svn.zope.org/repos/main@85447 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.catalogqueue/branches/dev/setup.py
+++ zc.catalogqueue/branches/dev/setup.py
@@ -35,7 +35,7 @@ long_description = (
 
 setup(
     name = 'zc.catalogqueue',
-    version = '0.1',
+    version = '0.2dev',
     author = 'Jim Fulton',
     author_email = 'jim@zope.com',
     description = '',
