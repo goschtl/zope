7365f37af015f4b7b65c69b64d5d57e4efdcf0e0
pcardune <pcardune@62d5b8a3-27da-0310-9561-8e5933582275>
2008-04-21 02:09:47 +0000

    Start working on how to reuse IPageTemplate adapters to generate the right javascript for different fields.  Now have TextArea widgets working.
    git-svn-id: svn://svn.zope.org/repos/main@85521 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/pcardune/z3c.formextjs/src/z3c/formextjs/browser/__init__.py
+++ Sandbox/pcardune/z3c.formextjs/src/z3c/formextjs/browser/__init__.py
@@ -1,42 +1,32 @@
-#extjs widgets
-import os.path, sys
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""ExtJS Form Widgets
+
+$Id$
+"""
 from zope.viewlet.interfaces import IViewletManager
 from zope.viewlet.viewlet import ViewletBase
 from zope.viewlet.manager import WeightOrderedViewletManager
 
-
-def package_home(gdict):
-    filename = gdict["__file__"]
-    return os.path.dirname(filename)
-
-class PythonTemplateFile(object):
-
-    def __init__(self, filename, _prefix=None):
-        path = self.get_path_from_prefix(_prefix)
-        self.filename = os.path.join(path, filename)
-        if not os.path.isfile(self.filename):
-            raise ValueError("No such file", self.filename)
-
-    def get_path_from_prefix(self, _prefix):
-        if isinstance(_prefix, str):
-            path = _prefix
-        else:
-            if _prefix is None:
-                _prefix = sys._getframe(2).f_globals
-            path = package_home(_prefix)
-        return path
-
-    def __call__(self, context, **kwargs):
-        inFile = open(self.filename, 'rb')
-        scope = {'r':'', 'context':context}
-        scope.update(kwargs)
-        exec inFile in scope
-        return scope['r']
-
+from z3c.formextjs.browser.pytemplate import PythonTemplateFile
 
 def escape(s):
     s = str(s).replace('\n','\\n').replace("'","\\'")
     return s
+
+def jsSafe(name):
+    return str(hash(name)).replace('-','_')
 
 
 class IJSFormViewletManager(IViewletManager):
@@ -77,6 +67,26 @@ class ExtJSWidgetsViewlet(FormAwareViewlet):
     def render(self):
         return self.template(self, **globals())
 
+
+class InputWidgetTemplate(object):
+    """provider of IPageTemplate for extjs widgets."""
+
+    template = PythonTemplateFile('form-widget.pyt')
+
+    def __init__(self, context, request, form, field, widget):
+        self.context = context
+        self.request = request
+        self.form = form
+        self.field = field
+        self.widget = widget
+
+    def __call__(self, widget):
+        scope = globals()
+        return self.template(widget, **scope)
+
+class TextAreaInputWidget(InputWidgetTemplate):
+
+    template = PythonTemplateFile('text-area-widget.pyt')
 
 ## class ExtJSButtonsViewlet(FormAwareViewlet):
 ##     """Viewlet that add JS hooks for displaying buttons."""
