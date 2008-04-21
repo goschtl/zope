fa8e9bf544e59b6a75e01ef512f87c2115c512cd
pcardune <pcardune@62d5b8a3-27da-0310-9561-8e5933582275>
2008-04-21 00:45:11 +0000

    Add hooks for providing the ext js library.  I wont stick ext itself into the zope repo as it is larger than 20mb!
    git-svn-id: svn://svn.zope.org/repos/main@85519 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/pcardune/z3c.formextjs/src/z3c/formextjs/__init__.py
+++ Sandbox/pcardune/z3c.formextjs/src/z3c/formextjs/__init__.py
@@ -1,1 +1,26 @@
-#extjs integration package.
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""ExtJS Form Integration Package
+
+$Id$
+"""
+
+from zope.viewlet.viewlet import JavaScriptBundleViewlet
+from zope.viewlet.viewlet import CSSViewlet
+
+
+ExtJavaScriptViewlet = JavaScriptBundleViewlet(('ext/adapter/ext/ext-base.js',
+                                               'ext/ext-all.js'))
+
+ExtCSSViewlet = CSSViewlet('ext/resources/css/ext-all.css')
