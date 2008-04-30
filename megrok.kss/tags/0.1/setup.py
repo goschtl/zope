640577cece602e54f62104560bb1269278b3f7d2
gotcha <gotcha@62d5b8a3-27da-0310-9561-8e5933582275>
2008-04-30 14:53:46 +0000

    prepare for release
    git-svn-id: svn://svn.zope.org/repos/main@85889 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ megrok.kss/tags/0.1/setup.py
@@ -0,0 +1,37 @@
+import os
+
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+        read('docs', 'README.txt')
+        )
+
+setup(name='megrok.kss',
+      version=version,
+      description="KSS for Grok. Ajax with Style, see http://kssproject.org",
+      long_description=long_description,
+      classifiers=[], 
+      keywords="",
+      author="Godefroid Chapelle",
+      author_email="gotcha@bubblenet.be",
+      url="",
+      license="ZPL",
+      package_dir={'': 'src'},
+      namespace_packages=['megrok'],      
+      packages=find_packages('src'),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'grok>=0.12',
+                        'kss.core>=1.4',
+                        # -*- Extra requirements: -*-
+                        ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
