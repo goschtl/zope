d8aca9531caef4b9acb66c55f44fb27f496b8c40
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2008-05-01 17:48:17 +0000

    Extended the science fiction.
    git-svn-id: svn://svn.zope.org/repos/main@86020 62d5b8a3-27da-0310-9561-8e5933582275

--- grokapps/rdbexample/trunk/src/rdbexample/app.py
+++ grokapps/rdbexample/trunk/src/rdbexample/app.py
@@ -1,21 +1,44 @@
 import grok
 from megrok import rdb
+from sqlalchemy.orm import *  # XXX
 
-class rdbexample(grok.Application, grok.Model):
+
+class RDBExample(grok.Application, grok.Model):
     def __init__(self):
         pass
 
     def traverse(self, name):
-        if name == 'departments':
-            self.departments = Departments()
-            
+        return rdb.query(Faculty).get(name)
 
+    
 class Index(grok.View):
-    pass # see app_templates/index.pt
+    pass
 
 
+class Faculty(rdb.Model):
+    # rdb.table_name('faculty') is the default
+
+    id = Column('id', Integer, primary_key=True)
+    title = Column('email', String(50))
+
+    departments = relation('Department',
+                           backref='faculty',
+                           collection_class=Departments)
+
+    def traverse(self, name):
+        return self.departments.get(name)
+    
+    
 class Departments(rdb.Container):
     pass
 
 class Department(rdb.Model):
-    pass
+    faculty_id = Column('faculty_id', Integer, ForeignKey(Faculty.id))
+    
+class DepartmentList(grok.View):
+    grok.context(Faculty)
+    def render(self):
+        result = ""
+        for department in self.context.departments.values():
+            result += department.title + '\n'
+        return result
