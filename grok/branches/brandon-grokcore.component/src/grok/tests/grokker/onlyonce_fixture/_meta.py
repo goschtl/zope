d1832f8bfb34f0c3d69842d1bf456b24338add3f
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2008-05-01 11:35:14 +0000

    Get rid of merged branch
    git-svn-id: svn://svn.zope.org/repos/main@85992 62d5b8a3-27da-0310-9561-8e5933582275

--- grok/branches/brandon-grokcore.component/src/grok/tests/grokker/onlyonce_fixture/_meta.py
+++
@@ -1,9 +0,0 @@
-import martian
-from component import Alpha
-
-class AlphaGrokker(martian.ClassGrokker):
-    component_class = Alpha
-
-    def grok(self, name, factory, module_info, **kw):
-        print "alpha"
-        return True
