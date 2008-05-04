0847dbc103a756e5d14941f48ed1e34838c9ed86
ctheune <ctheune@62d5b8a3-27da-0310-9561-8e5933582275>
2008-05-04 12:06:43 +0000

    Moved logging to its own feature.
    git-svn-id: svn://svn.zope.org/repos/main@86341 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.testing/branches/ctheune-cleanup/src/zope/testing/testrunner/runner.py
+++ zope.testing/branches/ctheune-cleanup/src/zope/testing/testrunner/runner.py
@@ -20,7 +20,6 @@ import re
 import cStringIO
 import gc
 import glob
-import logging
 import os
 import pdb
 import sys
@@ -39,6 +38,7 @@ from zope.testing.testrunner.refcount import TrackRefs
 from zope.testing.testrunner.options import get_options
 import zope.testing.testrunner.coverage
 import zope.testing.testrunner.doctest
+import zope.testing.testrunner.logsupport
 
 
 real_pdb_set_trace = pdb.set_trace
@@ -170,6 +170,7 @@ class Runner(object):
         # XXX I moved this here mechanically.
         self.test_directories = test_dirs(self.options, {})
 
+        self.features.append(zope.testing.testrunner.logsupport.Logging(self))
         self.features.append(zope.testing.testrunner.coverage.Coverage(self))
         self.features.append(zope.testing.testrunner.doctest.DocTest(self))
 
@@ -180,11 +181,6 @@ class Runner(object):
         # Make sure we start with real pdb.set_trace.  This is needed
         # to make tests of the test runner work properly. :)
         pdb.set_trace = real_pdb_set_trace
-
-        # Set the default logging policy.
-        # XXX There are no tests for this logging behavior.
-        # It's not at all clear that the test runner should be doing this.
-        configure_logging()
 
         # Setup profiling
         if (self.options.profile
@@ -638,43 +634,9 @@ def setup_layer(options, layer, setup_layers):
                         post_mortem(sys.exc_info())
                 else:
                     raise
-                    
+
         output.stop_set_up(time.time() - t)
         setup_layers[layer] = 1
-
-def configure_logging():
-    """Initialize the logging module."""
-    import logging.config
-
-    # Get the log.ini file from the current directory instead of
-    # possibly buried in the build directory.  TODO: This isn't
-    # perfect because if log.ini specifies a log file, it'll be
-    # relative to the build directory.  Hmm...  logini =
-    # os.path.abspath("log.ini")
-
-    logini = os.path.abspath("log.ini")
-    if os.path.exists(logini):
-        logging.config.fileConfig(logini)
-    else:
-        # If there's no log.ini, cause the logging package to be
-        # silent during testing.
-        root = logging.getLogger()
-        root.addHandler(NullHandler())
-        logging.basicConfig()
-
-    if os.environ.has_key("LOGGING"):
-        level = int(os.environ["LOGGING"])
-        logging.getLogger().setLevel(level)
-
-
-class NullHandler(logging.Handler):
-    """Logging handler that drops everything on the floor.
-
-    We require silence in the test environment.  Hush.
-    """
-
-    def emit(self, record):
-        pass
 
 
 class TestResult(unittest.TestResult):
