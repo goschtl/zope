f6ce80a590fda953f3bdcf891d5672c59b588bdd
mauritsvanrees <mauritsvanrees@62d5b8a3-27da-0310-9561-8e5933582275>
2008-05-05 12:57:48 +0000

    initial implementation, taking code from current grokproject
    git-svn-id: svn://svn.zope.org/repos/main@86448 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.recipe.eggbasket/trunk/README.txt
+++ z3c.recipe.eggbasket/trunk/README.txt
@@ -5,4 +5,4 @@
    Update the following URL to point to your code repository or remove
    it if one is not available.
 
-Code repository: http://svn.somewhere.com/...
+Code repository: 'http://svn.zope.org/*checkout*/z3c.recipe.eggbasket/trunk
--- z3c.recipe.eggbasket/trunk/z3c/recipe/eggbasket/__init__.py
+++ z3c.recipe.eggbasket/trunk/z3c/recipe/eggbasket/__init__.py
@@ -1,22 +1,85 @@
 # -*- coding: utf-8 -*-
 """Recipe eggbasket"""
 
-from zc.buildout import UserError
+import logging
+import tempfile
+import shutil
+import os
+import tarfile
+import urllib
+from zc.recipe.egg import Eggs
+from z3c.recipe.eggbasket.utils import distributions_are_installed_in_dir
+from z3c.recipe.eggbasket.utils import install_distributions
 
-class Recipe(object):
-    """zc.buildout recipe"""
 
-    def __init__(self, buildout, name, options):
-        self.buildout, self.name, self.options = buildout, name, options
+class Recipe(Eggs):
+    """zc.buildout recipe for getting eggs out of a tar ball"""
 
     def install(self):
-        """Installer"""
-        # XXX Implement recipe functionality here
-        
+        """Installer
+        """
+        log = logging.getLogger(self.name)
+        if self.buildout['buildout'].get('offline') == 'true':
+            log.error("Cannot run in offline mode.")
+            return tuple()
+        options = self.options
+
+        distributions = [
+            r.strip()
+            for r in options.get('eggs', self.name).split('\n')
+            if r.strip()]
+
+        if not distributions_are_installed_in_dir(distributions,
+                                                  options['eggs-directory']):
+            log.info("Grok is not installed. "
+                     "A grok tarball will be downloaded.")
+            url = self.options.get('url')
+            tarball_name = url.split('/')[-1]
+            log.info("Downloading %s ..." % url)
+
+            try:
+                # Make temporary files and directories.
+                extraction_dir = tempfile.mkdtemp()
+                filenum, temp_tarball_name = tempfile.mkstemp()
+
+                tarball = open(temp_tarball_name, 'w')
+                try:
+                    tarball.write(urllib.urlopen(url).read())
+                except IOError:
+                    log.error("Url not found: %s." % url)
+                    return tuple()
+                tarball.close()
+                log.info("Finished downloading.")
+                log.info("Installing eggs to %s which will take a while..."
+                         % options['eggs-directory'])
+
+                try:
+                    tf = tarfile.open(temp_tarball_name,
+                                      'r:gz')
+                except tarfile.ReadError:
+                    # Likely the download location is wrong and gives a 404.
+                    log.error("No correct tarball found at %s." % url)
+                    return tuple()
+
+                links = []
+                for name in tf.getnames():
+                    tf.extract(name, extraction_dir)
+                    links.append(os.path.join(extraction_dir, name))
+                tf.close()
+
+                log.info("installing grok.  This will take a while...")
+                result = install_distributions(
+                    distributions, options['eggs-directory'],
+                    links = links)
+                if result is False:
+                    log.error("Failed to install required eggs with the tar "
+                              "ball. Continuing with buildout instead.")
+            finally:
+                shutil.rmtree(extraction_dir)
+                os.unlink(temp_tarball_name)
+
         # Return files that were created by the recipe. The buildout
         # will remove all returned files upon reinstall.
         return tuple()
 
-    def update(self):
-        """Updater"""
-        pass
+    update = install
