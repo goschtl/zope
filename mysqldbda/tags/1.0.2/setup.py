f0bb1da158ffb6f60c4179a1331a20b3a2a23587
adamg <adamg@62d5b8a3-27da-0310-9561-8e5933582275>
2008-05-06 14:23:11 +0000

    making release
    git-svn-id: svn://svn.zope.org/repos/main@86512 62d5b8a3-27da-0310-9561-8e5933582275

--- mysqldbda/tags/1.0.2/setup.py
+++ mysqldbda/tags/1.0.2/setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='mysqldbda',
-      version = '1.0.1',
+      version = '1.0.2',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='MySQL Database adapter',
