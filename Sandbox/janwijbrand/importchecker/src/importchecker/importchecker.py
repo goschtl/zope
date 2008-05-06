c6dc884115099fe91762fddb02481b2e472952e0
janwijbrand <janwijbrand@62d5b8a3-27da-0310-9561-8e5933582275>
2008-05-06 10:31:08 +0000

    Small change to make the importchecker work with python 2.5,
    git-svn-id: svn://svn.zope.org/repos/main@86498 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/janwijbrand/importchecker/src/importchecker/importchecker.py
+++ Sandbox/janwijbrand/importchecker/src/importchecker/importchecker.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.4
+#!/usr/bin/env python
 ##############################################################################
 #
 # Copyright (c) 2003 Zope Corporation and Contributors.
