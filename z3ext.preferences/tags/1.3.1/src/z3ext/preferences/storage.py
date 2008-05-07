7412a39a461898da263b29d98fbd9d4948f23fa3
fafhrd <fafhrd@62d5b8a3-27da-0310-9561-8e5933582275>
2008-05-07 09:37:09 +0000

    tag for 1.3.1 egg
    git-svn-id: svn://svn.zope.org/repos/main@86524 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3ext.preferences/tags/1.3.1/src/z3ext/preferences/storage.py
@@ -0,0 +1,63 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+
+$Id$
+"""
+from BTrees.OOBTree import OOBTree
+
+from zope import interface, component
+from zope.security.interfaces import IPrincipal
+from zope.annotation.interfaces import IAnnotations
+
+from interfaces import ANNOTATION_KEY, IDataStorage, IPreferenceGroup
+
+
+@interface.implementer(IDataStorage)
+@component.adapter(IPrincipal, IPreferenceGroup)
+def getDefaultStorage(principal, preference):
+    ann = IAnnotations(principal)
+
+    # If no preferences exist, create the root preferences object.
+    if  ann.get(ANNOTATION_KEY) is None:
+        ann[ANNOTATION_KEY] = OOBTree()
+    prefs = ann[ANNOTATION_KEY]
+
+    # If no entry for the group exists, create a new entry.
+    id = preference.__schema__.getTaggedValue('preferenceID')
+    if id not in prefs:
+        prefs[id] = OOBTree()
+
+    return DataStorage(prefs[id], preference.__schema__)
+
+
+class DataStorage(object):
+    interface.implements(IDataStorage)
+
+    def __init__(self, btree, schema):
+        self.__dict__['__btree__'] = btree
+        self.__dict__['__schema__'] = schema
+
+    def __getattr__(self, attr):
+        if attr in self.__btree__:
+            return self.__btree__[attr]
+        else:
+            raise AttributeError()
+
+    def __setattr__(self, attr, value):
+        self.__btree__[attr] = value
+
+    def __delattr__(self, attr):
+        if attr in self.__btree__:
+            del self.__btree__[attr]
