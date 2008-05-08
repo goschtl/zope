b32074c37597cb318ec570fb568b76b5afac6d90
blackburnd <blackburnd@62d5b8a3-27da-0310-9561-8e5933582275>
2008-05-08 21:04:53 +0000

    Added some comments to dummySetup.py
    git-svn-id: svn://svn.zope.org/repos/main@86548 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.securitytool/trunk/src/z3c/securitytool/demoSetup.py
+++ z3c.securitytool/trunk/src/z3c/securitytool/demoSetup.py
@@ -13,10 +13,17 @@ class Participation:
     
 class CreateStructure(object):
     def __init__(self,event):
+        """ This method gets called on IDatabaseOpenedEvent when running the
+            Demo we add some seemingly random security permissions to the
+            folder tree created below so users of the demo can see what
+            security tool can display
+        """
         db, connection, root, root_folder = getInformationFromEvent(event)
         # Lets get the root folder so we can assign some permissions to
         # specific contexts
         root=zapi.getRoot(root_folder)
+
+        # If the following folders do not exist... lets create them
         if 'Folder1' not in root:
             root['Folder1'] = Folder()
 
@@ -26,19 +33,22 @@ class CreateStructure(object):
         if 'Folder3' not in root['Folder1']['Folder2']:
             root['Folder1']['Folder2']['Folder3'] = Folder()
 
+        # Lets get the list of all principals on the system.
         sysPrincipals = zapi.principals()
-        principals = [x.id for x in sysPrincipals.getPrincipals('') if x.id != 'group1']
+        principals = [x.id for x in sysPrincipals.getPrincipals('')
+                      if x.id != 'group1']
 
-
+# Here is where we begin to set the permissions for the root context level
         roleManager = IPrincipalRoleManager(root)
         permManager = IPrincipalPermissionManager(root)
-        
         roleManager.assignRoleToPrincipal('zope.Editor', 'zope.group1')
 
+        # Here we assign the group group1 to zope.daniel
         daniel  = sysPrincipals.getPrincipal('zope.daniel')
         group1  = sysPrincipals.getPrincipal('zope.group1')
         daniel.groups.append(group1)
 
+        # Here we assign the group group1 to zope.randy
         randy  = sysPrincipals.getPrincipal('zope.randy')
         group1  = sysPrincipals.getPrincipal('zope.group1')
         randy.groups.append(group1)
@@ -55,6 +65,8 @@ class CreateStructure(object):
                                               principal)
 
 
+# Here is where we begin to set the permissions for the context level of
+# Folder1.
         roleManager = IPrincipalRoleManager(root['Folder1'])
         permManager = IPrincipalPermissionManager(root['Folder1'])
 
@@ -73,13 +85,13 @@ class CreateStructure(object):
                                               principal)
 
 
-
+# Here is where we begin to set the permissions for the context level of
+# /root/Folder1/Folder2.
         roleManager = IPrincipalRoleManager(root['Folder1']['Folder2'])
         permManager = IPrincipalPermissionManager(root['Folder1']['Folder2'])
 
         roleManager.assignRoleToPrincipal('zope.Janitor', 'zope.markus')
         roleManager.assignRoleToPrincipal('zope.Writer', 'zope.daniel')
-
 
         permManager.denyPermissionToPrincipal('concord.CreateArticle',
                                               'zope.daniel')
@@ -92,8 +104,14 @@ class CreateStructure(object):
         permManager.denyPermissionToPrincipal('concord.CreateIssue',
                                               'zope.anybody')
 
-        roleManager = IPrincipalRoleManager(root['Folder1']['Folder2']['Folder3'])
-        permManager = IPrincipalPermissionManager(root['Folder1']['Folder2']['Folder3'])
+# Here is where we begin to set the permissions for the context level of
+# /root/Folder1/Folder2/Folder3.
+        roleManager = IPrincipalRoleManager(root['Folder1']\
+                                                ['Folder2']\
+                                                ['Folder3'])
+        permManager = IPrincipalPermissionManager(root['Folder1']\
+                                                      ['Folder2']\
+                                                      ['Folder3'])
 
         
         roleManager.removeRoleFromPrincipal('zope.Writer','zope.daniel')
