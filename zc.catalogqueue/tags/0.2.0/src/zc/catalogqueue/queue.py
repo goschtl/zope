a0792d9d2d8fdafd8bd892db8290efe451e057d4
fdrake <fdrake@62d5b8a3-27da-0310-9561-8e5933582275>
2008-05-09 18:44:55 +0000

    tag zc.catalogqueue release 0.2.0
    git-svn-id: svn://svn.zope.org/repos/main@86582 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zc.catalogqueue/tags/0.2.0/src/zc/catalogqueue/queue.py
@@ -0,0 +1,98 @@
+##############################################################################
+#
+# Copyright (c) Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from zc.catalogqueue.CatalogEventQueue import REMOVED, CHANGED, ADDED
+
+import BTrees.Length
+import datetime
+import logging
+import persistent
+import pytz
+import zc.catalogqueue.CatalogEventQueue
+import zc.catalogqueue.interfaces
+import zope.interface
+
+logger = logging.getLogger(__name__)
+
+
+class CatalogQueue(persistent.Persistent):
+
+    zope.interface.implements(zc.catalogqueue.interfaces.ICatalogQueue)
+
+    lastProcessedTime = None
+    totalProcessed = 0
+
+    _buckets = 1009 # Maybe configurable someday
+
+    def __init__(self):
+        self._length = BTrees.Length.Length()
+        self._queues = [
+            zc.catalogqueue.CatalogEventQueue.CatalogEventQueue()
+            for i in range(self._buckets)
+            ]
+
+    def __len__(self):
+        return self._length()
+
+    def _notify(self, id, event):
+        self._length.change(
+            self._queues[hash(id) % self._buckets].update(id, event))
+
+    def add(self, id):
+        self._notify(id, ADDED)
+
+    def update(self, id):
+        self._notify(id, CHANGED)
+
+    def remove(self, id):
+        self._notify(id, REMOVED)
+
+    def process(self, ids, catalogs, limit):
+        done = 0
+        for queue in self._queues:
+            for id, (_, event) in queue.process(limit-done).iteritems():
+                if event is REMOVED:
+                    for catalog in catalogs:
+                        catalog.unindex_doc(id)
+                else:
+                    ob = ids.queryObject(id)
+                    if ob is None:
+                        logger.warn("Couldn't find object for %s", id)
+                    else:
+                        for catalog in catalogs:
+                            catalog.index_doc(id, ob)
+                done += 1
+                self._length.change(-1)
+
+            if done >= limit:
+                break
+
+        self.lastProcessedTime = datetime.datetime.now(pytz.UTC)
+        self.totalProcessed += done
+
+        return done
+
+
+def addLengthSupport(queue):
+    """Update an older CatalogQueue to include the length support.
+
+    This should be called by generations that need to update catalog queues.
+
+    """
+    try:
+        queue._length
+    except AttributeError:
+        queue._length = BTrees.Length.Length()
+        for subqueue in queue._queues:
+            queue._length.change(len(subqueue))
