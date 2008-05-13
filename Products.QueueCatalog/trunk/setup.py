7ebd7d3f2476d2e27aa63f18bba9cafde07505f5
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2008-05-13 10:50:19 +0000

    added
    git-svn-id: svn://svn.zope.org/repos/main@86695 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Products.QueueCatalog/trunk/setup.py
@@ -0,0 +1,31 @@
+
+import os
+from setuptools import setup, find_packages
+
+
+CLASSIFIERS = [
+    'Programming Language :: Python',
+    'Framework :: Zope2',
+]
+
+version = '1.5'
+
+setup(name='Products.QueueCatalog',
+      version=version,
+      license='ZPL (see LICENSE.txt)',
+      author='Christian Theune',
+      author_email='ct@gocept.com',
+      maintainer='Christian Theune',
+      maintainer_email='ct@gocept.com',
+      classifiers=CLASSIFIERS,
+      keywords='Zope2 Indexing Catalog Queue',
+      url='http://www.zope.org/Members/ctheune/QueueCatalog',
+      description='QueueCatalog - asynchronous cataloging',
+      long_description=long_description,
+      packages=['Products', 'Products.QueueCatalog'],
+      include_package_data = True,
+      zip_safe=False,
+      install_requires=['setuptools'],
+      namespace_packages=['Products'],
+
+      )
