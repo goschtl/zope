1312e503c1666284680533782bf32c3f8f319cf6
malthe <malthe@62d5b8a3-27da-0310-9561-8e5933582275>
2008-05-18 14:08:05 +0000

    Preparing release.
    git-svn-id: svn://svn.zope.org/repos/main@86823 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.dobbin/trunk/setup.py
+++ z3c.dobbin/trunk/setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='z3c.dobbin',
-      version='0.1',
+      version='0.2',
       license='ZPL',
       author = "Malthe Borch, Stefan Eletzhofer and the Zope Community",
       author_email = "zope-dev@zope.org",
