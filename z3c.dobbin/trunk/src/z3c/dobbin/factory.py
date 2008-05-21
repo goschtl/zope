4eefee72937d1ebc385927c1d8805c4b8b98e145
malthe <malthe@62d5b8a3-27da-0310-9561-8e5933582275>
2008-05-21 13:28:56 +0000

    When generating unique ids, force first character between 'a' and 'z'.
    git-svn-id: svn://svn.zope.org/repos/main@86879 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.dobbin/trunk/src/z3c/dobbin/factory.py
+++ z3c.dobbin/trunk/src/z3c/dobbin/factory.py
@@ -2,8 +2,17 @@ from interfaces import IMapper
 from zope.interface.declarations import _normalizeargs
 
 from uuid import uuid1
+from random import randint
 
 from ore.alchemist import Session
+
+def uuid():
+    """Return new unique id as string.
+
+    Force first character between 'a' and 'z'.
+    """
+    
+    return chr(randint(ord('a'), ord('z'))) + uuid1().hex[:-1]
 
 def create(spec):
     # set up mapper
@@ -13,7 +22,7 @@ def create(spec):
     instance = mapper()
 
     # set soup attributes
-    instance.uuid = uuid1().hex
+    instance.uuid = uuid()
     instance.spec = mapper.__spec__
 
     # save to session
