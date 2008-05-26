5d7f61fc575a9299026e05db048a6362e018de76
philikon <philikon@62d5b8a3-27da-0310-9561-8e5933582275>
2008-05-26 12:43:34 +0000

    Use the new MethodGrokker from martian and grok (currently only available on philikon-methodgrokker branches). With this new baseclass, the grokker code is simplified even more.
    git-svn-id: svn://svn.zope.org/repos/main@86974 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.kss/branches/janjaapdriessen-grokcore.component-for-megrok.kss/src/megrok/kss/meta.py
+++ megrok.kss/branches/janjaapdriessen-grokcore.component-for-megrok.kss/src/megrok/kss/meta.py
@@ -10,50 +10,35 @@ from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 from megrok.kss.components import KSS
 
-class KSSGrokker(martian.ClassGrokker):
+class KSSGrokker(martian.MethodGrokker):
     component_class = KSS
     directives = [
         grok.view.bind(),
-        grok.require.bind(name='class_permission'),
+        grok.require.bind(name='permission'),
+        # TODO: We should allow name directives on methods
+        #name = grok.name.bind(...)
         ]
 
-    def execute(self, factory, config, view, class_permission, **kw):
-        methods = martian.util.methods_from_class(factory)
+    def execute(self, factory, method, config, view, permission, **kw):
 
-        for method in methods:
-            name = method.__name__
-            if name.startswith('__'):
-                continue
+        # Create a new class with a __view_name__ attribute so the
+        # KSSServerAction class knows what method to call.
+        name = method.__name__
+        method_view = type(
+            factory.__name__, (factory, BrowserPage),
+            {'__view_name__': name}
+            )
 
-            # Create a new class with a __view_name__ attribute so the
-            # KSSServerAction class knows what method to call.
-
-            # TODO: We should allow name directives on methods
-            #view_name = grok.name.bind(default=name).get(method)
-
-            method_view = type(
-                factory.__name__, (factory, BrowserPage),
-                {'__view_name__': name}
-                )
-
-            adapts = (view, IDefaultBrowserLayer)
-            config.action(
-                discriminator=('adapter', adapts, interface.Interface, name),
-                callable=component.provideAdapter,
-                args=(method_view, adapts, interface.Interface, name)
-                )
-
-            # Protect method_view with either the permission that was
-            # set on the method, the default permission from the class
-            # level or zope.Public.
-            permission = grok.require.bind().get(method)
-            if permission is None:
-                permission = class_permission
-
-            config.action(
-                discriminator=('protectName', method_view, '__call__'),
-                callable=make_checker,
-                args=(factory, method_view, permission)
-                )
+        adapts = (view, IDefaultBrowserLayer)
+        config.action(
+            discriminator=('adapter', adapts, interface.Interface, name),
+            callable=component.provideAdapter,
+            args=(method_view, adapts, interface.Interface, name)
+            )
+        config.action(
+            discriminator=('protectName', method_view, '__call__'),
+            callable=make_checker,
+            args=(factory, method_view, permission)
+            )
 
         return True
