cd362146a5948eb5df811a2c4a5fb7b6e7859f35
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-06-10 08:11:15 +0000

    Make grokadmin a namespace package.
    git-svn-id: svn://svn.zope.org/repos/main@87273 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/ulif/grokadmin/trunk/src/grokadmin/__init__.py
+++ Sandbox/ulif/grokadmin/trunk/src/grokadmin/__init__.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2007 Zope Corporation and Contributors.
+# Copyright (c) 2008 Zope Corporation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -12,3 +12,4 @@
 #
 ##############################################################################
 # a package
+__import__('pkg_resources').declare_namespace(__name__)
