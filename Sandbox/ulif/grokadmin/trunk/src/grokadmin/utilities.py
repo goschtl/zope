88a43ad92af7e77475fdc19d678837405649c33e
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-06-17 11:34:48 +0000

    Correct dotted name in utility tests.
    git-svn-id: svn://svn.zope.org/repos/main@87452 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/ulif/grokadmin/trunk/src/grokadmin/utilities.py
+++ Sandbox/ulif/grokadmin/trunk/src/grokadmin/utilities.py
@@ -32,32 +32,32 @@ def getPathLinksForObject(obj, root_url=''):
 
     We can use ObjectInfo objects to check this:
 
-      >>> from grok.admin.objectinfo import ObjectInfo
+      >>> from grokadmin.objectinfo import ObjectInfo
       >>> obj = ObjectInfo(None)
       >>> obj
-      <grok.admin.objectinfo.ObjectInfo object at ...>
+      <grokadmin.objectinfo.ObjectInfo object at ...>
 
     Obviously we have a string representation of the required form
     here. So we can get HTML with links to the documentation for
-    ``grok``, ``grok.admin`` and so on.
+    ``grok``, ``grokadmin`` and so on.
     
-      >>> from grok.admin.utilities import getPathLinksForObject
+      >>> from grokadmin.utilities import getPathLinksForObject
       >>> link = getPathLinksForObject(obj)
       >>> link
-      "&lt;<a href='/docgrok/grok/'>grok</a>... object at ..."
+      "&lt;<a href='/docgrok/grokadmin/'>grokadmin</a>... object at ..."
 
     We got a link to the ``grok`` documentation. Also links to
-    ``grok.admin``, ``grok.admin.objectinfo`` and
-    ``grok.admin.objectinfo.ObjectInfo`` are provided:
+    ``grokadmin``, ``grokadmin.objectinfo`` and
+    ``grokadmin.objectinfo.ObjectInfo`` are provided:
 
       >>> link
-      "&lt;...<a href='/docgrok/grok/admin/'>admin</a>... object at ..."
+      "&lt;...<a href='/docgrok/grokadmin/'>grokadmin</a>... object at ..."
 
     If we provide a root_url, we will find it in the links:
 
       >>> link = getPathLinksForObject(obj, 'http://localhost:8080')
       >>> link
-      "&lt;<a href='http://localhost:8080/docgrok/grok/'>grok</a>..."
+      "&lt;<a href='http://localhost:8080/docgrok/grokadmin/'>grokadmin</a>..."
 
     If no dotted path is included in objects strings representation, a
     simple string without links is returned:
@@ -115,20 +115,20 @@ def getPathLinksForClass(klass, root_url=''):
 
     We can use class ObjectInfo to check this:
 
-      >>> from grok.admin.objectinfo import ObjectInfo
+      >>> from grokadmin.objectinfo import ObjectInfo
       >>> ObjectInfo
-      <class 'grok.admin.objectinfo.ObjectInfo'>
+      <class 'grokadmin.objectinfo.ObjectInfo'>
 
-      >>> from grok.admin.utilities import getPathLinksForClass
+      >>> from grokadmin.utilities import getPathLinksForClass
       >>> htmlcode = getPathLinksForClass(ObjectInfo)
       >>> htmlcode
-      "&lt;class '<a href='/docgrok/grok/'>grok</a>...'&gt;"
+      "&lt;class '<a href='/docgrok/grokadmin/'>grokadmin</a>...'&gt;"
 
     When we provide a root_url the link will include it in the
     href-attribute:
 
       >>> getPathLinksForClass(ObjectInfo, 'http://localhost')
-      "&lt;class '<a href='http://localhost/docgrok/grok/'>grok</a>...'&gt;"
+      "&lt;class '<a href='http://localhost/docgrok/grokadmin/'>grokadmin</a>...'&gt;"
 
     If the class does not provide an appropriate string
     representation, we will get the representation without any links:
@@ -206,7 +206,7 @@ def isContainingEvilRegExpChars(strval):
 
     We indeed must provide a string:
 
-       >>> from grok.admin.utilities import isContainingEvilRegExpChars
+       >>> from grokadmin.utilities import isContainingEvilRegExpChars
        >>> isContainingEvilRegExpChars(None)
        Traceback (most recent call last):
        ...
@@ -231,7 +231,7 @@ def isContainingEvilRegExpChars(strval):
 def getParentURL(url):
     """Compute the parent URL for an object described by URL.
 
-       >>> from grok.admin.utilities import getParentURL
+       >>> from grokadmin.utilities import getParentURL
        >>> getParentURL('http://foo:8080/myobj')
        'http://foo:8080/'
 
