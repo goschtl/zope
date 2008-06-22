13092181839443c9670a9acf5c86ae02d4e2ba05
malthe <malthe@62d5b8a3-27da-0310-9561-8e5933582275>
2008-06-22 00:52:18 +0000

    Version bump.
    git-svn-id: svn://svn.zope.org/repos/main@87646 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.dobbin/trunk/setup.py
+++ z3c.dobbin/trunk/setup.py
@@ -18,7 +18,7 @@ def read(*files):
     return "\n".join((open(f).read() for f in files))
 
 setup(name='z3c.dobbin',
-      version='0.3dev',
+      version='0.3.1dev',
       license='ZPL',
       author = "Malthe Borch, Stefan Eletzhofer and the Zope Community",
       author_email = "zope-dev@zope.org",
