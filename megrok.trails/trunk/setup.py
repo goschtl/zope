d813cbc09704316c417d1259012edbcd29463fdb
brandon_rhodes <brandon_rhodes@62d5b8a3-27da-0310-9561-8e5933582275>
2008-06-25 04:51:00 +0000

    Gave megrok.trails halfway decent metadata.
    git-svn-id: svn://svn.zope.org/repos/main@87744 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.trails/trunk/setup.py
+++ megrok.trails/trunk/setup.py
@@ -1,21 +1,32 @@
 from setuptools import setup, find_packages
 
-version = '0.0'
+version = '1.0dev'
+
+def read(*filenames):
+    return open(os.path.join(os.path.dirname(__file__), *filenames)).read()
 
 setup(name='megrok.trails',
       version=version,
-      description="",
-      long_description="""\
-""",
+      description="URL patterns for Grok applications",
+      long_description=read('README.txt'),
       # Use classifiers that are already listed at:
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[], 
-      keywords="",
-      author="",
-      author_email="",
-      url="",
-      license="",
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Web Environment',
+                   'Framework :: Zope3',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Zope Public License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Internet :: WWW/HTTP',
+                   'Topic :: Software Development :: Libraries',
+                   ],
+      keywords="grok megrok trail trails url",
+      author="Brandon Craig Rhodes",
+      author_email="brandon@rhodesmill.org",
+      license="ZPL",
       package_dir={'': 'src'},
+      namespace_packages=['megrok'],
       packages=find_packages('src'),
       include_package_data=True,
       zip_safe=False,
