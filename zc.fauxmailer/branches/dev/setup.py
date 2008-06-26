3710c1d928728c7bbe19b7e504a7e0aabebf8aec
gary <gary@62d5b8a3-27da-0310-9561-8e5933582275>
2008-06-26 20:47:10 +0000

    support encoded headers and multipart messages. Tests would probably be a good idea soon. :-/
    git-svn-id: svn://svn.zope.org/repos/main@87814 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.fauxmailer/branches/dev/setup.py
+++ zc.fauxmailer/branches/dev/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = "zc.fauxmailer",
     description = "Simple printing mailer for development",
-    version = "1.1",
+    version = "1.2",
     license = "ZPL",
     packages = find_packages('src'),
     include_package_data = True,
--- zc.fauxmailer/branches/dev/src/zc/fauxmailer/__init__.py
+++ zc.fauxmailer/branches/dev/src/zc/fauxmailer/__init__.py
@@ -15,6 +15,7 @@
 """
 
 import email
+import email.Header
 import sys
 
 import zope.interface
@@ -26,14 +27,34 @@ class Mailer:
 
     def send(self, fromaddr, toaddrs, message_string):
         message = email.message_from_string(message_string)
-        print  '='*70
+        print '='*70
         print 'From', fromaddr
-        print  'To', toaddrs
-        print  '-'*70
+        print 'To', toaddrs
+        print '-'*70
         for h in message.keys():
             for v in message.get_all(h):
-                print  "%s: %s" % (h, v)
-            
-        print  message.get_payload(decode=True)
-        print  '='*70
+                # we want to decode encoded headers, showing the encoding.
+                # we also want to simplify the display, both for backwards
+                # compatibility and for ease of reading in tests
+                decoded = []
+                raw = email.Header.decode_header(v)
+                if isinstance(raw, tuple):
+                    raw = [raw]
+                for info in raw:
+                    if info[1] is None:
+                        decoded.append(info[0])
+                    else:
+                        decoded.append(info)
+                if len(decoded) == 1:
+                    decoded = decoded[0]
+                print "%s: %s" % (h, decoded)
+        if message.is_multipart():
+            # show each part
+            for m in message.get_payload():
+                print '.'*70
+                print 'Content-Type:', m.get_content_type()
+                print m.get_payload(decode=True)
+        else:
+            print message.get_payload(decode=True)
+        print '='*70
         sys.stdout.flush()
