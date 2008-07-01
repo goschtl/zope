cc5e1a9aa063128a912c07b56deb25f3324c72ad
adamg <adamg@62d5b8a3-27da-0310-9561-8e5933582275>
2008-07-01 18:21:20 +0000

    beginnings of adaptation based compilation
    git-svn-id: svn://svn.zope.org/repos/main@87893 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/adamg/ocql/branches/alg-compiler/src/ocql/compiler/compiler.py
+++ Sandbox/adamg/ocql/branches/alg-compiler/src/ocql/compiler/compiler.py
@@ -10,9 +10,13 @@ $Id$
 
 from zope.component import adapts
 from zope.interface import implements
+from zope.component import provideAdapter
 
 from ocql.interfaces import IAlgebraCompiler
 from ocql.interfaces import IOptimizedAlgebraObject
+from ocql.interfaces import ICompiledAlgebraObject
+
+from ocql.rewriter.interfaces import *
 
 from ocql.compiler.runnablequery import RunnableQuery
 
@@ -26,6 +30,33 @@ class AlgebraCompiler(object):
 
     def __call__(self, metadata, algebra):
         algebra = self.context
-        code = algebra.compile()
+        #code = algebra.compile()
+        code = IAlgebraCompiler(self.context)()
         run = RunnableQuery(metadata, algebra, code)
         return run
+
+class BaseCompiler(object):
+    def __init__(self, context):
+        #context becomes the adapted object
+        self.context = context
+
+class EmptyCompiler(BaseCompiler):
+    implements(IAlgebraCompiler)
+    adapts(IEmpty)
+
+    def __call__(self):
+        if self.context.klass == set:
+            return 'set()'
+        elif self.context.klass == list:
+            return '[]'
+
+class SingleCompiler(BaseCompiler):
+    def __call__(self):
+        if self.context.klass == set:
+            return 'set(['+IAlgebraCompiler(self.context.expr)()+'])'
+        elif self.context.klass == list:
+            return '['+IAlgebraCompiler(self.context.expr)()+']'
+
+def registerAdapters():
+    provideAdapter(EmptyCompiler)
+    provideAdapter(SingleCompiler)
