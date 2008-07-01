23ee2321b40a959b3b9180f6641b5a16ff890ff2
zvezdan <zvezdan@62d5b8a3-27da-0310-9561-8e5933582275>
2008-07-01 20:41:30 +0000

    Update certificate file.
    git-svn-id: svn://svn.zope.org/repos/main@87897 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.ssl/trunk/setup.py
+++ zc.ssl/trunk/setup.py
@@ -2,20 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name = "zc.ssl",
-    version = "1.0",
+    version = "1.1",
     author = "Zope Corporation",
-    author_email = "zope3-dev#zope.org",
+    author_email = "zope-dev@zope.org",
     description = "An HTTPSConnection implementation with the new ssl module",
     keywords = "ssl https",
-
     packages = find_packages('src'),
     include_package_data = True,
+    zip_safe = False,
     package_dir = {'':'src'},
     namespace_packages = ['zc'],
     install_requires = [
-       'zope.testing',
        'setuptools',
        'ssl-for-setuptools',
+       'zope.testing',
        ],
     dependency_links = ['http://download.zope.org/distribution/'],
     license = "ZPL 2.1",
