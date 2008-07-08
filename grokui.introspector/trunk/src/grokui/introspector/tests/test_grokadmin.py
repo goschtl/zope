90b4fe1bef902f71b86a7e8f5f8fa9e8f0fa72ae
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-07-08 14:06:55 +0000

    Enable ftesting.zcml.
    git-svn-id: svn://svn.zope.org/repos/main@88116 62d5b8a3-27da-0310-9561-8e5933582275

--- grokui.introspector/trunk/src/grokui/introspector/tests/test_grokadmin.py
+++ grokui.introspector/trunk/src/grokui/introspector/tests/test_grokadmin.py
@@ -14,10 +14,20 @@
 """
 Test setup for grok.admin.introspector.
 """
+import os
+import grokui.introspector
+from zope.app.testing.functional import ZCMLLayer
 from grok.testing import register_all_tests
 from grok.ftests.test_grok_functional import GrokFunctionalLayer
+
+ftesting_zcml = os.path.join(
+    os.path.dirname(grokui.introspector.__file__), 'ftesting.zcml')
+
+FunctionalLayer = ZCMLLayer(ftesting_zcml, __name__,
+                            'GrokUIIntrospectorFunctionalLayer')
+
 # This we say: include all testfiles in or below the
 # grok.admin.introspector package in the tests.
 #
 test_suite = register_all_tests('grokui.introspector',
-                                layer=GrokFunctionalLayer)
+                                layer=FunctionalLayer)
