8ba1d2d0cf0e3ad0f102ffeb654bfccae2a7fcc5
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-07-08 13:49:50 +0000

    Update interface of grok introspectors.
    git-svn-id: svn://svn.zope.org/repos/main@88111 62d5b8a3-27da-0310-9561-8e5933582275

--- grokui.introspector/trunk/src/grokui/introspector/interfaces.py
+++ grokui.introspector/trunk/src/grokui/introspector/interfaces.py
@@ -18,6 +18,14 @@ from zope.interface import Interface
 class IGrokIntrospector(Interface):
     """An introspector for scanning the runtime system.
     """
+    def __init__(obj, request):
+        """An introspector must handle requests on arbitrary objects.
+
+        It it the base for exploring the whole introspector facilities
+        provided and should deliver content based on the request.
+
+        How the request data is interpreted, is up to the introspector.
+        """
 
 class IGrokRegistryIntrospector(IGrokIntrospector):
     """An introspector for registries.
