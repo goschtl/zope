7d91ea664dee5482bc3d58921777b26de2aeff5f
paranaliyanage <paranaliyanage@62d5b8a3-27da-0310-9561-8e5933582275>
2008-07-14 15:27:58 +0000

    just a simple modification, to get a feedback from mentor
    git-svn-id: svn://svn.zope.org/repos/main@88357 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/adamg/ocql/branches/optimize-with-index/src/ocql/aoptimizer/aoptimizer.py
+++ Sandbox/adamg/ocql/branches/optimize-with-index/src/ocql/aoptimizer/aoptimizer.py
@@ -5,7 +5,6 @@ at the moment this is just a stub returning it's input
 
 $Id$
 """
-
 from zope.component import adapts
 from zope.interface import implements
 #from zope.security.proxy import removeSecurityProxy
@@ -16,11 +15,18 @@ from ocql.interfaces import IAlgebraOptimizer
 
 from ocql.interfaces import IAlgebraObjectHead
 from ocql.interfaces import IOptimizedAlgebraObject
+from ocql.rewriter.algebra import BaseAlgebra
 
 def addMarkerIF(obj, marker):
     #obj = removeSecurityProxy(obj)
     if not marker.providedBy(obj):
         directlyProvides(obj, directlyProvidedBy(obj), marker)
+
+def visit(algebra):
+    if isinstance(algebra , BaseAlgebra):
+        for child in algebra.children:
+            visit(child)
+    print str(algebra)
 
 class AlgebraOptimizer(object):
     implements(IAlgebraOptimizer)
@@ -32,4 +38,8 @@ class AlgebraOptimizer(object):
 
     def __call__(self):
         addMarkerIF(self.context, IOptimizedAlgebraObject)
+        visit(self.context.tree)
         return self.context
+    
+
+
