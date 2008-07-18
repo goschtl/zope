aaaff0bc296fde1f2cf170a65f04e4ba78f6905f
paranaliyanage <paranaliyanage@62d5b8a3-27da-0310-9561-8e5933582275>
2008-07-18 15:14:18 +0000

    tree pattern searching, target search for Iter, If and Make. Not very generic pattern maching
    git-svn-id: svn://svn.zope.org/repos/main@88524 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/adamg/ocql/branches/optimize-with-index/src/ocql/aoptimizer/aoptimizer.py
+++ Sandbox/adamg/ocql/branches/optimize-with-index/src/ocql/aoptimizer/aoptimizer.py
@@ -5,6 +5,7 @@ at the moment this is just a stub returning it's input
 
 $Id$
 """
+from collections import deque
 from zope.component import adapts
 from zope.interface import implements
 #from zope.security.proxy import removeSecurityProxy
@@ -15,34 +16,59 @@ from ocql.interfaces import IAlgebraOptimizer
 
 from ocql.interfaces import IAlgebraObjectHead
 from ocql.interfaces import IOptimizedAlgebraObject
-from ocql.rewriter.algebra import BaseAlgebra, If, Single, Make, Binary
+from ocql.rewriter.algebra import BaseAlgebra, If, Single, Make, Binary, Iter, Lambda
+
+class FindResults(object):
+    def __init__(self):
+        self.level = 0
+        self.parent = dict()
+
+def find(tree, algebra, startlevel=0):
+    results = FindResults()
+    bfs_list = deque()
+    bfs_list.append(tree.tree)
+    results.parent[algebra] = None
+    results.level = 0
+    
+    while len(bfs_list):
+        v = bfs_list.popleft()
+        if isinstance(v, algebra) and startlevel < results.level:
+            return results
+
+        for child in v.children:
+            if child not in results.parent:
+                results.parent[str(child.__class__)] = v
+                results.level += 1
+                bfs_list.append(child)
+    return None
+
+def findItrPattern(tree, algebra):
+#this has If and Make algebra objects
+    itr_reslts = find(tree, algebra)
+    if itr_reslts is not None:
+        #find for If and Make
+        for child in itr_reslts.parent.values():
+            if isinstance(child, Iter): 
+                r_iter = child
+                break
+
+        boolean_if = boolean_make = False
+        print r_iter
+        for i in r_iter.children:
+            if isinstance(i, Lambda):
+                boolean_lambda = True
+            if isinstance(i, Make):
+                boolean_make = True
+                
+        if boolean_lambda and boolean_make:
+            return r_iter
+
+    return  None
 
 def addMarkerIF(obj, marker):
     #obj = removeSecurityProxy(obj)
     if not marker.providedBy(obj):
         directlyProvides(obj, directlyProvidedBy(obj), marker)
-
-#only for single filter, improve later
-class Finder(object):
-    def __init__(self, metadata):
-        #self.algebra = algebra
-        self.metadata = metadata
-        self.condition = None
-        self.expression = None
-
-    def visit(self, algebra):
-        if isinstance(algebra , BaseAlgebra):
-            for child in algebra.children:
-                if isinstance(child, If):
-                    if isinstance(child.cond, Binary):
-                        self.condition = child.cond.left
-                if isinstance(child, Make):
-                    self.expression = child.expr1
-                self.visit(child)
-
-    def hasIndex(self):
-        if (self.condition and self.expression) is not None:
-            return self.metadata.hasPropertyIndex(self.expression, self.condition)
 
 class AlgebraOptimizer(object):
     implements(IAlgebraOptimizer)
@@ -54,7 +80,7 @@ class AlgebraOptimizer(object):
 
     def __call__(self, metadata):
         addMarkerIF(self.context, IOptimizedAlgebraObject)
-        finder = Finder(metadata)
-        finder.visit(self.context.tree)
-        finder.hasIndex()
+        results = findItrPattern(self.context, Iter)
+        if results is not None:
+            print results
         return self.context
