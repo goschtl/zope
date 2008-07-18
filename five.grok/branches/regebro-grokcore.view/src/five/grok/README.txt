07a125614dde4aaf1cca8fd4422c53676ac8f290
regebro <regebro@62d5b8a3-27da-0310-9561-8e5933582275>
2008-07-18 08:54:34 +0000

    A branch for using grokcore.view.
    git-svn-id: svn://svn.zope.org/repos/main@88476 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ five.grok/branches/regebro-grokcore.view/src/five/grok/README.txt
@@ -0,0 +1,14 @@
+five.grok Package Readme
+=========================
+
+Overview
+--------
+
+
+
+
+Your tests here
+---------------
+
+    >>> 1 + 1
+    3
---
+++ five.grok/branches/regebro-grokcore.view/src/five/grok/__init__.py
@@ -0,0 +1,14 @@
+# Import adapter and utility support from grokcore.component.
+
+from zope.interface import implements
+from zope.component import adapts
+
+from grokcore.component import Adapter, MultiAdapter, GlobalUtility
+from grokcore.component.directive import context, name, provides
+from grokcore.component.decorators import subscribe
+
+from five.grok.components import View, Model, PageTemplate, IGrokLayer, Skin
+from five.grok.directive import require, layer, template, templatedir
+
+# I don't know why this is necessary:
+from five.grok import testing
