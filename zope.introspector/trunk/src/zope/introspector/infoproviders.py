a734fe93f9f3a396420c9f1b66e8aa12a3a08d2b
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-07-21 12:41:43 +0000

    Merge changes from grokui integration branch.
    git-svn-id: svn://svn.zope.org/repos/main@88649 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.introspector/trunk/src/zope/introspector/infoproviders.py
+++ zope.introspector/trunk/src/zope/introspector/infoproviders.py
@@ -23,20 +23,20 @@ from zope.introspector.objectinfo import ObjectInfo, PackageInfo
 class SimpleDescriptionProvider(DescriptionProvider):
     name = 'simple'
     priority(1001)
-    def getDescription(self, obj, *args, **kw):
-        return IObjectInfo(obj)
+    def getDescription(self, obj, dotted_name=None, **kw):
+        return ObjectInfo(obj, dotted_name=dotted_name)
 
-    def canHandle(self, obj, *args, **kw):
+    def canHandle(self, obj, dotted_name=None, **kw):
         return True
 
 
 class PackageDescriptionProvider(DescriptionProvider):
     name = 'package'
     priority(1000)
-    def getDescription(self, obj, *args, **kw):
-        return PackageInfo(obj)
+    def getDescription(self, obj, dotted_name=None, **kw):
+        return PackageInfo(obj, dotted_name)
 
-    def canHandle(self, obj, *args, **kw):
+    def canHandle(self, obj=None, dotted_name=None, **kw):
         if not inspect.ismodule(obj):
             return False
         info = martian.scan.module_info_from_module(obj)
