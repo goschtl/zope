fb99b57e752be752dd8bce19571d7e6b5473849d
fafhrd <fafhrd@62d5b8a3-27da-0310-9561-8e5933582275>
2008-07-21 20:49:52 +0000

    tag for 1.1.0 release
    git-svn-id: svn://svn.zope.org/repos/main@88661 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3ext.layoutform/tags/1.1.0/src/z3ext/layoutform/__init__.py
@@ -0,0 +1,8 @@
+# This file is necessary to make this directory a package.
+
+from z3c.form import button
+
+from field import Fields
+from form import PageletForm
+from add import PageletAddForm
+from edit import PageletEditForm
