16e7844db57aff6d4b0c76a80d97d6a38bb2b67f
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2008-07-22 16:55:47 +0000

    Improve the code browser with support for files.
    git-svn-id: svn://svn.zope.org/repos/main@88714 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.introspector/trunk/src/zope/introspector/code.py
+++ zope.introspector/trunk/src/zope/introspector/code.py
@@ -1,8 +1,11 @@
+import grokcore.component as grok
 from grokcore.component.interfaces import IContext
 import types
 from martian.scan import module_info_from_dotted_name
 from martian.util import isclass
 from zope.interface import implements
+from zope.introspector.interfaces import IInfo
+import os
 
 class Code(object):
     implements(IContext)
@@ -16,13 +19,33 @@ class PackageOrModule(Code):
         self._module_info = module_info_from_dotted_name(dotted_name)
     
 class Package(PackageOrModule):
+    def getPath(self):
+        return os.path.dirname(self._module_info.path)
+    
     def __getitem__(self, name):
         sub_module = self._module_info.getSubModuleInfo(name)
         if sub_module is None:
-            raise KeyError
+            file = File(self.dotted_name, name)
+            # if the file exists, use it, otherwise it's a KeyError - no
+            # file is here
+            if file.exists():
+                return file
+            else:
+                raise KeyError
         if sub_module.isPackage():
             return Package(sub_module.dotted_name)
         return Module(sub_module.dotted_name)
+
+class PackageInfo(grok.Adapter):
+    grok.context(Package)
+    grok.provides(IInfo)
+    grok.name('package')
+
+    def getPackageFiles(self):
+        pkg_file_path = self.context.getPath()
+        return sorted([x for x in os.listdir(pkg_file_path)
+               if os.path.isfile(os.path.join(pkg_file_path, x))
+               and (x.endswith('.txt') or x.endswith('.rst'))])
 
 class Module(PackageOrModule):
     def __getitem__(self, name):
@@ -37,6 +60,18 @@ class Module(PackageOrModule):
             return Function(sub_dotted_name)
         else:
             return Instance(sub_dotted_name)
+
+class File(Code):
+    def __init__(self, dotted_name, name):
+        super(File, self).__init__(dotted_name)
+        self.name = name
+
+    def exists(self):
+        """Check whether the file is a file we want to consider."""
+        module_info = module_info_from_dotted_name(self.dotted_name)
+        path = module_info.getResourcePath(self.name)
+        return (os.path.isfile(path) and
+                (path.endswith('.rst') or path.endswith('.txt')))
 
 class Class(Code):
     pass
