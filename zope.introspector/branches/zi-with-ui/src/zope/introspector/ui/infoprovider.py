95090a757f0cb30503dddaf9013335b3b42434ce
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-07-29 13:11:24 +0000

    Add missing license headers.
    git-svn-id: svn://svn.zope.org/repos/main@88965 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.introspector/branches/zi-with-ui/src/zope/introspector/ui/infoprovider.py
+++ zope.introspector/branches/zi-with-ui/src/zope/introspector/ui/infoprovider.py
@@ -1,3 +1,16 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 from grokcore.view import View
 from grokcore.component import context, name
 from zope.introspector.interfaces import (IObjectInfo, IModuleInfo,
