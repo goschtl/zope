2c3cd419a89df451f67c78f78eab312b87cfc1dd
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-07-29 15:21:23 +0000

    Get rid of zope.introspectorui dependency.
    git-svn-id: svn://svn.zope.org/repos/main@88970 62d5b8a3-27da-0310-9561-8e5933582275

--- grokui.introspector/trunk/setup.py
+++ grokui.introspector/trunk/setup.py
@@ -41,7 +41,6 @@ setup(
     install_requires=['setuptools',
                       'grok',
                       'zope.introspector',
-                      'zope.introspectorui',
                       ],
     tests_require = tests_require,
     extras_require = dict(test=tests_require), 
