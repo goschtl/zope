e2e277723687dc01cd70c8f17f8c847fd3e1eb7e
malthe <malthe@62d5b8a3-27da-0310-9561-8e5933582275>
2008-07-30 15:57:02 +0000

    Bump version.
    git-svn-id: svn://svn.zope.org/repos/main@89025 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.layout/trunk/setup.py
+++ z3c.layout/trunk/setup.py
@@ -19,7 +19,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='z3c.layout',
-      version = '0.1',
+      version = '0.1dev',
       description='HTML layout engine',
       long_description=read('README.txt')+read('src/z3c/layout/README.txt'),
       keywords = "zope3 layout HTML",
