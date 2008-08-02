3f4b378cb3a5ffb02feac7912c71804b2188e764
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-08-02 02:29:42 +0000

    Display some more infos about packages.
    git-svn-id: svn://svn.zope.org/repos/main@89183 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.introspectorui/trunk/src/zope/introspectorui/code.py
+++ zope.introspectorui/trunk/src/zope/introspectorui/code.py
@@ -10,12 +10,44 @@ class Package(grok.View):
 
     def update(self):
         self.files = self.getTextFileUrls()
+        self.zcmlfiles = self.getZCMLFileUrls()
+        self.subpks = self.getSubPackageUrls()
+        self.modules = self.getModuleUrls()
 
-    def getTextFileUrls(self):
-        filenames = self.context.getPackageFiles()
+    def _getFileUrls(self, filenames):
         result = []
         package = self.context.context
         for name in filenames:
-            file = located(package[name], package, name)
-            result.append(dict(name=name, url=self.url(file)))
+            try:
+                file = located(package[name], package, name)
+                result.append(dict(name=name, url=self.url(file)))
+            except:
+                print "PROBLEM: ", name
+        return sorted(result)
+
+    def getTextFileUrls(self):
+        filenames = self.context.getPackageFiles()
+        return self._getFileUrls(filenames)
+
+    def getZCMLFileUrls(self):
+        try:
+            filenames = self.context.getZCMLFiles()
+        except:
+            print "PROBLEM."
+        return self._getFileUrls(filenames)
+
+    def _getItemUrls(self, mod_infos):
+        result = []
+        package = self.context.context
+        for info in mod_infos:
+            mod = located(package[info.name], package, info.name)
+            result.append(dict(name=info.name, url=self.url(mod)))
         return result
+        
+    def getSubPackageUrls(self):
+        mod_infos = self.context.getSubPackages()
+        return sorted(self._getItemUrls(mod_infos))
+
+    def getModuleUrls(self):
+        mod_infos = self.context.getModules()
+        return sorted(self._getItemUrls(mod_infos))
