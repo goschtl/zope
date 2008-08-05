1b0de3566966269ed064f04f19bd07b25a1f2e98
pcardune <pcardune@62d5b8a3-27da-0310-9561-8e5933582275>
2008-08-05 04:42:36 +0000

    add setuptools to the dependencies since this package uses namespace packages.
    git-svn-id: svn://svn.zope.org/repos/main@89357 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.selenium/trunk/setup.py
+++ zc.selenium/trunk/setup.py
@@ -14,6 +14,7 @@ setup(
     license='ZPL 2.1',
     namespace_packages = ['zc'],
     install_requires =[
+        'setuptools',
         'zope.interface',
         'zope.component',
         'zope.publisher',
