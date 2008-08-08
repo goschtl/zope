9e6acda3edfb20eaae53f7b3093a4f52cff7dedf
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-08-08 13:58:36 +0000

    It is enough to apply the skin once. Remove unneeded imports.
    git-svn-id: svn://svn.zope.org/repos/main@89542 62d5b8a3-27da-0310-9561-8e5933582275

--- grokui.introspector/trunk/src/grokui/introspector/namespace.py
+++ grokui.introspector/trunk/src/grokui/introspector/namespace.py
@@ -1,13 +1,11 @@
 import grok
 
 from zope.interface import Interface
-from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from zope.traversing.interfaces import ITraversable
 from zope.publisher.interfaces.browser import IBrowserRequest
 from zope.publisher.browser import applySkin
-from zope.location.location import locate
 
-class IntrospectorLayer(IDefaultBrowserLayer):
+class IntrospectorLayer(grok.IDefaultBrowserLayer):
     """A basic layer for all introspection stuff.
     """
     pass
@@ -26,7 +24,7 @@ class GrokIntrospectorNamespace(grok.MultiAdapter):
     def __init__(self, context, request):
         self.context = context
         self.request = request
+        applySkin(self.request, IntrospectorLayer)
         
     def traverse(self, name, ignore):
-        applySkin(self.request, IntrospectorLayer)
         return self.context
