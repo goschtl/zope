a9cc7f71f297347b44abe23ac4dc9080e52677be
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2008-08-12 11:56:36 +0000

    updated
    git-svn-id: svn://svn.zope.org/repos/main@89715 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.mirrorcheeseshopslashsimple/trunk/setup.py
+++ zc.mirrorcheeseshopslashsimple/trunk/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='zc.mirrorcheeseshopslashsimple',
     version='0.3',
+    description='A script for mirroring the PyPI simple index',
     package_dir = {'': 'src'},
     install_requires = ['zc.lockfile'],
     entry_points = dict(console_scripts=[
