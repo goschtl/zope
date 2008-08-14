77cc084e09dc0aa51e82d139b74e83c874d629ed
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2008-08-14 12:03:56 +0000

    "creating branch ajung-compressed"
    git-svn-id: svn://svn.zope.org/repos/main@89833 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ lovely.memcached/branches/ajung-compressed/src/lovely/memcached/browser/tests.py
@@ -0,0 +1,41 @@
+##############################################################################
+#
+# Copyright (c) 2007 Lovely Systems and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id$
+"""
+__docformat__ = "reStructuredText"
+
+import unittest
+from zope.testing import doctest
+from zope.app.testing import functional
+from z3c.testing import layer
+from z3c.configurator import configurator
+
+def appSetUp(app):
+    configurator.configure(app, {},
+                           names = ['lovely.memcachedclient'])
+
+
+layer.defineLayer('MemcachedLayer', zcml='ftesting.zcml',
+                  appSetUp=appSetUp,
+                  clean=True)
+
+def test_suite():
+    suite = functional.FunctionalDocFileSuite('README.txt')
+    suite.layer = MemcachedLayer
+    suite.level = 2
+    return unittest.TestSuite((suite,))
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
