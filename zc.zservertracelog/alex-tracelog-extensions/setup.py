d74162dda27122006c49562ea1264fa20fd2c5c0
asmith <asmith@62d5b8a3-27da-0310-9561-8e5933582275>
2008-08-20 21:46:20 +0000

    created branch for tracelog extensions.
    git-svn-id: svn://svn.zope.org/repos/main@90044 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zc.zservertracelog/alex-tracelog-extensions/setup.py
@@ -0,0 +1,38 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from setuptools import setup, find_packages
+
+entry_points = """
+[console_scripts]
+tracereport = zc.zservertracelog.tracereport:main
+"""
+
+name = 'zc.zservertracelog'
+setup(
+    name = name,
+    version = '0.3',
+    author = 'Jim Fulton',
+    author_email = 'jim@zope.com',
+    description = 'Zope 3 tracelog implementation for zserver',
+    license = 'ZPL 2.1',
+    keywords = 'zope3',
+
+    packages = find_packages('src'),
+    namespace_packages = ['zc'],
+    package_dir = {'': 'src'},
+    install_requires = 'setuptools',
+    include_package_data = True,
+    zip_safe = False,
+    entry_points=entry_points,
+    )
