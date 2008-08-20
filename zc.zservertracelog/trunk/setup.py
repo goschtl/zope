18fb5525f732f78d9e363447ee70f8c54c5832b6
asmith <asmith@62d5b8a3-27da-0310-9561-8e5933582275>
2008-08-20 21:05:36 +0000

    created repository trunk.
    git-svn-id: svn://svn.zope.org/repos/main@90037 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zc.zservertracelog/trunk/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+entry_points = """
+[console_scripts]
+tracereport = zc.zservertracelog.tracereport:main
+"""
+
+name = 'zc.zservertracelog'
+setup(
+    name = name,
+    version = '0.2',
+    author = 'Jim Fulton',
+    author_email = 'jim@zope.com',
+    description = 'Zope 3 tracelog implementation for zserver',
+    license = 'ZPL 2.1',
+    keywords = 'zope3',
+    
+    packages = find_packages('src'),
+    namespace_packages = ['zc'],
+    package_dir = {'': 'src'},
+    install_requires = 'setuptools',
+    include_package_data = True,
+    zip_safe = False,
+    entry_points=entry_points,
+    )
