dece41fc929aefee6bdc3e126fe9d461036d14d4
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2008-08-25 16:34:09 +0000

    "creating branch local-mirror"
    git-svn-id: svn://svn.zope.org/repos/main@90228 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zc.mirrorcheeseshopslashsimple/branches/local-mirror/src/zc/tests.py
@@ -0,0 +1,27 @@
+# *-* coding: iso-8859-15 *-*
+
+"""
+Tests
+"""
+
+import unittest
+import util
+
+
+class UtilityTests(unittest.TestCase):
+
+    def testIsAscii(self):
+
+        self.assertEqual(util.isASCII('foo'), True)
+        self.assertEqual(util.isASCII(u'foo'), True)
+        self.assertEqual(util.isASCII('üצה'), False)
+        self.assertEqual(util.isASCII(u'üצה'), False)
+        self.assertRaises(TypeError, util.isASCII, 2)
+
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(UtilityTests))
+    return suite
+
