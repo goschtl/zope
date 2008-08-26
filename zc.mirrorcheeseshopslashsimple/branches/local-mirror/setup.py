50f7c62a395985b93a5277067e0c05d82dc12db1
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2008-08-26 09:32:24 +0000

    more metadata
    git-svn-id: svn://svn.zope.org/repos/main@90268 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.mirrorcheeseshopslashsimple/branches/local-mirror/setup.py
+++ zc.mirrorcheeseshopslashsimple/branches/local-mirror/setup.py
@@ -1,9 +1,26 @@
 from setuptools import setup
 
+CLASSIFIERS = [
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Zope Public License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+]
+
+long_desc = open('README.txt').read()
+
 setup(
     name='zc.mirrorcheeseshopslashsimple',
-    version='0.3',
+    version='0.4',
     description='A script for mirroring the PyPI simple index',
+    license='ZPL (see LICENSE.txt)',
+    author='Jim Fulton',
+    author_email='jim@zope.com',
+    maintainer='Jim Fulton',
+    maintainer_email='jim@zope.com',
+    classifiers=CLASSIFIERS,
+    long_description=long_desc,
     package_dir = {'': 'src'},
     install_requires = ['zc.lockfile'],
     entry_points = dict(console_scripts=[
