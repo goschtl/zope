87e6a2ef399c1ba84e592927eac8fd901c01d46c
asmith <asmith@62d5b8a3-27da-0310-9561-8e5933582275>
2008-08-27 22:37:00 +0000

    cleaned up dependencies.
    git-svn-id: svn://svn.zope.org/repos/main@90476 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.zservertracelog/branches/alex-tests/setup.py
+++ zc.zservertracelog/branches/alex-tests/setup.py
@@ -27,20 +27,19 @@ setup(
     description = 'Zope 3 tracelog implementation for zserver',
     license = 'ZPL 2.1',
     keywords = 'zope3',
-
     packages = find_packages('src'),
     namespace_packages = ['zc'],
     package_dir = {'': 'src'},
     install_requires = [
-        'setuptools',
-        'zope.app.testing',
-        'zope.testbrowser',
-        'zope.testing',
-        ],
-    extras_require=dict(
-        test=[
+            'setuptools',
+            'zope.app.appsetup',
             'zope.app.server',
-            'zope.app.securitypolicy',
+            'zope.app.wsgi',
+            'zope.server',
+            ],
+    extras_require = dict(
+        test = [
+            'zope.testing',
             ]),
     include_package_data = True,
     zip_safe = False,
