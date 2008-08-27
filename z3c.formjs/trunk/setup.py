795ccf74b11906b58bd163250c4147c2b5f709ce
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2008-08-27 04:51:17 +0000

    Get ready for release.
    git-svn-id: svn://svn.zope.org/repos/main@90386 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.formjs/trunk/setup.py
+++ z3c.formjs/trunk/setup.py
@@ -32,7 +32,7 @@ chapters = '\n'.join(
 
 setup (
     name='z3c.formjs',
-    version='0.3.1dev',
+    version='0.4.0',
     author = "Paul Carduner and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "Javascript integration into ``z3c.form``",
