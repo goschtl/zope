62e7302f081c1aef423ac6576f14b046db228301
fafhrd <fafhrd@62d5b8a3-27da-0310-9561-8e5933582275>
2008-09-04 10:57:08 +0000

    tag for release
    git-svn-id: svn://svn.zope.org/repos/main@90798 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3ext.product/tags/1.2.0/src/z3ext/product/interfaces.py
@@ -0,0 +1,124 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" z3ext.product interfaces
+
+$Id: interfaces.py 1843 2008-03-25 18:39:00Z fafhrd91 $
+"""
+from zope import schema, interface
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory('z3ext')
+
+
+class ProductError(Exception):
+    """ base error class for product management """
+
+
+class ProductNotInstalledError(ProductError):
+    """ """
+
+
+class ProductAlreadyInstalledError(ProductError):
+    """ """
+
+
+class InvalidProduct(ProductError):
+    """ """
+
+
+class ProductWarningError(ProductError):
+    """ dependencies error """
+
+
+class RequiredProductNotFound(ProductError):
+    """ """
+
+
+class IProduct(interface.Interface):
+    """ product information """
+
+    __product_name__ = schema.TextLine(
+        title = u'Product name',
+        required = True)
+
+    __require__ = interface.Attribute(u'Require products.')
+
+    __installed__ = interface.Attribute(u'Is product installed.')
+
+    def install():
+        """ install and configure product """
+
+    def uninstall():
+        """ uninstall product """
+
+    def update():
+        """ update product """
+
+    def isInstalled():
+        """ is product installed """
+
+    def listExtensions():
+        """ list IProductExtension for this product """
+
+    def isUninstallable():
+        """ is product uninstallable """
+        
+
+class IProductExtension(interface.Interface):
+    """ product extension """
+
+
+class IProductInstaller(interface.Interface):
+    """ installer for external products """
+
+
+class IAbstractProductEvent(interface.Interface):
+    """ base event interface """
+
+    id = schema.TextLine(
+        title = u'Product id',
+        required = True)
+
+    product = interface.Attribute('IProduct object')
+
+
+class IProductInstalledEvent(IAbstractProductEvent):
+    """ new product installed """
+
+
+class IProductUninstalledEvent(IAbstractProductEvent):
+    """ product uninstalled """
+
+
+class IProductUpdatedEvent(IAbstractProductEvent):
+    """ product updated """
+
+
+class AbstractProductEvent(object):
+
+    def __init__(self, id, product):
+        self.id = id
+        self.product = product
+
+
+class ProductInstalledEvent(AbstractProductEvent):
+    interface.implements(IProductInstalledEvent)
+
+
+class ProductUninstalledEvent(AbstractProductEvent):
+    interface.implements(IProductUninstalledEvent)
+
+
+class ProductUpdatedEvent(AbstractProductEvent):
+    interface.implements(IProductUpdatedEvent)
---
+++ z3ext.product/tags/1.2.0/src/z3ext/product/zcml.py
@@ -0,0 +1,105 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" z3ext:product directive
+
+$Id: interfaces.py 802 2007-12-28 11:25:55Z fafhrd91 $
+"""
+from zope import schema
+from zope.configuration import fields
+from zope.component.zcml import utility
+from zope.component import globalSiteManager
+from zope.component.interfaces import IComponents
+from zope.security.checker import CheckerPublic
+
+import z3ext.product
+from z3ext.controlpanel.zcml import IConfigletDirective, ConfigletDirective
+
+from interfaces import IProduct
+from registry import ProductRegistry
+from product import Product, ProductExtension
+
+
+class IProductDirective(IConfigletDirective):
+    
+    configurable = schema.Bool(
+        title = u'Configurable',
+        default = False,
+        required = False)
+
+    require = fields.Tokens(
+        title = u'Require',
+        value_type = schema.TextLine(),
+        required = False)
+
+
+class ProductDirective(ConfigletDirective):
+
+    def __init__(self, _context, name, schema, title,
+                 description='', class_=None, provides=(),
+                 permission='z3ext.ManageProducts', tests=(),
+                 configurable=False, require = ()):
+
+        if '.' not in name:
+            product_class = Product
+        else:
+            product_class = ProductExtension
+
+        if class_ is None:
+            class_ = product_class
+        else:
+            class_ = (class_, product_class)
+
+        if configurable:
+            test = ProductTest()
+            tests = (test,) + tuple(tests)
+        else:
+            tests = (NotConfigurable,)
+
+        # create component registry
+        registry = ProductRegistry(name, title)
+        z3ext.product.registries[name] = registry
+        setattr(z3ext.product, name, registry)
+        utility(_context, IComponents, registry, name=name)
+
+        # register configlet
+        productName = name
+        name = 'product.' + name
+
+        super(ProductDirective, self).__init__(
+            _context, name, schema, title,
+            description, class_, provides, permission, tests)
+
+        self._class.__require__ = require
+        self._class.__product_name__ = productName
+
+        if configurable:
+            test.product = self._configlet
+
+        utility(_context, IProduct, self._configlet, name=productName)
+
+        self.require(_context, permission, interface=(IProduct,))
+        self.require(_context, CheckerPublic,
+                     attributes=('isInstalled', '__installed__'))
+
+
+class ProductTest(object):
+
+    def __init__(self, product=None):
+        self.product = product
+
+    def __call__(self, *args, **kw):
+        return self.product.isInstalled()
+
+def NotConfigurable(configlet):
+    return False
