04dea071a62854221c97c53ffee1f27308fabe80
mgedmin <mgedmin@62d5b8a3-27da-0310-9561-8e5933582275>
2008-09-04 20:00:05 +0000

    Prepare to release z3c.rest 0.2.4.
    git-svn-id: svn://svn.zope.org/repos/main@90828 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.rest/trunk/setup.py
+++ z3c.rest/trunk/setup.py
@@ -31,7 +31,7 @@ chapters = '\n'.join(
 
 setup (
     name='z3c.rest',
-    version='0.2.4dev',
+    version='0.2.4',
     author = "Stephan Richter and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "A REST Framework for Zope 3 Applications",
