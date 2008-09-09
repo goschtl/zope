3c97f035ac02203f0437d35c14a018eedecbaba6
nadako <nadako@62d5b8a3-27da-0310-9561-8e5933582275>
2008-09-09 09:39:10 +0000

    Add standard header.
    git-svn-id: svn://svn.zope.org/repos/main@90990 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.blobfile/trunk/src/z3c/blobfile/browser/image.py
+++ z3c.blobfile/trunk/src/z3c/blobfile/browser/image.py
@@ -1,3 +1,22 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Image view.
+
+$Id$
+"""
+__docformat__ = 'restructuredtext'
+
 from z3c.blobfile.browser.file import FileView
 from zope.app.file.browser.image import ImageData
 
