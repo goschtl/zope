b4821b4f060ec0a6eaf1adc57686b1332ad55076
jukart <jukart@62d5b8a3-27da-0310-9561-8e5933582275>
2008-09-11 14:15:08 +0000

    next version
    git-svn-id: svn://svn.zope.org/repos/main@91061 62d5b8a3-27da-0310-9561-8e5933582275

--- lovely.responsecache/trunk/setup.py
+++ lovely.responsecache/trunk/setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'lovely.responsecache',
-    version = '1.2.0',
+    version = '1.3.0',
     author = "Lovely Systems",
     author_email = "office@lovelysystems.com",
     description = "Cache results of ContentProviders",
