1ad048cd114b04b739b20ae11c70d9aaa7f989a3
wichert <wichert@62d5b8a3-27da-0310-9561-8e5933582275>
2008-09-26 13:43:57 +0000

    Various fixes
    git-svn-id: svn://svn.zope.org/repos/main@91521 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/malthe/chameleon.genshi/src/chameleon/genshi/loader.py
+++ Sandbox/malthe/chameleon.genshi/src/chameleon/genshi/loader.py
@@ -1,12 +1,12 @@
 from chameleon.core.loader import TemplateLoader as BaseLoader
-from chameleon.core.genshi import language
-from chameleon.core.genshi import template
+from chameleon.genshi import language
+from chameleon.genshi import template
 
 
 class TemplateLoader(BaseLoader):
     default_parser = language.Parser()
 
     def load(self, filename):
-        return super(BaseLoader, self).load(filename,
-                klass=template.GenshiTemplateFile)
+        return super(TemplateLoader, self).load(filename,
+                template.GenshiTemplateFile)
 
