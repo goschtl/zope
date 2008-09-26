50f833361044e8c428138548e1b0d3df26a14631
wichert <wichert@62d5b8a3-27da-0310-9561-8e5933582275>
2008-09-26 13:43:52 +0000

    Various fixes
    git-svn-id: svn://svn.zope.org/repos/main@91520 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/malthe/chameleon.zpt/src/chameleon/zpt/loader.py
+++ Sandbox/malthe/chameleon.zpt/src/chameleon/zpt/loader.py
@@ -1,12 +1,13 @@
 from chameleon.core.loader import TemplateLoader as BaseLoader
-from chameleon.core.zpt import language
-from chameleon.core.zpt import template
+from chameleon.zpt import language
+from chameleon.zpt import template
+
 
 class TemplateLoader(BaseLoader):
     default_parser = language.Parser()
 
     def load(self, filename):
-        return super(BaseLoader, self).load(filename,
-                klass=template.PageTemplateFile)
+        return super(TemplateLoader, self).load(filename,
+                template.PageTemplateFile)
 
 
