8e78fea1c75ada290e07f9c219f52902b3a51cb6
mgedmin <mgedmin@62d5b8a3-27da-0310-9561-8e5933582275>
2008-09-29 22:16:36 +0000

    Remove what seems to be a debugging print to stderr.
    git-svn-id: svn://svn.zope.org/repos/main@91634 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.selenium/trunk/src/zc/selenium/results.py
+++ zc.selenium/trunk/src/zc/selenium/results.py
@@ -27,7 +27,6 @@ class Results(zc.selenium.resource.ResourceBase):
     template = zope.app.pagetemplate.ViewPageTemplateFile('results.pt')
 
     def POST(self):
-        print >>sys.__stderr__, "in results resource"
         # get the queue used to communicate with the test thread, this will
         # fail horribly if not running in "Selenium test" mode
         messages = sys.modules['__main__'].messages
