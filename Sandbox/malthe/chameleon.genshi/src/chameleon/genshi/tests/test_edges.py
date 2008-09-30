5ddc224513c80a44db87bdd37c9fa0c0071e53b4
chrism <chrism@62d5b8a3-27da-0310-9561-8e5933582275>
2008-09-30 01:03:14 +0000

    Wiggy this test doesnt actually fail.
    git-svn-id: svn://svn.zope.org/repos/main@91636 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/malthe/chameleon.genshi/src/chameleon/genshi/tests/test_edges.py
+++ Sandbox/malthe/chameleon.genshi/src/chameleon/genshi/tests/test_edges.py
@@ -14,16 +14,21 @@ class UnicodeTortureTests(unittest.TestCase):
         <div id="${foo}" py:attrs="dict(label=foo)"/>
         </html>
         """
-        expected = """\
+        expected = """
         <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-        <html>
+        <html xmlns="http://www.w3.org/1999/xhtml">
         <title>\xc2\xa9</title>
         <div label="\xc2\xa9" id="\xc2\xa9"/>
         </html>"""
-
         c = unicode('\xc2\xa9', 'utf-8')
-        self.assertEqual(render_template(body, foo=c), expected)
+        result = render_template(body, foo=c).encode('utf-8')
+        self.assertEqual(norm(result), norm(expected))
+
+def norm(s):
+    s = s.replace(' ', '')
+    s = s.replace('\n', '')
+    return s
 
 def test_suite():
     return unittest.findTestCases(sys.modules[__name__])
