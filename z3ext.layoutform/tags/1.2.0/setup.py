5ef10d19e4f913fa97fb4f371169ab3d46856fa1
fafhrd <fafhrd@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-05 19:38:16 +0000

    release tag
    git-svn-id: svn://svn.zope.org/repos/main@91786 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3ext.layoutform/tags/1.2.0/setup.py
@@ -0,0 +1,68 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for z3ext.layoutform package
+
+$Id$
+"""
+import sys, os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version='1.2.0'
+
+
+setup(name='z3ext.layoutform',
+      version=version,
+      description="UI Skin for z3c.form based on z3ext.layout",
+      long_description=(
+          'Detailed Documentation\n' +
+          '======================\n'
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+      author='Nikolay Kim',
+      author_email='fafhrd91@gmail.com',
+      url='http://z3ext.net/',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'':'src'},
+      namespace_packages=['z3ext'],
+      install_requires = ['setuptools',
+			  'zope.publisher',
+			  'zope.component',
+			  'zope.pagetemplate',
+			  'zope.app.pagetemplate',
+			  'zope.i18nmessageid',
+			  'z3c.autoinclude',
+
+                          'z3ext.layout>=1.5.0',
+                          'z3ext.resource>=1.2.0',
+			  'z3ext.resourcepackage>=1.2.0',
+			  'z3ext.statusmessage',
+                          ],
+      include_package_data = True,
+      zip_safe = False
+      )
