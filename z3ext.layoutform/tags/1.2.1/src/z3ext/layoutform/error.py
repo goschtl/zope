9521a718f5e1cca7c2519a4464d4cc3492128ff2
fafhrd <fafhrd@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-07 19:46:59 +0000

    release tag
    git-svn-id: svn://svn.zope.org/repos/main@91881 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3ext.layoutform/tags/1.2.1/src/z3ext/layoutform/error.py
@@ -0,0 +1,29 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" 
+
+$Id:  2007-12-12 12:27:02Z fafhrd $
+"""
+from zope import interface
+from z3ext.statusmessage.message import Message
+
+from interfaces import IFormErrorStatusMessage
+
+
+class FormErrorStatusMessage(Message):
+    interface.implements(IFormErrorStatusMessage)
+
+    def __init__(self, message):
+        self.message = message[0]
+        self.errors = [err for err in message[1:] if err.widget is None]
