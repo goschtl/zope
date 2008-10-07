212c953a69653d2b45b2e0bed8ace4496fdf5098
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-07 15:42:32 +0000

    Post-release.
    git-svn-id: svn://svn.zope.org/repos/main@91863 62d5b8a3-27da-0310-9561-8e5933582275

--- hurry.resource/trunk/setup.py
+++ hurry.resource/trunk/setup.py
@@ -15,7 +15,7 @@ long_description = (
 
 setup(
     name='hurry.resource',
-    version='0.1dev',
+    version='0.2dev',
     description="Flexible resources for web applications.",
     long_description=long_description,
     classifiers=[],
