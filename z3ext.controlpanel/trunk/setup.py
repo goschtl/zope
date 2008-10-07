56500a87e363c3ef32ec378abf2b4d52c2ba36b6
fafhrd <fafhrd@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-07 13:44:56 +0000

    vb
    git-svn-id: svn://svn.zope.org/repos/main@91848 62d5b8a3-27da-0310-9561-8e5933582275

--- z3ext.controlpanel/trunk/setup.py
+++ z3ext.controlpanel/trunk/setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version='1.3.0dev'
+version='1.3.1dev'
 
 
 setup(name = 'z3ext.controlpanel',
