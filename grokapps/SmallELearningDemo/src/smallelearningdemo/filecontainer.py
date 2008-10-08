08ad74670edfaecc5f55b6f3897368e9fd26d214
yusei <yusei@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-08 00:48:21 +0000

    Add example code of zope.file.
    git-svn-id: svn://svn.zope.org/repos/main@91888 62d5b8a3-27da-0310-9561-8e5933582275

--- grokapps/SmallELearningDemo/src/smallelearningdemo/filecontainer.py
+++ grokapps/SmallELearningDemo/src/smallelearningdemo/filecontainer.py
@@ -1,5 +1,8 @@
 import grok
-import zope.app.file
+import zope.schema
+import zope.file.file
+import zope.file.upload
+import zope.file.download
 
 
 class FileContainer(grok.Container):
@@ -7,25 +10,60 @@ class FileContainer(grok.Container):
   pass
 
 
+#### Example of using zope.app.file
+## import zope.app.file
+##
+## class Add(grok.AddForm):
+##   """An addview for zope.app.file"""
+##   grok.context(FileContainer)
+##
+##   form_fields = grok.AutoFields(zope.app.file.interfaces.IFile).select('data')
+##
+##   @grok.action('Add file')
+##   def add(self, **data):
+##     self.upload(**data)
+##     self.redirect(self.url(self.context.__parent__))
+##
+##   def upload(self, **data):
+##     fileupload = self.request['form.data']
+##     if fileupload and fileupload.filename:
+##       contenttype = fileupload.headers.get('Content-Type')
+##       file_ = zope.app.file.File(data['data'], contenttype)
+##       self.context[fileupload.filename] = file_
+
+
 class Add(grok.AddForm):
+  """An addview for zope.file"""
 
   grok.context(FileContainer)
 
-  form_fields = grok.AutoFields(zope.app.file.interfaces.IFile).select('data')
+  form_fields = grok.Fields(
+    zope.schema.Bytes(__name__='data',
+                      title=u'Upload data',
+                      description=u'Upload file',),
+    )
 
   @grok.action('Add file')
-  def add(self, **data):
+  def addFile(self, data):
     self.upload(**data)
     self.redirect(self.url(self.context.__parent__))
 
-  def upload(self, **data):
+  def upload(self, data):
     fileupload = self.request['form.data']
-    if fileupload and fileupload.filename:
-      contenttype = fileupload.headers.get('Content-Type')
-      file_ = zope.app.file.File(data['data'], contenttype)
-      self.context[fileupload.filename] = file_
+    file_ = zope.file.file.File()
+    zope.file.upload.updateBlob(file_, fileupload)
+    self.context[fileupload.filename] = file_
 
 
 class Index(grok.View):
 
   pass
+
+
+class FileIndex(zope.file.download.Display, grok.View):
+  """Default view for zope.file.file.File"""
+  grok.name('index.html')
+  grok.context(zope.file.file.File)
+
+  def render(self):
+    return self()
