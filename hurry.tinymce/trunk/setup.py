5443266ea4092b6cba5e54110209be6c52d7a70b
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-09 16:21:01 +0000

    Initial import.
    git-svn-id: svn://svn.zope.org/repos/main@91919 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ hurry.tinymce/trunk/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+TINYMCE_VERSION = '2.6.0'
+
+setup(
+    name='hurry.tinymce',
+    version=TINYMCE_VERSION + 'dev',
+    description="tinymce for hurry.resource.",
+    classifiers=[],
+    keywords='',
+    author='Martijn Faassen',
+    author_email='faassen@startifact.com',
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'hurry.resource',
+        ],
+    entry_points= {
+    'console_scripts': [
+      'tinymceprepare = hurry.tinymce.prepare:main',
+      ]
+    },
+
+    )
