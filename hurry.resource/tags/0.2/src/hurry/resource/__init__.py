2f8ea98e4770aeceefd14a57347c7ebf379ab39b
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-13 19:49:14 +0000

    A bit too fast.
    git-svn-id: svn://svn.zope.org/repos/main@92157 62d5b8a3-27da-0310-9561-8e5933582275

--- hurry.resource/tags/0.2/src/hurry/resource/__init__.py
+++
@@ -1,5 +0,0 @@
-from hurry.resource.core import (Library, ResourceInclusion, NeededInclusions,
-                                 mode, bottom,
-                                 sort_inclusions_topological,
-                                 sort_inclusions_by_extension,
-                                 generate_code)
