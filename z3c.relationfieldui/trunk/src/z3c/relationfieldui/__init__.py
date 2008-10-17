a1ddaa4c1711e22c4f2d628366763691e2bc8041
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-17 15:58:34 +0000

    Cut out everything but the widget stuff.
    git-svn-id: svn://svn.zope.org/repos/main@92315 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.relationfieldui/trunk/src/z3c/relationfieldui/__init__.py
+++ z3c.relationfieldui/trunk/src/z3c/relationfieldui/__init__.py
@@ -1,5 +1,1 @@
-from z3c.relationfield.relation import RelationValue, TemporaryRelationValue
-from z3c.relationfield.index import RelationCatalog
-from z3c.relationfield.schema import Relation
-from z3c.relationfield.event import realize_relations
-from z3c.relationfield.widget import RelationWidget, RelationDisplayWidget
+from z3c.relationfieldui.widget import RelationWidget, RelationDisplayWidget
