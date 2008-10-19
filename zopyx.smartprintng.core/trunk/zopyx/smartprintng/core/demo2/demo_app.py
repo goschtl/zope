cd3eee437266d549942df21a54c67668b429f8fb
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-19 09:10:21 +0000

    fixes
    git-svn-id: svn://svn.zope.org/repos/main@92373 62d5b8a3-27da-0310-9561-8e5933582275

--- zopyx.smartprintng.core/trunk/zopyx/smartprintng/core/demo2/demo_app.py
+++ zopyx.smartprintng.core/trunk/zopyx/smartprintng/core/demo2/demo_app.py
@@ -30,7 +30,7 @@ def main():
 
     from zopyx.convert2.registry import availableConverters
 
-    for fullname in ('Andreas Jung', 'Heinz Becker', ' Hilde Becker'):
+    for fullname in ('Andreas Jung', 'Heinz Becker', 'Hilde Becker'):
 
         context = TestContent()
         result = convert(context=context,
@@ -39,6 +39,7 @@ def main():
                          resource_name='demo',
                          converter='pdf-prince',
                          template_options=dict(fullname=fullname),
+                         destination_filename='%s.pdf' % fullname,
                         )
                         
         print 'Generated:', os.path.abspath(result)
