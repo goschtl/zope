31b153093f1602a30083abb2e371ff2a8ac27fe0
kevingill2 <kevingill2@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-20 08:35:35 +0000

    New skin to provide zope.app.boston functionality with z3c.pagelet and z3c.forms.
    git-svn-id: svn://svn.zope.org/repos/main@92389 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.boston/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.1dev'
+
+setup(name='z3c.boston',
+      version=version,
+      description="A version of the zope.app.boston skin which support pagelets.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='zope3 pagelet form skin',
+      author='Kevin Gill and the Zope Community',
+      author_email='zope-dev@zope.org',
+      url='http://pypi.python.org/pypi/z3c.boston',
+      license='ZPL 2.1',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['z3c'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'zope.app.boston',
+          'z3c.pagelet',
+          'z3c.formui',
+          'z3c.layer',
+          'jquery.layer',
+          'z3c.template',
+      ],
+      extras_require = dict(
+          test = [
+          ]
+      ),
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
