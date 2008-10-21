c1312bf6736781a1400072416ebdb618ac9bd3df
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-21 10:40:38 +0000

    Add locatedproperty decorator. This may not be the perfect way to spell what's needed to expose QueryContainer objects but it does work well enough.
    git-svn-id: svn://svn.zope.org/repos/main@92418 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.rdb/trunk/src/megrok/rdb/__init__.py
+++ megrok.rdb/trunk/src/megrok/rdb/__init__.py
@@ -3,6 +3,7 @@ from megrok.rdb.schema import Fields
 from megrok.rdb.directive import key, metadata, tablename, reflected
 from megrok.rdb.setup import setupDatabase
 from megrok.rdb.interfaces import IDatabaseSetupEvent
+from megrok.rdb.prop import locatedproperty
 
 from sqlalchemy import MetaData
 
