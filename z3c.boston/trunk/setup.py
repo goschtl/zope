145c537278821553517571672ddd58a26bb690a2
kevingill2 <kevingill2@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-21 20:40:02 +0000

    bumped the version to 1.0.2 to get Cheeseshop to use an egg without the subversion suffix.
    git-svn-id: svn://svn.zope.org/repos/main@92456 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.boston/trunk/setup.py
+++ z3c.boston/trunk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.1'
+version = '1.0.2'
 
 setup(name='z3c.boston',
       version=version,
@@ -9,10 +9,16 @@ setup(name='z3c.boston',
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
       keywords='zope3 pagelet form skin',
       author='Kevin Gill and the Zope Community',
       author_email='zope-dev@zope.org',
