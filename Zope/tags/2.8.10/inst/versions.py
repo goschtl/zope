4ed3b5df5c2bef14e8d879513a0bd18af9dca471
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-25 09:28:45 +0000

    removed
    git-svn-id: svn://svn.zope.org/repos/main@92547 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope/tags/2.8.10/inst/versions.py
+++
@@ -1,7 +0,0 @@
-ZOPE_MAJOR_VERSION  = '2.8'
-ZOPE_MINOR_VERSION  = '10'
-ZOPE_BRANCH_NAME    = '$Name$'[6:] or 'no-branch'
-
-# always start prerelease branches with '0' to avoid upgrade
-# issues in RPMs
-VERSION_RELEASE_TAG = 'final'
