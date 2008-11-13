f2bb6b583d21ca720273688be37f59e9b0f0cd64
wichert <wichert@62d5b8a3-27da-0310-9561-8e5933582275>
2008-11-13 18:52:23 +0000

    Expose text templates through the template loader
    git-svn-id: svn://svn.zope.org/repos/main@92913 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/malthe/chameleon.genshi/src/chameleon/genshi/loader.py
+++ Sandbox/malthe/chameleon.genshi/src/chameleon/genshi/loader.py
@@ -6,7 +6,13 @@ from chameleon.genshi import template
 class TemplateLoader(BaseLoader):
     default_parser = language.Parser()
 
-    def load(self, filename):
-        return super(TemplateLoader, self).load(filename,
-                template.GenshiTemplateFile)
+    formats = { "xml"  : template.GenshiTemplateFile,
+                "text" : template.GenshiTextTemplateFile,
+               }
+
+    def load(self, filename, format="xml"):
+        """Load and return a template file. The format parameter determines
+        will parse the file. Valid options are `xml` and `text`."""
+        return super(TemplateLoader, self).load(filename, self.formats[format])
+
 
