d908edd575fc387fe5f4f7b5ee8b24b4fd737012
jfroche <jfroche@62d5b8a3-27da-0310-9561-8e5933582275>
2008-11-17 23:14:33 +0000

    flash/amf in zope2. Initial import
    git-svn-id: svn://svn.zope.org/repos/main@93063 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.amf/trunk/z3c/amf/interfaces.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+z3c.amf
+
+Licensed under the <+ LICENSE +> license, see LICENCE.txt for more details.
+Copyright by Affinitic sprl
+
+$Id$
+"""
+from zope.interface import Interface
+
+
+class IAMFResponse(Interface):
+    """
+    Marker interface
+    """
+
+
+class IAMFRequest(Interface):
+    """
+    Marker interface
+    """
