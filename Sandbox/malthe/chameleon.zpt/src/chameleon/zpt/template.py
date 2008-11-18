9c3e651d6d220de2ac4025b68919561235761c69
malthe <malthe@62d5b8a3-27da-0310-9561-8e5933582275>
2008-11-18 10:32:23 +0000

    Removed dubious language negotiation support.
    git-svn-id: svn://svn.zope.org/repos/main@93073 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/malthe/chameleon.zpt/src/chameleon/zpt/template.py
+++ Sandbox/malthe/chameleon.zpt/src/chameleon/zpt/template.py
@@ -1,24 +1,6 @@
-from zope import i18n
-
 from chameleon.core import template
-from chameleon.core import config
 
 import language
-
-def prepare_language_support(**kwargs):
-    target_language = kwargs.get('target_language')
-
-    if config.DISABLE_I18N:
-        if target_language:
-            del kwargs['target_language']
-        return
-    
-    if not target_language:
-        context = kwargs.get(config.SYMBOLS.i18n_context)
-        target_language = i18n.negotiate(context)
-
-        if target_language:
-            kwargs['target_language'] = target_language    
 
 class PageTemplate(template.Template):
     __doc__ = template.Template.__doc__ # for Sphinx autodoc
@@ -29,10 +11,6 @@ class PageTemplate(template.Template):
         if parser is None:
             parser = self.default_parser
         super(PageTemplate, self).__init__(body, parser, format, doctype)
-
-    def render(self, **kwargs):
-        prepare_language_support(**kwargs)
-        return super(PageTemplate, self).render(**kwargs)
 
 class PageTemplateFile(template.TemplateFile):
     __doc__ = template.TemplateFile.__doc__ # for Sphinx autodoc
@@ -45,7 +23,3 @@ class PageTemplateFile(template.TemplateFile):
             parser = self.default_parser
         super(PageTemplateFile, self).__init__(filename, parser, format,
                                                doctype, **kwargs)
-
-    def render(self, **kwargs):
-        prepare_language_support(**kwargs)
-        return super(PageTemplateFile, self).render(**kwargs)
