e90550132223ec2646bd4afb810783c519526d2f
malthe <malthe@62d5b8a3-27da-0310-9561-8e5933582275>
2008-11-19 13:25:47 +0000

    Preparing release.
    git-svn-id: svn://svn.zope.org/repos/main@93137 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/malthe/chameleon.html/setup.py
+++ Sandbox/malthe/chameleon.html/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 import sys
 
-version = '0.5'
+version = '0.6'
 
 install_requires = [
     'setuptools',
     'zope.interface',
     'zope.component',
     'zope.i18n >= 3.5',
-    'chameleon.core',
+    'chameleon.core >= 1.0b9',
     'cssutils >= 0.9.5.1',
     'lxml >= 2.1.1',
     ]
