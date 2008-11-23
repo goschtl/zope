0d19726472af718af0f17feae72d169ac187ba0f
fafhrd <fafhrd@62d5b8a3-27da-0310-9561-8e5933582275>
2008-11-23 17:07:15 +0000

    release tag
    git-svn-id: svn://svn.zope.org/repos/main@93280 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3ext.cacheheaders/tags/1.0.2/setup.py
@@ -0,0 +1,74 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for z3ext.cacheheaders package
+
+$Id$
+"""
+import sys, os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.0.2'
+
+setup(name='z3ext.cacheheaders',
+      version=version,
+      description="Cache headers manager",
+      long_description=(
+          'Detailed Dcoumentation\n' +
+          '======================\n'
+          + '\n\n' +
+          read('src', 'z3ext', 'cacheheaders', 'README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+      author='Nikolay Kim',
+      author_email='fafhrd91@gmail.com',
+      url='http://z3ext.net/',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'':'src'},
+      namespace_packages=['z3ext'],
+      install_requires = ['setuptools',
+			  'zope.proxy',
+                          'zope.event',
+                          'zope.schema',
+			  'zope.datetime',
+                          'zope.component',
+                          'zope.interface',
+			  'zope.publisher',
+                          'zope.dublincore',
+                          'zope.cachedescriptors',
+                          'zope.app.publication',
+                          ],
+      extras_require = dict(test=['zope.app.testing',
+                                  'zope.testing',
+                                  'zope.traversing',
+                                  'zope.security',
+                                  'zope.app.security',
+                                  ]),
+      include_package_data = True,
+      zip_safe = False
+      )
