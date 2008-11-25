9eb9f3646816825d498f82b430701a65daf1ac95
fafhrd <fafhrd@62d5b8a3-27da-0310-9561-8e5933582275>
2008-11-25 07:07:13 +0000

    release tag
    git-svn-id: svn://svn.zope.org/repos/main@93335 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3ext.resource/tags/1.2.1/setup.py
@@ -0,0 +1,75 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for z3ext.resource package
+
+$Id$
+"""
+import sys, os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.2.1'
+
+
+setup(name='z3ext.resource',
+      version=version,
+      description="Extended resource",
+      long_description=(
+          'Detailed Dcoumentation\n' +
+          '======================\n'
+          + '\n\n' +
+          read('src', 'z3ext', 'resource', 'README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+      author='Nikolay Kim',
+      author_email='fafhrd91@gmail.com',
+      url='http://z3ext.net/',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'':'src'},
+      namespace_packages=['z3ext'],
+      install_requires = ['setuptools',
+                          'zope.schema',
+                          'zope.component',
+                          'zope.interface',
+			  'zope.configuration',
+			  'zope.publisher',
+			  'zope.security',
+                          'zope.app.publisher',
+                          'z3c.autoinclude',
+                          'z3c.zrtresource>=1.1.0',
+			  'z3ext.cacheheaders',
+			  'z3ext.cssregistry>=1.4.0',
+                          ],
+      extras_require = dict(test=['zope.traversing',
+                                  'zope.app.testing',
+                                  'zope.testing',
+                                  'zope.testbrowser',
+                                  ]),
+      include_package_data = True,
+      zip_safe = False
+      )
