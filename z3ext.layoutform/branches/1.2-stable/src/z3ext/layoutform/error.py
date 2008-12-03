df3f3e4e4fa546198782c203627fa8a4f704ed01
fafhrd <fafhrd@62d5b8a3-27da-0310-9561-8e5933582275>
2008-12-03 08:21:02 +0000

    create branch for 1.2 table branch
    git-svn-id: svn://svn.zope.org/repos/main@93566 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3ext.layoutform/branches/1.2-stable/src/z3ext/layoutform/error.py
@@ -0,0 +1,34 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" 
+
+$Id:  2007-12-12 12:27:02Z fafhrd $
+"""
+from zope import interface
+from zope.app.pagetemplate import ViewPageTemplateFile
+from z3ext.statusmessage.message import Message
+
+
+class FormErrorStatusMessage(Message):
+
+    index = ViewPageTemplateFile('browser/message.pt')
+
+    @property
+    def context(self):
+        return self
+
+    def render(self, message):
+        self.message = message[0]
+        self.errors = [err for err in message[1:] if err.widget is None]
+        return self.index()
