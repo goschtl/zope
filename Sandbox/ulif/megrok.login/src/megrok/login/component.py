127df406f60efd555da460deee5f8bf244d81a75
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-12-06 00:11:23 +0000

    Add directives.
    git-svn-id: svn://svn.zope.org/repos/main@93704 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Sandbox/ulif/megrok.login/src/megrok/login/component.py
@@ -0,0 +1,22 @@
+import martian
+from martian.error import GrokImportError
+
+class enable(martian.MarkerDirective):
+    scope = martian.CLASS
+    store = martian.ONCE
+
+class viewname(martian.Directive):
+    scope = martian.CLASS
+    store = martian.ONCE
+    default = u'loginForm.html'
+    
+    def factory(self, viewname=None):
+        if viewname is None:
+            viewname = self.default
+        if martian.util.not_unicode_or_ascii(viewname):
+            raise GrokImportError(
+                "You can only pass unicode, None or ASCII "
+                "to the 'megrok.login.viewname' directive.")
+        return viewname
+
+    
