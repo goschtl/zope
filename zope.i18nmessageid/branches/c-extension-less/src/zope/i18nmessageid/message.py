7bf9fd537f038a47bdb9eb5dd7effb842f68b302
malthe <malthe@62d5b8a3-27da-0310-9561-8e5933582275>
2008-12-12 15:58:24 +0000

    Make sure messages are immutable.
    git-svn-id: svn://svn.zope.org/repos/main@93992 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.i18nmessageid/branches/c-extension-less/src/zope/i18nmessageid/message.py
+++ zope.i18nmessageid/branches/c-extension-less/src/zope/i18nmessageid/message.py
@@ -17,14 +17,19 @@ $Id$
 """
 __docformat__ = "reStructuredText"
 
+from weakref import WeakKeyDictionary
+registry = WeakKeyDictionary()
+
 class Message(unicode):
     """Message (Python implementation)
 
-    This is a string used as a message.  It has a domain attribute that is
-    its source domain, and a default attribute that is its default text to
-    display when there is no translation.  domain may be None meaning there is
-    no translation domain.  default may also be None, in which case the
-    message id itself implicitly serves as the default text.
+    This is a string used as a translation message. It has a
+    ``domain`` attribute that is its translation domain, and a
+    ``default`` attribute that is its default text to display when
+    there is no translation. ``domain`` may be ``None`` meaning there
+    is no translation domain; ``default`` may also be ``None``, in
+    which case the message id itself implicitly serves as the default
+    text.
 
     >>> from zope.i18nmessageid.message import Message
     >>> robot = Message(u"robot-message", 'futurama', u"${name} is a robot.")
@@ -41,54 +46,75 @@ class Message(unicode):
     >>> robot.domain = "planetexpress"
     Traceback (most recent call last):
     ...
-    TypeError: readonly attribute
+    AttributeError: can't set attribute
 
     >>> robot.default = u"${name} is not a robot."
     Traceback (most recent call last):
     ...
-    TypeError: readonly attribute
+    AttributeError: can't set attribute
 
     >>> robot.mapping = {u'name': u'Bender'}
     Traceback (most recent call last):
     ...
-    TypeError: readonly attribute
+    AttributeError: can't set attribute
 
     >>> new_robot = Message(robot, mapping={u'name': u'Bender'})
+    
     >>> new_robot
     u'robot-message'
+    
     >>> new_robot.domain
     'futurama'
+    
     >>> new_robot.default
     u'${name} is a robot.'
+    
     >>> new_robot.mapping
     {u'name': u'Bender'}
+
+    Verify that messages reduces to their arguments.
 
     >>> callable, args = new_robot.__reduce__()
     >>> callable is Message
     True
+    
     >>> args
     (u'robot-message', 'futurama', u'${name} is a robot.', {u'name': u'Bender'})
 
     >>> fembot = Message(u'fembot')
+    
     >>> callable, args = fembot.__reduce__()
     >>> callable is Message
     True
+    
     >>> args
     (u'fembot', None, None, None)
-
-    Verify pickling.
+    
+    Verify pickling. First, pickle the message.
     
     >>> from pickle import dumps, loads
     >>> pystate = dumps(new_robot)
+    
+    Verify that we have an empty registry when there are no hard
+    references to the messages.
+    
+    >>> from zope.i18nmessageid.message import registry
+    >>> del robot, fembot, new_robot
+    >>> len(registry)
+    0
+    
+    Load the pickle and verify registry.
+    
     >>> pickle_bot = loads(pystate)
+    >>> len(registry)
+    1
+
+    We expect the message properties to be available.
+    
     >>> pickle_bot, pickle_bot.domain, pickle_bot.default, pickle_bot.mapping
     (u'robot-message', 'futurama', u'${name} is a robot.', {u'name': u'Bender'})
-    >>> pickle_bot.__reduce__()[0] is Message
-    True
     
     """
-
-    __slots__ = ('domain', 'default', 'mapping', '_readonly')
 
     def __new__(cls, ustr, domain=None, default=None, mapping=None):
         self = unicode.__new__(cls, ustr)
@@ -101,26 +127,27 @@ class Message(unicode):
         # make sure a non-trivial default value is a unicode string
         if default is not None:
             default = unicode(default)
-            
-        unicode.__setattr__(self, 'domain', domain)
-        unicode.__setattr__(self, 'default', default)
-        unicode.__setattr__(self, 'mapping', mapping)
-        
+
+        registry[self] = (domain, default, mapping)
         return self
 
-    def __setattr__(self, key, value):
-        """Message is immutable.
-
-        It cannot be changed once the message id is created.
-        """
-
-        raise TypeError('readonly attribute')
-    
     def __reduce__(self):
-        return self.__class__, self.__getstate__()
+        return type(self), self.__getstate__()
 
     def __getstate__(self):
         return unicode(self), self.domain, self.default, self.mapping
+
+    @property
+    def domain(self):
+        return registry[self][0]
+
+    @property
+    def default(self):
+        return registry[self][1]
+
+    @property
+    def mapping(self):
+        return registry[self][2]
 
 class MessageFactory(object):
     """Factory for creating i18n messages."""
