cf931d140705b99af6ba42bd8a2326ab767961ea
nadako <nadako@62d5b8a3-27da-0310-9561-8e5933582275>
2008-12-15 22:17:37 +0000

    Upgrade jQuery libraries to version 1.2.6
    git-svn-id: svn://svn.zope.org/repos/main@94088 62d5b8a3-27da-0310-9561-8e5933582275

--- jquery.javascript/trunk/setup.py
+++ jquery.javascript/trunk/setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='jquery.javascript',
-      version='1.1.0',
+      version='1.2.6dev',
       author = "Roger Ineichen and the Zope Community",
       author_email = "zope3-dev@zope.org",
       description = "JQuery integration into Zope 3 via Viewlets",
