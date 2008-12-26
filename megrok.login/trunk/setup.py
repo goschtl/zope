e0fcc7547c770a07d3e1f4ba3980392786b9df81
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2008-12-26 16:59:34 +0000

    Bump version.
    git-svn-id: svn://svn.zope.org/repos/main@94363 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.login/trunk/setup.py
+++ megrok.login/trunk/setup.py
@@ -26,7 +26,7 @@ long_description = (
 
 setup(
     name='megrok.login',
-    version='0.1dev',
+    version='0.2dev',
     author='Uli Fouquet and the Zope Community',
     author_email='uli@gnufix.de',
     url = 'http://pypi.python.org/pypi/megrok.login',
