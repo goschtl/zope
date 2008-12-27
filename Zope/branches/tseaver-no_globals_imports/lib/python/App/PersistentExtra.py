9085790dfaa532c6b515afa9db92349be72fea6b
tseaver <tseaver@62d5b8a3-27da-0310-9561-8e5933582275>
2008-12-27 03:27:23 +0000

    Branch for removing imports from the Globals module.
    git-svn-id: svn://svn.zope.org/repos/main@94367 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Zope/branches/tseaver-no_globals_imports/lib/python/App/PersistentExtra.py
@@ -0,0 +1,97 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+"""Patch for Persistent to support IPersistentExtra.
+
+$Id$
+"""
+
+class PersistentUtil:
+
+    def _datetime(self, x=None):
+        from DateTime.DateTime import DateTime
+        if x is None:
+            return DateTime()
+        return DateTime(x)
+
+    def bobobase_modification_time(self):
+        jar=self._p_jar
+        oid=self._p_oid
+        if jar is None or oid is None:
+            return self._datetime()
+
+        try:
+            t = self._p_mtime
+        except:
+            t = 0
+        return self._datetime(t)
+
+    def locked_in_version(self):
+        """Was the object modified in any version?
+        """
+        import Globals
+        jar=self._p_jar
+        oid=self._p_oid
+        if jar is None or oid is None: return None
+        try: mv=jar.modifiedInVersion
+        except: pass
+        else: return mv(oid)
+
+        # BoboPOS 2 code:
+        oid=self._p_oid
+        return (oid
+                and Globals.VersionBase.locks.has_key(oid)
+                and Globals.VersionBase.verify_lock(oid)
+                and 'some version')
+
+    def modified_in_version(self):
+        """Was the object modified in this version?
+        """
+        jar=self._p_jar
+        oid=self._p_oid
+        if jar is None or oid is None: return None
+        try: mv=jar.modifiedInVersion
+        except: pass
+        else: return mv(oid)==jar.getVersion()
+
+        # BoboPOS 2 code:
+        jar=self._p_jar
+        if jar is None:
+            if hasattr(self,'aq_parent') and hasattr(self.aq_parent, '_p_jar'):
+                jar=self.aq_parent._p_jar
+            if jar is None: return 0
+        if not jar.name: return 0
+        try: jar.db[self._p_oid]
+        except: return 0
+        return 1
+
+
+_patched = False
+
+def patchPersistent():
+    global _patched
+    if _patched:
+        return
+
+    _patched = True
+
+    from zope.interface import classImplements
+    from Persistence import Persistent
+    from App.class_init import default__class_init__
+    from App.interfaces import IPersistentExtra
+    Persistent.__class_init__ = default__class_init__
+
+    for k, v in PersistentUtil.__dict__.items():
+        if k[0] != '_':
+            setattr(Persistent, k, v)
+
+    classImplements(Persistent, IPersistentExtra)
