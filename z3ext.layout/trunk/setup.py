351bf20fa7b97ed1d2ce33a533ccc0193f15dbb1
fafhrd <fafhrd@62d5b8a3-27da-0310-9561-8e5933582275>
2009-01-08 07:26:31 +0000

    vb
    git-svn-id: svn://svn.zope.org/repos/main@94610 62d5b8a3-27da-0310-9561-8e5933582275

--- z3ext.layout/trunk/setup.py
+++ z3ext.layout/trunk/setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version='2.0.2'
+version='2.0.3dev'
 
 
 setup(name='z3ext.layout',
