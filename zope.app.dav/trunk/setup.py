82656c469decb7fadc1a40e00ccfdeee0f96d40c
wosc <wosc@62d5b8a3-27da-0310-9561-8e5933582275>
2009-01-27 12:20:01 +0000

    vb
    git-svn-id: svn://svn.zope.org/repos/main@95145 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.app.dav/trunk/setup.py
+++ zope.app.dav/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 
 setup(name='zope.app.dav',
-      version = '3.4.2',
+      version = '3.4.3dev',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Zope WebDAV Support(Basic)',
