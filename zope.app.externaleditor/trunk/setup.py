4b10eb19ce940157af4db02218b0b1acb32a0abc
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2009-01-29 18:25:58 +0000

    Switch to a major release as we changed dependencies.
    git-svn-id: svn://svn.zope.org/repos/main@95481 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.app.externaleditor/trunk/setup.py
+++ zope.app.externaleditor/trunk/setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.externaleditor',
-      version = '3.5.0dev',
+      version = '3.5dev',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Editing Zope 3 Content with an External Editor',
