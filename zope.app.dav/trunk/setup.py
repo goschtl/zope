e905b7faa8e2729b4f52bc44aaeb983b9adc7ced
thefunny42 <thefunny42@62d5b8a3-27da-0310-9561-8e5933582275>
2009-01-29 18:24:56 +0000

    Update CHANGES, and version.
    git-svn-id: svn://svn.zope.org/repos/main@95480 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.app.dav/trunk/setup.py
+++ zope.app.dav/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 
 setup(name='zope.app.dav',
-      version = '3.4.3dev',
+      version = '3.5dev',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Zope WebDAV Support(Basic)',
