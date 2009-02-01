7f7e3586387dee3df640cc1d12b79ca9425b5a6c
ctheune <ctheune@62d5b8a3-27da-0310-9561-8e5933582275>
2009-02-01 12:45:46 +0000

    Pre-release cleanup, update metadata, fill-in release date.
    git-svn-id: svn://svn.zope.org/repos/main@95838 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.app.homefolder/trunk/setup.py
+++ zope.app.homefolder/trunk/setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 setup(name='zope.app.homefolder',
       version = '3.5.0dev',
       author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
+      author_email='zope-dev@zope.org',
       description='User Home Folders',
       long_description=(
           read('README.txt')
@@ -47,7 +47,7 @@ setup(name='zope.app.homefolder',
           'Operating System :: OS Independent',
           'Topic :: Internet :: WWW/HTTP',
           'Framework :: Zope3'],
-      url='http://cheeseshop.python.org/pypi/zope.app.homefolder',
+      url='http://pypi.python.org/pypi/zope.app.homefolder',
       license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
