f30fc3ec0220243f0b987acd6f784e246671f11b
ctheune <ctheune@62d5b8a3-27da-0310-9561-8e5933582275>
2009-02-01 12:41:37 +0000

    Pre-release cleanup, update metadata, enter release date.
    git-svn-id: svn://svn.zope.org/repos/main@95834 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.app.ftp/trunk/setup.py
+++ zope.app.ftp/trunk/setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 setup(name = 'zope.app.ftp',
       version = '3.5.0dev',
       author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
+      author_email='zope-dev@zope.org',
       description='Zope FTP Support',
       long_description=(
           read('README.txt')
@@ -47,7 +47,7 @@ setup(name = 'zope.app.ftp',
           'Operating System :: OS Independent',
           'Topic :: Internet :: WWW/HTTP',
           'Framework :: Zope3'],
-      url='http://cheeseshop.python.org/pypi/zope.app.ftp',
+      url='http://pypi.python.org/pypi/zope.app.ftp',
       license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
