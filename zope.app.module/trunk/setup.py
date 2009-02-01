af3c65e24cb0df2589f46bfa85de69ae2060d14b
ctheune <ctheune@62d5b8a3-27da-0310-9561-8e5933582275>
2009-02-01 16:29:56 +0000

    Pre-release cleanup, update metadata, insert release date.
    git-svn-id: svn://svn.zope.org/repos/main@95868 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.app.module/trunk/setup.py
+++ zope.app.module/trunk/setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 setup(name='zope.app.module',
       version = '3.5.0dev',
       author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
+      author_email='zope-dev@zope.org',
       description='Zope 3 persistent code/module support',
       long_description=(
           read('README.txt')
@@ -49,7 +49,7 @@ setup(name='zope.app.module',
           'Operating System :: OS Independent',
           'Topic :: Internet :: WWW/HTTP',
           'Framework :: Zope3'],
-      url='http://cheeseshop.python.org/pypi/zope.app.module',
+      url='http://pypi.python.org/pypi/zope.app.module',
       license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
