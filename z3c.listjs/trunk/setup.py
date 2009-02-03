d39151ba593cdf700d74d6921167945af0ae5978
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2009-02-03 14:15:33 +0000

    vb
    git-svn-id: svn://svn.zope.org/repos/main@96034 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.listjs/trunk/setup.py
+++ z3c.listjs/trunk/setup.py
@@ -16,7 +16,7 @@ long_description = (
 
 setup(
     name='z3c.listjs',
-    version='1.0a3dev',
+    version='1.0a4dev',
     description="A formlib list widget that uses Javascript",
     long_description=long_description,
     keywords='zope3 form widget',
