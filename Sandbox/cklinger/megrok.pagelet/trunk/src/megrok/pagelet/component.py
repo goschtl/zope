62df73fa8b0ee7f2592ef6aacc691c367e5101de
cklinger <cklinger@62d5b8a3-27da-0310-9561-8e5933582275>
2009-02-03 15:15:12 +0000

    the __getitem__ is not good remove it
    git-svn-id: svn://svn.zope.org/repos/main@96039 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/cklinger/megrok.pagelet/trunk/src/megrok/pagelet/component.py
+++ Sandbox/cklinger/megrok.pagelet/trunk/src/megrok/pagelet/component.py
@@ -49,20 +49,3 @@ class Pagelet(BrowserPage):
                 (self.context, self.request), ILayoutTemplate)
             return layout(self)
         return self.layout()
-
-
-    def __getitem__(self, key):
-        # This is BBB code for Zope page templates only:
-        if not isinstance(self.template, PageTemplate):
-            raise AttributeError("View has no item %s" % key)
-
-        value = self.template._template.macros[key]
-        # When this deprecation is done with, this whole __getitem__ can
-        # be removed.
-        warnings.warn("Calling macros directly on the view is deprecated. "
-                      "Please use context/@@viewname/macros/macroname\n"
-                      "View %r, macro %s" % (self, key),
-                      DeprecationWarning, 1)
-        return value
-
-
