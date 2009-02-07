933d9da3667c6646d3981e532eb010d8f87547cd
icemac <icemac@62d5b8a3-27da-0310-9561-8e5933582275>
2009-02-07 14:59:57 +0000

    -dev
    git-svn-id: svn://svn.zope.org/repos/main@96209 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.layer/tags/0.3.0/setup.py
+++ z3c.layer/tags/0.3.0/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name = 'z3c.layer',
-      version = '0.3.0dev',
+      version = '0.3.0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Collection of Alternative Base Layers',
