910c399152521d5a6025d5fae7b0370cbcb0af35
hannosch <hannosch@62d5b8a3-27da-0310-9561-8e5933582275>
2009-02-09 12:24:33 +0000

    Retrofit a 3.3.2 tag from the code inside the 3.3 full release
    git-svn-id: svn://svn.zope.org/repos/main@96305 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.publisher/tags/3.3.2/setup.py
@@ -0,0 +1,53 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.publisher package
+
+$Id$
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+setup(name='zope.publisher',
+      version = '3.3.2',
+      url='http://svn.zope.org/zope.publisher',
+      license='ZPL 2.1',
+      description='Zope publisher',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description="Publish Python objects on web servers."
+                       "Provide an apply-like facility that"
+                       "works with any mapping object.",
+
+      packages=find_packages('src'),
+	  package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      install_requires=['setuptools',
+                        'zope.component',
+                        'zope.event',
+                        'zope.exceptions',
+                        'zope.i18n',
+                        'zope.interface',
+                        'zope.location',
+                        'zope.proxy',
+                        'zope.security',
+                        'zope.testing',
+                        'zope.deprecation',
+                        'zope.deferredimport'],
+      include_package_data = True,
+      zip_safe = False,
+      )
