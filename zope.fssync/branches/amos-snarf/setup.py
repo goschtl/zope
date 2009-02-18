4263d9b4510535ebf035b5526640a4ec7a292d8e
amos <amos@62d5b8a3-27da-0310-9561-8e5933582275>
2009-02-18 23:25:46 +0000

    branch for snarf work
    git-svn-id: svn://svn.zope.org/repos/main@96717 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.fssync/branches/amos-snarf/setup.py
@@ -0,0 +1,45 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.fssync package
+
+$Id$
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+setup(name='zope.fssync',
+      version = '3.4.0b1',
+      url='http://svn.zope.org/zope.fssync',
+      license='ZPL 2.1',
+      description='Zope fssync',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description="Filesystem synchronization utility for Zope 3.",
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      install_requires=['setuptools',
+                        'zope.interface',
+                        'zope.proxy',
+                        'zope.traversing',
+                        'zope.xmlpickle'],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
