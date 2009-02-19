81db99c4effa3fb36115d29af50a75f72933c57a
icemac <icemac@62d5b8a3-27da-0310-9561-8e5933582275>
2009-02-19 19:55:17 +0000

    Preparing release 0.6.0
    git-svn-id: svn://svn.zope.org/repos/main@96793 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.profiler/trunk/setup.py
+++ z3c.profiler/trunk/setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.5.2dev'
+version = '0.6.0'
 
 setup (
     name='z3c.profiler',
