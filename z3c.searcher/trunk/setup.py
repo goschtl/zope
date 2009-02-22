6f0ca7d308e6532be7111ffcd268fe9ca092a3ad
rogerineichen <rogerineichen@62d5b8a3-27da-0310-9561-8e5933582275>
2009-02-22 07:57:48 +0000

    added dev marker
    git-svn-id: svn://svn.zope.org/repos/main@97006 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.searcher/trunk/setup.py
+++ z3c.searcher/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.searcher',
-    version='0.5.1',
+    version='0.5.2dev',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "Persistent and session based search form for Zope3",
