4ee35507d043c0f38fbddb686cec42894804fa36
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2009-02-22 14:07:19 +0000

    Remove obsolete sandbox (moved to main).
    git-svn-id: svn://svn.zope.org/repos/main@97044 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/ulif/megrok.chameleon/src/megrok/chameleon/tests/genshi_fixture/app_templates/berries.cg
+++
@@ -1,1 +0,0 @@
-<strong>Lovely blueberries!</strong>
--- Sandbox/ulif/megrok.chameleon/src/megrok/chameleon/tests/test_megrok_chameleon.py
+++
@@ -1,5 +0,0 @@
-import z3c.testsetup
-from megrok.chameleon.tests import FunctionalLayer
-
-test_suite = z3c.testsetup.register_all_tests('megrok.chameleon',
-                                              layer=FunctionalLayer)
