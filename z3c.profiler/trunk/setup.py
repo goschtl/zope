3ccb7895073ab791723afa6c2fb5f704de2f914b
icemac <icemac@62d5b8a3-27da-0310-9561-8e5933582275>
2009-03-14 09:24:55 +0000

    Preparing release 0.7.0
    git-svn-id: svn://svn.zope.org/repos/main@98083 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.profiler/trunk/setup.py
+++ z3c.profiler/trunk/setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.6.1 dev'
+version = '0.7.0'
 
 setup (
     name='z3c.profiler',
@@ -69,7 +69,7 @@ setup (
         ),
     install_requires = [
         'setuptools',
-        'zc.configuration',
+        'zope.configuration >= 3.5.0',
         'zope.app.component',
         'zope.app.pagetemplate',
         'zope.app.publisher',
