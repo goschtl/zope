4a47c06d90ae8a19e18111f0b3f0a08fca7f0589
tlotze <tlotze@62d5b8a3-27da-0310-9561-8e5933582275>
2009-03-16 19:40:08 +0000

    pre-release version bump
    git-svn-id: svn://svn.zope.org/repos/main@98160 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.dict/tags/1.3b1/setup.py
+++ zc.dict/tags/1.3b1/setup.py
@@ -5,7 +5,7 @@ long_description = (open("src/zc/dict/dict.txt").read() +
                     open("src/zc/dict/ordered.txt").read())
 setup(
     name="zc.dict",
-    version="1.3dev",
+    version="1.3b1",
     license="ZPL 2.1",
     author="Zope Corporation",
     author_email="zope3-dev@zope.org",
