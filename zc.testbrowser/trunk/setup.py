93cb32f9dd9756e534f2bf0549f11c57c7e7b9f1
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2009-03-21 00:46:52 +0000

    version bump.
    git-svn-id: svn://svn.zope.org/repos/main@98296 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.testbrowser/trunk/setup.py
+++ zc.testbrowser/trunk/setup.py
@@ -23,7 +23,7 @@ long_description = (
 
 setup(
     name = 'zc.testbrowser',
-    version = '1.0.0a5',
+    version = '1.0.0a6dev',
     url = 'http://pypi.python.org/pypi/zc.testbrowser',
     license = 'ZPL 2.1',
     description = 'Programmable web browser for functional black-box testing '
