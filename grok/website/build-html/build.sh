fe0ab74bb619ccae2e00afae9cda2ada3be9534d
d2m <d2m@62d5b8a3-27da-0310-9561-8e5933582275>
2009-03-24 10:22:55 +0000

    initial upload, doc creation tool
    git-svn-id: svn://svn.zope.org/repos/main@98331 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ grok/website/build-html/build.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+builddocs()
+{
+cd $WORKDIR
+RELEASE=$1
+SVNFOLDER=$2
+BUILDFOLDER=$3
+# checkout or update trunk/tag
+svn co $SVN/$SVNFOLDER $SOURCE/$BUILDFOLDER
+# create html
+$SPHINXBUILD -q -c $WORKDIR/$CONF -D release="$RELEASE" -D version="$RELEASE" \
+             -b html $SOURCE/$BUILDFOLDER/doc/ $BUILD/$BUILDFOLDER
+# create latex
+$SPHINXBUILD -q -c $WORKDIR/$CONF -D release="$RELEASE" -D version="$RELEASE" \
+             -b latex $SOURCE/$BUILDFOLDER/doc/ $BUILD/$BUILDFOLDER/latex
+# create pdf
+cd $BUILD/$BUILDFOLDER/latex
+make all-pdf
+# copy pdf files
+cp tutorial.pdf ..
+cp reference.pdf ../reference
+}
+
+# variables
+WORKDIR=/var/www/build-html
+CONF=conf
+SOURCE=source
+BUILD=/var/www/html/grok/doc
+SPHINXBUILD=bin/sphinx-build
+
+# constants
+SVN=svn://svn.zope.org/repos/main/grok
+
+# builddocs release svnfolder buildfolder
+#  release: version/release name used in the docs
+#  svnfolder: relative to svn://svn.zope.org/repos/main/grok
+#  buildfolder: folder id for the built docs
+builddocs 1.0b1dev trunk dev
+builddocs 1.0a1 tags/1.0a1 1.0a1
+builddocs 0.14.1 tags/0.14.1 0.14.1
+builddocs 0.13.1 tags/0.13.1 0.13.1
