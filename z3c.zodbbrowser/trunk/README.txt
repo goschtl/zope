de402ded7d64a78c3524522c215b35700fe3aa74
rogerineichen <rogerineichen@62d5b8a3-27da-0310-9561-8e5933582275>
2009-04-24 04:46:53 +0000

    improve setup
    git-svn-id: svn://svn.zope.org/repos/main@99447 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.zodbbrowser/trunk/README.txt
@@ -0,0 +1,2 @@
+This package provides a wxPython client application which can visualize the 
+ZODB internals for Zope3.
--- z3c.zodbbrowser/trunk/setup.py
+++ z3c.zodbbrowser/trunk/setup.py
@@ -12,7 +12,11 @@
 #
 ##############################################################################
 
+import os
 from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(
     name="z3c.zodbbrowser",
@@ -20,16 +24,38 @@ setup(
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description='ZODB Browser',
-
-    packages=find_packages('src'),
-    package_dir={"": "src"},
-    namespace_packages=['z3c'],
-
-    install_requires=["setuptools",
-                      "ZODB3",
-                      ],
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n\n' +
+        read('src', 'z3c', 'zodbbrowser', 'README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license = "ZPL 2.1",
+    keywords = "zope3 wx client browser ZODB",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://pypi.python.org/pypi/z3c.zodbbrowser',
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['z3c',],
+    install_requires = [
+        'setuptools',
+        'ZODB3',
+        ],
     entry_points={'console_scripts':
-                      ['zodbbrowser = z3c.zodbbrowser.main:main']},
-    include_package_data=True,
+        ['zodbbrowser = z3c.zodbbrowser.main:main']},
     zip_safe=False,
-    )
+)
