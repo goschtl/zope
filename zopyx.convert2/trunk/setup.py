b42edf2cf5d547c0455364a119d2d3398bb662a6
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2009-04-28 04:50:23 +0000

    2.0.0b4
    git-svn-id: svn://svn.zope.org/repos/main@99546 62d5b8a3-27da-0310-9561-8e5933582275

--- zopyx.convert2/trunk/setup.py
+++ zopyx.convert2/trunk/setup.py
@@ -12,7 +12,7 @@ CLASSIFIERS = [
     'Programming Language :: Python',
 ]
 
-version = '2.0.0b3'
+version = '2.0.0b4'
 
 desc = unicode(file('README.txt').read().strip(), 'utf-8')
 changes = file('CHANGES.txt').read().strip()
