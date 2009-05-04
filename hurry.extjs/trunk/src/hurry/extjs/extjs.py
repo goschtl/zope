206c251adb3f8cef5633fe7996b2652052d8578e
sancho_0 <sancho_0@62d5b8a3-27da-0310-9561-8e5933582275>
2009-05-04 22:39:13 +0000

    Fix on resource inclusion
    git-svn-id: svn://svn.zope.org/repos/main@99757 62d5b8a3-27da-0310-9561-8e5933582275

--- hurry.extjs/trunk/src/hurry/extjs/extjs.py
+++ hurry.extjs/trunk/src/hurry/extjs/extjs.py
@@ -2,10 +2,9 @@ from hurry.resource import Library, ResourceInclusion
 
 extjs = Library('extjs')
 
-extjs_core = ResourceInclusion(extjs, 'ext-core.js',
-                               debug='ext-core-debug.js')
+extjs_base = ResourceInclusion(extjs, 'adapter/ext/ext-base.js')
 
-extjs_all = ResourceInclusion(extjs, 'ext-all.js', depends=[extjs_core],
+extjs_all = ResourceInclusion(extjs, 'ext-all.js', depends=[extjs_base],
                               debug='ext-all-debug.js')
 
 extjs_css = ResourceInclusion(extjs, 'resources/css/ext-all.css')
