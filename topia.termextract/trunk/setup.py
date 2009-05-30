862be5a76155aafead2365e8ac24dc06c66a4505
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2009-05-30 16:14:09 +0000

    vb.
    git-svn-id: svn://svn.zope.org/repos/main@100562 62d5b8a3-27da-0310-9561-8e5933582275

--- topia.termextract/trunk/setup.py
+++ topia.termextract/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='topia.termextract',
-    version='1.0.0',
+    version='1.1.0dev',
     author = "Stephan Richter, Russ Ferriday and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "Content Term Extraction using POS Tagging",
