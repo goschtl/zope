0ef5c8e69d38f1090aa038fe62ab8fc1c0ff2e8a
adamg <adamg@62d5b8a3-27da-0310-9561-8e5933582275>
2009-06-07 09:42:10 +0000

    release
    git-svn-id: svn://svn.zope.org/repos/main@100679 62d5b8a3-27da-0310-9561-8e5933582275

--- zam.api/trunk/setup.py
+++ zam.api/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='zam.api',
-    version='0.5.3dev',
+    version='0.5.3',
     author = "Stephan Richter, Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "API for ZAM (Zope Application Management",
