6a2fbafa77cd646586e86d7175ff0a89ad136776
wosc <wosc@62d5b8a3-27da-0310-9561-8e5933582275>
2009-06-16 08:31:34 +0000

    Preparing release 1.0
    git-svn-id: svn://svn.zope.org/repos/main@101041 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.hashedresource/trunk/setup.py
+++ z3c.hashedresource/trunk/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='z3c.hashedresource',
-    version = '1.0dev',
+    version = '1.0',
     author='Wolfgang Schnerring',
     author_email='ws@gocept.com',
     description='Provides URLs for resources that change whenever their content changes.',
