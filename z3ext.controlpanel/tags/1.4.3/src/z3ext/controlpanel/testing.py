6d837e934a89bf6e2d2abe5bf53025ace806c15e
fafhrd <fafhrd@62d5b8a3-27da-0310-9561-8e5933582275>
2009-06-21 10:02:33 +0000

    release tag
    git-svn-id: svn://svn.zope.org/repos/main@101184 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3ext.controlpanel/tags/1.4.3/src/z3ext/controlpanel/testing.py
@@ -0,0 +1,56 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" setup z3ext.controlpanel
+
+$Id$
+"""
+import os
+from zope import component, interface
+from zope.app.testing import setup
+from zope.annotation.attribute import AttributeAnnotations
+from zope.app.component.hooks import getSite, setSite
+from zope.app.testing.functional import ZCMLLayer
+
+from z3ext.controlpanel import storage, root, interfaces
+
+
+controlPanelData = None
+
+@component.adapter(interface.Interface)
+@interface.implementer(interfaces.IConfigletDataStorage)
+def getConfigletDataStorage(siteManager):
+    global controlPanelData
+    if controlPanelData is None:
+        controlPanelData = storage.ConfigletDataStorage()
+    return controlPanelData
+
+
+def setUpControlPanel():
+    setup.setUpTraversal()
+    setup.setUpSiteManagerLookup()
+
+    component.provideAdapter(root.getSettings, name='settings')
+    component.provideAdapter(AttributeAnnotations)
+    component.provideUtility(root.RootConfiglet(), interfaces.IConfiglet)
+
+    global controlPanelData
+    controlPanelData = None
+    component.provideAdapter(storage.getConfigletData)
+    component.provideAdapter(getConfigletDataStorage)
+    component.provideAdapter(storage.DefaultConfigletDataFactory)
+
+
+z3extControlPanelLayer = ZCMLLayer(
+    os.path.join(os.path.split(__file__)[0], 'ftesting.zcml'),
+    __name__, 'z3extControlPanelLayer', allow_teardown=True)
