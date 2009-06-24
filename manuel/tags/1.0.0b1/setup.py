ec13914698ce09b427429c8869c005205887d02a
benji_york <benji_york@62d5b8a3-27da-0310-9561-8e5933582275>
2009-06-24 12:01:08 +0000

    tag 1.0.0b1
    git-svn-id: svn://svn.zope.org/repos/main@101260 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ manuel/tags/1.0.0b1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+long_description = (
+    open(os.path.join('README.txt')).read()
+    + '\n\n'
+    + open('CHANGES.txt').read()
+    )
+
+setup(
+    name='manuel',
+    version='0',
+    url = 'http://pypi.python.org/pypi/manuel',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    zip_safe=False,
+    author='Benji York',
+    author_email='benji@benjiyork.com',
+    description=
+        'Manuel lets you combine traditional doctests with new test syntax '
+        'that you create yourself or is included in Manuel.',
+    license='ZPL',
+    install_requires=[
+        'setuptools',
+        'zope.testing',
+        ],
+    include_package_data=True,
+    long_description = long_description,
+    )
