42a2ac0ed72a58cffc9d467c0c342f737ab1e6f1
cklinger <cklinger@62d5b8a3-27da-0310-9561-8e5933582275>
2009-06-26 08:58:51 +0000

    Added a Table-Based Layer
    git-svn-id: svn://svn.zope.org/repos/main@101282 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.z3cform/branches/megrok.z3cfrom-layout/src/megrok/z3cform/__init__.py
+++ megrok.z3cform/branches/megrok.z3cfrom-layout/src/megrok/z3cform/__init__.py
@@ -2,5 +2,5 @@
 from components import Form, AddForm, EditForm, DisplayForm
 from components import PageForm, PageAddForm, PageEditForm, PageDisplayForm
 from components import WidgetTemplate
-from skin import FormLayer
+from skin import FormLayer, TableLayer
 from directives import field
--- megrok.z3cform/branches/megrok.z3cfrom-layout/src/megrok/z3cform/skin.py
+++ megrok.z3cform/branches/megrok.z3cfrom-layout/src/megrok/z3cform/skin.py
@@ -4,4 +4,7 @@ import z3c.formui.interfaces
 from z3c.form.interfaces import IFormLayer
 
 class FormLayer(grok.IDefaultBrowserLayer, IFormLayer, z3c.formui.interfaces.IDivFormLayer):
-    """ A layer for a z3c.forms"""
+    """ A div -based layer for a z3c.forms"""
+
+class TableLayer(grok.IDefaultBrowserLayer, IFormLayer, z3c.formui.interfaces.ITableFormLayer):
+    """ A table -based layer for a z3c.forms"""
