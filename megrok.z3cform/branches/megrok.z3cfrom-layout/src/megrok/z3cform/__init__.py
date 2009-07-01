9fc4ac5b6c9320bf4792e09f9f3fec1b30318c83
cklinger <cklinger@62d5b8a3-27da-0310-9561-8e5933582275>
2009-07-01 14:45:01 +0000

    Forgot to add - extends
    git-svn-id: svn://svn.zope.org/repos/main@101368 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.z3cform/branches/megrok.z3cfrom-layout/src/megrok/z3cform/__init__.py
+++ megrok.z3cform/branches/megrok.z3cfrom-layout/src/megrok/z3cform/__init__.py
@@ -4,5 +4,5 @@ from components import PageForm, PageAddForm, PageEditForm, PageDisplayForm
 from components import WidgetTemplate
 from skin import FormLayer, TableLayer
 from directives import field
-from z3c.form import widget, field, button, action
+from z3c.form import widget, field, button, action, extends
 from utils import apply_data_event
