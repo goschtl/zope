d696fb4dacf39b06241f4f1dbf42cf9d82edd3b7
cklinger <cklinger@62d5b8a3-27da-0310-9561-8e5933582275>
2009-07-01 15:54:27 +0000

    Fixing a typo
    git-svn-id: svn://svn.zope.org/repos/main@101373 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.z3cform/branches/megrok.z3cfrom-layout/src/megrok/z3cform/utils.py
+++ megrok.z3cform/branches/megrok.z3cfrom-layout/src/megrok/z3cform/utils.py
@@ -1,11 +1,11 @@
 import grok
-import z3c.form
+from z3c.form.form import applyChanges
 from zope.lifecycleevent import Attributes
 
 def apply_data_event(form, context, data):
     """ Updates the object with the data and sends an IObjectModifiedEvent
     """
-    changes = z3c.form.applyChanges(form, context, data)
+    changes = applyChanges(form, context, data)
     if changes:
 	descriptions = []
 	for interface, names in changes.items():
