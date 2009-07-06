256398cb764421da43c43f05a31566aab5877ce5
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2009-07-06 16:49:00 +0000

    cleanup
    git-svn-id: svn://svn.zope.org/repos/main@101643 62d5b8a3-27da-0310-9561-8e5933582275

--- zopyx.smartprintng.server/branches/bfg/zopyx/smartprintng/server/views.py
+++ zopyx.smartprintng.server/branches/bfg/zopyx/smartprintng/server/views.py
@@ -1,18 +1,17 @@
 from repoze.bfg.chameleon_zpt import render_template_to_response
 from repoze.bfg.view import static
 
-static_view = static('templates/static')
 
-def my_view(context, request):
-    return render_template_to_response('templates/mytemplate.pt',
+def index(context, request):
+    return render_template_to_response('templates/index.pt',
                                        request = request,
                                        project = 'zopyx.smartprintng.server')
-def my_view2(context, request):
-    return render_template_to_response('templates/mytemplate2.pt',
-                                       request = request,
-                                       project = 'zopyx.smartprintng.server')
+
 from repoze.bfg.xmlrpc import xmlrpc_view
 
 @xmlrpc_view
-def hello(context, request):
-    return 'it"me'
+def convertZIP(context, zip_archive, converter_name='pdf_prince'):
+
+    from base import ServerCore
+    core = ServerCore()
+    return core.convertZIP(zip_archive, converter_name)
