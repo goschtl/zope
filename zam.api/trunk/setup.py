b36e1fa0cd255a2b90a20093b0d5ec8b14a79466
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2009-07-06 19:05:45 +0000

    vb
    git-svn-id: svn://svn.zope.org/repos/main@101668 62d5b8a3-27da-0310-9561-8e5933582275

--- zam.api/trunk/setup.py
+++ zam.api/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='zam.api',
-    version='0.6.1',
+    version='0.6.2dev',
     author = "Stephan Richter, Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "API for ZAM (Zope Application Management",
