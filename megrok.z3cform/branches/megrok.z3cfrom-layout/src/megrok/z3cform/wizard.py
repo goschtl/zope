b121149ed71a36172fa3b7e124abd925dddab441
cklinger <cklinger@62d5b8a3-27da-0310-9561-8e5933582275>
2009-07-08 09:44:21 +0000

    Override the call function to a grok form way
    git-svn-id: svn://svn.zope.org/repos/main@101724 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.z3cform/branches/megrok.z3cfrom-layout/src/megrok/z3cform/wizard.py
+++ megrok.z3cform/branches/megrok.z3cfrom-layout/src/megrok/z3cform/wizard.py
@@ -1,9 +1,10 @@
 import martian
 import grokcore.view
 
-from z3c.wizard import wizard, step
-from megrok.z3cform.components import GrokForm
 from z3c.form import form 
+from z3c.wizard import wizard, step
+from zope.publisher.publish import mapply
+from megrok.z3cform.components import GrokForm
 
 class WizardForm(GrokForm, wizard.Wizard, grokcore.view.View):
     """Base Class for a z3cwizdard.
@@ -17,3 +18,11 @@ class WizardForm(GrokForm, wizard.Wizard, grokcore.view.View):
 class Step(step.EditStep):
     """A Step for the Witzard
     """
+    def __call__(self):
+        mapply(self.update, (), self.request)
+        if self.request.response.getStatus() in (302, 303):
+            # A redirect was triggered somewhere in update().  Don't
+            # continue rendering the template or doing anything else.
+            return
+        return self.render()
+   
