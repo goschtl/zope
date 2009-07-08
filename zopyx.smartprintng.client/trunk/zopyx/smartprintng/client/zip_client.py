63bc87b2db4f1d8999592060987010bcd80a3a1b
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2009-07-08 11:01:15 +0000

    cleanup
    git-svn-id: svn://svn.zope.org/repos/main@101731 62d5b8a3-27da-0310-9561-8e5933582275

--- zopyx.smartprintng.client/trunk/zopyx/smartprintng/client/zip_client.py
+++ zopyx.smartprintng.client/trunk/zopyx/smartprintng/client/zip_client.py
@@ -1,3 +1,8 @@
+##########################################################################
+# zopyx.smartprintng.client - client library for the SmartPrintNG server
+# (C) 2009, ZOPYX Ltd & Co. KG, Tuebingen, Germany
+##########################################################################
+
 import os
 import sys
 import base64
@@ -6,9 +11,9 @@ import zipfile
 import tempfile
 import zipfile
 
-# XMLRPC server instance
-
 def convertZIP(dirname, converter_name='pdf-prince', host='localhost', port=6543):
+    """ XMLRPC client to SmartPrintNG server """
+    cwd = os.getcwd()
     os.chdir(dirname)
     server = xmlrpclib.ServerProxy('http://%s:%d/convertZIP' % (host, port))
     zip_filename = tempfile.mktemp()
@@ -22,20 +27,22 @@ def convertZIP(dirname, converter_name='pdf-prince', host='localhost', port=6543
 
     # send the ZIP archive base64 encoded
     zip_data = server.convertZIP(base64.encodestring(file(zip_filename, 'rb').read()),
-                                converter_name)
+                                 converter_name)
 
     # and receive the result PDF as base64 encoded ZIP archive
     zip_temp = tempfile.mktemp()
     file(zip_temp, 'wb').write(base64.decodestring(zip_data))
     ZF = zipfile.ZipFile(zip_temp, 'r')
     names = ZF.namelist()
-    output_filename = names[0]
-    file(output_filename, 'wb').write(ZF.read(output_filename))
+    output_filename = os.path.abspath(names[0])
+    file(output_filename, 'wb').write(ZF.read(names[0]))
     ZF.close()
     os.unlink(zip_filename)
     os.unlink(zip_temp)
+    os.chdir(cwd)
     return output_filename
 
 if __name__ == '__main__':
+    # usage: convertZIP <dirname>
     print convertZIP(sys.argv[1])
 
