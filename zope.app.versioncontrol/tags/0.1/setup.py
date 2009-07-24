b1796f0c4b9cd4de17714967b42cec62e48b5f5e
roymathew <roymathew@62d5b8a3-27da-0310-9561-8e5933582275>
2009-07-24 17:36:45 +0000

    create release tag
    git-svn-id: svn://svn.zope.org/repos/main@102264 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.app.versioncontrol/tags/0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'zope.app.versioncontrol',
+    version = '0.1',
+    author = 'Jim Fulton',
+    author_email = 'jim@zope.com',
+    description = '',
+    license = 'ZPL 2.1',
+
+    packages = find_packages('src'),
+    namespace_packages = ['zope', 'zope.app'],
+    package_dir = {'': 'src'},
+    extras_require=dict(test=['zope.event',
+                              'zope.traversing',
+                              'zope.annotation>=3.4.0b1.dev-r75055', 
+                              'zope.component']),
+    install_requires = ['setuptools',
+                        'ZODB3',
+                        'zope.datetime',
+                        'zope.location',
+                        'zope.security'],
+    zip_safe = False,
+    )
