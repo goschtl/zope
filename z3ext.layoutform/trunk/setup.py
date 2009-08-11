6f700776f5d8844cc1ad79c2d0e4f94b85b77a15
fafhrd <fafhrd@62d5b8a3-27da-0310-9561-8e5933582275>
2009-08-11 08:41:11 +0000

    reindent code; change copyright holder; prepare release
    git-svn-id: svn://svn.zope.org/repos/main@102647 62d5b8a3-27da-0310-9561-8e5933582275

--- z3ext.layoutform/trunk/setup.py
+++ z3ext.layoutform/trunk/setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2007 Zope Corporation and Contributors.
+# Copyright (c) 2007 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version='1.5.1dev'
+version='1.6.0'
 
 
 setup(name='z3ext.layoutform',
