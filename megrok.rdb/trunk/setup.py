38269f733ee93ddcceabfed140078d9443b3093e
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2009-08-14 16:30:30 +0000

    Prepare for fixed non-zipped release.
    git-svn-id: svn://svn.zope.org/repos/main@102784 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.rdb/trunk/setup.py
+++ megrok.rdb/trunk/setup.py
@@ -8,7 +8,7 @@ long_description = read('src/megrok/rdb/README.txt') + '\n' +\
     read('CHANGES.txt')
 
 setup(name='megrok.rdb',
-      version = '0.10dev',
+      version = '0.9.1dev',
       description="SQLAlchemy based RDB support for Grok.",
       long_description=long_description,
       classifiers=['Development Status :: 4 - Beta',
@@ -30,7 +30,7 @@ setup(name='megrok.rdb',
       package_dir = {'': 'src'},
       namespace_packages=['megrok'],
       include_package_data=True,
-      zip_safe=True,
+      zip_safe=False,
       install_requires=[
           'setuptools',
           'zope.interface',
