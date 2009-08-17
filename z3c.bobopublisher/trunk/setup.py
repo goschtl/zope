db3fed956d623a15a39774033f35fdcaa88d1943
kobold <kobold@62d5b8a3-27da-0310-9561-8e5933582275>
2009-08-17 12:08:10 +0000

    preparing the first public release
    git-svn-id: svn://svn.zope.org/repos/main@102873 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.bobopublisher/trunk/setup.py
+++ z3c.bobopublisher/trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='z3c.bobopublisher',
-    version='0.0.1dev',
+    version='0.1.0',
     url='http://pypi.python.org/pypi/z3c.bobopublisher',
     license='ZPL 2.1',
     author='Fabio Tranchitella',
