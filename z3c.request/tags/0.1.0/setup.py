6a06a563e69bae5a316b62da357a5a9735413afa
kobold <kobold@62d5b8a3-27da-0310-9561-8e5933582275>
2009-08-18 14:50:56 +0000

    tagging the release
    git-svn-id: svn://svn.zope.org/repos/main@102913 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.request/tags/0.1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='z3c.request',
+    version='0.1.0',
+    url='http://pypi.python.org/pypi/z3c.request',
+    license='ZPL 2.1',
+    author='Fabio Tranchitella and the Zope Community',
+    author_email='zope-dev@zope.org',
+    description="Common interface for browser request implementations.",
+    long_description=(
+        open('src/z3c/request/README.txt').read() + '\n\n' +
+        open('CHANGES.txt').read()
+    ),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['z3c'],
+    tests_require=[
+        'WebOb',
+        'zope.publisher',
+        'zope.testing',
+    ],
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+    ],
+    extras_require=dict(
+        test=[
+            'WebOb',
+            'zope.publisher',
+            'zope.testing',
+        ],
+    ),
+    include_package_data=True,
+    zip_safe=False,
+)
---
+++ z3c.request/tags/0.1.0/src/z3c/request/tests.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+# Copyright (c) 2009 Fabio Tranchitella <fabio@tranchitella.it>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+"""
+$Id$
+"""
+
+import unittest
+
+from zope.testing import doctest
+
+
+docfiles = [
+    'README.txt',
+]
+
+
+def test_suite():
+    tests = []
+    for d in docfiles:
+        tests.append(doctest.DocFileSuite(d, optionflags=doctest.ELLIPSIS))
+    return unittest.TestSuite(tests)
