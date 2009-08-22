ead162ecf49412cb34288f83612384c2f3b706b1
hannosch <hannosch@62d5b8a3-27da-0310-9561-8e5933582275>
2009-08-22 16:08:21 +0000

    Update version after release
    git-svn-id: svn://svn.zope.org/repos/main@103088 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.i18nmessage/trunk/setup.py
+++ zope.i18nmessage/trunk/setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.i18nmessage',
-    version = '1.0',
+    version = '1.0.1dev',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description='Message Identifiers for internationalization',
