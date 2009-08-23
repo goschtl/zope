ec2b599d8665444e64fbc5d24ef1947e2d83485d
kobold <kobold@62d5b8a3-27da-0310-9561-8e5933582275>
2009-08-23 08:33:18 +0000

    tagging release 0.1.4
    git-svn-id: svn://svn.zope.org/repos/main@103100 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3c.bobopublisher/tags/0.1.4/src/z3c/bobopublisher/application.py
@@ -0,0 +1,32 @@
+##############################################################################
+#
+# Copyright (c) 2009 Fabio Tranchitella <fabio@tranchitella.it>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+"""
+$Id$
+"""
+
+import bobo
+
+from zope.component import getGlobalSiteManager
+
+
+class Application(bobo.Application):
+    """Create a WSGI application based on bobo"""
+
+    def __init__(self, DEFAULT=None, **config):
+        # add the z3c.bobopublisher publication subroute
+        config['bobo_resources'] = ('z3c.bobopublisher.publication' + \
+            '\n' + config.get('bobo_resources', '')).rstrip()
+        # call the original __init__ method
+        return bobo.Application.__init__(self, DEFAULT, **config)
---
+++ z3c.bobopublisher/tags/0.1.4/src/z3c/bobopublisher/interfaces.py
@@ -0,0 +1,97 @@
+##############################################################################
+#
+# Copyright (c) 2009 Fabio Tranchitella <fabio@tranchitella.it>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+"""
+$Id$
+"""
+
+from z3c.request import interfaces
+
+from zope.interface import Attribute, Interface
+
+
+class IPublishTraverse(Interface):
+    """Interface for publish traverse adapters"""
+
+    def publishTraverse(request, name):
+        """Lookup a name
+
+        The 'request' argument is the publisher request object.  The
+        'name' argument is the name that is to be looked up; it must
+        be an ASCII string or Unicode object.
+
+        If a lookup is not possible, raise a KeyError error.
+        """
+
+
+class IDefaultViewName(Interface):
+    """Marker interface for the bobo:defaultView directive"""
+
+
+class IRequest(interfaces.IWebObRequest):
+    """Interface for webob.Request objects"""
+
+
+class IGETRequest(IRequest):
+    """Interface for webob.Request objects (GET method)"""
+
+
+class IPOSTRequest(IRequest):
+    """Interface for webob.Request objects (POST method)"""
+
+
+class IPUTRequest(IRequest):
+    """Interface for webob.Request objects (PUT method)"""
+
+
+class IDELETERequest(IRequest):
+    """Interface for webob.Request objects (DELETE method)"""
+
+
+class IBrowserView(Interface):
+    """Browser views are multi-adapters for context and request objects"""
+
+    context = Attribute("The context object the view renders")
+
+    request = Attribute("The request object driving the view")
+
+
+class IBrowserPage(IBrowserView):
+    """Browser pages are callable browser views"""
+
+    def __call__():
+        """Return the result of the page"""
+
+
+class IAbsoluteURL(Interface):
+    """Absolute URL"""
+
+    def __unicode__():
+        """Returns the URL as a unicode string."""
+
+    def __str__():
+        """Returns an ASCII string with all unicode characters url quoted."""
+
+    def __repr__():
+        """Get a string representation"""
+
+    def __call__():
+        """Returns an ASCII string with all unicode characters url quoted."""
+
+    def breadcrumbs():
+        """Returns a tuple like ({'name':name, 'url':url}, ...)
+
+        Name is the name to display for that segment of the breadcrumbs.
+        URL is the link for that segment of the breadcrumbs.
+        """
