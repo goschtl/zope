503681e00437eb7aad26185d17d1392592b5d066
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2009-09-01 19:25:44 +0000

    initial
    git-svn-id: svn://svn.zope.org/repos/main@103470 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Sandbox/J1m/zeologhack/src/zc/zeologhack/__init__.py
@@ -0,0 +1,48 @@
+
+import marshal
+import os
+import time
+import ZEO.zrpc.connection
+
+
+base = '/tmp'
+
+path = None
+
+def log(connection, action=None):
+    """Enable, disable, or get the path of the zeo input log
+    """
+    global path
+
+    if action is None:
+        connection.write("%r\n" % path)
+        return
+
+    if action == 'disable':
+        if path is None:
+            connection.write("Already disabled\n")
+        else:
+            connection.write("disabled %r\n" % path)
+            del ZEO.zrpc.connection.ManagedServerConnection.message_input
+            path = None
+        return
+
+    if action != 'enable':
+        connection.write("Unknown action: %r\n" % action)
+
+    if path:
+        log(connection, 'disable')
+
+    path = os.path.join(base, time.strftime("%y%m%d%H%M%S", time.gmtime()))
+
+    log_file = open(path, 'w')
+    base_message_input = ZEO.zrpc.connection.Connection.message_input
+    dump = marshal.dump
+    timetime = time.time
+
+    def message_input(self, message):
+        dump((id(self), timetime(), message), log_file)
+        base_message_input(self, message)
+
+    ZEO.zrpc.connection.ManagedServerConnection.message_input = message_input
+    connection.write("enabled %r\n" % path)
