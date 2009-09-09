5e6da11cfdc3d8bb3bb75d1bfcd89c1d4131a05a
naro <naro@62d5b8a3-27da-0310-9561-8e5933582275>
2009-09-09 17:24:02 +0000

    fix weird site problem  in Zope2  initialize code....hm, right, all zope2 tweaks here are weird...
    git-svn-id: svn://svn.zope.org/repos/main@103694 62d5b8a3-27da-0310-9561-8e5933582275

--- lovely.remotetask/branches/port-for-zope210/src/lovely/remotetask/__init__.py
+++ lovely.remotetask/branches/port-for-zope210/src/lovely/remotetask/__init__.py
@@ -11,21 +11,23 @@ except ImportError:
 if ZOPE2:
     from lovely.remotetask.service import TaskService, getAutostartServiceNames
     from lovely.remotetask.interfaces import ITaskService
-    from zope.component import getUtilitiesFor
-    from zope.app.component.hooks import getSite
+    from zope.component import ComponentLookupError
+    from zope.app.component.hooks import getSite, setSite
     from Products.CMFCore.interfaces._content import ISiteRoot
 
     def initialize(context):
         # dirty trick, but it works
         app = context._ProductContext__app
         services = getAutostartServiceNames()
-
+        old_site = getSite()
         for service in services:
             site_name, service_name = service.split('@')
             if site_name:
                 site = getattr(app, site_name, None)
                 if site:
                     registry = site.getSiteManager()
-                    service = registry.queryUtility(ITaskService, name=service_name)
+                    setSite(site)  # blah, five/localsitemanager/registry.py ver. 1.1, line 108, in _wrap can't find site.
+                    service = registry.getUtility(ITaskService, name=service_name)
                     if ITaskService.providedBy(service) and not service.isProcessing():
                         service.startProcessing()
+        setSite(old_site)
