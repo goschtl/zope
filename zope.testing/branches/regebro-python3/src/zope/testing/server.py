2720a9a111317fcc520e00fc0b66e78b87023809
wosc <wosc@62d5b8a3-27da-0310-9561-8e5933582275>
2009-09-13 08:39:36 +0000

    py3: urllib2.urlparse-->urllib.parse
    git-svn-id: svn://svn.zope.org/repos/main@103887 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.testing/branches/regebro-python3/src/zope/testing/server.py
+++ zope.testing/branches/regebro-python3/src/zope/testing/server.py
@@ -24,10 +24,13 @@ functional test.
 
 $Id$
 """
-import urllib2
+import sys
+if sys.version_info < (3,):
+    import urllib2.urlparse as urlparse
+else:
+    import urllib.parse as urlparse
 import webbrowser
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-import sys
 
 
 def makeRequestHandler(http, user=None, password=None):
@@ -80,10 +83,10 @@ def addPortToURL(url, port):
         'http://localhost:555/index.html'
 
     """
-    (scheme, netloc, url, query, fragment) = urllib2.urlparse.urlsplit(url)
+    (scheme, netloc, url, query, fragment) = urlparse.urlsplit(url)
     netloc = netloc.split(':')[0]
     netloc = "%s:%s" % (netloc, port)
-    url = urllib2.urlparse.urlunsplit((scheme, netloc, url, query, fragment))
+    url = urlparse.urlunsplit((scheme, netloc, url, query, fragment))
     return url
 
 
