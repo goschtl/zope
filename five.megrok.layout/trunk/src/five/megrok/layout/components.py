a62d855ebc729e6a76e5d44ba2042050f785eae3
thefunny42 <thefunny42@62d5b8a3-27da-0310-9561-8e5933582275>
2009-09-14 16:17:48 +0000

    Make a Zope2 compliant version of megrok.layout.
    git-svn-id: svn://svn.zope.org/repos/main@103962 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ five.megrok.layout/trunk/src/five/megrok/layout/components.py
@@ -0,0 +1,48 @@
+#############################################################################
+#
+# Copyright (c) 2009 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import megrok.layout
+
+import Acquisition
+
+
+class Layout(megrok.layout.Layout, Acquisition.Explicit):
+
+    def __init__(self, *args):
+        super(View, self).__init__(*args)
+        if not (self.static is None):
+            # static should be wrapper correctly with acquisition,
+            # otherwise you will not be able to compute URL for
+            # resources.
+            self.static = self.static.__of__(self)
+
+    # We let getPhysicalPath to be acquired. This make static URL's
+    # work, and prevent us to inherit from Acquisition.Implicit
+    getPhysicalPath = Acquisition.Acquired
+
+
+class Page(megrok.layout.Layout, Acquisition.Explicit):
+
+
+    def __init__(self, *args):
+        super(View, self).__init__(*args)
+        if not (self.static is None):
+            # static should be wrapper correctly with acquisition,
+            # otherwise you will not be able to compute URL for
+            # resources.
+            self.static = self.static.__of__(self)
+
+    # We let getPhysicalPath to be acquired. This make static URL's
+    # work, and prevent us to inherit from Acquisition.Implicit
+    getPhysicalPath = Acquisition.Acquired
