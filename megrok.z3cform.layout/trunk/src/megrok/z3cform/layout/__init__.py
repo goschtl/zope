b0587f0c838b2457af9db1a49146c6efb1c27af3
trollfot <trollfot@62d5b8a3-27da-0310-9561-8e5933582275>
2009-09-15 08:03:46 +0000

    Removed cancel button handlers and directives from megrok.z3cform.layout and moved it to megrok.z3cform.base
    git-svn-id: svn://svn.zope.org/repos/main@103991 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.z3cform.layout/trunk/src/megrok/z3cform/layout/__init__.py
+++ megrok.z3cform.layout/trunk/src/megrok/z3cform/layout/__init__.py
@@ -1,2 +1,1 @@
 from interfaces import *
-from directives import cancellable
--- megrok.z3cform.layout/trunk/src/megrok/z3cform/layout/directives.py
+++
@@ -1,6 +0,0 @@
-import martian
-
-class cancellable(martian.Directive):
-    scope = martian.CLASS
-    store = martian.ONCE
-    default = False
