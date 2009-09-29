e2ba69c2aa7eaac679522e261496063b84f8505c
trollfot <trollfot@62d5b8a3-27da-0310-9561-8e5933582275>
2009-09-29 18:30:29 +0000

    Corrected indentation error
    git-svn-id: svn://svn.zope.org/repos/main@104622 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.z3cform.base/trunk/src/megrok/z3cform/base/utils.py
+++ megrok.z3cform.base/trunk/src/megrok/z3cform/base/utils.py
@@ -13,5 +13,5 @@ def apply_data_event(form, context, data):
         descriptions = []
         for interface, names in changes.items():
             descriptions.append(Attributes(interface, *names))
-    notify(ObjectModifiedEvent(context, *descriptions))
+        notify(ObjectModifiedEvent(context, *descriptions))
     return changes
