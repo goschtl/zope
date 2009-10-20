97d462e7c09362d136fffaa35bfc4e968743086a
trollfot <trollfot@62d5b8a3-27da-0310-9561-8e5933582275>
2009-10-20 18:27:08 +0000

    Added README.txt doctest. Preparing release.
    git-svn-id: svn://svn.zope.org/repos/main@105175 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.z3cform.base/trunk/src/megrok/z3cform/base/tests/__init__.py
+++ megrok.z3cform.base/trunk/src/megrok/z3cform/base/tests/__init__.py
@@ -1,11 +1,6 @@
 import os.path                                                                  
-import megrok.z3cform.base
 from zope.app.testing.functional import ZCMLLayer
 
-ftesting_zcml = os.path.join(
-    os.path.dirname(__file__),
-    'ftesting.zcml'
-    )
-FunctionalLayer = ZCMLLayer(ftesting_zcml, __name__, 'FunctionalLayer',
-                            allow_teardown=True)
-
+ftesting_zcml = os.path.join(os.path.dirname(__file__), 'ftesting.zcml')
+FunctionalLayer = ZCMLLayer(
+    ftesting_zcml, __name__, 'FunctionalLayer', allow_teardown=True)
