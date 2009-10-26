de388cd020c583d42b2d98227af5d0bbba703e18
cklinger <cklinger@62d5b8a3-27da-0310-9561-8e5933582275>
2009-10-26 14:25:04 +0000

    Back to development: 0.2
    git-svn-id: svn://svn.zope.org/repos/main@105288 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.z3cform.ui/trunk/setup.py
+++ megrok.z3cform.ui/trunk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = '0.2dev'
 setup(name='megrok.z3cform.ui',
       version=version,
       description="Installation Helper for z3c.formui",
