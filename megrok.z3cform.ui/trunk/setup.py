40e8595eea94b11446effba5dbd2b2e0f8160ccf
cklinger <cklinger@62d5b8a3-27da-0310-9561-8e5933582275>
2009-10-26 14:09:27 +0000

    Copy from Sandbox
    git-svn-id: svn://svn.zope.org/repos/main@105283 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ megrok.z3cform.ui/trunk/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+setup(name='megrok.z3cform.ui',
+      version=version,
+      description="Installation Helper for z3c.formui",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='grok z3c.form megrok.z3cform.ui',
+      author='Christian Klinger',
+      author_email='cklinger@novareto.de',
+      url='',
+      license='GPL',
+      package_dir={'':'src'},
+      packages=find_packages('src', exclude=['ez_setup']),
+      namespace_packages=['megrok', 'megrok.z3cform'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'grok',
+          'z3c.formui',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
