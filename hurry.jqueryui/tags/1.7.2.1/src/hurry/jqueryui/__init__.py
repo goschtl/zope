2e2496745b27852b2db001a69a01c1e61d33e9fe
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2009-11-08 10:52:48 +0000

    Tagging a bit too soon.
    git-svn-id: svn://svn.zope.org/repos/main@105520 62d5b8a3-27da-0310-9561-8e5933582275

--- hurry.jqueryui/tags/1.7.2.1/src/hurry/jqueryui/__init__.py
+++
@@ -1,9 +0,0 @@
-from hurry.resource import Library, ResourceInclusion
-from hurry.jquery import jquery
-from hurry.jqueryui._themes import *
-
-jqueryui = Library('jqueryui')
-
-jqueryui = ResourceInclusion(jqueryui, 'jquery-ui.js', depends=[jquery],
-                             minified='jquery-ui.min.js')
-
--- hurry.jqueryui/tags/1.7.2.1/src/hurry/jqueryui/tests.py
+++
@@ -1,9 +0,0 @@
-import unittest, doctest
-
-def test_suite():
-    globs = {}
-    optionflags = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
-    suite = unittest.TestSuite()
-    suite.addTest(doctest.DocFileSuite(
-        'jqueryui.txt', globs=globs, optionflags=optionflags))
-    return suite
