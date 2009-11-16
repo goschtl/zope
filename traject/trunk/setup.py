85a9951fac7b8d91920a976493476b90f3b65a41
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2009-11-16 11:44:53 +0000

    Release preparations.
    git-svn-id: svn://svn.zope.org/repos/main@105698 62d5b8a3-27da-0310-9561-8e5933582275

--- traject/trunk/setup.py
+++ traject/trunk/setup.py
@@ -1,20 +1,26 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.1dev'
-
 def read(*filenames):
     return open(os.path.join(os.path.dirname(__file__), *filenames)).read()
 
+long_description = (
+    read('src', 'traject', 'traject.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Download\n'
+    '********\n'
+    )
+
 setup(name='traject',
-      version=version,
+      version='0.9dev',
       description="A URL dispatch to object system that combines aspects of routing and traversal.",
-      # long_description=read('README.txt'),
+      long_description=long_description,
       # Use classifiers that are already listed at:
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=['Development Status :: 5 - Production/Stable',
+      classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
-                   'Framework :: Zope3',
                    'Intended Audience :: Developers',
                    'License :: OSI Approved :: Zope Public License',
                    'Operating System :: OS Independent',
