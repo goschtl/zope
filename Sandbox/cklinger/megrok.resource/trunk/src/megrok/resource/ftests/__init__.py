f28d5c1c73b09f2f99f950d93d68b1f3fb5aedd4
trollfot <trollfot@62d5b8a3-27da-0310-9561-8e5933582275>
2009-12-19 18:28:56 +0000

    Moving all tests to README.txt, to avoid repeating ourselves. It's not like we have much to test...
    git-svn-id: svn://svn.zope.org/repos/main@106785 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/cklinger/megrok.resource/trunk/src/megrok/resource/ftests/__init__.py
+++ Sandbox/cklinger/megrok.resource/trunk/src/megrok/resource/ftests/__init__.py
@@ -1,9 +1,9 @@
+# -*- coding: utf-8 -*-
+
 import os.path
 import megrok.resource
-
 from zope.app.testing.functional import ZCMLLayer
-ftesting_zcml = os.path.join(os.path.dirname(megrok.resource.__file__),
-                             'ftesting.zcml')
-FunctionalLayer = ZCMLLayer(ftesting_zcml, __name__, 'FunctionalLayer',
-                            allow_teardown=True)
 
+ftesting_zcml = os.path.join(os.path.dirname(__file__), 'ftesting.zcml')
+FunctionalLayer = ZCMLLayer(
+    ftesting_zcml, __name__, 'FunctionalLayer', allow_teardown=True)
