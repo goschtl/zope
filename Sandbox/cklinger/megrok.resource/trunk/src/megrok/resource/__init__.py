fd6d9394cdb4a26d3f4e0d9918bcb6926c82694a
trollfot <trollfot@62d5b8a3-27da-0310-9561-8e5933582275>
2009-12-20 18:17:52 +0000

    Added docs folder and some nicer imports.
    git-svn-id: svn://svn.zope.org/repos/main@106812 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/cklinger/megrok.resource/trunk/src/megrok/resource/__init__.py
+++ Sandbox/cklinger/megrok.resource/trunk/src/megrok/resource/__init__.py
@@ -1,6 +1,7 @@
 from grokcore.view import path
 from grokcore.component import name
-
-from directive import include, use_hash
-from components import IResourcesIncluder, ILibrary, Library
 from hurry.resource import ResourceInclusion, GroupInclusion
+
+from megrok.resource.directive import include, use_hash
+from megrok.resource.components import IResourcesIncluder, ILibrary, Library
+from megrok.resource.utils import component_includes
