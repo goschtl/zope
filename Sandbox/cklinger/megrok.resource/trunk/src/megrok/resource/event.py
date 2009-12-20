7f87113c1455348ed93ad8cbdb3c34ccde0c014b
trollfot <trollfot@62d5b8a3-27da-0310-9561-8e5933582275>
2009-12-20 01:08:00 +0000

    Removed unused import
    git-svn-id: svn://svn.zope.org/repos/main@106789 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/cklinger/megrok.resource/trunk/src/megrok/resource/event.py
+++ Sandbox/cklinger/megrok.resource/trunk/src/megrok/resource/event.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import grokcore.component as grok
-import grokcore.view as view
-
 from zope.app.publication.interfaces import IBeforeTraverseEvent
 from zope.security.proxy import removeSecurityProxy
 from megrok.resource import include, IResourcesIncluder
@@ -10,7 +8,6 @@ from megrok.resource import include, IResourcesIncluder
 
 @grok.subscribe(IResourcesIncluder, IBeforeTraverseEvent)
 def handle_inclusion(view, event):
-    with_bottom = False
     view = removeSecurityProxy(view)
     needs = include.bind().get(view)
     for resource in needs:
