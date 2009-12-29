43ae425d9a1510eb1c9b9baf0662a49bbcb95ed4
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2009-12-29 15:07:07 +0000

    added
    git-svn-id: svn://svn.zope.org/repos/main@107261 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zopyx.findtrashonpypi/cli.py
@@ -0,0 +1,32 @@
+from xmlrpclib import Server
+
+def main():
+
+    server = Server('http://pypi.python.org/pypi')
+    packages = server.list_packages()
+    num_packages = len(packages)
+    for i, package in enumerate(packages):
+        print 'Processing %r (%d/%d)' % (package, i+1, num_packages)
+
+        versions = server.package_releases(package)
+        versions.sort()
+        for version in versions:
+            print '  ', version
+            urls = server.release_urls(package, version)
+
+            # PyPI hosted packages
+            if urls:
+                for url in urls:
+                    print '    ', url['url']
+
+            # externally hosted packages
+            else:
+                metadata = server.release_data(package, version)
+                download_url = metadata['download_url']
+                if download_url == 'UNKNOWN':
+                    print 'CRAP: %s==%s - no release files, no valid download_url' % (package, version)
+
+
+
+if __name__ == '__main__':
+    main()
