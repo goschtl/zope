c5a23ab0058cb64a243eb5a4b0875cbd0ad40282
baijum <baijum@62d5b8a3-27da-0310-9561-8e5933582275>
2010-01-02 14:13:30 +0000

    update version
    git-svn-id: svn://svn.zope.org/repos/main@107553 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/baijum/bluebream/tags/0.1.1/setup.py
+++ Sandbox/baijum/bluebream/tags/0.1.1/setup.py
@@ -20,7 +20,7 @@ def read(*rnames):
 
 setup(
     name="bluebream",
-    version="0.1.1dev",
+    version="0.1.1",
     author="Baiju M",
     author_email="baiju.m.mail@gmail.com",
     url="https://launchpad.net/bluebream",
