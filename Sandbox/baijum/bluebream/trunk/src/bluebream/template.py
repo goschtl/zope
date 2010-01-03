f3acd01c22b97e2ed295f2cc75f4a14c95c9ce47
baijum <baijum@62d5b8a3-27da-0310-9561-8e5933582275>
2010-01-03 08:38:30 +0000

    Fix LP #502529: Update wizard to ask all package meta to be updated in setup.py
    git-svn-id: svn://svn.zope.org/repos/main@107580 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/baijum/bluebream/trunk/src/bluebream/template.py
+++ Sandbox/baijum/bluebream/trunk/src/bluebream/template.py
@@ -20,5 +20,18 @@ class BlueBream(templates.Template):
 
     _template_dir = 'project_template'
     summary = "A Zope project"
-    vars = [var('namespace_package', '')]
+    vars = [
+        var('namespace_package', 'Namespace package name'),
+        var('version', 'Version (like 0.1)'),
+        var('description', 'One-line description of the package'),
+        var('long_description', 'Multi-line description (in reST)'),
+        var('keywords', 'Space-separated keywords/tags'),
+        var('author', 'Author name'),
+        var('author_email', 'Author email'),
+        var('url', 'URL of homepage'),
+        var('license_name', 'License name'),
+        var('zip_safe',
+            'True/False: if the package can be distributed as a .zip file',
+            default=False),
+        ]
 
