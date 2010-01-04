174b5c1e0c143ad153d4b642d389818c2f59bed2
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2010-01-04 23:07:44 +0000

    Preparing release 3.5.0
    git-svn-id: svn://svn.zope.org/repos/main@107683 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.app.boston/trunk/setup.py
+++ zope.app.boston/trunk/setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.boston',
-      version = '3.5.0dev',
+      version = '3.5.0',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Boston -- A Zope 3 ZMI Skin',
@@ -74,7 +74,7 @@ setup(name='zope.app.boston',
                           'zope.container',
                           'zope.i18nmessageid',
                           'zope.interface',
-                          'zope.publisher',
+                          'zope.publisher>=3.12',
                           'zope.viewlet',
                           ],
       include_package_data = True,
