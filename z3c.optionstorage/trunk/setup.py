f6a002e2482beb45da66e06914dd2c31e335d542
mgedmin <mgedmin@62d5b8a3-27da-0310-9561-8e5933582275>
2010-01-04 15:28:58 +0000

    Post-release version bump.
    git-svn-id: svn://svn.zope.org/repos/main@107634 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.optionstorage/trunk/setup.py
+++ z3c.optionstorage/trunk/setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='z3c.optionstorage',
-      version = '1.0.5',
+      version = '1.0.6dev',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Optional Storages -- Persistent, Managable Vocabularies',
