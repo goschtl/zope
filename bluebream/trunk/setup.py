356dc83edefe603b7837eb46d79d5a4708a6fa59
baijum <baijum@62d5b8a3-27da-0310-9561-8e5933582275>
2010-01-07 12:57:43 +0000

    post release version bump
    git-svn-id: svn://svn.zope.org/repos/main@107783 62d5b8a3-27da-0310-9561-8e5933582275

--- bluebream/trunk/setup.py
+++ bluebream/trunk/setup.py
@@ -20,7 +20,7 @@ def read(*rnames):
 
 setup(
     name="bluebream",
-    version="0.1.6dev",
+    version="0.1.7dev",
     author="BlueBream Team",
     author_email="zope-dev@zope.org",
     url="http://packages.python.org/bluebream",
