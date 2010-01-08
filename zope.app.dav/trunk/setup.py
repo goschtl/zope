7a2780811849eadc68b9369c0d733949d23db67d
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2010-01-08 21:43:22 +0000

    Preparing release 3.5.2
    git-svn-id: svn://svn.zope.org/repos/main@107836 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.app.dav/trunk/setup.py
+++ zope.app.dav/trunk/setup.py
@@ -28,7 +28,7 @@ def read(*rnames):
 
 
 setup(name='zope.app.dav',
-      version = '3.5.2dev',
+      version = '3.5.2',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope WebDAV Support (Basic)',
