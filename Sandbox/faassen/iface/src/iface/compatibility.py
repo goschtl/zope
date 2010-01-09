983ac0414f3103f05cd1fac5de066d51046aefa0
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2010-01-09 16:40:23 +0000

    Set up some tests to explore compatibility with zope.interface, in particular the AdapterRegistry.
    Right now we don't test very much yet.
    
    
    git-svn-id: svn://svn.zope.org/repos/main@107942 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Sandbox/faassen/iface/src/iface/compatibility.py
@@ -0,0 +1,20 @@
+from iface import MapKey, MultiMap
+
+class ZopeInterfaceMapKey(MapKey):
+    def __init__(self, interface):
+        super(ZopeInterfaceMapKey, self).__init__(
+            interface, map(ZopeInterfaceMapKey, interface.__bases__))
+
+class CompatibilityAdapterRegistry(object):
+    def __init__(self):
+        self._map = MultiMap()
+        
+    def register(self, required, provided, name, value):
+        required = map(ZopeInterfaceMapKey, required)
+        self._map[required] = value
+
+    def lookup(self, required, provided, name='', default=None):
+        required = map(ZopeInterfaceMapKey, required)
+        return self._map[required]
+    
+    
--- Sandbox/faassen/iface/src/iface/tests.py
+++ Sandbox/faassen/iface/src/iface/tests.py
@@ -1,12 +1,26 @@
 import unittest, doctest
 
+from zope.interface.adapter import AdapterRegistry
+from iface.compatibility import CompatibilityAdapterRegistry
+
 def test_suite():
     suite = unittest.TestSuite()
 
     optionflags = doctest.NORMALIZE_WHITESPACE + doctest.ELLIPSIS
 
     suite.addTests([
-            doctest.DocFileSuite('mapping.txt',
-                                 optionflags=optionflags)])
+            doctest.DocFileSuite(
+                'mapping.txt',
+                optionflags=optionflags),
+            doctest.DocFileSuite(
+                'compatibility.txt',
+                optionflags=optionflags,
+                globs=dict(AdapterRegistry=AdapterRegistry)),
+            doctest.DocFileSuite(
+                'compatibility.txt',
+                optionflags=optionflags,
+                globs=dict(AdapterRegistry=CompatibilityAdapterRegistry)),            
+            ])
+            
     return suite
 
