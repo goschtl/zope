d5a323add5fdc4810781ada6775dffb7b51761fa
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2010-01-10 15:22:23 +0000

    Tagging 3.7.1
    git-svn-id: svn://svn.zope.org/repos/main@107991 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.app.intid/tags/3.7.1/src/zope/app/intid/interfaces.py
@@ -0,0 +1,18 @@
+"""
+Interfaces for the unique id utility.
+
+$Id$
+"""
+
+# BBB
+from zope.intid.interfaces import (
+    IIntIdsQuery,
+    IIntIdsSet,
+    IIntIdsManage,
+    IIntIds,
+    IIntIdEvent,
+    IIntIdRemovedEvent,
+    IntIdRemovedEvent,
+    IIntIdAddedEvent,
+    IntIdAddedEvent
+)
