f197d71f09631536c627a135af9cf878b63ed6ec
witsch <witsch@62d5b8a3-27da-0310-9561-8e5933582275>
2010-01-12 16:27:39 +0000

    make sure the `ZPublisher.Publish.get_module_info` monkey from `plone.app.linkintegrity` gets applied first, so that our monkey will use the right exception hook, which in turn keeps plone's linkintegrity feature working...
    git-svn-id: svn://svn.zope.org/repos/main@108071 62d5b8a3-27da-0310-9561-8e5933582275

--- ZPublisherEventsBackport/trunk/src/ZPublisherEventsBackport/__init__.py
+++ ZPublisherEventsBackport/trunk/src/ZPublisherEventsBackport/__init__.py
@@ -13,7 +13,17 @@ except ImportError:
             iface.__module__ = 'ZPublisher.interfaces'
             iface.__identifier__ = "%s.%s" % (iface.__module__, iface.__name__)
             iface.changed(None)
-    
+
+    # the monkey patch for `ZPublisher.Publish.get_module_info` from
+    # `plone.app.linkintegrity` needs to be applied first in order for
+    # our patch to find the right exception hook, which in turn keeps
+    # plone's linkintegrity feature working...
+    try:
+        from plone.app.linkintegrity import monkey
+        monkey.installExceptionHook()
+    except ImportError:
+        pass
+
     from ZPublisherEventsBackport import patch
     ZPublisher.Publish.publish=patch.publish
     logging.info("Monkeypatch ZPublisher publish with publication events")
