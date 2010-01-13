f2c4fc29a088d4981c81d998a2c24643c3412847
baijum <baijum@62d5b8a3-27da-0310-9561-8e5933582275>
2010-01-13 13:35:47 +0000

    remove "dev" marker
    git-svn-id: svn://svn.zope.org/repos/main@108110 62d5b8a3-27da-0310-9561-8e5933582275

--- bluebream/tags/0.1.9/setup.py
+++ bluebream/tags/0.1.9/setup.py
@@ -20,7 +20,7 @@ def read(*rnames):
 
 setup(
     name="bluebream",
-    version="0.1.9dev",
+    version="0.1.9",
     author="BlueBream Team",
     author_email="zope-dev@zope.org",
     url="http://packages.python.org/bluebream",
