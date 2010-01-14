e0c35bcdad5296dc3a138275f0abd553d1d3142c
baijum <baijum@62d5b8a3-27da-0310-9561-8e5933582275>
2010-01-14 16:50:47 +0000

    2010 would be fine ?
    git-svn-id: svn://svn.zope.org/repos/main@108123 62d5b8a3-27da-0310-9561-8e5933582275

--- bluebream/trunk/docs/source/conf.py
+++ bluebream/trunk/docs/source/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'BlueBream'
-copyright = u'2003-2010, Zope Foundation'
+copyright = u'2010, Zope Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
