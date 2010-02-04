19e80ebd718646631c10685ba03dedaaad4b0ac3
gotcha <gotcha@62d5b8a3-27da-0310-9561-8e5933582275>
2010-02-04 15:17:28 +0000

    imports cleanup
    git-svn-id: svn://svn.zope.org/repos/main@108771 62d5b8a3-27da-0310-9561-8e5933582275

--- lovely.remotetask/branches/port-for-zope210/src/lovely/remotetask/tests.py
+++ lovely.remotetask/branches/port-for-zope210/src/lovely/remotetask/tests.py
@@ -17,13 +17,11 @@
 __docformat__ = "reStructuredText"
 
 from lovely.remotetask import service
-from zope.app.testing import placelesssetup
 from zope.app.testing.setup import (placefulSetUp, placefulTearDown)
 from zope.testing.doctest import INTERPRET_FOOTNOTES
 from zope.testing.doctestunit import DocFileSuite
 from zope.testing.loggingsupport import InstalledHandler
 import doctest
-import logging
 import random
 import unittest
 
