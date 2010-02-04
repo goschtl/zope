2bf236e48d8be69dac73e594999e37e0b4ce7dc6
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2010-02-04 18:43:33 +0000

    Refactoring a bit.
    git-svn-id: svn://svn.zope.org/repos/main@108776 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.schema/branches/faassen-transformer/src/zope/schema/_transformer.py
+++ zope.schema/branches/faassen-transformer/src/zope/schema/_transformer.py
@@ -13,10 +13,7 @@ class transformer(object):
                                            order=order)
             order += 1
             
-        return transformer(InterfaceClass(name=self.schema.__name__,
-                                          bases=self.schema.__bases__,
-                                          __module__=self.schema.__module__,
-                                          attrs=attrs))
+        return self._transformer(attrs)
 
     def omit(self, names):
         attrs = {}
@@ -26,11 +23,8 @@ class transformer(object):
                 attrs[name] = self._copy_field(self.schema[name],
                                                order=order)
             order += 1
-            
-        return transformer(InterfaceClass(name=self.schema.__name__,
-                                          bases=self.schema.__bases__,
-                                          __module__=self.schema.__module__,
-                                          attrs=attrs))
+
+        return self._transformer(attrs)
 
     def override(self, name, **kw):
         attrs = {}
@@ -40,13 +34,16 @@ class transformer(object):
                                            order=order,
                                            **kw)
             order += 1
+        return self._transformer(attrs)
     
+ 
+
+    def _transformer(self, attrs):
         return transformer(InterfaceClass(name=self.schema.__name__,
                                           bases=self.schema.__bases__,
                                           __module__=self.schema.__module__,
                                           attrs=attrs))
-
-        
+    
     def _copy_field(self, field, **kw):
         # by passing None as context, we can trick the binding
         # machinery into making a straight copy
