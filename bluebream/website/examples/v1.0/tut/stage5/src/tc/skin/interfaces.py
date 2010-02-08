e7fe6bbecc9434c5ee50cc6cb0402a303d468373
baijum <baijum@62d5b8a3-27da-0310-9561-8e5933582275>
2010-02-08 02:03:07 +0000

    new stage
    git-svn-id: svn://svn.zope.org/repos/main@108864 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ bluebream/website/examples/v1.0/tut/stage5/src/tc/skin/interfaces.py
@@ -0,0 +1,5 @@
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+
+class IBlueWaves(IDefaultBrowserLayer):
+    """BlueWaves skin that contains the default layer and nothing
+    else"""
