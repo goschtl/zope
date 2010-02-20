acb89a8be74e102b5968259a82ed1c5fac918003
baijum <baijum@62d5b8a3-27da-0310-9561-8e5933582275>
2010-02-20 14:57:55 +0000

    - Use a template to display default page for the root   folder
    - Use ``zope.formlib.form.DisplayForm`` as base class for
      views instead of ``zope.publisher.browser.BrowserView``
    
    - Sample application add form view name is changed from
      ``@@add`` to ``@@add_sample_app``
    
    - Add links to to BlueBream website & mailing list
    
    
    git-svn-id: svn://svn.zope.org/repos/main@109179 62d5b8a3-27da-0310-9561-8e5933582275

--- bluebream/trunk/src/bluebream/project_template/src/+namespace_package+/+main_package+/views.py
+++ bluebream/trunk/src/bluebream/project_template/src/+namespace_package+/+main_package+/views.py
@@ -1,9 +1,15 @@
-from zope.publisher.browser import BrowserView
+from zope.browserpage import ViewPageTemplateFile
 from zope.container.interfaces import INameChooser
 from zope.formlib import form
 
 from interfaces import ISampleApplication
 from app import SampleApplication
+
+
+class RootDefaultView(form.DisplayForm):
+
+    __call__ = ViewPageTemplateFile('index.pt')
+
 
 class AddSampleApplication(form.AddForm):
 
@@ -19,29 +25,8 @@ class AddSampleApplication(form.AddForm):
         self.request.response.redirect(name)
 
 
-class SampleApplicationDefaultView(BrowserView):
+class SampleApplicationDefaultView(form.DisplayForm):
 
     def __call__(self):
         return """Welcome to the Sample application"""
 
-
-class RootDefaultView(BrowserView):
-
-    def __call__(self):
-        return """\
-<html><head><title>Welcome to BlueBream!</title></head><body>
-<h1>Welcome to BlueBream!</h1>
-<ul>
-<li><a href="http://pypi.python.org/pypi/bluebream">PyPI page</a></li>
-<li><a href="http://packages.python.org/bluebream">Documentation</a></li>
-<li><a href="https://launchpad.net/bluebream">Issue Tracker</a></li>
-<li><a href="http://wiki.zope.org/bluebream">Wiki</a></li>
-<li><a href="http://twitter.com/bluebream">Twitter</a></li>
-<li><a href="https://mail.zope.org/mailman/listinfo/zope3-users">Mailing list</a></li>
-<li><a href="http://webchat.freenode.net/?randomnick=1&channels=bluebream">IRC Channel: #bluebream at irc.freenode.net</a></li>
-</ul>
-<a href="@@login.html">Login</a>
-<br/>
-<a href="@@add">Add Sample application</a>
-</body></html>
-"""
