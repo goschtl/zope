e4c01cf37f643c070bd1ac9454856e9e6b761b69
menesis <menesis@62d5b8a3-27da-0310-9561-8e5933582275>
2010-02-20 13:09:17 +0000

    Release 1.0.6
    git-svn-id: svn://svn.zope.org/repos/main@109177 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.optionstorage/trunk/setup.py
+++ z3c.optionstorage/trunk/setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='z3c.optionstorage',
-      version = '1.0.6dev',
+      version = '1.0.6',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Optional Storages -- Persistent, Managable Vocabularies',
