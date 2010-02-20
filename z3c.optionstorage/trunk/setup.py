39434d4ea07d1fc9ad784d02e6f92b5579cc18c8
menesis <menesis@62d5b8a3-27da-0310-9561-8e5933582275>
2010-02-20 13:03:03 +0000

    Update author email and homepage. Remove DEPENDENCIES.cfg
    git-svn-id: svn://svn.zope.org/repos/main@109176 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.optionstorage/trunk/setup.py
+++ z3c.optionstorage/trunk/setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 setup(name='z3c.optionstorage',
       version = '1.0.6dev',
       author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
+      author_email='zope-dev@zope.org',
       description='Optional Storages -- Persistent, Managable Vocabularies',
       long_description=(
           read('README.txt')
@@ -42,7 +42,7 @@ setup(name='z3c.optionstorage',
           'Operating System :: OS Independent',
           'Topic :: Internet :: WWW/HTTP',
           'Framework :: Zope3'],
-      url='http://cheeseshop.python.org/pypi/z3c.optionstorage',
+      url='http://pypi.python.org/pypi/z3c.optionstorage',
       license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
