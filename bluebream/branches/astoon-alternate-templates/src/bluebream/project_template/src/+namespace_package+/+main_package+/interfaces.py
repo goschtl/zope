a605a0bca910192d2e51be0caa6f422c5b42d3a5
astoon <astoon@62d5b8a3-27da-0310-9561-8e5933582275>
2010-02-24 14:24:08 +0000

    branch to create alternate templates
    git-svn-id: svn://svn.zope.org/repos/main@109408 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ bluebream/branches/astoon-alternate-templates/src/bluebream/project_template/src/+namespace_package+/+main_package+/interfaces.py
@@ -0,0 +1,19 @@
+from zope.container.interfaces import IContainer
+from zope.schema import TextLine
+from zope.schema import Text
+
+
+class ISampleApplication(IContainer):
+    """The main application container"""
+
+    name = TextLine(
+        title=u"Name",
+        description=u"Name of application",
+        default=u"",
+        required=True)
+
+    description = Text(
+        title=u"Description",
+        description=u"Description about the application",
+        default=u"",
+        required=False)
