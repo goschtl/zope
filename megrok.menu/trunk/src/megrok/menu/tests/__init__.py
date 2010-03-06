b649e6af58bffd8272c11f9407b43ee1822697d0
trollfot <trollfot@62d5b8a3-27da-0310-9561-8e5933582275>
2010-03-06 18:13:02 +0000

    Cleaning process : dependencies and tests have been cleared from all the unused packages AND updated to the most recent ZTK use.
    git-svn-id: svn://svn.zope.org/repos/main@109713 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.menu/trunk/src/megrok/menu/tests/__init__.py
+++ megrok.menu/trunk/src/megrok/menu/tests/__init__.py
@@ -1,8 +1,31 @@
-import os.path
-import megrok.menu
-from zope.app.testing.functional import ZCMLLayer
 
-ftesting_zcml = os.path.join(os.path.dirname(megrok.menu.__file__),
-                             'ftesting.zcml')
-FunctionalLayer = ZCMLLayer(ftesting_zcml, __name__, 'FunctionalLayer',
-                            allow_teardown=True)
+# -*- coding: utf-8 -*-
+
+import zope.component
+from zope.component.interfaces import IComponentLookup
+from zope.component.testlayer import ZCMLFileLayer
+from zope.interface import Interface
+from zope.site.folder import rootFolder
+from zope.site.site import LocalSiteManager, SiteManagerAdapter
+
+
+class MegrokMenuLayer(ZCMLFileLayer):
+
+    def setUp(self):
+        ZCMLFileLayer.setUp(self)
+
+        # Set up site manager adapter
+        zope.component.provideAdapter(
+            SiteManagerAdapter, (Interface,), IComponentLookup)
+
+        # Set up site
+        site = rootFolder()
+        site.setSiteManager(LocalSiteManager(site))
+        zope.component.hooks.setSite(site)
+
+        return site
+
+    def tearDown(self):
+        ZCMLFileLayer.tearDown(self)
+        zope.component.hooks.resetHooks()
+        zope.component.hooks.setSite()
