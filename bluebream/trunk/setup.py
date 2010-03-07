857878d9f0c593d64f93ce89efe3944bc031b6aa
baijum <baijum@62d5b8a3-27da-0310-9561-8e5933582275>
2010-03-07 06:01:39 +0000

    one more alpha will be there
    git-svn-id: svn://svn.zope.org/repos/main@109760 62d5b8a3-27da-0310-9561-8e5933582275

--- bluebream/trunk/setup.py
+++ bluebream/trunk/setup.py
@@ -20,7 +20,7 @@ def read(*rnames):
 
 setup(
     name="bluebream",
-    version='1.0b1dev',
+    version='1.0a3dev',
     author="BlueBream Team",
     author_email="zope-dev@zope.org",
     url="http://bluebream.zope.org",
