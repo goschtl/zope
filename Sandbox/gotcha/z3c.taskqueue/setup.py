8482fea94e48ce6a022b8b84aea25438ef695ee1
gotcha <gotcha@62d5b8a3-27da-0310-9561-8e5933582275>
2010-03-08 09:15:30 +0000

    import skeleton
    git-svn-id: svn://svn.zope.org/repos/main@109796 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ Sandbox/gotcha/z3c.taskqueue/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='z3c.taskqueue',
+      version=version,
+      description="Task queue service",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Godefroid Chapelle and the Zope community',
+      author_email='zope-dev@zope.org',
+      url='',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['z3c'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
