332b3a902b41479814e22575ada20b889f06ab10
ctheune <ctheune@62d5b8a3-27da-0310-9561-8e5933582275>
2010-03-16 14:36:48 +0000

    Fix copyright and license.
    git-svn-id: svn://svn.zope.org/repos/main@109988 62d5b8a3-27da-0310-9561-8e5933582275

--- gocept.bsquare/trunk/setup.py
+++ gocept.bsquare/trunk/setup.py
@@ -1,20 +1,20 @@
 import os
 from setuptools import setup, find_packages
 
+
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(
     name='gocept.bsquare',
-    version = '0.1.0-dev',
-    author='GoCept',
+    version='0.1.0-dev',
+    author='Zope Foundation and Contributors',
     packages=find_packages('src'),
-    package_dir = {'': 'src'},
-    license = "ZPL",
+    package_dir={'': 'src'},
+    license="ZPL 2.1",
     install_requires=[
         'setuptools',
         'buildbot',
         ],
-    include_package_data = True,
-    zip_safe = False,
-    )
+    include_package_data=True,
+    zip_safe=False)
