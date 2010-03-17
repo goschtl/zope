62ce8882567973ab042c31c65df132879b6f4743
tseaver <tseaver@62d5b8a3-27da-0310-9561-8e5933582275>
2010-03-17 12:45:55 +0000

    Add ZPL.
    git-svn-id: svn://svn.zope.org/repos/main@110018 62d5b8a3-27da-0310-9561-8e5933582275

--- stxutils/trunk/setup.py
+++ stxutils/trunk/setup.py
@@ -1,3 +1,16 @@
+##############################################################################
+#
+# Copyright (c) 2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 from setuptools import setup
 
 
@@ -7,6 +20,7 @@ setup(name='stxutils',
       long_description=(open('README.txt').read() + '\n\n' +
                         open('CHANGES.txt').read()
                        ),
+      license='ZPL 2.1',
       packages=['stxutils'],
       include_package_data=True,
       zip_safe=False,
