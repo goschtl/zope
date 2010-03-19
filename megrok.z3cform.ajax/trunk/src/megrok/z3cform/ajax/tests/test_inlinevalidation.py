532be5ecac3c8e7fc8a165f2bdac40623fe3543a
cklinger <cklinger@62d5b8a3-27da-0310-9561-8e5933582275>
2010-03-19 07:47:02 +0000

    added more tests
    git-svn-id: svn://svn.zope.org/repos/main@110062 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.z3cform.ajax/trunk/src/megrok/z3cform/ajax/tests/test_inlinevalidation.py
+++ megrok.z3cform.ajax/trunk/src/megrok/z3cform/ajax/tests/test_inlinevalidation.py
@@ -11,12 +11,38 @@ Let's import and init the necessary work environment::
   >>> browser.headers['STATUS'].upper()
   '200 OK'
 
-  >>> browser.open('http://localhost/@@myform++validation++/field?fieldname=name')
-  >>> browser.contents
+   
+  >>> from zope.publisher.browser import TestRequest
+  >>> from zope.component import getMultiAdapter
+  >>> from zope.traversing.interfaces import ITraversable
+
+  >>> request = TestRequest() 
+  >>> egon = Mammoth()
+  >>> myform = getMultiAdapter((egon, request), name=u"myform")
+  >>> myform
+  <megrok.z3cform.ajax.tests.test_inlinevalidation.MyForm object at ...>
+
+  >>> validator = getMultiAdapter((myform, request), ITraversable, name=u"validate")
+  >>> validator
+  <megrok.z3cform.ajax.validation.InlineValidation object at ...>
+
+  >>> validator.update()
+  >>> validator.validate(['name',])
+  {'form-widgets-name': u'Required input is missing.'}
+
+
+  >>> jsonvalidator = getMultiAdapter((validator, request), name=u"field")
+  >>> jsonvalidator
+  <grok.meta.Validators object at ...>
+
+  >>> jsonvalidator.field('form.widgets.name')
+  {'form-widgets-name': u'Required input is missing.'}
+  
 """
 
 import grokcore.view as grok
 
+from grokcore.component import Context
 from megrok.z3cform.ajax import InlineValidation 
 from zope.interface import Interface
 from zope.schema import TextLine
@@ -25,6 +51,11 @@ from megrok.z3cform.base import Form, Fields
 
 class IMammoth(Interface):
     name = TextLine(title=u"Name")
+
+
+class Mammoth(Context):
+    pass
+
 
 class MyForm(Form):
     grok.context(Interface)
