454e1c5143056362e43f9fe255740252ff89799a
cklinger <cklinger@62d5b8a3-27da-0310-9561-8e5933582275>
2010-03-19 09:02:25 +0000

    started the jquery validation
    git-svn-id: svn://svn.zope.org/repos/main@110063 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.z3cform.ajax/trunk/src/megrok/z3cform/ajax/libs/validation.js
+++ megrok.z3cform.ajax/trunk/src/megrok/z3cform/ajax/libs/validation.js
@@ -1,1 +1,26 @@
-/* */
+$(document).ready(function() {
+    var form = $('form');
+    $('input').blur(function(){
+        value = $(this).val()
+        console.log('hhhh', value);
+      $.ajax({
+          type: "POST",
+          url: form.attr('action') + '/++validate++field',
+          data: value,
+          dataType: "json",
+          async: false,
+          success: function(data) {
+              if (data.success == true) {
+                  success = true;
+              }
+              else {
+                  success = false;
+                  errors = data.errors
+              }
+          }
+      });
+
+    })
+});
+
+
