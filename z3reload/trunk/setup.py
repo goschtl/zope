543372f5ab24881fd9c03aca9fddc7a0639d951c
tseaver <tseaver@62d5b8a3-27da-0310-9561-8e5933582275>
2010-03-26 14:18:21 +0000

    Eggify, buildoutify, and add ZPL.
    git-svn-id: svn://svn.zope.org/repos/main@110196 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ z3reload/trunk/setup.py
@@ -0,0 +1,44 @@
+##############################################################################
+#
+# Copyright (c) 2006--2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for z3reload package
+"""
+from setuptools import find_packages
+from setuptools import setup
+
+long_description = (open('README.txt').read() +
+                    '\n\n' +
+                    open('CHANGES.txt').read())
+
+
+setup(name='z3reload',
+      version='0.1dev',
+      license='ZPL 2.1',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      description="Enables automatic reloading of Zope3 view code.",
+      long_description=long_description,
+
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      tests_require=['zope.testing'],
+      install_requires=['setuptools',
+                        'zope.app.pagetemplate',
+                        'zope.app.publisher',
+                        'zope.component',
+                        'zope.configuration',
+                        'zope.publisher',
+                       ],
+      include_package_data=True,
+      zip_safe=False,
+      )
