a3dbf3af0548b036b84f7a4fac5180555fa0da2d
baijum <baijum@62d5b8a3-27da-0310-9561-8e5933582275>
2010-03-28 01:58:40 +0000

    Preparing release 1.0a5
    git-svn-id: svn://svn.zope.org/repos/main@110250 62d5b8a3-27da-0310-9561-8e5933582275

--- bluebream/trunk/setup.py
+++ bluebream/trunk/setup.py
@@ -20,7 +20,7 @@ def read(*rnames):
 
 setup(
     name="bluebream",
-    version = '1.0a5dev',
+    version = '1.0a5',
     author="BlueBream Team",
     author_email="zope-dev@zope.org",
     url="http://bluebream.zope.org",
