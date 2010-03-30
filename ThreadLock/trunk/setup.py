0622e027d908b3c8cf9cec04f0c27437c093cc2d
hannosch <hannosch@62d5b8a3-27da-0310-9561-8e5933582275>
2010-03-30 17:53:09 +0000

    Bump version
    git-svn-id: svn://svn.zope.org/repos/main@110329 62d5b8a3-27da-0310-9561-8e5933582275

--- ThreadLock/trunk/setup.py
+++ ThreadLock/trunk/setup.py
@@ -16,7 +16,7 @@ from os.path import join
 from setuptools import setup, find_packages, Extension
 
 setup(name='ThreadLock',
-      version = '2.13.0',
+      version = '2.13.1dev',
       url='http://pypi.python.org/pypi/ThreadLock',
       license='ZPL 2.1',
       description="Special ThreadLock objects used in Zope2.",
