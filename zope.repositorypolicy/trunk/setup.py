0d8269763e6d4ace55c054e32bef3d562c9bfc4c
tseaver <tseaver@62d5b8a3-27da-0310-9561-8e5933582275>
2010-04-01 16:45:45 +0000

    Qui custodiet custodiens?
    git-svn-id: svn://svn.zope.org/repos/main@110412 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.repositorypolicy/trunk/setup.py
+++ zope.repositorypolicy/trunk/setup.py
@@ -1,9 +1,22 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 from setuptools import setup, find_packages
 
 setup(
     name='zope.repositorypolicy',
     version='0.1dev',
-    author='Zope Developers',
+    author='Zope Foundation and contributors',
     author_email='zope-dev@zope.org',
     url='http://pypi.python.org/pypi/zope.repositorypolicy',
     description="""\
