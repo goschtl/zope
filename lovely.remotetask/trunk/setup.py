b8e3013b8f4c19c236c6042fd236a7ef67fc24ba
zagy <zagy@62d5b8a3-27da-0310-9561-8e5933582275>
2010-04-14 12:46:06 +0000

    Preparing release 0.5.1
    git-svn-id: svn://svn.zope.org/repos/main@110873 62d5b8a3-27da-0310-9561-8e5933582275

--- lovely.remotetask/trunk/setup.py
+++ lovely.remotetask/trunk/setup.py
@@ -9,7 +9,7 @@ def read(*names):
 
 setup(
     name='lovely.remotetask',
-    version='0.6dev',
+    version = '0.5.1',
     author="Lovely Systems",
     author_email="office@lovelysystems.com",
     description="A remotetask client utiltiy for zope 3",
