200fc88442f8c3591cc211971a39211918ff149c
tseaver <tseaver@62d5b8a3-27da-0310-9561-8e5933582275>
2010-04-14 16:39:12 +0000

    Normalize project metadata.
    git-svn-id: svn://svn.zope.org/repos/main@110904 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.bugchecker/trunk/README.txt
@@ -0,0 +1,5 @@
+zope.bugchecker README
+======================
+
+This package provides a script for checking the status of a Zope
+project's bugtracker on Launchpad.
--- zope.bugchecker/trunk/setup.py
+++ zope.bugchecker/trunk/setup.py
@@ -1,25 +1,46 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 from setuptools import setup, find_packages
-import sys, os
+
+README = open('README.txt').read()
+CHANGES = open('CHANGES.txt').read()
 
 version = '0.1'
 
-setup(name='zope.bugtracker',
+setup(name='zope.bugchecker',
       version=version,
       description="Check the Zope bugtracker for new bugs",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
+      long_description='\n\n'.join([README,CHANGES]),
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+      ],
+      keywords='zope launchpad bugchecker',
       author='Charlie Clark',
       author_email='',
-      url='',
-      license='ZPL',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      url='http://pypi.python.org/pypi/zope.bugchecker',
+      license='ZPL 2.1',
+      packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      install_requires=['launchpadlib'
+      install_requires=[
+        'launchpadlib'
       ],
       entry_points="""
-      # -*- Entry points: -*-
+      [console_scripts]
       """,
-      )
+)
