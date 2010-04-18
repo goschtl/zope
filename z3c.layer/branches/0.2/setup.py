563f9d2aa8e22aa73e10491ecd3cab400827a1b3
ccomb <ccomb@62d5b8a3-27da-0310-9561-8e5933582275>
2010-04-18 00:43:38 +0000

    return to dev
    git-svn-id: svn://svn.zope.org/repos/main@111028 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.layer/branches/0.2/setup.py
+++ z3c.layer/branches/0.2/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name = 'z3c.layer',
-      version = '0.2.4',
+      version = '0.2.5dev',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Collection of Alternative Base Layers',
