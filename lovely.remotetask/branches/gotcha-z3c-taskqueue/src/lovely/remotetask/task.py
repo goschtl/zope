8aec3c397674c7fe13fb2ee61f5d93ad24139560
gotcha <gotcha@62d5b8a3-27da-0310-9561-8e5933582275>
2010-04-22 11:19:39 +0000

    import extracted code
    git-svn-id: svn://svn.zope.org/repos/main@111258 62d5b8a3-27da-0310-9561-8e5933582275

--- lovely.remotetask/branches/gotcha-z3c-taskqueue/src/lovely/remotetask/task.py
+++ lovely.remotetask/branches/gotcha-z3c-taskqueue/src/lovely/remotetask/task.py
@@ -11,46 +11,4 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""Task Service Implementation
-
-$Id$
-"""
-__docformat__ = 'restructuredtext'
-
-import zope.interface
-
-from zope.schema.fieldproperty import FieldProperty
-
-from lovely.remotetask import interfaces
-
-
-class TaskError(Exception):
-    """An error occurred while executing the task."""
-    pass
-
-
-class SimpleTask(object):
-    """A simple, non-persistent task implementation."""
-    zope.interface.implements(interfaces.ITask)
-
-    inputSchema = FieldProperty(interfaces.ITask['inputSchema'])
-    outputSchema = FieldProperty(interfaces.ITask['outputSchema'])
-
-    def __init__(self, func):
-        self.func = func
-
-    def __call__(self, service, jobid, input):
-        return self.func(input)
-
-    def __repr__(self):
-        return '<%s %r>' %(self.__class__.__name__, self.func)
-
-
-class EchoTask(object):
-    zope.interface.implements(interfaces.ITask)
-
-    def __call__(self, service, input):
-        return input
-
-    def __repr__(self):
-        return '<%s>' %(self.__class__.__name__)
+from z3c.taskqueue.task import *
