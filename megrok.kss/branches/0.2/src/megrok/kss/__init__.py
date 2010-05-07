d21fde036df79c9148df7623d7a3619380d5213d
gotcha <gotcha@62d5b8a3-27da-0310-9561-8e5933582275>
2010-05-07 18:56:27 +0000

    can be created again from tag 0.2
    git-svn-id: svn://svn.zope.org/repos/main@112161 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.kss/branches/0.2/src/megrok/kss/__init__.py
+++
@@ -1,1 +0,0 @@
-from megrok.kss.components import KSS
--- megrok.kss/branches/0.2/src/megrok/kss/testing.py
+++
@@ -1,7 +0,0 @@
-import os.path
-import megrok.kss
-from zope.app.testing.functional import ZCMLLayer
-
-integration_zcml = os.path.join(
-    os.path.dirname(megrok.kss.__file__), 'integration.zcml')
-IntegrationLayer = ZCMLLayer(integration_zcml, __name__, 'IntegrationLayer')
