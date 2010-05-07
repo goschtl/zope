e04a25908a83e92f757b37876a061752964ea076
trollfot <trollfot@62d5b8a3-27da-0310-9561-8e5933582275>
2010-05-07 10:04:40 +0000

    removed malformed tag.
    git-svn-id: svn://svn.zope.org/repos/main@112119 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.z3ctable/tags/1.4.1/src/megrok/z3ctable/__init__.py
+++
@@ -1,3 +0,0 @@
-from megrok.z3ctable.components import *
-from megrok.z3ctable.directive import table
-from z3c.table.interfaces import ITable, IColumn
--- megrok.z3ctable/tags/1.4.1/src/megrok/z3ctable/directive.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import martian
-
-
-class table(martian.Directive):
-    scope = martian.CLASS_OR_MODULE
-    store = martian.ONCE
-    validate = martian.validateInterfaceOrClass
