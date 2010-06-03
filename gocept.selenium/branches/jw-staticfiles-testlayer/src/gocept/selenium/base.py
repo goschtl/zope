9a6222ebaefd90d82f817884af9ad15d3b2e4745
janwijbrand <janwijbrand@62d5b8a3-27da-0310-9561-8e5933582275>
2010-06-03 12:01:25 +0000

    Create a branch for adding testcase/layer baseclasses for running selenium against a simple HTTP server, serving static files.
    git-svn-id: svn://svn.zope.org/repos/main@112998 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ gocept.selenium/branches/jw-staticfiles-testlayer/src/gocept/selenium/base.py
@@ -0,0 +1,60 @@
+#############################################################################
+#
+# Copyright (c) 2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import gocept.selenium.selenese
+import selenium
+
+class Layer(object):
+
+    # override in subclass:
+    # hostname and port of the app web server
+    host = None
+    port = None
+
+    __name__ = 'Layer'
+
+    def __init__(self, *bases):
+        self.__bases__ = bases
+        self.__name__ = '.'.join(x.__name__ for x in bases) + '.selenium'
+
+    def setUp(self):
+        pass
+
+    def switch_db(self):
+        raise NotImplemented
+
+
+class TestCase(object):
+
+    # XXX make configurable:
+    # hostname and port of the Selenium RC server
+    _server = 'localhost'
+    _port = 4444
+
+    browser = '*firefox'
+
+    def setUp(self):
+        super(TestCase, self).setUp()
+        url = 'http://%s:%s/' % (self.layer.host, self.layer.port)
+        self._selenium = selenium.selenium(
+            self._server, self._port, self.browser, url)
+        self._selenium.start()
+
+        self.selenium = gocept.selenium.selenese.Selenese(self._selenium, self)
+
+        self.layer.switch_db()
+
+    def tearDown(self):
+        self._selenium.stop()
+        super(TestCase, self).tearDown()
