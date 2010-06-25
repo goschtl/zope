d897b2f5300063eb9ab9481d095d6ecdfc950941
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2010-06-25 15:21:06 +0000

    added
    git-svn-id: svn://svn.zope.org/repos/main@113840 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zopyx.versioning/trunk/zopyx/versioning/interfaces.py
@@ -0,0 +1,17 @@
+from zope.interface import Interface
+
+
+class IVersionStorage(Interface):
+
+    def store(id, version_data):
+        """ Store 'version_data' for a given 'id'.
+            Returns revision number.
+        """
+
+    def retrieve(id, revision):
+        """ Return 'version_data' for a given 'id' and 'revision' """
+
+    def list_versions(id):
+        """ Return all versions stored for a particular
+            content piece by its 'id'.
+        """
