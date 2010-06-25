add10490c69f774b54911d530b5524be5c065cfb
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2010-06-25 15:15:14 +0000

    added
    git-svn-id: svn://svn.zope.org/repos/main@113838 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zopyx.versioning/trunk/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='zopyx.versioning',
+      version=version,
+      description="A flexible and pluggable versioning system for schema-oriented documents",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['zopyx'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
