a1bb7e90bf8c70ca5ff1c395fe3aee910e5c3404
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2010-06-27 09:49:17 +0000

    updated
    git-svn-id: svn://svn.zope.org/repos/main@113916 62d5b8a3-27da-0310-9561-8e5933582275

--- zopyx.versioning/trunk/zopyx/versioning/interfaces.py
+++ zopyx.versioning/trunk/zopyx/versioning/interfaces.py
@@ -1,18 +1,32 @@
+""" 
+Interfaces related to the versioning API
+"""
+
+
 from zope.interface import Interface
 
 
 class IVersionSupport(Interface):
+    """ API for retrieving data to be versioned from an object
+        and restoring a previous state of an object.
+        The data format is JSON.
+
+        Objects must provide their unique ID through the 'id' field.
+
+        This API applies to single objects only 
+        (no support for object collections).
+    """
 
     def getVersionableData():
-        """ Return versionable data """
+        """ Return versionable data (in JSON format) """
 
     def restoreFromVersion(version_data):
-        """ Restore object based on 'version_data' """
+        """ Restore object based on 'version_data' (JSON format) """
 
 
 class IVersionStorage(Interface):
 
-    def store(id, version_data):
+    def store(id, version_data, creator_id, comment):
         """ Store 'version_data' for a given 'id'.
             Returns revision number.
         """
@@ -20,7 +34,16 @@ class IVersionStorage(Interface):
     def retrieve(id, revision):
         """ Return 'version_data' for a given 'id' and 'revision' """
 
-    def list_versions(id):
-        """ Return all versions stored for a particular
+    def has_revision(id, revision):
+        """ Check if there is a revison 'revision' for a given object 'id' """
+
+    def remove_revision(id, revision):
+        """ Remove a particular 'revision' for a given object 'id' """
+
+    def remove_all(id):
+        """ Remove all revisions for a given object 'id' """
+
+    def list_revisions(id):
+        """ Return all revisions stored for a particular
             content piece by its 'id'.
         """
