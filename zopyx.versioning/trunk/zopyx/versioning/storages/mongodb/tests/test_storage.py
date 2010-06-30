a36463c74a50c54c6b7a83e0ce5e19e53767a1d6
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2010-06-30 14:46:28 +0000

    updated
    git-svn-id: svn://svn.zope.org/repos/main@114020 62d5b8a3-27da-0310-9561-8e5933582275

--- zopyx.versioning/trunk/zopyx/versioning/storages/mongodb/tests/test_storage.py
+++ zopyx.versioning/trunk/zopyx/versioning/storages/mongodb/tests/test_storage.py
@@ -3,14 +3,21 @@ MongoDB storage tests
 """
 
 import unittest2
-from zopyx.versioning.storages.mongodb.storage import MongoDBStorage
+
+from zope.interface.verify import verifyClass
+
 from zopyx.versioning import errors
+from zopyx.versioning.storages.mongodb.storage import MongoDBStorage
+from zopyx.versioning.interfaces import IVersionStorage
 
 class StorageTests(unittest2.TestCase):
 
     def setUp(self):
         self.storage = MongoDBStorage('localhost', 10200, 'test-db')
         self.storage.clear()
+
+    def testInterface(self):
+        verifyClass(IVersionStorage, MongoDBStorage)
 
     def testRetrievalNonExistingDocuments(self):
         with self.assertRaises(errors.NoDocumentFound):
