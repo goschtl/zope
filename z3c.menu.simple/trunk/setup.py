14da2555e0e40327231140e3345e2ab3bb7a5c4c
icemac <icemac@62d5b8a3-27da-0310-9561-8e5933582275>
2010-07-14 06:57:16 +0000

    Back to development: 0.6.1
    git-svn-id: svn://svn.zope.org/repos/main@114732 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.menu.simple/trunk/setup.py
+++ z3c.menu.simple/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.menu.simple',
-    version='0.6.0',
+    version='0.6.1dev',
     author = "Stephan Richter, Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "A simple menu system for Zope3",
