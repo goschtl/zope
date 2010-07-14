d4578529098e1e2cb63d640728fae377377c5b9f
icemac <icemac@62d5b8a3-27da-0310-9561-8e5933582275>
2010-07-14 06:23:11 +0000

    Preparing release 1.1.0
    git-svn-id: svn://svn.zope.org/repos/main@114725 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.macroviewlet/trunk/setup.py
+++ z3c.macroviewlet/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.macroviewlet',
-    version='1.0.0',
+    version='1.1.0',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "Viewlets based on ZPT macros.",
