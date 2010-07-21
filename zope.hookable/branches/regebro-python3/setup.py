d20ae702e20a99b6237edb25bd2d6aa1680a4e84
regebro <regebro@62d5b8a3-27da-0310-9561-8e5933582275>
2010-07-21 13:09:57 +0000

    Branch for Python 3 compatibility.
    git-svn-id: svn://svn.zope.org/repos/main@114897 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zope.hookable/branches/regebro-python3/setup.py
@@ -0,0 +1,48 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.hookable package
+"""
+
+import os
+
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.hookable',
+      version = '3.4.2dev',
+      url='http://svn.zope.org/zope.hookable',
+      license='ZPL 2.1',
+      description='Zope hookable',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description="Hookable object support.",
+
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      ext_modules=[Extension("zope.hookable._zope_hookable",
+                             [os.path.join('src', 'zope', 'hookable',
+                                           "_zope_hookable.c")
+                              ]),
+                   ],
+      namespace_packages=['zope',],
+      extras_require=dict(test=['zope.testing']),
+      install_requires=['setuptools'],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
