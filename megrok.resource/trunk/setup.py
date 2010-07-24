b94784c65a454eaf63500eb73870242cc681bf2d
fretin <fretin@62d5b8a3-27da-0310-9561-8e5933582275>
2010-07-24 13:24:20 +0000

    Preparing release 0.5
    git-svn-id: svn://svn.zope.org/repos/main@114999 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.resource/trunk/setup.py
+++ megrok.resource/trunk/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 from os.path import join
 
-version = '0.4.2dev'
+version = '0.5'
 HISTORY = open(join("docs", "HISTORY.txt")).read()
 README = open(join("src", "megrok", "resource", "README.txt")).read()
 
