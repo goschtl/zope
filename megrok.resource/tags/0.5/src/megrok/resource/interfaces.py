b510cdff0060963794841e7cfed255e04b152f80
fretin <fretin@62d5b8a3-27da-0310-9561-8e5933582275>
2010-07-24 13:26:30 +0000

    Removed tag, url is missing in setup.py
    git-svn-id: svn://svn.zope.org/repos/main@115002 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.resource/tags/0.5/src/megrok/resource/interfaces.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from zope.interface import Interface, Attribute
-
-
-class IResourcesIncluder(Interface):
-    """A publishable component that can include resources.
-    """
