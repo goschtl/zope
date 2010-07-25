0e1aa9979e250e0b4fca50e63bf9728c9ed4a351
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2010-07-25 14:52:49 +0000

    Remove (yet unreleased) faulty tag.
    git-svn-id: svn://svn.zope.org/repos/main@115084 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.testsetup/tags/0.8.1/src/z3c/testsetup/tests/cave/broken_mod.py
+++
@@ -1,6 +0,0 @@
-"""
-This module is broken, but does not contain tests.
-
-It should be ignored while scanning for test modules.
-"""
-import non_existing_package
--- z3c.testsetup/tags/0.8.1/src/z3c/testsetup/tests/layered_cave/layeredsetup01.py
+++
@@ -1,2 +0,0 @@
-from z3c.testsetup import register_all_tests
-test_suite = register_all_tests('z3c.testsetup.tests.layered_cave')
