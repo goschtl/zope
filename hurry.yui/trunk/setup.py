c08fa5d7b5ac5e77648042ea6d806e0d6fe4b9fc
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2010-07-29 16:22:30 +0000

    Preparing release 2.7.0.1
    git-svn-id: svn://svn.zope.org/repos/main@115224 62d5b8a3-27da-0310-9561-8e5933582275

--- hurry.yui/trunk/setup.py
+++ hurry.yui/trunk/setup.py
@@ -17,7 +17,7 @@ long_description = (
 
 setup(
     name='hurry.yui',
-    version='2.7.0.1dev',
+    version='2.7.0.1',
     description="hurry.resource style resources for YUI.",
     long_description = long_description,
     classifiers=[],
