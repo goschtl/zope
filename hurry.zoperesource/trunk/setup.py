a0f6622023b1c1203d31366f81c73515a84326e4
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2010-07-29 16:00:24 +0000

    Preparing release 0.6
    git-svn-id: svn://svn.zope.org/repos/main@115216 62d5b8a3-27da-0310-9561-8e5933582275

--- hurry.zoperesource/trunk/setup.py
+++ hurry.zoperesource/trunk/setup.py
@@ -15,7 +15,7 @@ long_description = (
 
 setup(
     name='hurry.zoperesource',
-    version = '0.6dev',
+    version='0.6',
     description="hurry.resource integration for Zope.",
     long_description=long_description,
     classifiers=['Framework :: Zope3'],
