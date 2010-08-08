0bdbb3c6a8414f09b14ea3908e2469f9c8af9672
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2010-08-08 14:53:33 +0000

    Back to development: 1.1
    git-svn-id: svn://svn.zope.org/repos/main@115553 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.traject/trunk/setup.py
+++ megrok.traject/trunk/setup.py
@@ -14,7 +14,7 @@ long_description = (
     )
 
 setup(name='megrok.traject',
-      version='1.0',
+      version='1.1dev',
       description="Traject integration for Grok applications",
       long_description=long_description,
       # Use classifiers that are already listed at:
