7f63b5355859e35be83977edf34fb479ee08f65b
icemac <icemac@62d5b8a3-27da-0310-9561-8e5933582275>
2010-08-19 07:18:02 +0000

    Back to development: 0.6.1
    git-svn-id: svn://svn.zope.org/repos/main@115775 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.recipe.dev/trunk/setup.py
+++ z3c.recipe.dev/trunk/setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
 
 setup(
     name = 'z3c.recipe.dev',
-    version='0.6.0',
+    version='0.6.1dev',
     author = 'Roger Ineichen and the Zope Community',
     author_email = 'zope-dev@zope.org',
     description = 'Zope3 development server setup recipes',
