80ad4b8de7d666fad2c753836acdc3ddcee7ff1b
cklinger <cklinger@62d5b8a3-27da-0310-9561-8e5933582275>
2010-09-08 09:59:33 +0000

    fixing name bug
    git-svn-id: svn://svn.zope.org/repos/main@116226 62d5b8a3-27da-0310-9561-8e5933582275

--- hurry.jquerytools/trunk/src/hurry/jquerytools/__init__.py
+++ hurry.jquerytools/trunk/src/hurry/jquerytools/__init__.py
@@ -1,7 +1,7 @@
 from hurry.resource import Library, ResourceInclusion, GroupInclusion
 from hurry.jquery import jquery
 
-JqueryToolsLibrary = Library('jquery-build')
+JqueryToolsLibrary = Library('JqueryToolsLibrary')
 
 jquerytools = ResourceInclusion(
     JqueryToolsLibrary, 'jquery.tools.min.js', depends=[jquery])
