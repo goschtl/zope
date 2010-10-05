0ba4c6b28f94a168ded4ae315bf6db018451ad64
icemac <icemac@62d5b8a3-27da-0310-9561-8e5933582275>
2010-10-05 06:44:21 +0000

    Preparing release 0.3.0
    git-svn-id: svn://svn.zope.org/repos/main@117228 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.rest/trunk/setup.py
+++ z3c.rest/trunk/setup.py
@@ -31,7 +31,7 @@ chapters = '\n'.join(
 
 setup (
     name='z3c.rest',
-    version='0.2.6dev',
+    version='0.3.0',
     author = "Stephan Richter and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "A REST Framework for Zope 3 Applications",
