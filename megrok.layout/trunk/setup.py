f3797fe051ba21f7c1c63787f1db2b2a62af824b
trollfot <trollfot@62d5b8a3-27da-0310-9561-8e5933582275>
2010-10-18 09:59:12 +0000

    Updates requirement with a install/test split
    git-svn-id: svn://svn.zope.org/repos/main@117660 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.layout/trunk/setup.py
+++ megrok.layout/trunk/setup.py
@@ -3,10 +3,16 @@ import os
 
 readme_filename = os.path.join('src', 'megrok', 'layout', 'README.txt')
 long_description = open(readme_filename).read() + '\n\n' + \
-                    open(os.path.join('docs', 'HISTORY.txt')).read()
+                   open(os.path.join('docs', 'HISTORY.txt')).read()
+
+test_requires = [
+    'zope.security',
+    'zope.securitypolicy',
+    'zope.testbrowser',
+    'zope.app.zcmlfiles']
 
 setup(name='megrok.layout',
-      version='0.9',
+      version='1.0b1',
       description="A layout component package for zope3 and Grok.",
       long_description = long_description,
       classifiers=[
@@ -27,19 +33,17 @@ setup(name='megrok.layout',
       namespace_packages=['megrok'],
       include_package_data=True,
       zip_safe=False,
+      extras_require={'test': test_requires},
       install_requires=[
           'grokcore.component',
           'grokcore.formlib',
           'grokcore.view >= 1.13.1',
           'setuptools',
           'z3c.flashmessage',
-          'zope.app.zcmlfiles',
           'zope.component',
           'zope.interface',
           'zope.publisher',
-          'zope.site',
           'zope.session',
-          'zope.securitypolicy',
-          'zope.testbrowser',
-        ],
+          'zope.site'
+          ],
       )
