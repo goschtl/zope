4a697e3b00fccfeec09a6354be1be13454cb205c
fdrake <fdrake@62d5b8a3-27da-0310-9561-8e5933582275>
2010-10-18 18:18:47 +0000

    prep for release
    git-svn-id: svn://svn.zope.org/repos/main@117729 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.fssync/trunk/setup.py
+++ zope.fssync/trunk/setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.fssync',
-      version = '3.5.2dev',
+      version = '3.5.2',
       url='http://pypi.python.org/pypi/zope.fssync',
       license='ZPL 2.1',
       description="Filesystem synchronization utility for Zope 3.",
