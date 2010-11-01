1ea9d3f6bae7a1f2ee1f96ec37b0a4a816973db7
trollfot <trollfot@62d5b8a3-27da-0310-9561-8e5933582275>
2010-11-01 16:38:41 +0000

    Oops. Only the trunk is needed.
    git-svn-id: svn://svn.zope.org/repos/main@118064 62d5b8a3-27da-0310-9561-8e5933582275

--- grokcore.json/branches/ksmith_mcweekly-groklets/doc/reference/README.txt
+++
@@ -1,6 +0,0 @@
-=========================
-The grok reference manual
-=========================
-
-NOTE: The Grok reference manual is in the process of being migrated to a
-RestructuredText infrastructure.
--- grokcore.json/tags/0.10/src/grok/tests/grokker/onlyonce_fixture/_meta.py
+++
@@ -1,9 +0,0 @@
-import martian
-from component import Alpha
-
-class AlphaGrokker(martian.ClassGrokker):
-    component_class = Alpha
-
-    def grok(self, name, factory, context, module_info, templates):
-        print "alpha"
-        return True
