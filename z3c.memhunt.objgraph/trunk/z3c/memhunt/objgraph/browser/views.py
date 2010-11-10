241831677ed1d3911976a25b4570354c5cbc2ee3
blackburnd <blackburnd@62d5b8a3-27da-0310-9561-8e5933582275>
2010-11-10 11:14:48 +0000

    Another round of cleanup
    git-svn-id: svn://svn.zope.org/repos/main@118314 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.memhunt.objgraph/trunk/z3c/memhunt/objgraph/browser/views.py
+++ z3c.memhunt.objgraph/trunk/z3c/memhunt/objgraph/browser/views.py
@@ -10,35 +10,28 @@ except ImportError:
     from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile    
     from zope.publisher.browser import BrowserView
 
-import checks
+import objgraph
 import pprint 
 import guppy
 
 heapy = guppy.hpy()
 
 
-class DebugView(BrowserView):
-    __allow_access_to_unprotected_subobjects__ = 1
+class Start(BrowserView):
+    template = ViewPageTemplateFile("zpt/start.pt")
 
-    ref_template = ViewPageTemplateFile("zpt/ref_count.pt")
-    ref_common_template = ViewPageTemplateFile("zpt/ref_common_count.pt")
-    start_template = ViewPageTemplateFile("zpt/start.pt")
+    def __call__(self):
+        return self.template(self)
 
-    evenOddClasses = ('even','odd')
-    evenodd = 0
 
-    def start(self):
-        return self.start_template(self)
-
-    def cssclass(self):
-        """ determiner what background color to use for lists """
-        if self.evenodd != 1:
-            self.evenodd = 1
-        else:
-            self.evenodd = 0
-        return self.evenOddClasses[self.evenodd]
-
-    def ref_count(self):
+class RefCount(BrowserView):
+    template = ViewPageTemplateFile("zpt/ref_count.pt")
+    
+    def __call__(self):
+        self.update()
+        return self.template(self)
+        
+    def update(self):
         d = {}
         # collect all classes
         self.garbage_containing = len(gc.garbage)
@@ -65,21 +58,6 @@ class DebugView(BrowserView):
         self.pairs = sorted(self.pairs, key=lambda x: x['refcount'])
         self.pairs.reverse()
 
-
-        return self.ref_template(self)
-
-    def most_common(self):
-        pairs = checks.most_common_types()
-        self.pairs = []
-        for pair in pairs:
-            self.pairs.append({'refcount':pair[1],
-                               'name':pair[0]})
-
-        return self.ref_common_template(self)
-
-    def display_mem(self):
-        import malloc_stats
-        return malloc_stats.malloc_stats()
 
 
     @property
@@ -123,14 +101,14 @@ class DebugView(BrowserView):
     def ref_file(self):
         self.request.response.setHeader('content-type','image/png')
         f = NamedTemporaryFile('wb', suffix='.png')
-        checks.show_refs([self.target], max_depth=6, filename=f.name)
+        objgraph.show_refs([self.target], max_depth=6, filename=f.name)
         return open(f.name,'r').read()
 
     @property
     def back_ref_file(self):
         self.request.response.setHeader('content-type','image/png')
         f = NamedTemporaryFile('wb', suffix='.png')
-        checks.show_backrefs([self.target], max_depth=6, filename=f.name)
+        objgraph.show_backrefs([self.target], max_depth=6, filename=f.name)
         return open(f.name,'r').read()
 
 
@@ -141,6 +119,31 @@ class DebugView(BrowserView):
     def context_backrefs(self):
         self._target = self.context
         return self.back_ref_file
+
+
+
+
+
+class DebugView(BrowserView):
+    __allow_access_to_unprotected_subobjects__ = 1
+
+
+    ref_common_template = ViewPageTemplateFile("zpt/ref_common_count.pt")
+
+
+    def most_common(self):
+        pairs = objgraph.most_common_types()
+        self.pairs = []
+        for pair in pairs:
+            self.pairs.append({'refcount':pair[1],
+                               'name':pair[0]})
+
+        return self.ref_common_template(self)
+
+    def display_mem(self):
+        import malloc_stats
+        return malloc_stats.malloc_stats()
+
 
     def reset_heap(self):
         # Resets for testing
