d17f015e34831cd6ded0f8e5270229b6a034833a
janjaapdriessen <janjaapdriessen@62d5b8a3-27da-0310-9561-8e5933582275>
2010-11-15 16:43:08 +0000

    Register a custom DirectoryResource and DirectoryResourceFactory in order to mark resources with a marker interface and render URLs that know about hurry.resource.publisher.
    git-svn-id: svn://svn.zope.org/repos/main@118410 62d5b8a3-27da-0310-9561-8e5933582275

--- hurry.zoperesource/branches/janjaapdriessen-wsgi/src/hurry/zoperesource/zopesupport.py
+++ hurry.zoperesource/branches/janjaapdriessen-wsgi/src/hurry/zoperesource/zopesupport.py
@@ -1,12 +1,20 @@
 # zope integration for hurry.resource
+from zope.interface import alsoProvides
+from zope.component import adapts
 import zope.security.management
-from zope.publisher.interfaces import IRequest
-from zope.traversing.browser.absoluteurl import absoluteURL
 from grokcore.component import subscribe
-from zope.publisher.interfaces import IEndRequestEvent
+
+from zope.publisher.interfaces import IEndRequestEvent, IRequest
+from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.traversing.browser.absoluteurl import absoluteURL
+import zope.browserresource.resource
+from zope.browserresource.directory import DirectoryResourceFactory
+from zope.browserresource.directory import DirectoryResource
 
 from hurry.resource import NeededInclusions
 from hurry.resource.wsgi import NEEDED, PUBLISHER_PREFIX
+
+from hurry.zoperesource.interfaces import IHurryResource
 
 class NoRequestError(Exception):
     pass
@@ -58,3 +66,32 @@ def set_base_url_on_needed_inclusions(event):
             needed.base_url = absolute_url + publisher_prefix
         else:
             needed.base_url = absolute_url + '/@@/'
+
+# Adapter for constructing URLs from page templates using
+# `context/++resource++foo` that may point to the hurry.resource publisher.
+class AbsoluteURL(zope.browserresource.resource.AbsoluteURL):
+
+    adapts(IHurryResource, IBrowserRequest)
+
+    def __str__(self):
+        request = self.request
+        url = absoluteURL(None, request)
+        publisher_prefix = request._orig_env.get(PUBLISHER_PREFIX)
+        name = self.context.__name__
+        if publisher_prefix is None:
+            return self._createUrl(url, name)
+        return url + publisher_prefix + '/' + name
+
+
+class HurryDirectoryResource(DirectoryResource):
+
+    def get(self, *args, **kwargs):
+        result = super(HurryDirectoryResource, self).get(*args, **kwargs)
+        alsoProvides(result, IHurryResource)
+        return result
+
+class HurryDirectoryResourceFactory(DirectoryResourceFactory):
+
+    factoryClass = HurryDirectoryResource
+
+HurryDirectoryResource.directory_factory = HurryDirectoryResourceFactory
