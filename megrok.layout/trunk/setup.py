82b4c0637423eab14d780ee0eed1fd8ed20e766d
janwijbrand <janwijbrand@62d5b8a3-27da-0310-9561-8e5933582275>
2010-12-16 09:54:57 +0000

    Preparing release 1.2.0
    git-svn-id: svn://svn.zope.org/repos/main@118954 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.layout/trunk/setup.py
+++ megrok.layout/trunk/setup.py
@@ -17,7 +17,7 @@ test_requires = [
     ]
 
 setup(name='megrok.layout',
-      version='1.2.0dev',
+      version='1.2.0',
       description="A layout component package for zope3 and Grok.",
       long_description = long_description,
       classifiers=[
