2944fefe6b4a1a28e07dd28205923cfb0c80bcce
ulif <ulif@62d5b8a3-27da-0310-9561-8e5933582275>
2011-01-04 16:22:28 +0000

    Brush up setup.py to be more compatible with ZF guidelines.
    git-svn-id: svn://svn.zope.org/repos/main@119364 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.pytest/trunk/setup.py
+++ zope.pytest/trunk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys,os
 
-version = '1.0'
+version = '0.1dev'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
@@ -19,16 +19,21 @@ setup(name='zope.pytest',
       version=version,
       description="zope pytest integration",
       long_description=long_description,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Programming Language :: Python",
+        "Framework :: Zope3",
         ],
       keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='BSD',
+      author='Martijn Faassen and contributors',
+      author_email='zope-dev at zope dot org',
+      url='http://pypi.python.org/pypi/zope.pytest',
+      license='ZPL',
       packages=find_packages('src',exclude=['ez_setup']),
       namespace_packages=['zope'],
       package_dir={'': 'src'},
