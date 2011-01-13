e86776377e75a7035269556468026cb4fa169698
icemac <icemac@62d5b8a3-27da-0310-9561-8e5933582275>
2011-01-13 10:06:44 +0000

    Back to development: 0.7.1
    git-svn-id: svn://svn.zope.org/repos/main@119557 62d5b8a3-27da-0310-9561-8e5933582275

--- zam.api/trunk/setup.py
+++ zam.api/trunk/setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 
 setup (
     name='zam.api',
-    version='0.7.0',
+    version='0.7.1dev',
     author = "Stephan Richter, Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "API for ZAM (Zope Application Management",
