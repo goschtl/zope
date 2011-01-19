14737d9181f05a1eea93692ceae5778a48fb41ea
icemac <icemac@62d5b8a3-27da-0310-9561-8e5933582275>
2011-01-19 07:41:37 +0000

    Preparing release 0.7.0
    git-svn-id: svn://svn.zope.org/repos/main@119680 62d5b8a3-27da-0310-9561-8e5933582275

--- zam.skin/trunk/setup.py
+++ zam.skin/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='zam.skin',
-    version='0.6.1dev',
+    version='0.7.0',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "Zope 3 Application Management skin",
