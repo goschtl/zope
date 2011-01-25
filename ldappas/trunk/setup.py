1139fbfda9bb9b583be988f62c27673433b69ced
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2011-01-25 17:14:15 +0000

    Back to development: 0.8.2
    git-svn-id: svn://svn.zope.org/repos/main@119927 62d5b8a3-27da-0310-9561-8e5933582275

--- ldappas/trunk/setup.py
+++ ldappas/trunk/setup.py
@@ -6,7 +6,7 @@ tests_require = [
 
 setup(
     name='ldappas',
-    version='0.8.1',
+    version='0.8.2dev',
     author='Zope developers',
     author_email='zope-dev@zope.org',
     url='http://svn.zope.org/ldappas',
