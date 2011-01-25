2c18ba6c9d72cb880dc08cf4593833702f055886
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2011-01-25 16:52:43 +0000

    Back to development: 0.9
    git-svn-id: svn://svn.zope.org/repos/main@119919 62d5b8a3-27da-0310-9561-8e5933582275

--- ldapadapter/trunk/setup.py
+++ ldapadapter/trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ldapadapter',
-    version='0.8',
+    version='0.9dev',
     author='Zope developers',
     author_email='zope-dev@zope.org',
     url='http://svn.zope.org/ldapadapter',
