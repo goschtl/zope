22a6ef527bd8f50c3fe244e9872cab118a513ee6
icemac <icemac@62d5b8a3-27da-0310-9561-8e5933582275>
2011-01-27 07:33:32 +0000

    Preparing release 0.6.0
    git-svn-id: svn://svn.zope.org/repos/main@119957 62d5b8a3-27da-0310-9561-8e5933582275

--- zamplugin.error/trunk/setup.py
+++ zamplugin.error/trunk/setup.py
@@ -23,9 +23,9 @@ def read(*rnames):
 
 setup (
     name='zamplugin.error',
-    version='0.5.1dev',
+    version='0.6.0',
     author = "Roger Ineichen and the Zope Community",
-    author_email = "zope3-dev@zope.org",
+    author_email = "zope-dev@zope.org",
     description = "Error utility for Zope Application Management",
     long_description=(
         read('README.txt')
@@ -44,7 +44,7 @@ setup (
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
         'Framework :: Zope3'],
-    url = 'http://cheeseshop.python.org/pypi/zamplugin.error',
+    url = 'http://pypi.python.org/pypi/zamplugin.error',
     packages = find_packages('src'),
     include_package_data = True,
     package_dir = {'':'src'},
