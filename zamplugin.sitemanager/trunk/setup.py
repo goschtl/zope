ffd6f7650a96083d67eab2d7fb5d3e7ab6cc8a2d
icemac <icemac@62d5b8a3-27da-0310-9561-8e5933582275>
2011-01-28 08:00:26 +0000

    Back to development: 0.6.1
    git-svn-id: svn://svn.zope.org/repos/main@119990 62d5b8a3-27da-0310-9561-8e5933582275

--- zamplugin.sitemanager/trunk/setup.py
+++ zamplugin.sitemanager/trunk/setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='zamplugin.sitemanager',
-    version='0.6.0',
+    version='0.6.1dev',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "Site manager support for ZAM (Zope Application Management)",
