305ed033b2f1ad4fbad6d7c41efdbe9c28563b47
janwijbrand <janwijbrand@62d5b8a3-27da-0310-9561-8e5933582275>
2011-02-08 14:01:32 +0000

    Remove obsolete branch.
    git-svn-id: svn://svn.zope.org/repos/main@120213 62d5b8a3-27da-0310-9561-8e5933582275

--- grokcore.view/branches/sylvain-template-inheritance-fix/src/grokcore/view/tests/view/cross_package.py
+++
@@ -1,7 +0,0 @@
-"""
-  >>> import cross_package_fixture
-
-  >>> grok.testing.grok(cross_package_fixture.__name__)
-
-"""
-import grokcore.view as grok
--- grokcore.view/branches/sylvain-template-inheritance-fix/src/grokcore/view/tests/view/cross_package_fixture/zbase.py
+++
@@ -1,8 +0,0 @@
-#
-
-import grokcore.view as grok
-
-class BaseView(grok.View):
-    grok.template('view')
-    grok.context(object)
-
