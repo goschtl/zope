e293c44e391086a2c44ea82165f1e8bf046439c3
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2011-05-10 14:32:16 +0000

    *** empty log message ***
    git-svn-id: svn://svn.zope.org/repos/main@121648 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.zeoinputlog/tags/0.2.0/setup.py
+++ zc.zeoinputlog/tags/0.2.0/setup.py
@@ -11,7 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-name, version = 'zc.zeoinputlog', '0.1.0'
+name, version = 'zc.zeoinputlog', '0.2.0'
 
 install_requires = [
     'setuptools',
