0f16b2167f59998dadf7319db368cc0e93e56a12
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2011-05-18 21:06:44 +0000

    Added tests for parser. (In doing so, realized that overriding ps2 doesn't work and it's not worth fixing.)
    git-svn-id: svn://svn.zope.org/repos/main@121720 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/J1m/customdoctests/src/zc/customdoctests/__init__.py
+++ Sandbox/J1m/customdoctests/src/zc/customdoctests/__init__.py
@@ -21,7 +21,6 @@ class DocTestParser(doctest.DocTestParser):
 
     def __init__(self, *args, **kw):
         ps1 = kw.pop('ps1', '>>>')
-        ps2 = kw.pop('ps2', r'\.\.\.')
         comment_prefix = kw.pop('comment_prefix', '#')
         self.transform = kw.pop('transform', lambda s: s)
         getattr(doctest.DocTestParser, '__init__', lambda : None)(*args, **kw)
@@ -31,14 +30,14 @@ class DocTestParser(doctest.DocTestParser):
             # Source consists of a PS1 line followed by zero or more PS2 lines.
             (?P<source>
                 (?:^(?P<indent> [ ]*) %(ps1)s    .*)    # PS1 line
-                (?:\n           [ ]*  %(ps2)s    .*)*)  # PS2 lines
+                (?:\n           [ ]*  \.\.\.     .*)*)  # PS2 lines
             \n?
             # Want consists of any non-blank lines that do not start with PS1.
             (?P<want> (?:(?![ ]*$)        # Not a blank line
                          (?![ ]*%(ps1)s)  # Not a line starting with PS1
                          .*$\n?           # But any other line
                       )*)
-        ''' % dict(ps1=ps1, ps2=ps2), re.MULTILINE | re.VERBOSE)
+        ''' % dict(ps1=ps1), re.MULTILINE | re.VERBOSE)
 
         self._OPTION_DIRECTIVE_RE = re.compile(
             comment_prefix +r'\s*doctest:\s*([^\n\'"]*)$', re.MULTILINE)
