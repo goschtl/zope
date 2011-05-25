82497935886f8a5d38b2053ca96a3722fefba8d3
janwijbrand <janwijbrand@62d5b8a3-27da-0310-9561-8e5933582275>
2011-05-25 14:34:35 +0000

    this will not be a mere bugfix release
    git-svn-id: svn://svn.zope.org/repos/main@121803 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.login/trunk/setup.py
+++ megrok.login/trunk/setup.py
@@ -28,7 +28,7 @@ long_description = (
 
 setup(
     name='megrok.login',
-    version='0.4.1dev',
+    version='0.5dev',
     author='Uli Fouquet and the Zope Community',
     author_email='uli@gnufix.de',
     url = 'http://pypi.python.org/pypi/megrok.login',
