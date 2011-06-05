c0b3b18c54a53511ff33937b57c1947590bcb376
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2011-06-05 14:58:15 +0000

    updated
    git-svn-id: svn://svn.zope.org/repos/main@121862 62d5b8a3-27da-0310-9561-8e5933582275

--- zopyx.convert2/branches/pisa/setup.py
+++ zopyx.convert2/branches/pisa/setup.py
@@ -12,7 +12,7 @@ CLASSIFIERS = [
     'Programming Language :: Python',
 ]
 
-version = '2.2.5'
+version = '2.3.0'
 
 desc = unicode(file('README.txt').read().strip(), 'utf-8')
 changes = file('CHANGES.txt').read().strip()
