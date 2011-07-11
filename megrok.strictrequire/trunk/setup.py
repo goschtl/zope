2369c742fedd9fc5029dc73697ce19974ced7983
janjaapdriessen <janjaapdriessen@62d5b8a3-27da-0310-9561-8e5933582275>
2011-07-11 19:36:55 +0000

    Back to development: 0.5
    git-svn-id: svn://svn.zope.org/repos/main@122151 62d5b8a3-27da-0310-9561-8e5933582275

--- megrok.strictrequire/trunk/setup.py
+++ megrok.strictrequire/trunk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os.path
 
-version = '0.4'
+version = '0.5dev'
 detailed = open(
     os.path.join('src', 'megrok', 'strictrequire', 'README.txt')).read()
 changes = open('CHANGES.txt').read()
