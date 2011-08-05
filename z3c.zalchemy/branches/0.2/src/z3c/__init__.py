429a8dc9466f6f0acc868a3bcacc0bf569c06b93
wosc <wosc@62d5b8a3-27da-0310-9561-8e5933582275>
2011-08-05 09:27:26 +0000

    Declare namespace package
    git-svn-id: svn://svn.zope.org/repos/main@122476 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.zalchemy/branches/0.2/src/z3c/__init__.py
+++ z3c.zalchemy/branches/0.2/src/z3c/__init__.py
@@ -17,3 +17,9 @@ $Id$
 """
 __docformat__ = "reStructuredText"
 
+# namespace package boilerplate
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError, e:
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
