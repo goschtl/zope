69ff64f6133eafe318a611b0eaf0b8cee8a3c78d
chrism <chrism@62d5b8a3-27da-0310-9561-8e5933582275>
2011-09-11 16:38:03 +0000

    move ISite and IPossibleSite back to zope.component
    git-svn-id: svn://svn.zope.org/repos/main@122765 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.interface/branches/chrism-componentregistry/src/zope/interface/interfaces.py
+++ zope.interface/branches/chrism-componentregistry/src/zope/interface/interfaces.py
@@ -1661,22 +1661,4 @@ class IComponents(IComponentLookup, IComponentRegistry):
     """
 
 
-class IPossibleSite(Interface):
-    """An object that could be a site.
-    """
-
-    def setSiteManager(sitemanager):
-        """Sets the site manager for this object.
-        """
-
-    def getSiteManager():
-        """Returns the site manager contained in this object.
-
-        If there isn't a site manager, raise a component lookup.
-        """
-
-
-class ISite(IPossibleSite):
-    """Marker interface to indicate that we have a site"""
-
 # end formerly in zope.component
