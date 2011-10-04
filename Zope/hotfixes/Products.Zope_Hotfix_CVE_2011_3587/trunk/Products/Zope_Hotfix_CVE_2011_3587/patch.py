8c362f7be503c74e992a13294ae96c561163e020
hannosch <hannosch@62d5b8a3-27da-0310-9561-8e5933582275>
2011-10-04 15:00:59 +0000

    Add patch
    git-svn-id: svn://svn.zope.org/repos/main@123014 62d5b8a3-27da-0310-9561-8e5933582275

--- Zope/hotfixes/Products.Zope_Hotfix_CVE_2011_3587/trunk/Products/Zope_Hotfix_CVE_2011_3587/patch.py
+++ Zope/hotfixes/Products.Zope_Hotfix_CVE_2011_3587/trunk/Products/Zope_Hotfix_CVE_2011_3587/patch.py
@@ -1,4 +1,9 @@
+from types import ModuleType
 
 
 def install_patch():
-    pass
+    from OFS.misc_ import p_
+
+    for k, v in list(p_.__dict__.items()):
+        if isinstance(v, ModuleType):
+            del p_.__dict__[k]
