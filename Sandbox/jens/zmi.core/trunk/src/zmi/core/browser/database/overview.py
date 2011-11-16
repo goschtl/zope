036de6db82ade441c401af8700b0aad1bac5f3e9
charlie_x <charlie_x@62d5b8a3-27da-0310-9561-8e5933582275>
2011-11-16 22:43:19 +0000

    Matches functionality for existing main database.
    git-svn-id: svn://svn.zope.org/repos/main@123390 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/jens/zmi.core/trunk/src/zmi/core/browser/database/overview.py
+++ Sandbox/jens/zmi.core/trunk/src/zmi/core/browser/database/overview.py
@@ -15,14 +15,46 @@
 ZODB Overview
 """
 
-class View(object):
+import datetime
 
-    db_name = "Name of the ZODB"
-    db_size = "Size of the ZODB"
+from Acquisition import aq_inner
 
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
+from zmi.core.browser.base import ZMIView
+from zmi.core.browser.base import Message as _
+
+class View(ZMIView):
 
     def __call__(self):
+        if 'pack' in self.request.form:
+            self.handle_pack(data=self.request.form)
+            return self.redirect()
         return self.index()
+
+    @property
+    def _db(self):
+        """Return the database connection"""
+        return aq_inner(self.context)._p_jar.db()
+
+    def db_name(self):
+        """Return the name of the database"""
+        return self._db.getName()
+
+    def db_size(self):
+        """Return the size of the database, unformatted if it is a string,
+        otherwise in kilobytes or megabytes"""
+        size = self._db.getSize()
+        if isinstance(size, str):
+            return size
+
+        if size >= 1048576.0:
+            return '%.1fM' % (size/1048576.0)
+        return '%.1fK' % (size/1024.0)
+
+    def handle_pack(self, data):
+        """Pack the database by removing revisions older than the number of days
+        converted into seconds
+        """
+        db = self._db
+        packtime = datetime.timedelta(days=int(data['days']))
+        db.pack(packtime.seconds)
+        self.status = _(u'The database was packed')
