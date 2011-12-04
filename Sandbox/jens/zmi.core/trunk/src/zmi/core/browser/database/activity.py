161be5a4b2926e28c178b5b868f2c1acd42de4b2
charlie_x <charlie_x@62d5b8a3-27da-0310-9561-8e5933582275>
2011-12-04 18:45:10 +0000

    Spacer not required for charting and dummy data supplied for testing. Activity results dictionary only needs extending not rewriting. Link generation moved from template to view for less repetition.
    git-svn-id: svn://svn.zope.org/repos/main@123570 62d5b8a3-27da-0310-9561-8e5933582275

--- Sandbox/jens/zmi.core/trunk/src/zmi/core/browser/database/activity.py
+++ Sandbox/jens/zmi.core/trunk/src/zmi/core/browser/database/activity.py
@@ -18,18 +18,25 @@ ZODB Activity
 from Acquisition import aq_inner
 from DateTime import DateTime
 
+#dummy data
+
+activity = {'start_time': 'Dec 4, 2011 6:15 pm GMT+1', 'total_load_count': 0, 'end_time': 'Dec 4, 2011 7:15 pm GMT+1',
+            'divs': [{'stores': 0, 'connections': 0, 'store_len': 50, 'end': 1323019271.8497, 'start': 1323018911.8497, 'load_len': 100, 'loads': 0, 'time_offset': '-54m', 'link':''},
+                     {'stores': 0, 'connections': 0, 'store_len': 40, 'end': 1323019631.8497, 'start': 1323019271.8497, 'load_len': 100, 'loads': 0, 'time_offset': '-48m', 'link':''},
+                     {'stores': 0, 'connections': 0, 'store_len': 30, 'end': 1323019991.8497, 'start': 1323019631.8497, 'load_len': 100, 'loads': 0, 'time_offset': '-42m', 'link':''},
+                     {'stores': 0, 'connections': 0, 'store_len': 20, 'end': 1323020351.8497, 'start': 1323019991.8497, 'load_len': 110, 'loads': 0, 'time_offset': '-36m', 'link':''},
+                     {'stores': 0, 'connections': 0, 'store_len': 10, 'end': 1323020711.8497, 'start': 1323020351.8497, 'load_len': 120, 'loads': 0, 'time_offset': '-30m', 'link':''},
+                     {'stores': 0, 'connections': 0, 'store_len': 0,  'end': 1323021071.8497, 'start': 1323020711.8497, 'load_len': 130, 'loads': 0, 'time_offset': '-24m', 'link':''},
+                     {'stores': 0, 'connections': 0, 'store_len': 100,'end': 1323021431.8497, 'start': 1323021071.8497, 'load_len': 100, 'loads': 0, 'time_offset': '-18m', 'link':''},
+                     {'stores': 0, 'connections': 0, 'store_len': 110,'end': 1323021791.8497, 'start': 1323021431.8497, 'load_len': 80, 'loads': 0, 'time_offset': '-12m', 'link':''},
+                     {'stores': 0, 'connections': 0, 'store_len': 10, 'end': 1323022151.8497, 'start': 1323021791.8497, 'load_len': 20, 'loads': 0, 'time_offset': '-6m', 'link':''},
+                     {'stores': 0, 'connections': 0, 'store_len': 40, 'end': 1323022511.8497, 'start': 1323022151.8497, 'load_len': 30, 'loads': 0, 'time_offset': '0m', 'link':''}],
+            'total_store_count': 0, 'total_connections': 0}
+
 class View(object):
 
-    # dummy variables for bootstrapping view
-    getHistoryLength = 1
-    start_time = "start_time"
-    end_time = "end_time"
-    divs = ()
-    connections = "connections"
-    trans_len = "trans_len"
-    store_len = "store_len"
-    load_len = "load_len"
-    total_store_count = "total_store_count"
+    start_time = 'start_time'
+    end_time = 'end_time'
 
     def __init__(self, context, request):
         self.context = context
@@ -73,14 +80,14 @@ class View(object):
         else:
             analysis = am.getActivityAnalysis()
 
-        total_load_count = 0
-        total_store_count = 0
+        total_loads = 0
+        total_stores = 0
         total_connections = 0
         limit = 0
         divs = []
         for div in analysis:
-            total_store_count = total_store_count + div['stores']
-            total_load_count = total_load_count + div['loads']
+            total_stores = total_stores + div['stores']
+            total_loads = total_loads + div['loads']
             total_connections = total_connections + div['connections']
             sum = div['stores'] + div['loads']
             if sum > limit:
@@ -116,17 +123,13 @@ class View(object):
             else:
                 # Show fractions.
                 time_offset = '%.2fs' % t
-            divs.append({
-                'store_len': store_len,
-                'load_len': load_len,
-                'trans_len': max(segment_height - store_len - load_len, 0),
-                'store_count': div['stores'],
-                'load_count': div['loads'],
-                'connections': div['connections'],
-                'start': div['start'],
-                'end': div['end'],
-                'time_offset': time_offset,
-            })
+            div.update({'link': 'chart_start=%s&chart_end=%s' % (div['start'], div['end']),
+                        'time_offset': time_offset,
+                        'store_len': store_len,
+                        'load_len': load_len,
+                        })
+            divs.append(div)
+
 
         if analysis:
             start_time = DateTime(divs[0]['start']).aCommonZ()
@@ -138,9 +141,12 @@ class View(object):
         res = {'start_time': start_time,
                'end_time': end_time,
                'divs': divs,
-               'total_store_count': total_store_count,
-               'total_load_count': total_load_count,
+               'total_store_count': total_stores,
+               'total_load_count': total_loads,
                'total_connections': total_connections,
                }
         return res
 
+    #def getActivityChartData(self):
+        #"""test graphing"""
+        #return activity
