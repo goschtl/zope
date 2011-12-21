3ebf82cba07867537efa0585f414b154bb9f5edf
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2011-12-21 11:37:38 +0000

    *** empty log message ***
    git-svn-id: svn://svn.zope.org/repos/main@123848 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.parse_addr/tags/1.0/setup.py
+++ zc.parse_addr/tags/1.0/setup.py
@@ -11,7 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-name, version = 'zc.parse_addr', '0'
+name, version = 'zc.parse_addr', '1.0.0'
 
 install_requires = ['setuptools']
 
