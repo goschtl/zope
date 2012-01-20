86d9dcfe3292e2ecc45a38bb9f21dca6047b7b01
jancarel <jancarel@62d5b8a3-27da-0310-9561-8e5933582275>
2012-01-20 11:23:16 +0000

    Add TreeVocabulary with tests.
    git-svn-id: svn://svn.zope.org/repos/main@124096 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.schema/branches/jcbrand-treevocabulary/src/zope/schema/tests/test_vocabulary.py
+++ zope.schema/branches/jcbrand-treevocabulary/src/zope/schema/tests/test_vocabulary.py
@@ -187,10 +187,135 @@ class SimpleVocabularyTests(unittest.TestCase):
             self.assertEqual(term.value + 1, term.nextvalue)
 
 
+class TreeVocabularyTests(unittest.TestCase):
+
+    list_vocab = vocabulary.SimpleVocabulary.fromValues([1, 2, 3])
+    items_vocab = vocabulary.SimpleVocabulary.fromItems(
+        [('one', 1), ('two', 2), ('three', 3), ('fore!', 4)])
+
+    tree_vocab_2 = vocabulary.TreeVocabulary.fromDict(
+        { ('regions', 'Regions'): {
+                ('aut', 'Austria'): {
+                    ('tyr', 'Tyrol'): {
+                        ('auss', 'Ausserfern'): {},
+                    }
+                },
+                ('ger', 'Germany'): {
+                    ('bav', 'Bavaria'):{}
+                },
+            }
+        })
+
+    tree_vocab_3 = vocabulary.TreeVocabulary.fromDict(
+        { ('marketing', 'marketing', 'Marketing'): {
+                ('marketing_marketing', 'marketing_marketing', 'Marketing'): {},
+                ('marketing_loyalty', 'marketing_loyalty', 'Loyalty'): {},
+                ('marketing_sales', 'marketing_sales', 'Sales'): {
+                    ('marketing_sales_call_center', 'marketing_sales_call_center', 'Call Center'): {},
+                },
+            }
+        })
+
+    def test_implementation(self):
+        for v in [self.tree_vocab_2, self.tree_vocab_3]:
+            self.assertTrue(verifyObject(interfaces.IVocabulary, v))
+            self.assertTrue(verifyObject(interfaces.IVocabularyTokenized, v))
+            self.assertTrue(dict, type(v._terms))
+
+    def test_addt_interfaces(self):
+        class IStupid(Interface):
+            pass
+        v = vocabulary.TreeVocabulary.fromDict({('one', '1'): {}}, IStupid)
+        self.assertTrue(IStupid.providedBy(v))
+
+    def test_termpath(self):
+        self.assertEqual(
+                    self.tree_vocab_2.getTermPath('Bavaria'), 
+                    ['Regions', 'Germany', 'Bavaria'])
+        self.assertEqual(
+                    self.tree_vocab_2.getTermPath('Austria'), 
+                    ['Regions', 'Austria'])
+        self.assertEqual(
+                    self.tree_vocab_2.getTermPath('Ausserfern'), 
+                    ['Regions', 'Austria', 'Tyrol', 'Ausserfern'])
+        self.assertEqual(
+                    self.tree_vocab_2.getTermPath('Non-existent'), 
+                    [])
+        self.assertEqual(
+                    self.tree_vocab_3.getTermPath('marketing_sales_call_center'),
+                    ['marketing', 'marketing_sales', 'marketing_sales_call_center'])
+
+    def test_len(self):
+        """ len returns the number of all nodes in die dict
+        """
+        self.assertEqual(len(self.tree_vocab_2), 6)
+        self.assertEqual(len(self.tree_vocab_3), 5)
+
+    def test_contains(self):
+        self.assertTrue('Regions' in self.tree_vocab_2 and \
+                        'Austria' in self.tree_vocab_2 and \
+                        'Bavaria' in self.tree_vocab_2)
+
+        self.assertTrue('bav' not in self.tree_vocab_2)
+        self.assertTrue('xxx' not in self.tree_vocab_2)
+
+        self.assertTrue('marketing' in self.tree_vocab_3 and \
+                        'marketing_sales' in self.tree_vocab_3 and \
+                        'marketing_loyalty' in self.tree_vocab_3)
+
+        self.assertTrue('Marketing' not in self.tree_vocab_3)
+        self.assertTrue('Call Center' not in self.tree_vocab_3)
+
+    def test_iter_and_get_term(self):
+        for v in (self.tree_vocab_2, self.tree_vocab_3):
+            for term in v:
+                self.assertTrue(v.getTerm(term.value) is term)
+                self.assertTrue(v.getTermByToken(term.token) is term)
+
+    def test_nonunique_values(self):
+        self.assertRaises(
+            ValueError, vocabulary.TreeVocabulary.fromDict,
+            { ('one', '1'): {},
+              ('two', '1'): {},
+            })
+        vocabulary.TreeVocabulary.fromDict({
+                ('one', '1', 'One'): {},
+                ('two', '2', 'One'): {},
+            })
+
+    def test_nonunique_value_message(self):
+        try:
+            vocabulary.TreeVocabulary.fromDict(
+            { ('one', '1'): {},
+              ('two', '1'): {},
+            })
+        except ValueError as e:
+            self.assertEqual(str(e), "term values must be unique: '1'")
+
+    def test_overriding_createTerm(self):
+        class MyTerm(object):
+            def __init__(self, value):
+                self.value = value
+                self.token = repr(value)
+                self.nextvalue = value + 1
+
+        class MyVocabulary(vocabulary.TreeVocabulary):
+            def createTerm(cls, token, value, title=None):
+                return MyTerm(value)
+            createTerm = classmethod(createTerm)
+
+        vocab = MyVocabulary.fromDict(
+                    {(1, 1):{}, (2, 2):{}, (3, 3):{ (4,4):{} }} )
+        for term in vocab:
+            self.assertEqual(term.value + 1, term.nextvalue)
+
+
 def test_suite():
     suite = unittest.makeSuite(RegistryTests)
     suite.addTest(unittest.makeSuite(SimpleVocabularyTests))
+    suite.addTest(unittest.makeSuite(TreeVocabularyTests))
     return suite
 
 if __name__ == "__main__":
     unittest.main(defaultTest="test_suite")
+
