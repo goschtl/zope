b6ce8e4d0d0a58fb50bd90b680607a8b001f00b8
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2012-04-29 16:33:47 +0000

    versions
    git-svn-id: svn://svn.zope.org/repos/main@125415 62d5b8a3-27da-0310-9561-8e5933582275

--- bobo/tags/1.0.0/bobodoctestumentation/setup.py
+++ bobo/tags/1.0.0/bobodoctestumentation/setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = 'bobodoctestumentation'
-version = '0.0.0'
+version = '1.0.0'
 
 from setuptools import setup
 
