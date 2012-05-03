55ca1a881e064fdfb27875d91a44c749d78cc7e8
tlotze <tlotze@62d5b8a3-27da-0310-9561-8e5933582275>
2012-05-03 21:14:09 +0000

    tagged 2.0.0a2 release
    git-svn-id: svn://svn.zope.org/repos/main@125642 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ zc.buildout/tags/2.0.0a2/setup.py
@@ -0,0 +1,60 @@
+##############################################################################
+#
+# Copyright (c) 2006-2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+name = "zc.buildout"
+version = "2.0.0a2"
+
+import os
+from setuptools import setup
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description=read('READMEFIRST.txt')
+
+entry_points = """
+[console_scripts]
+buildout = %(name)s.buildout:main
+
+[zc.buildout]
+debug = %(name)s.testrecipes:Debug
+
+""" % dict(name=name)
+
+setup(
+    name = name,
+    version = version,
+    author = "Jim Fulton",
+    author_email = "jim@zope.com",
+    description = "System for managing development buildouts",
+    long_description=long_description,
+    license = "ZPL 2.1",
+    keywords = "development build",
+    url='http://pypi.python.org/pypi/zc.buildout',
+
+    data_files = [('.', ['README.txt'])],
+    packages = ['zc', 'zc.buildout'],
+    package_dir = {'': 'src'},
+    namespace_packages = ['zc'],
+    install_requires = 'setuptools',
+    include_package_data = True,
+    entry_points = entry_points,
+    extras_require = dict(test=['zope.testing']),
+    zip_safe=False,
+    classifiers = [
+       'Intended Audience :: Developers',
+       'License :: OSI Approved :: Zope Public License',
+       'Topic :: Software Development :: Build Tools',
+       'Topic :: Software Development :: Libraries :: Python Modules',
+       ],
+    )
