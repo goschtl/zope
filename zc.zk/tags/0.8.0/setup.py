eaf7cc0cc23cbb66030e33f49a5edcea503c566c
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2012-05-15 18:23:38 +0000

    version
    git-svn-id: svn://svn.zope.org/repos/main@125891 62d5b8a3-27da-0310-9561-8e5933582275

--- zc.zk/tags/0.8.0/setup.py
+++ zc.zk/tags/0.8.0/setup.py
@@ -11,7 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-name, version = 'zc.zk', '0'
+name, version = 'zc.zk', '0.8.0'
 
 install_requires = ['setuptools', 'zc.thread']
 extras_require = dict(
