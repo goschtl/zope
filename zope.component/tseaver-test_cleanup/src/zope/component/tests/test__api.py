060a36a18dff63661e01ae9bfde5cd097529195c
tseaver <tseaver@62d5b8a3-27da-0310-9561-8e5933582275>
2012-06-22 17:08:39 +0000

    Move the interface call tests into the package test module.
    git-svn-id: svn://svn.zope.org/repos/main@127029 62d5b8a3-27da-0310-9561-8e5933582275

--- zope.component/tseaver-test_cleanup/src/zope/component/tests/test__api.py
+++ zope.component/tseaver-test_cleanup/src/zope/component/tests/test__api.py
@@ -616,61 +616,6 @@ class Test_queryAdapter(unittest.TestCase):
         self.assertTrue(adapted.context is bar)
 
 
-class Test_Interface_call(unittest.TestCase):
-
-    from zope.component.testing import setUp, tearDown
-
-    def test_miss(self):
-        from zope.interface import Interface
-        class IFoo(Interface):
-            pass
-        self.assertRaises(TypeError, IFoo, object())
-
-    def test_miss_w_default(self):
-        from zope.interface import Interface
-        class IFoo(Interface):
-            pass
-        marker = object()
-        self.assertTrue(IFoo(object(), marker) is marker)
-
-    def test_hit(self):
-        from zope.interface import Interface
-        from zope.interface import implementer
-        from zope.component import getGlobalSiteManager
-        class IFoo(Interface):
-            pass
-        class IBar(Interface):
-            pass
-        @implementer(IBar)
-        class Bar(object):
-            pass
-        @implementer(IFoo)
-        class Baz(object):
-            def __init__(self, context):
-                self.context = context
-        getGlobalSiteManager().registerAdapter(Baz, (IBar,), IFoo, '')
-        bar = Bar()
-        adapted = IFoo(bar)
-        self.assertTrue(adapted.__class__ is Baz)
-        self.assertTrue(adapted.context is bar)
-
-    def test_hit_registered_for_None(self):
-        from zope.interface import Interface
-        from zope.interface import implementer
-        from zope.component import getGlobalSiteManager
-        class IFoo(Interface):
-            pass
-        @implementer(IFoo)
-        class Baz(object):
-            def __init__(self, context):
-                self.context = context
-        getGlobalSiteManager().registerAdapter(Baz, (None,), IFoo, '')
-        ctx = object()
-        adapted = IFoo(ctx)
-        self.assertTrue(adapted.__class__ is Baz)
-        self.assertTrue(adapted.context is ctx)
-
-
 class PackageAPITests(unittest.TestCase):
 
     from zope.component.testing import setUp, tearDown
@@ -960,7 +905,6 @@ def test_suite():
         unittest.makeSuite(Test_queryAdapterInContext),
         unittest.makeSuite(Test_getAdapter),
         unittest.makeSuite(Test_queryAdapter),
-        unittest.makeSuite(Test_Interface_call),
         unittest.makeSuite(PackageAPITests),
     ))
 
